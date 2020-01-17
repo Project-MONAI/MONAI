b94530e8f9613330989524cb9eb0a556c31e8993
Wenqi Li <wenqil@nvidia.com>
2020-01-17 12:45:18 +0000

    unit tests update
     - triggering unit tests via github workflow
     - renamed testconvolutions.py to test_convolutions.py
     - test unet test cases as variables for readability

--- tests/test_unet.py
+++ tests/test_unet.py
@@ -16,35 +16,49 @@ from parameterized import parameterized
 
 from monai.networks.nets.unet import UNet
 
+TEST_CASE_1 = [  # single channel 2D, batch 16
+    {
+        'dimensions': 2,
+        'in_channels': 1,
+        'num_classes': 3,
+        'channels': (16, 32, 64),
+        'strides': (2, 2),
+        'num_res_units': 1,
+    },
+    torch.randn(16, 1, 32, 32),
+    (16, 32, 32),
+]
+
+TEST_CASE_2 = [  # single channel 3D, batch 16
+    {
+        'dimensions': 3,
+        'in_channels': 1,
+        'num_classes': 3,
+        'channels': (16, 32, 64),
+        'strides': (2, 2),
+        'num_res_units': 1,
+    },
+    torch.randn(16, 1, 32, 24, 48),
+    (16, 32, 24, 48),
+]
+
+TEST_CASE_3 = [  # 4-channel 3D, batch 16
+    {
+        'dimensions': 3,
+        'in_channels': 4,
+        'num_classes': 3,
+        'channels': (16, 32, 64),
+        'strides': (2, 2),
+        'num_res_units': 1,
+    },
+    torch.randn(16, 4, 32, 64, 48),
+    (16, 32, 64, 48),
+]
+
 
 class TestUNET(unittest.TestCase):
 
-    @parameterized.expand([
-        [
-            {
-                'dimensions': 2,
-                'in_channels': 1,
-                'num_classes': 3,
-                'channels': (16, 32, 64),
-                'strides': (2, 2),
-                'num_res_units': 1,
-            },
-            torch.randn(16, 1, 32, 32),
-            (16, 32, 32),
-        ],
-        [
-            {
-                'dimensions': 3,
-                'in_channels': 1,
-                'num_classes': 3,
-                'channels': (16, 32, 64),
-                'strides': (2, 2),
-                'num_res_units': 1,
-            },
-            torch.randn(16, 1, 32, 32, 32),
-            (16, 32, 32, 32),
-        ],
-    ])
+    @parameterized.expand([TEST_CASE_1, TEST_CASE_2, TEST_CASE_3])
     def test_shape(self, input_param, input_data, expected_shape):
         result = UNet(**input_param).forward(input_data)[1]
         self.assertEqual(result.shape, expected_shape)
