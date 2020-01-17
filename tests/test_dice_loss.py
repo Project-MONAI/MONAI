2ef578c0ba0bd0b6f86fe329da4037771e5d7ed7
Wenqi Li <wenqil@nvidia.com>
2020-01-17 13:29:48 +0000

    test case as variable, merging test templates

--- tests/test_dice_loss.py
+++ tests/test_dice_loss.py
@@ -16,33 +16,34 @@ from parameterized import parameterized
 
 from monai.networks.losses.dice import DiceLoss
 
+TEST_CASE_1 = [
+    {
+        'include_background': False,
+    },
+    {
+        'pred': torch.tensor([[[[1., -1.], [-1., 1.]]]]),
+        'ground': torch.tensor([[[[1., 0.], [1., 1.]]]]),
+        'smooth': 1e-6,
+    },
+    0.307576,
+]
+
+TEST_CASE_2 = [
+    {
+        'include_background': True,
+    },
+    {
+        'pred': torch.tensor([[[[1., -1.], [-1., 1.]]], [[[1., -1.], [-1., 1.]]]]),
+        'ground': torch.tensor([[[[1., 1.], [1., 1.]]], [[[1., 0.], [1., 0.]]]]),
+        'smooth': 1e-4,
+    },
+    0.416636,
+]
+
 
 class TestDiceLoss(unittest.TestCase):
 
-    @parameterized.expand([
-        [
-            {
-                'include_background': False,
-            },
-            {
-                'pred': torch.tensor([[[[1., -1.], [-1., 1.]]]]),
-                'ground': torch.tensor([[[[1., 0.], [1., 1.]]]]),
-                'smooth': 1e-6,
-            },
-            0.307576,
-        ],
-        [
-            {
-                'include_background': True,
-            },
-            {
-                'pred': torch.tensor([[[[1., -1.], [-1., 1.]]], [[[1., -1.], [-1., 1.]]]]),
-                'ground': torch.tensor([[[[1., 1.], [1., 1.]]], [[[1., 0.], [1., 0.]]]]),
-                'smooth': 1e-4,
-            },
-            0.416636,
-        ],
-    ])
+    @parameterized.expand([TEST_CASE_1, TEST_CASE_2])
     def test_shape(self, input_param, input_data, expected_val):
         result = DiceLoss(**input_param).forward(**input_data)
         self.assertAlmostEqual(result.item(), expected_val, places=5)
