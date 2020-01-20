2ce4a21e932e81373f6a4072646254874cde9765
Nic Ma <nma@nvidia.com>
2020-01-20 22:05:34 +0800

    [DLMED] update timer_handler to store in train_time dict

--- monai/application/handlers/timer_handler.py
+++ monai/application/handlers/timer_handler.py
@@ -7,22 +7,22 @@ class TimerHandler(TrainHandler):
         TrainHandler.__init__(self)
 
     def started_callback(self, engine):
-        engine.state.metrics['TRAIN_START_TIME'] = time.time()
+        engine.state.train_time = {'TRAIN_START': time.time()}
 
     def completed_callback(self, engine):
-        engine.state.metrics['TRAIN_END_TIME'] = time.time()
+        engine.state.train_time['TRAIN_END'] = time.time()
 
     def epoch_started_callback(self, engine):
-        engine.state.metrics['EPOCH_START_TIME'] = time.time()
+        engine.state.train_time['EPOCH_START'] = time.time()
 
     def epoch_completed_callback(self, engine):
-        engine.state.metrics['EPOCH_END_TIME'] = time.time()
+        engine.state.train_time['EPOCH_END'] = time.time()
 
     def step_started_callback(self, engine):
-        engine.state.metrics['STEP_START_TIME'] = time.time()
+        engine.state.train_time['STEP_START'] = time.time()
 
     def step_completed_callback(self, engine):
-        engine.state.metrics['STEP_END_TIME'] = time.time()
+        engine.state.train_time['STEP_END'] = time.time()
 
     def exception_raised_callback(self, engine, e):
-        engine.state.metrics['TRAIN_END_TIME'] = time.time()
+        engine.state.train_time['TRAIN_END'] = time.time()
