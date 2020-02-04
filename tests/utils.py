7b2d73f443cae8bb07bc5f8bfe79ae9d98dc9e51
Wenqi Li <wenqil@nvidia.com>
2020-02-04 15:53:00 +0000

    11 integration test resolves conflicts (#36)
    * Adding integration test for segmentation with UNet
    
    * update integration test for MVP
    
    * enable CI commands
    
    * update README.md, pytorch-ignite dep 0.3.0
    
    * rm torchvision dep
    
    Co-authored-by: Eric Kerfoot <17726042+ericspod@users.noreply.github.com>
    Co-authored-by: Isaac Yang <isaacy@nvidia.com>

--- tests/utils.py
+++ tests/utils.py
@@ -15,7 +15,7 @@ import unittest
 import numpy as np
 import torch
 
-from monai.utils.arrayutils import rescale_array
+from monai.utils.generateddata import create_test_image_2d
 
 quick_test_var = "QUICKTEST"
 
@@ -26,35 +26,6 @@ def skip_if_quick(obj):
     return unittest.skipIf(is_quick, "Skipping slow tests")(obj)
 
 
-def create_test_image(width, height, num_objs=12, rad_max=30, noise_max=0.0, num_seg_classes=5):
-    """
-    Return a noisy 2D image with `numObj' circles and a 2D mask image. The maximum radius of the circles is given as
-    `radMax'. The mask will have `numSegClasses' number of classes for segmentations labeled sequentially from 1, plus a
-    background class represented as 0. If `noiseMax' is greater than 0 then noise will be added to the image taken from
-    the uniform distribution on range [0,noiseMax).
-    """
-    image = np.zeros((width, height))
-
-    for i in range(num_objs):
-        x = np.random.randint(rad_max, width - rad_max)
-        y = np.random.randint(rad_max, height - rad_max)
-        rad = np.random.randint(5, rad_max)
-        spy, spx = np.ogrid[-x : width - x, -y : height - y]
-        circle = (spx * spx + spy * spy) <= rad * rad
-
-        if num_seg_classes > 1:
-            image[circle] = np.ceil(np.random.random() * num_seg_classes)
-        else:
-            image[circle] = np.random.random() * 0.5 + 0.5
-
-    labels = np.ceil(image).astype(np.int32)
-
-    norm = np.random.uniform(0, num_seg_classes * noise_max, size=image.shape)
-    noisyimage = rescale_array(np.maximum(image, norm))
-
-    return noisyimage, labels
-
-
 class NumpyImageTestCase2D(unittest.TestCase):
     im_shape = (128, 128)
     input_channels = 1
@@ -62,7 +33,7 @@ class NumpyImageTestCase2D(unittest.TestCase):
     num_classes = 3
 
     def setUp(self):
-        im, msk = create_test_image(self.im_shape[0], self.im_shape[1], 4, 20, 0, self.num_classes)
+        im, msk = create_test_image_2d(self.im_shape[0], self.im_shape[1], 4, 20, 0, self.num_classes)
 
         self.imt = im[None, None]
         self.seg1 = (msk[None, None] > 0).astype(np.float32)
