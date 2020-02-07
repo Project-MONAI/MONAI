cd818eb57bd724918d51e19476ef0ec5931e68ff
Ben Murray <ben.murray@gmail.com>
2020-02-07 11:44:58 +0000

    59-compose-functionality: adding documentation to compose

--- monai/data/transforms/compose.py
+++ monai/data/transforms/compose.py
@@ -29,12 +29,32 @@ class Compose:
     `monai.data.transforms.adaptor` to wrap transforms that don't conform
     to the requirements. This approach allows you to use transforms from
     otherwise incompatible libraries with minimal additional work.
+
+    Note:
+    In many cases, Compose is not the best way to create pre-processing
+    pipelines. Pre-processing is often not a strictly sequential series of
+    operations, and much of the complexity arises when a not-sequential
+    set of functions must be called as if it were a sequence.
+
+    Example: images and labels
+    Images typically require some kind of normalisation that labels do not.
+    Both are then typically augmented through the use of random rotations,
+    flips, and deformations.
+    Compose can be used with a series of transforms that take a dictionary
+    that contains 'image' and 'label' entries. This might require wrapping
+    `torchvision` transforms before passing them to compose.
+    Alternatively, one can create a class with a __call__ function that
+    calls your pre-processing functions taking into account that not all of
+    them are called on the labels
+
+    TODO: example / links to alternative approaches
+
     """
     def __init__(self, transforms=None):
         if transforms is None:
             transforms = []
-        if not isinstance(transforms, list):
-            raise ValueError("Parameters 'transforms' must be an array")
+        if not isinstance(transforms, (list, tuple)):
+            raise ValueError("Parameters 'transforms' must be a list or tuple")
         self.transforms = transforms
 
     def __call__(self, input_):
