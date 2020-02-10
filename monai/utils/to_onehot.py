68e6378af23c60c39052f9f0db3e7d075d30791f
Wenqi Li <wenqil@nvidia.com>
2020-02-10 12:52:51 +0000

    fixes to_onehot

--- monai/utils/to_onehot.py
+++ monai/utils/to_onehot.py
@@ -13,27 +13,25 @@ import torch
 import torch.nn.functional as f
 
 
-def to_onehot(data, num_classes):
-    """Util function to convert PyTorch tensor to One-Hot encoding format.
+def to_onehot(data, num_classes: int):
+    """Utility function to convert PyTorch tensor to One-Hot encoding format.
     The input data should only have 1 channel and the first dim is batch.
     Example shapes: [16, 1, 96, 96], [16, 1, 96, 96, 32].
     And the data values must match "num_classes"， example: num_classes = 10 and values in [0 ... 9].
 
     Args:
         data (torch.Tensor): target data to convert One-Hot format.
-        num_classes (Int): the class number in the Tensor data.
+        num_classes (int): the class number in the Tensor data.
 
     """
-    assert num_classes is not None and type(num_classes) == int, 'must set class number for one-hot.'
+    num_dims = data.dim()
+    if num_dims < 2 or data.shape[1] != 1:
+        raise ValueError('data should have a channel with length equals to one.')
 
     data = torch.squeeze(data, 1)
     data = f.one_hot(data.long(), num_classes)
-    num_dims = len(data.shape)
-    assert num_dims == 4 or num_dims == 5, 'unsupported input shape.'
-    if num_dims == 5:
-        data = data.permute(0, 4, 1, 2, 3)
-    elif num_dims == 4:
-        data = data.permute(0, 3, 1, 2)
-    if data.is_contiguous() is False:
-        data = data.contiguous()
+    new_axes = [0, -1] + list(range(1, num_dims - 1))
+    data = data.permute(*new_axes)
+    if not data.is_contiguous():
+        return data.contiguous()
     return data
--- tests/test_to_onehot.py
+++ tests/test_to_onehot.py
@@ -11,26 +11,38 @@
 
 import unittest
 
+import numpy as np
 import torch
 from parameterized import parameterized
 
 from monai.utils.to_onehot import to_onehot
 
-TEST_CASE_1 = [  # single channel 2D, batch 16
-    {
-        'data': torch.tensor([[[[0, 1], [1, 2]]], [[[2, 1], [1, 0]]]]),
-        'num_classes': 3
-    },
+TEST_CASE_1 = [  # single channel 2D, batch 3, shape (2, 1, 2, 2)
+    {'data': torch.tensor([[[[0, 1], [1, 2]]], [[[2, 1], [1, 0]]]]), 'num_classes': 3},
     (2, 3, 2, 2),
+]
+
+TEST_CASE_2 = [  # single channel 1D, batch 2, shape (2, 1, 4)
+    {'data': torch.tensor([[[1, 2, 2, 0]], [[2, 1, 0, 1]]]), 'num_classes': 3},
+    (2, 3, 4),
+    np.array([[[0, 0, 0, 1], [1, 0, 0, 0], [0, 1, 1, 0]], [[0, 0, 1, 0], [0, 1, 0, 1], [1, 0, 0, 0]]]),
+]
+
+TEST_CASE_3 = [  # single channel 0D, batch 2, shape (2, 1)
+    {'data': torch.tensor([[1.], [2.]]), 'num_classes': 3},
+    (2, 3),
+    np.array([[0, 1, 0], [0, 0, 1]]),
 ]
 
 
 class TestToOneHot(unittest.TestCase):
 
-    @parameterized.expand([TEST_CASE_1])
-    def test_shape(self, input_data, expected_shape):
+    @parameterized.expand([TEST_CASE_1, TEST_CASE_2, TEST_CASE_3])
+    def test_shape(self, input_data, expected_shape, expected_result=None):
         result = to_onehot(**input_data)
         self.assertEqual(result.shape, expected_shape)
+        if expected_result is not None:
+            self.assertTrue(np.allclose(expected_result, result.numpy()))
 
 
 if __name__ == '__main__':
