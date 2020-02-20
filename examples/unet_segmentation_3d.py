e96d62b496518fe8a4267003ce6f7a44ae9f13f4
Mohammad Adil <madil@nvidia.com>
2020-02-20 00:56:43 -0800

    Fixes all notebooks and unet example (#97).

--- examples/unet_segmentation_3d.py
+++ examples/unet_segmentation_3d.py
@@ -18,14 +18,17 @@ import logging
 import nibabel as nib
 import numpy as np
 import torch
-import monai.transforms.compose as transforms
 from torch.utils.tensorboard import SummaryWriter
 from ignite.engine import Events, create_supervised_trainer, create_supervised_evaluator
 from ignite.handlers import ModelCheckpoint, EarlyStopping
 from torch.utils.data import DataLoader
 
+# assumes the framework is found here, change as necessary
+sys.path.append("..")
+
 import monai
-from monai import config
+import monai.transforms.compose as transforms
+
 from monai.data.nifti_reader import NiftiDataset
 from monai.transforms import (AddChannel, Rescale, ToTensor, UniformRandomPatch)
 from monai.handlers.stats_handler import StatsHandler
@@ -34,16 +37,13 @@ from monai.visualize import img2tensorboard
 from monai.data.synthetic import create_test_image_3d
 from monai.handlers.utils import stopping_fn_from_metric
 
-# assumes the framework is found here, change as necessary
-sys.path.append("..")
-
-config.print_config()
+monai.config.print_config()
 
 # Create a temporary directory and 50 random image, mask paris
 tempdir = tempfile.mkdtemp()
 
 for i in range(50):
-    im, seg = create_test_image_3d(256, 256, 256)
+    im, seg = create_test_image_3d(128, 128, 128)
 
     n = nib.Nifti1Image(im, np.eye(4))
     nib.save(n, os.path.join(tempdir, 'im%i.nii.gz' % i))
@@ -55,8 +55,17 @@ images = sorted(glob(os.path.join(tempdir, 'im*.nii.gz')))
 segs = sorted(glob(os.path.join(tempdir, 'seg*.nii.gz')))
 
 # Define transforms for image and segmentation
-imtrans = transforms.Compose([Rescale(), AddChannel(), UniformRandomPatch((64, 64, 64)), ToTensor()])
-segtrans = transforms.Compose([AddChannel(), UniformRandomPatch((64, 64, 64)), ToTensor()])
+imtrans = transforms.Compose([
+    Rescale(), 
+    AddChannel(), 
+    UniformRandomPatch((96, 96, 96)), 
+    ToTensor()
+])
+segtrans = transforms.Compose([
+    AddChannel(), 
+    UniformRandomPatch((96, 96, 96)), 
+    ToTensor()
+])
 
 # Define nifti dataset, dataloader.
 ds = NiftiDataset(images, segs, transform=imtrans, seg_transform=segtrans)
@@ -64,8 +73,7 @@ loader = DataLoader(ds, batch_size=10, num_workers=2, pin_memory=torch.cuda.is_a
 im, seg = monai.utils.misc.first(loader)
 print(im.shape, seg.shape)
 
-
-lr = 1e-3
+lr = 1e-5
 
 # Create UNet, DiceLoss and Adam optimizer.
 net = monai.networks.nets.UNet(
@@ -80,8 +88,6 @@ net = monai.networks.nets.UNet(
 loss = monai.losses.DiceLoss(do_sigmoid=True)
 opt = torch.optim.Adam(net.parameters(), lr)
 
-train_epochs = 3
-
 # Since network outputs logits and segmentation, we need a custom function.
 def _loss_fn(i, j):
     return loss(i[0], j)
@@ -91,18 +97,13 @@ device = torch.device("cuda:0")
 trainer = create_supervised_trainer(net, opt, _loss_fn, device, False,
                                     output_transform=lambda x, y, y_pred, loss: [y_pred, loss.item(), y])
 
-checkpoint_handler = ModelCheckpoint('./', 'net', n_saved=10, require_empty=False)
+# adding checkpoint handler to save models (network params and optimizer stats) during training
+checkpoint_handler = ModelCheckpoint('./runs/', 'net', n_saved=10, require_empty=False)
 trainer.add_event_handler(event_name=Events.EPOCH_COMPLETED,
                           handler=checkpoint_handler,
                           to_save={'net': net, 'opt': opt})
-
-dice_metric = MeanDice(add_sigmoid=True, output_transform=lambda output: (output[0][0], output[2]))
-dice_metric.attach(trainer, "Training Dice")
-
-logging.basicConfig(stream=sys.stdout, level=logging.INFO)
-stats_logger = StatsHandler()
-stats_logger.attach(trainer)
-
+train_stats_handler = StatsHandler()
+train_stats_handler.attach(trainer)
 
 @trainer.on(Events.EPOCH_COMPLETED)
 def log_training_loss(engine):
@@ -134,30 +135,47 @@ def log_training_loss(engine):
                                      255, engine.state.epoch)
     engine.logger.info("Epoch[%s] Loss: %s", engine.state.epoch, engine.state.output[1])
 
-
-loader = DataLoader(ds, batch_size=20, num_workers=8, pin_memory=torch.cuda.is_available())
-val_loader = DataLoader(ds, batch_size=20, num_workers=8, pin_memory=torch.cuda.is_available())
 writer = SummaryWriter()
 
-# Define mean dice metric and Evaluator.
+# Set parameters for validation
 validation_every_n_epochs = 1
+metric_name = 'Mean_Dice'
 
-val_metrics = {'Mean Dice': MeanDice(add_sigmoid=True)}
+# add evaluation metric to the evaluator engine
+val_metrics = {metric_name: MeanDice(add_sigmoid=True)}
 evaluator = create_supervised_evaluator(net, val_metrics, device, True,
                                         output_transform=lambda x, y, y_pred: (y_pred[0], y))
 
+# Add stats event handler to print validation stats via evaluator
+logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 val_stats_handler = StatsHandler()
 val_stats_handler.attach(evaluator)
 
 # Add early stopping handler to evaluator.
 early_stopper = EarlyStopping(patience=4,
-                              score_function=stopping_fn_from_metric('Mean Dice'),
+                              score_function=stopping_fn_from_metric(metric_name),
                               trainer=trainer)
 evaluator.add_event_handler(event_name=Events.EPOCH_COMPLETED, handler=early_stopper)
+
+# create a validation data loader
+val_ds = NiftiDataset(images[-20:], segs[-20:], transform=imtrans, seg_transform=segtrans)
+val_loader = DataLoader(ds, batch_size=5, num_workers=8, pin_memory=torch.cuda.is_available())
+
 
 @trainer.on(Events.EPOCH_COMPLETED(every=validation_every_n_epochs))
 def run_validation(engine):
     evaluator.run(val_loader)
 
+@evaluator.on(Events.EPOCH_COMPLETED)
+def log_metrics_to_tensorboard(engine):
+    for name, value in engine.state.metrics.items():
+        writer.add_scalar(f'Metrics/{name}', value, trainer.state.epoch)
 
-state = trainer.run(loader, train_epochs)
+# create a training data loader
+logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+
+train_ds = NiftiDataset(images[:20], segs[:20], transform=imtrans, seg_transform=segtrans)
+train_loader = DataLoader(train_ds, batch_size=5, num_workers=8, pin_memory=torch.cuda.is_available())
+
+train_epochs = 30
+state = trainer.run(train_loader, train_epochs)
