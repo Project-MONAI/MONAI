c48804a4b82b067f0ec67d1fdd58735e018baceb
Wenqi Li <wenqil@nvidia.com>
2020-02-28 14:21:16 +0000

    add rand rotate90d test

---
+++ tests/test_rand_rotate90d.py
@@ -0,0 +1,62 @@
+# Copyright 2020 MONAI Consortium
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#     http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import unittest
+
+import numpy as np
+
+from monai.transforms.composables import RandRotate90d
+from tests.utils import NumpyImageTestCase2D
+
+
+class Rotate90Test(NumpyImageTestCase2D):
+
+    def test_default(self):
+        key = None
+        rotate = RandRotate90d(keys=key)
+        rotate.set_random_state(123)
+        rotated = rotate({key: self.imt})
+        expected = np.rot90(self.imt, 0, (1, 2))
+        self.assertTrue(np.allclose(rotated[key], expected))
+
+    def test_k(self):
+        key = 'test'
+        rotate = RandRotate90d(keys=key, max_k=2)
+        rotate.set_random_state(234)
+        rotated = rotate({key: self.imt})
+        expected = np.rot90(self.imt, 0, (1, 2))
+        self.assertTrue(np.allclose(rotated[key], expected))
+
+    def test_axes(self):
+        key = ['test']
+        rotate = RandRotate90d(keys=key, axes=(1, 2))
+        rotate.set_random_state(234)
+        rotated = rotate({key[0]: self.imt})
+        expected = np.rot90(self.imt, 0, (1, 2))
+        self.assertTrue(np.allclose(rotated[key[0]], expected))
+
+    def test_prob_k_axes(self):
+        key = ('test',)
+        rotate = RandRotate90d(keys=key, prob=1.0, max_k=2, axes=(2, 3))
+        rotate.set_random_state(234)
+        rotated = rotate({key[0]: self.imt})
+        expected = np.rot90(self.imt, 1, (2, 3))
+        self.assertTrue(np.allclose(rotated[key[0]], expected))
+
+    def test_no_key(self):
+        key = 'unknown'
+        rotate = RandRotate90d(keys=key, prob=1.0, max_k=2, axes=(2, 3))
+        with self.assertRaisesRegex(KeyError, ''):
+            rotated = rotate({'test': self.imt})
+
+
+if __name__ == '__main__':
+    unittest.main()
