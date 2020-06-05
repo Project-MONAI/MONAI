dda0949562b35972e0b28db643b0c3771df498f4
Wenqi Li <wenqil@nvidia.com>
2020-06-05 12:46:10 +0100

    fixes #490

--- monai/losses/__init__.py
+++ monai/losses/__init__.py
@@ -9,6 +9,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from .dice import *
-from .focal_loss import *
-from .tversky import *
+from .dice import Dice, DiceLoss, GeneralizedDiceLoss, dice, generalized_dice
+from .focal_loss import FocalLoss
+from .tversky import TverskyLoss
