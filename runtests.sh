3a1f6f9d8174f59db9c652f408de7742aa334259
Hans Johnson <hans-johnson@uiowa.edu>
2020-06-05 12:12:33 -0500

    Add subcommands to the individual codeformat test
    One often wants to run the very fast codeformatting tests without
    running any of the unit tests. Add a commands to allow this
    behavior with "./runtests.sh [--black|--pytype|--mypy|--flake8]".
    
    One often wants the `black` formatting tool to automatically
    fix formatting violations without requiring explicit
    manual call. Use "./runtests.sh --black-fix"
    
    Add mypy static checking tool to monai
    
    Run the complete mypy static checking tool during evaluations
    of code format style checks.
    
    Related to #476 subtask for enable mypy
    
    Expand the documentation for runtest.sh script.  Provide more expressive
    documentation for `-h`
    
    The dryrun request to print the commands used for static
    and format checking without actually running the commands.

--- runtests.sh
+++ runtests.sh
@@ -15,17 +15,26 @@ doQuickTests=false
 doNetTests=false
 doDryRun=false
 doZooTests=false
-doCodeFormat=false
+
+doUnitTests=true
+
+doCodeFormatFix=false
+doBlackFormat=false
+doPytypeFormat=false
+doMypyFormat=false
+doFlake8Format=false
+
+NUM_PARALLEL=1
 
 # testing command to run
 cmd="python3"
-cmdprefix=""
 
 
 # parse arguments
-for i in "$@"
+while [[ $# -gt 0 ]]
 do
-    case $i in
+    key="$1"
+    case $key in
         --coverage)
             doCoverage=true
         ;;
@@ -38,33 +47,98 @@ do
         --dryrun)
             doDryRun=true
         ;;
+        --nounittest*) # allow --nounittest | --nounittests | --nounittesting  etc..
+            doUnitTests=false
+        ;;
         --zoo)
             doZooTests=true
         ;;
         --codeformat)
-            doCodeFormat=true
-
+          doBlackFormat=true
+          doFlakeFormat=true
+          doPytypeFormat=true
+          doMypyFormat=true
+        ;;
+        --black)
+          doBlackFormat=true
+        ;;
+        --black-fix)
+          doCodeFormatFix=true
+          doBlackFormat=true
+        ;;
+        --flake8)
+          doFlake8Format=true
+        ;;
+        --pytype)
+          doPytypeFormat=true
+        ;;
+        --mypy)
+          doMypyFormat=true
+        ;;
+        -j)
+          NUM_PARALLEL=$2
+          shift
         ;;
         *)
-            echo "runtests.sh [--codeformat] [--coverage] [--quick] [--net] [--dryrun] [--zoo]"
+            echo "ERROR: Incorrect commandline provided"
+            echo "Invalid key: $key"
+            echo "runtests.sh [--codeformat] [--black] [--black-fix] [--flake8] [--pytype] [--mypy] "
+            echo "            [--nounittests] [--coverage] [--quick] [--net] [--dryrun] [--zoo] [-j number]"
+            echo "      --codeformat      : shorthand to run all code style and static analysis tests"
+            echo "      --black           : Run the \"black\" autoformatting tools as a lint checker"
+            echo "      --black-fix       : Apply \"black\" autofix feature"
+            echo "      --flake8          : Perform flake8 source code checking"
+            echo "      --pytype          : Perform pytype type hint checking"
+            echo "      --mypy            : Perform mypy optional static type checker"
+            echo "      --nounittests     : skip doing unit testing (i.e. only format lint testers)"
+            echo "      --coverage        : Peforms coverage analysis of code for tests run."
+            echo "      --quick           : disable long running tests."
+            echo "      --net             : perform training/inference/eval integration testing"
+            echo "      --dryrun          : display the commands to the screen without running"
+            echo "      --zoo             : not yet implmented"
+            echo "       -j               : number of parallel jobs to run"
             exit 1
         ;;
     esac
+    echo $@
+    shift
 done
 
-# report on code format
-if [ "$doCodeFormat" = 'true' ]
+cmdprefix=""
+# commands are echoed instead of run in this case
+if [ "$doDryRun" = 'true' ]
 then
-     # Ensure that the necessary packages for code format testing are installed
-     if [[ ! -f "$(which flake8)" ]] || [[ ! -f "$(which black)" ]]; then
-       pip install -r requirements-dev.txt
-     fi
+    echo "Dry run commands:"
+    cmdprefix="dryrun "
 
+    # create a dry run function which prints the command prepended with spaces for neatness
+    function dryrun { echo "  " "$@" ; }
+fi
+
+# Unconditionally report on the state of monai
+python -c 'import monai; monai.config.print_config()'
+
+# report on code format
+if [ "$doBlackFormat" = 'true' ]
+then
      set +e  # Disable exit on failure so that diagnostics can be given on failure
      echo "----------------------------------"
      echo "Verifying black formatting checks."
-     black --check "$(pwd)"
-     black_status=$?
+     if [[ ! -f "$(which black)" ]]; then
+       # Ensure that the necessary packages for code format testing are installed
+       pip install -r requirements-dev.txt
+     fi
+     black --version
+
+     if [ ${doCodeFormatFix} = 'true' ]; then
+       echo "Automaticaly formatting with  black."
+       ${cmdprefix}black "$(pwd)"
+       black_status=$?
+     else
+       echo "Verifying black formatting checks."
+       ${cmdprefix}black --check "$(pwd)"
+       black_status=$?
+     fi
      echo "----------------------------------"
      if [ ${black_status} -ne 0 ];
      then
@@ -78,10 +152,24 @@ then
      else
        echo "*** black code format tests passed. ***"
      fi
+     set -e # Enable exit on failure
+fi
 
+if [ "$doFlake8Format" = 'true' ]
+then
+     set +e  # Disable exit on failure so that diagnostics can be given on failure
      echo "-----------------------------------"
      echo "Verifying flake8 formatting checks."
-     MYPYPATH="$(pwd)/monai" flake8 "$(pwd)" --count --statistics
+     # Ensure that the necessary packages for code format testing are installed
+     if [[ ! -f "$(which flake8)" ]]; then
+       pip install -r requirements-dev.txt
+     fi
+     flake8 --version
+     if [ "$doDryRun" = 'true' ]; then
+       echo 'MYPYPATH="$(pwd)/monai" flake8 "$(pwd)" --count --statistics'
+     else
+       MYPYPATH="$(pwd)/monai" flake8 "$(pwd)" --count --statistics
+     fi
      flake8_status=$?
      echo "-----------------------------------"
      if [ ${flake8_status} -ne 0 ];
@@ -93,22 +181,61 @@ then
      else
        echo "*** flake8 code format tests passed. ***"
      fi
+     set -e # Enable exit on failure
+fi
 
+if [ "$doPytypeFormat" = 'true' ]
+then
+     set +e  # Disable exit on failure so that diagnostics can be given on failure
      echo "-----------------------------------"
      echo "Verifying pytype typehint checks."
-     pytype
+     pytype --version
+     # Ensure that the necessary packages for code format testing are installed
+     if [[ ! -f "$(which mypy)" ]]; then
+       pip install -r requirements-dev.txt
+     fi
+     ${cmdprefix}pytype -j ${NUM_PARALLEL} --python-version=$(python -c "import sys; print(f'{sys.version_info.major}.{sys.version_info.minor}')")
      pytype_status=$?
      echo "-----------------------------------"
      if [ ${pytype_status} -ne 0 ];
      then
        echo "----------------------------------"
-       echo "Typehinting test failed!"
+       echo "Typehinting 'pytype' test failed!"
        echo "Manually review and fix listed typehint errors"
        exit ${pytype_status}
      else
        echo "*** pytype code typehint consistency tests passed. ***"
      fi
-     set -e # Re-enable exit on failure
+     set -e # Enable exit on failure
+fi
+
+if [ "$doMypyFormat" = 'true' ]
+then
+     set +e  # Disable exit on failure so that diagnostics can be given on failure
+     echo "-----------------------------------"
+     echo "Verifying mypy typehint checks."
+     mypy --version
+     # Ensure that the necessary packages for code format testing are installed
+     if [[ ! -f "$(which mypy)" ]]; then
+       pip install -r requirements-dev.txt
+     fi
+     if [ "$doDryRun" = 'true' ]; then
+       echo 'MYPYPATH="$(pwd)/monai" mypy "$(pwd)"'
+     else
+       MYPYPATH=$(pwd)/monai mypy $(pwd)
+     fi
+     mypy_status=$?
+     echo "-----------------------------------"
+     if [ ${mypy_status} -ne 0 ];
+     then
+       echo "----------------------------------"
+       echo "Typehinting 'mypy' test failed!"
+       echo "Manually review and fix listed typehint errors"
+       exit ${mypy_status}
+     else
+       echo "*** mypy code typehint consistency tests passed. ***"
+     fi
+     set -e # Enable exit on failure
 fi
 
 # When running --quick, require doCoverage as well and set QUICKTEST environmental
@@ -117,16 +244,6 @@ if [ "$doQuickTests" = 'true' ]
 then
     doCoverage=true
     export QUICKTEST=True
-fi
-
-# commands are echoed instead of run in this case
-if [ "$doDryRun" = 'true' ]
-then
-    echo "Dry run commands:"
-    cmdprefix="dryrun "
-
-    # create a dry run function which prints the command prepended with spaces for neatness
-    function dryrun { echo "  " "$@" ; }
 fi
 
 # set command and clear previous coverage data
@@ -144,7 +261,11 @@ fi
 
 
 # unit tests
-${cmdprefix}${cmd} -m unittest -v
+if [ "$doUnitTests" = 'true' ]
+then
+  python -c 'import torch; print(torch.__version__); print(torch.rand(5,3))'
+  ${cmdprefix}${cmd} -m unittest -v
+fi
 
 
 # network training/inference/eval tests
