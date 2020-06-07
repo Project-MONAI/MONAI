ffe75775073d51eccaf13262cfb5dd5ab2142a30
Wenqi Li <wenqil@nvidia.com>
2020-06-07 19:55:28 +0100

    fixes float32->float64

--- monai/networks/utils.py
+++ monai/networks/utils.py
@@ -96,7 +96,7 @@ def normalize_transform(shape, device=None, dtype=None, align_corners: bool = Fa
         norm[norm <= 0.0] = 2.0
         norm = 2.0 / norm
         norm = torch.diag(torch.cat((norm, torch.ones((1,), dtype=torch.float64, device=device))))
-        norm[:-1, -1] = 1.0 / torch.tensor(shape, dtype=torch.float32, device=device) - 1.0
+        norm[:-1, -1] = 1.0 / torch.tensor(shape, dtype=torch.float64, device=device) - 1.0
     norm = norm.unsqueeze(0).to(dtype=dtype)
     norm.requires_grad = False
     return norm
