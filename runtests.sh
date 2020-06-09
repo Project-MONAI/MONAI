4527bc1f8c0641e9b622251d1e14dbb0a9eb04d2
FALCOCER <falcocer@NSL-FALCOCERs-MacBook-Pro.local>
2020-06-09 00:50:00 -0500

    Adding confirmation when deleting files in ./runtest.sh --clean

--- runtests.sh
+++ runtests.sh
@@ -127,7 +127,8 @@ python -c 'import monai; monai.config.print_config()'
 #Cleaning the files when ussing --clean in args, if theres no files exit just exit 
 if [ "$clean_cache_file" = true ]
 then
-  rm !( ".mypy_cache" | ".pytype" )
+  rm -i .mypy_cache
+  rm -i .pytype
   echo "All temporary files where erased!"
   exit
 fi
