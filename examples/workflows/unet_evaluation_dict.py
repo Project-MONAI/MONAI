f5e82eb01d242819e7d5df985776aa0e6a4649aa
Nic Ma <nma@nvidia.com>
2020-06-12 13:56:31 +0800

    [DLMED] fix integration test issue (#541)
    Signed-off-by: Nic Ma <nma@nvidia.com>

--- examples/workflows/unet_evaluation_dict.py
+++ examples/workflows/unet_evaluation_dict.py
@@ -84,7 +84,7 @@ def main():
         [
             Activationsd(keys="pred", output_postfix="act", sigmoid=True),
             AsDiscreted(keys="pred_act", output_postfix="dis", threshold_values=True),
-            KeepLargestConnectedComponentd(keys="pred_act_dis", applied_values=[1], output_postfix=None),
+            KeepLargestConnectedComponentd(keys="pred_act_dis", applied_labels=[1], output_postfix=None),
         ]
     )
     val_handlers = [
