c655aedadecb530cc541ff405c10602b49900757
Nic Ma <nma@nvidia.com>
2020-07-01 19:05:42 +0800

    659 Fix doc-string issue of Randomizable (#660)
    * [DLMED] update doc-string of Randomizable
    
    * [MONAI] python code formatting
    
    * [DLMED] update according to comments
    
    Co-authored-by: monai-bot <monai.miccai2019@gmail.com>

--- monai/transforms/compose.py
+++ monai/transforms/compose.py
@@ -72,8 +72,20 @@ class Transform(ABC):
 
 class Randomizable(ABC):
     """
-    An interface for handling local numpy random state.
-    this is mainly for randomized data augmentation transforms.
+    An interface for handling random state locally, currently based on a class variable `R`,
+    which is an instance of `np.random.RandomState`.
+    This is mainly for randomized data augmentation transforms. For example::
+
+        class RandShiftIntensity(Randomizable):
+            def randomize():
+                self._offset = self.R.uniform(low=0, high=100)
+            def __call__(self, img):
+                self.randomize()
+                return img + self._offset
+
+        transform = RandShiftIntensity()
+        transform.set_random_state(seed=0)
+
     """
 
     R: np.random.RandomState = np.random.RandomState()
