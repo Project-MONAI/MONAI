d58b3ae2b77a85690d040735dde3043d49602327
Nic Ma <nma@nvidia.com>
2020-07-20 20:12:17 +0800

    [DLMED] update according to comments

--- monai/losses/dice.py
+++ monai/losses/dice.py
@@ -147,7 +147,7 @@ class DiceLoss(_Loss):
 
 class MaskedDiceLoss(DiceLoss):
     """
-    Add an additional `masking` progress before `DiceLoss`, accept a binary mask ([0, 1]) indicating a region,
+    Add an additional `masking` process before `DiceLoss`, accept a binary mask ([0, 1]) indicating a region,
     `input` and `target` will be masked by the region: region with mask `1` will keep the original value,
     region with `0` mask will be converted to `0`. Then feed `input` and `target` to normal `DiceLoss` computation.
     """
