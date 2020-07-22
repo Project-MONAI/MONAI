5c29112fb9d4ec562a61b350767e037197dc8f69
yiheng-wang-nv <vennw@nvidia.com>
2020-07-22 16:45:30 +0800

    Import missed package

--- monai/networks/blocks/squeeze_and_excitation.py
+++ monai/networks/blocks/squeeze_and_excitation.py
@@ -9,7 +9,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from typing import Type, Any, Dict, Optional
+from typing import Type, Any, Dict, Union, Optional
 
 import math
 import torch
