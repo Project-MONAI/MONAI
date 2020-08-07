eb7c74e9dbe156656d5be472bda5958f95754926
elitap <elitap@users.noreply.github.com>
2020-08-07 12:25:07 +0200

    854 sliding window infer less gpumemory (#855)
    * reduce memory consumption of the sliding window inference by doing not parallalizable computations on cpu
    
    * coding style fix
    
    * add device as param to optionally move window memory to cpu
    
    * typo add forgotten optional import
    
    * keep memory on device set and add tests
    
    * check device type before converstion to numpy
    
    * reveret removed return type
    
    * copy paste error removed
    
    * [MONAI] python code formatting
    
    * [MONAI] python code formatting
    
    * remove unused numpy import
    
    Co-authored-by: monai-bot <monai.miccai2019@gmail.com>
    Signed-off-by: Nic Ma <nma@nvidia.com>

--- tests/test_sliding_window_inference.py
+++ tests/test_sliding_window_inference.py
@@ -17,35 +17,46 @@ from parameterized import parameterized
 
 from monai.inferers import sliding_window_inference
 
-TEST_CASE_0 = [(1, 3, 16, 15, 7), (4, -1, 7), 3, 0.25, "constant"]  # 3D small roi
+TEST_CASE_0 = [(1, 3, 16, 15, 7), (4, -1, 7), 3, 0.25, "constant", torch.device("cpu:0")]  # 3D small roi
 
-TEST_CASE_1 = [(1, 3, 16, 15, 7), (4, 10, 7), 3, 0.25, "constant"]  # 3D small roi
+TEST_CASE_1 = [(1, 3, 16, 15, 7), (4, 10, 7), 3, 0.25, "constant", torch.device("cpu:0")]  # 3D small roi
 
-TEST_CASE_2 = [(1, 3, 16, 15, 7), (20, 22, 23), 10, 0.25, "constant"]  # 3D large roi
+TEST_CASE_2 = [(1, 3, 16, 15, 7), (20, 22, 23), 10, 0.25, "constant", torch.device("cpu:0")]  # 3D large roi
 
-TEST_CASE_3 = [(1, 3, 15, 7), (2, 6), 1000, 0.25, "constant"]  # 2D small roi, large batch
+TEST_CASE_3 = [(1, 3, 15, 7), (2, 6), 1000, 0.25, "constant", torch.device("cpu:0")]  # 2D small roi, large batch
 
-TEST_CASE_4 = [(1, 3, 16, 7), (80, 50), 7, 0.25, "constant"]  # 2D large roi
+TEST_CASE_4 = [(1, 3, 16, 7), (80, 50), 7, 0.25, "constant", torch.device("cpu:0")]  # 2D large roi
 
-TEST_CASE_5 = [(1, 3, 16, 15, 7), (20, 22, 23), 10, 0.5, "constant"]  # 3D large overlap
+TEST_CASE_5 = [(1, 3, 16, 15, 7), (20, 22, 23), 10, 0.5, "constant", torch.device("cpu:0")]  # 3D large overlap
 
-TEST_CASE_6 = [(1, 3, 16, 7), (80, 50), 7, 0.5, "gaussian"]  # 2D large overlap, gaussian
+TEST_CASE_6 = [(1, 3, 16, 7), (80, 50), 7, 0.5, "gaussian", torch.device("cpu:0")]  # 2D large overlap, gaussian
 
-TEST_CASE_7 = [(1, 3, 16, 15, 7), (4, 10, 7), 3, 0.25, "gaussian"]  # 3D small roi, gaussian
+TEST_CASE_7 = [(1, 3, 16, 15, 7), (4, 10, 7), 3, 0.25, "gaussian", torch.device("cpu:0")]  # 3D small roi, gaussian
+
+TEST_CASE_8 = [
+    (1, 3, 16, 15, 7),
+    (4, 10, 7),
+    3,
+    0.25,
+    "gaussian",
+    torch.device("cuda:0"),
+]  # test inference on gpu if availabe
 
 
 class TestSlidingWindowInference(unittest.TestCase):
     @parameterized.expand(
         [TEST_CASE_0, TEST_CASE_1, TEST_CASE_2, TEST_CASE_3, TEST_CASE_4, TEST_CASE_5, TEST_CASE_6, TEST_CASE_7]
     )
-    def test_sliding_window_default(self, image_shape, roi_shape, sw_batch_size, overlap, mode):
+    def test_sliding_window_default(self, image_shape, roi_shape, sw_batch_size, overlap, mode, device):
         inputs = torch.ones(*image_shape)
-        device = torch.device("cpu:0")
+        if device.type == "cuda" and not torch.cuda.is_available():
+            device = torch.device("cpu:0")
 
         def compute(data):
             return data + 1
 
         result = sliding_window_inference(inputs.to(device), roi_shape, sw_batch_size, compute, overlap, mode=mode)
+        np.testing.assert_string_equal(device.type, result.device.type)
         expected_val = np.ones(image_shape, dtype=np.float32) + 1
         np.testing.assert_allclose(result.numpy(), expected_val)
 
