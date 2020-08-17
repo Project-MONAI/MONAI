6ef047bd17062dc0e6c46410f08d19f81d057f28
Nic Ma <nma@nvidia.com>
2020-08-17 20:26:12 +0800

    [DLMED] re-organize folders

--- monai/apps/federated_learning/clara/server/server_trainer.py
+++ monai/apps/federated_learning/clara/server/server_trainer.py
@@ -1,3 +1,14 @@
+# Copyright 2020 MONAI Consortium
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#     http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 from fed_learn.server.fed_server import FederatedServer
 from fed_learn.components.pt_model_saver import PTModelSaver
 from fed_learn.server.model_aggregator import ModelAggregator
