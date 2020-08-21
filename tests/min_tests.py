a8d3521ced0e2614a205dd1c2dc0ade7dfc3a9f3
Nic Ma <nma@nvidia.com>
2020-08-21 08:28:00 +0800

    [DLMED] remove test_load_image from min tests

--- tests/min_tests.py
+++ tests/min_tests.py
@@ -72,6 +72,8 @@ def run_testsuit():
         "test_zoom",
         "test_zoom_affine",
         "test_zoomd",
+        "test_load_image",
+        "test_load_imaged",
     ]
 
     files = glob.glob(os.path.join(os.path.dirname(__file__), "test_*.py"))
