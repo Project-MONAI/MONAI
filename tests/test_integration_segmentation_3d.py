84387abd195540780707c7795600410ded7dc601
Nic Ma <nma@nvidia.com>
2020-08-27 15:16:21 +0800

    [DLMED] update test cases

--- tests/test_integration_segmentation_3d.py
+++ tests/test_integration_segmentation_3d.py
@@ -323,7 +323,7 @@ class IntegrationSegmentation3D(unittest.TestCase):
             ]
             for (output, s) in zip(output_files, sums):
                 ave = np.mean(nib.load(output).get_fdata())
-                np.testing.assert_allclose(ave, s, rtol=5e-3)
+                np.testing.assert_allclose(ave, s, rtol=1e-2)
                 repeated[i].append(ave)
         np.testing.assert_allclose(repeated[0], repeated[1])
         np.testing.assert_allclose(repeated[0], repeated[2])
