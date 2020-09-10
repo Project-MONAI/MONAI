0f10c445bdb1d6fb37531dcea107f8549342f382
Nic Ma <nma@nvidia.com>
2020-09-10 19:22:00 +0800

    1002 Enhance DecathlonDataset to load properties (#1010)
    * [DLMED] add support to load properties
    
    Signed-off-by: Nic Ma <nma@nvidia.com>
    
    * [MONAI] python code formatting
    
    Signed-off-by: Nic Ma <nma@nvidia.com>
    
    * [DLMED] fix flake8 issue
    
    Signed-off-by: Nic Ma <nma@nvidia.com>
    
    * [DLMED] fix flake8 error
    
    Signed-off-by: Nic Ma <nma@nvidia.com>
    
    * [DLMED] update according to comments
    
    Signed-off-by: Nic Ma <nma@nvidia.com>
    
    * [MONAI] python code formatting
    
    Signed-off-by: monai-bot <monai.miccai2019@gmail.com>
    
    Co-authored-by: monai-bot <monai.miccai2019@gmail.com>
    Signed-off-by: richard <33289025+rijobro@users.noreply.github.com>

--- monai/data/__init__.py
+++ monai/data/__init__.py
@@ -12,7 +12,7 @@
 from .csv_saver import CSVSaver
 from .dataloader import DataLoader
 from .dataset import ArrayDataset, CacheDataset, Dataset, PersistentDataset, SmartCacheDataset, ZipDataset
-from .decathlon_datalist import load_decathlon_datalist
+from .decathlon_datalist import load_decathlon_datalist, load_decathlon_properties
 from .grid_dataset import GridPatchDataset
 from .image_reader import *
 from .nifti_reader import NiftiDataset
