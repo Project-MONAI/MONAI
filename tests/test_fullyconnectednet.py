46b9e76141f3797b0fc68c3d6722bb2658741a65
Richard Brown <33289025+rijobro@users.noreply.github.com>
2020-11-12 10:51:40 +0000

    add vfcnet test
    Signed-off-by: Richard Brown <33289025+rijobro@users.noreply.github.com>

--- tests/test_fullyconnectednet.py
+++ tests/test_fullyconnectednet.py
@@ -3,14 +3,28 @@ import unittest
 import torch
 from parameterized import parameterized
 
-from monai.networks.nets import FullyConnectedNet
+from monai.networks.nets import FullyConnectedNet, VarFullyConnectedNet
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-TEST_CASE_0 = [0]
-TEST_CASE_1 = [0.15]
+FC_TEST_CASE_0 = [0]
+FC_TEST_CASE_1 = [0.15]
 
-CASES = [TEST_CASE_0, TEST_CASE_1]
+FC_CASES = [FC_TEST_CASE_0, FC_TEST_CASE_1]
+
+VFC_TEST_CASE_0 = [
+    {
+        "in_channels": 2,
+        "out_channels": 2,
+        "latent_size": 64,
+        "encode_channels": (16,32,64),
+        "decode_channels": (16,32,64),
+    },
+    (3, 2, 64, 64),
+    (3, 2, 64, 64),
+]
+
+VFC_CASES = [VFC_TEST_CASE_0]
 
 
 class TestFullyConnectedNet(unittest.TestCase):
@@ -22,11 +36,20 @@ class TestFullyConnectedNet(unittest.TestCase):
         self.channels = [8, 16]
         self.arr = torch.randn(self.arrShape, dtype=torch.float32).to(device)
 
-    @parameterized.expand(CASES)
-    def test_shape(self, dropout):
+    @parameterized.expand(FC_CASES)
+    def test_fc_shape(self, dropout):
         net = FullyConnectedNet(self.inSize, self.outSize, self.channels, dropout).to(device)
         out = net(self.arr)
         self.assertEqual(out.shape, (self.batch_size, self.outSize))
+
+
+    @parameterized.expand(VFC_CASES)
+    def test_vfc_shape(self, input_param, input_shape, expected_shape):
+        net = VarFullyConnectedNet(**input_param).to(device)
+        net.eval()
+        with torch.no_grad():
+            result = net.forward(torch.randn(input_shape).to(device))
+            self.assertEqual(result.shape, expected_shape)
 
 
 if __name__ == "__main__":
