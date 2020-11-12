f534b522f0e4778e61ddf5558cee05e3451c80f0
charliebudd <charles.budd@kcl.ac.uk>
2020-11-12 17:02:50 +0000

    comparison script
    Signed-off-by: charliebudd <charles.budd@kcl.ac.uk>

--- filter_example.py
+++ filter_example.py
@@ -2,29 +2,73 @@ import numpy as np
 import torch
 
 from monai.networks.layers.filtering import BilateralFilter
+from monai.networks.layers.permutohedrallattice import PermutohedralLattice as phl
 
 import skimage
 from skimage.restoration import denoise_bilateral as skimage_bilateral
 import matplotlib.pyplot as plt
 import time
 
+def bilateral_phl(input_tensor, spatial_sigma, color_sigma):
+
+
+    x_coords, y_coords = torch.meshgrid(torch.arange(input_tensor.size(2)), torch.arange(input_tensor.size(3))) 
+    spatial_features = torch.stack([x_coords, y_coords]).unsqueeze(0).type(torch.FloatTensor)
+    
+    feature_tensor = torch.cat([spatial_features / spatial_sigma, input_tensor / color_sigma], dim=1)
+
+    # flattening spatial dimensions
+    input_tensor_shape = input_tensor.size()
+
+    input_tensor = torch.flatten(input_tensor, start_dim=2)
+    feature_tensor = torch.flatten(feature_tensor, start_dim=2)
+
+    # applying phl twice, once to acquire a normalisation mask, then again with the real data
+    normalisation_mask_tensor = phl.apply(feature_tensor, torch.ones_like(input_tensor))
+    filtered_tensor = phl.apply(feature_tensor, input_tensor) / normalisation_mask_tensor
+
+    # reshaping output
+    filtered_tensor = filtered_tensor.reshape(input_tensor_shape)
+
+    return filtered_tensor
+
 # filter parameters
 spatial_sigma = 10.0
 color_sigma = 0.3
 
-# 2d data
-data2d = np.array(skimage.data.astronaut()) / 255.0
-data2d_tensor = torch.from_numpy(data2d).type(torch.FloatTensor).permute(2, 0, 1).unsqueeze(0)
+data = np.array(skimage.data.astronaut()) / 255.0
+data_tensor = torch.from_numpy(data).type(torch.FloatTensor).permute(2, 0, 1).unsqueeze(0)
 
 start = time.time()
-results2d_tensor = BilateralFilter.apply(data2d_tensor, spatial_sigma, color_sigma)
-print(time.time() - start)
+cpp_phl_tensor = BilateralFilter.apply(data_tensor, spatial_sigma, color_sigma, True)
+cpp_phl_time = time.time() - start
+print(cpp_phl_time)
 
-results2d = results2d_tensor.squeeze(0).permute(1, 2, 0).numpy()
+start = time.time()
+cpp_std_tensor = BilateralFilter.apply(data_tensor, spatial_sigma, color_sigma, False)
+cpp_std_time = time.time() - start
+print(cpp_std_time)
+
+start = time.time()
+pyt_phl_tensor = bilateral_phl(data_tensor, spatial_sigma, color_sigma)
+pyt_phl_time = time.time() - start
+print(pyt_phl_time)
+
+cpp_phl_result = cpp_phl_tensor.squeeze(0).permute(1, 2, 0).numpy()
+cpp_std_result = cpp_std_tensor.squeeze(0).permute(1, 2, 0).numpy()
+pyt_phl_result = pyt_phl_tensor.squeeze(0).permute(1, 2, 0).numpy()
 
 # results
-plt.subplot(121).axis('off')
-plt.imshow(data2d)
-plt.subplot(122).axis('off')
-plt.imshow(results2d)
+plt.subplot(221).axis('off')
+plt.title('input image')
+plt.imshow(data)
+plt.subplot(222).axis('off')
+plt.title('pytorch phl: {}sec'.format('%.2f' % pyt_phl_time))
+plt.imshow(pyt_phl_result)
+plt.subplot(223).axis('off')
+plt.title('c++ simple: {}sec'.format('%.2f' % cpp_std_time))
+plt.imshow(cpp_std_result)
+plt.subplot(224).axis('off')
+plt.title('c++ phl: {}sec'.format('%.2f' % cpp_phl_time))
+plt.imshow(cpp_phl_result)
 plt.show()
