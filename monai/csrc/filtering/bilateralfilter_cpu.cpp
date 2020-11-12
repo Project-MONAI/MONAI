3b986769028a827f48f864f746325b0b3c6f2e86
charliebudd <charles.budd@kcl.ac.uk>
2020-11-12 11:02:08 +0000

    allowing generic dimensionality
    Signed-off-by: charliebudd <charles.budd@kcl.ac.uk>

--- monai/csrc/filtering/bilateralfilter_cpu.cpp
+++ monai/csrc/filtering/bilateralfilter_cpu.cpp
@@ -11,9 +11,60 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
+// This cpu implementaion of a bilateral filter is inteded as a backup for the gpu version.
+// It is fully generic in number of channels and spatial dimensions. This flexability
+// comes at the cost of speed. Any future optimisation should aim to implement loop 
+// unrolling by specialising for common channel numbers (1, 3) and spatial dimensions (1, 2, 3) 
+// From my initial test this would bring about a 30% performance boost for those cases. 
+
 #include <torch/extension.h>
 #include <math.h>
 
+// This structures allows us perform a nested for loop
+// with an arbitrary depth. Most use cases will likely be 1-3d
+struct Indexer
+{
+public:
+    Indexer(int dimensions, int64_t* sizes)
+    {
+        m_dimensions = dimensions;
+        m_sizes = sizes;
+        m_index = new int[dimensions]{0};
+    }
+
+    bool operator++(int)
+    {
+        for(int i = 0; i < m_dimensions; i++)
+        {
+            m_index[i] += 1;
+
+            if(m_index[i] < m_sizes[i])
+            {
+                return true;
+            }
+            else
+            {
+                m_index[i] = 0;
+            }
+        }
+
+        return false;
+    }
+
+    int& operator[](int dimensionIndex)
+    {
+        return m_index[dimensionIndex];
+    }
+
+private:
+    int m_dimensions;
+    int64_t* m_sizes;
+    int* m_index;
+};
+
+
+// These helper functions handle operations for an n dimensional vectors.
+// These loops would ideally be unrolled for reasonable vector lengths (<16?)
 inline void ReadValue(float* dst, float* src, int offset, int stride, int count)
 {
     for(int i = 0; i < count; i++)
@@ -78,6 +129,10 @@ torch::Tensor BilateralFilterCpu(torch::Tensor input, float spatialSigma, float 
     int width = input.size(2);
     int height = input.size(3);
 
+    int spatialDimensionCount = input.dim() - 2;
+    int64_t* spatialDimensionSizes = (int64_t*)input.sizes().data() + 2;
+    int64_t* spatialDimensionStrides = (int64_t*)input.strides().data() + 2;
+
     int batchStride = input.stride(0);
     int channelStride = input.stride(1);
     int widthStride = input.stride(2);
@@ -92,6 +147,14 @@ torch::Tensor BilateralFilterCpu(torch::Tensor input, float spatialSigma, float 
     int halfWindowSize = 0.5f * windowSize;
     float spatialExpConstant = -1.0f / (2 * spatialSigma * spatialSigma);
     float colorExpConstant = -1.0f / (2 * colorSigma * colorSigma);
+
+    // Kernel size array
+    int64_t* kernelSize = new int64_t[spatialDimensionCount];
+
+    for (int i = 0; i < spatialDimensionCount; i++)
+    {
+        kernelSize[i] = windowSize;
+    }
 
     // Pre-calculate gaussian kernel in 1D.
     float* gaussianKernel = new float[windowSize];
@@ -111,67 +174,76 @@ torch::Tensor BilateralFilterCpu(torch::Tensor input, float spatialSigma, float 
     float* valueSum = new float[channelCount];
     float weightSum = 0;
 
-    // Looping over the input data, calculating 
-    // offsets into our tensor data.
+    // Looping over the batches
     for (int b = 0; b < batchCount; b++)
     {
         int batchOffset = b * batchStride;
 
-        for (int x = 0; x < width; x++)
+        // Looping over all dimensions for the home element
+        Indexer homeIndex = Indexer(spatialDimensionCount, spatialDimensionSizes);
+        do // while(homeIndex++)
         {
-            int xOffset = x * widthStride;
+            // Calculating indexing offset for the home element
+            int homeOffset = 0;
 
-            for (int y = 0; y < height; y++)
+            for(int i = 0; i < spatialDimensionCount; i++)
             {
-                int yOffset = y * heightStride;
-
-                // Reading the home "color" value.
-                ReadValue(homeColor, inputData, batchOffset + xOffset + yOffset, channelStride, channelCount);
-
-                // Zero kernel aggregates.
-                SetValue(valueSum, 0, channelCount);
-
-                weightSum = 0;
-
-                // Looping over the kernel clamping pixel 
-                // reads to the edge of the input.
-                for (int i = 0; i < windowSize; i++)
-                {
-                    int xKernelOffset = i - halfWindowSize;
-                    int neighbourX = std::min(width - 1, std::max(0, x + xKernelOffset));
-                    int neighbourXStride = neighbourX * widthStride;
-
-                    for (int j = 0; j < windowSize; j++)
-                    {
-                        int yKernelOffset = j - halfWindowSize;
-                        int neighbourY = std::min(height - 1, std::max(0, y + yKernelOffset));
-                        int neighbourYStride = neighbourY * heightStride;
-
-                        // Read the neighbour "color" value.
-                        ReadValue(neighbourColor, inputData, batchOffset + neighbourXStride + neighbourYStride, channelStride, channelCount);
-
-                        // Euclidean color distance.
-                        float colorDistanceSquared = DistanceSquared(homeColor, neighbourColor, channelCount);
-
-                        // Calculating and combining the spatial 
-                        // and color weights.
-                        float spatialWeight = gaussianKernel[i] * gaussianKernel[j];
-                        float colorWeight = exp(colorDistanceSquared * colorExpConstant);
-                        float totalWeight = spatialWeight * colorWeight;
-
-                        // Aggregating values.
-                        MulValue(neighbourColor, totalWeight, channelCount);
-                        AddValue(valueSum, neighbourColor, channelCount);
-
-                        weightSum += totalWeight;
-                    }
-                }
-                
-                MulValue(valueSum, 1.0f/weightSum, channelCount);
-                WriteValue(outputData, valueSum, batchOffset + xOffset + yOffset, channelStride, channelCount);
+                homeOffset += homeIndex[i] * spatialDimensionStrides[i];
             }
-        }
-    }
+
+            // Reading the home "color" value.
+            ReadValue(homeColor, inputData, batchOffset + homeOffset, channelStride, channelCount);
+
+            // Zero kernel aggregates.
+            SetValue(valueSum, 0, channelCount);
+            weightSum = 1.0f;
+
+            // Looping over all dimensions for the neighbour element
+            Indexer kernelIndex = Indexer(spatialDimensionCount, kernelSize);
+            do // while(kernelIndex++)
+            {
+                // Calculating buffer offset for the neighbour element
+                // Index is clamped to the border in each dimension.
+                int neighbourOffset = 0;
+
+                for(int i = 0; i < spatialDimensionCount; i++)
+                {
+                    int neighbourIndex = homeIndex[i] + kernelIndex[i] - halfWindowSize;
+                    int neighbourIndexClamped = std::min(width - 1, std::max(0, neighbourIndex));
+                    neighbourOffset += neighbourIndexClamped * spatialDimensionStrides[i];
+                }
+
+                // Read the neighbour "color" value.
+                ReadValue(neighbourColor, inputData, batchOffset + neighbourOffset, channelStride, channelCount);
+
+                // Euclidean color distance.
+                float colorDistanceSquared = DistanceSquared(homeColor, neighbourColor, channelCount);
+
+                // Calculating and combining the spatial 
+                // and color weights.
+                float spatialWeight = 1;
+
+                for (int i = 0; i < spatialDimensionCount; i++)
+                {
+                    spatialWeight *= gaussianKernel[kernelIndex[i]];
+                }
+
+                float colorWeight = exp(colorDistanceSquared * colorExpConstant);
+
+                float totalWeight = spatialWeight * colorWeight;
+
+                // Aggregating values.
+                MulValue(neighbourColor, totalWeight, channelCount);
+                AddValue(valueSum, neighbourColor, channelCount);
+                weightSum += totalWeight;
+            } 
+            while(kernelIndex++);
+            
+            MulValue(valueSum, 1.0f/weightSum, channelCount);
+            WriteValue(outputData, valueSum, batchOffset + homeOffset, channelStride, channelCount);
+        } 
+        while(homeIndex++);
+    } 
 
     return output;
 }
