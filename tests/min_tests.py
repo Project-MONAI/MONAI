fd24c44c65a2932f13f514ab53eef176d42d3915
Nic Ma <nma@nvidia.com>
2020-11-26 18:21:36 +0800

    1254 Support to cache N transforms in PersistentDataset (#1291)
    * [DLMED] add support to cache N transforms and unit tests
    
    Signed-off-by: Nic Ma <nma@nvidia.com>
    
    * [MONAI] python code formatting
    
    Signed-off-by: monai-bot <monai.miccai2019@gmail.com>
    
    * [DLMED] update LmdbDataset
    
    Signed-off-by: Nic Ma <nma@nvidia.com>
    
    * [DLMED] update according to comments
    
    Signed-off-by: Nic Ma <nma@nvidia.com>
    
    * [MONAI] python code formatting
    
    Signed-off-by: monai-bot <monai.miccai2019@gmail.com>
    
    * [DLMED] fix min test
    
    Signed-off-by: Nic Ma <nma@nvidia.com>
    
    Co-authored-by: monai-bot <monai.miccai2019@gmail.com>

--- tests/min_tests.py
+++ tests/min_tests.py
@@ -76,6 +76,7 @@ def run_testsuit():
         "test_orientationd",
         "test_parallel_execution",
         "test_persistentdataset",
+        "test_cachentransdataset",
         "test_pil_reader",
         "test_plot_2d_or_3d_image",
         "test_png_rw",
