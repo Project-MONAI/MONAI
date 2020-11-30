6790d801d7661717dbf0bfe487f03735de089279
Wenqi Li <wenqil@nvidia.com>
2020-11-30 18:55:31 +0000

    update based on comments
    Signed-off-by: Wenqi Li <wenqil@nvidia.com>

--- monai/visualize/class_activation_maps.py
+++ monai/visualize/class_activation_maps.py
@@ -9,22 +9,35 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import torch
+from typing import Callable, Dict
 
-from monai.utils import ensure_tuple
+import numpy as np
+import torch
+import torch.nn.functional as F
+
+from monai.transforms import ScaleIntensity
+from monai.utils import InterpolateMode, ensure_tuple
 
 
 class ModelWithHooks:
     """
-    A model wrapper to run model forward and store intermediate forward, backward information.
+    A model wrapper to run model forward/backward steps and storing some intermediate feature/gradient information.
     """
 
-    def __init__(self, model, target_layers, register_forward: bool = False, register_backward: bool = False):
-        self.model = model
-        self.target_layers = ensure_tuple(target_layers)
+    def __init__(self, model, target_layer_names, register_forward: bool = False, register_backward: bool = False):
+        """
 
-        self.gradients = {}
-        self.activations = {}
+        Args:
+            model: the model to be wrapped.
+            target_layer_names: the names of the layer to
+            register_forward: whether to cache the forward pass output corresponding to `target_layer_names`.
+            register_backward: whether to cache the backward pass output corresponding to `target_layer_names`.
+        """
+        self.model = model
+        self.target_layers = ensure_tuple(target_layer_names)
+
+        self.gradients: Dict[str, torch.Tensor] = {}
+        self.activations: Dict[str, torch.Tensor] = {}
         self.register_backward = register_backward
         self.register_forward = register_forward
 
@@ -66,54 +79,125 @@ class ModelWithHooks:
         return logits, acti, grad
 
 
+def default_upsampler(spatial_size) -> Callable[[torch.Tensor], torch.Tensor]:
+    """
+    A linear interpolation method for upsampling the feature map.
+    The output of this function is a callable `func`,
+    such that `func(activation_map)` returns an upsampled tensor.
+    """
+
+    def up(acti_map):
+        linear_mode = [InterpolateMode.LINEAR, InterpolateMode.BILINEAR, InterpolateMode.TRILINEAR]
+        interp_mode = linear_mode[len(spatial_size) - 1]
+        return F.interpolate(acti_map, size=spatial_size, mode=str(interp_mode.value), align_corners=False)
+
+    return up
+
+
+def default_normalizer(acti_map) -> np.ndarray:
+    """
+    A linear intensity scaling by mapping the (min, max) to (1, 0).
+    """
+    if isinstance(acti_map, torch.Tensor):
+        acti_map = acti_map.detach().cpu().numpy()
+    scaler = ScaleIntensity(minv=1.0, maxv=0.0)
+    acti_map = [scaler(x) for x in acti_map]
+    return np.stack(acti_map, axis=0)
+
+
 class CAM:
-    def __init__(self, model, target_layers):
+    def __init__(
+        self,
+        model,
+        target_layers,
+        fc_weights=lambda m: m.fc.weight,
+        upsampler=default_upsampler,
+        postprocessing: Callable = default_normalizer,
+    ):
+        """
+
+        Args:
+            model: the model to be visualised
+            target_layers: name of the model layer to generate the feature map.
+            fc_weights: a callable used to get fully-connected weights to compute activation map
+                from the target_layers (without pooling). The default is `lambda m: m.fc.weight`, that is
+                getting the fully-connected layer by `model.fc.weight`.
+            upsampler: an upsampling method to upsample the feature map.
+            postprocessing: a callable that applies on the upsampled feature map.
+        """
         self.net = ModelWithHooks(model, target_layers, register_forward=True)
+        self.upsampler = upsampler
+        self.postprocessing = postprocessing
+        self.fc_weights = fc_weights
 
-    def norm_features(self, feature):
-        feature -= feature.min()
-        feature /= feature.max()
-        return 1.0 - feature
-
-    def __call__(self, x, class_idx=None):
+    def compute_map(self, x, class_idx=None):
         logits, acti, _ = self.net(x)
         acti = acti[0]
         if class_idx is None:
             class_idx = torch.argmax(logits, dim=1)
         b, c, *spatial = acti.shape
-        weight = self.net.model.fc.weight[class_idx].view(c, *[1 for _ in spatial])
-        map = [self.norm_features((weight * a).sum(0)) for a in acti]
-        return torch.stack(map, dim=0)
+        weights = self.fc_weights(self.net.model)
+        weights = weights[class_idx].view(c, *[1 for _ in spatial])
+        return (weights * acti).sum(1, keepdim=True)
+
+    def feature_map_size(self, input_size, device="cpu"):
+        return self.compute_map(torch.zeros(*input_size, device=device)).shape
+
+    def __call__(self, x, class_idx=None):
+        acti_map = self.compute_map(x, class_idx)
+        if self.upsampler:
+            img_spatial = x.shape[2:]
+            acti_map = self.upsampler(img_spatial)(acti_map)
+        if self.postprocessing:
+            acti_map = self.postprocessing(acti_map)
+        return acti_map
 
 
 class GradCAM:
-    def __init__(self, model, target_layers):
+    def __init__(self, model, target_layers, upsampler=default_upsampler, postprocessing=default_normalizer):
+        """
+
+        Args:
+            model: the model to be visualised
+            target_layers: name of the model layer to generate the feature map.
+            upsampler: an upsampling method to upsample the feature map.
+            postprocessing: a callable that applies on the upsampled feature map.
+        """
         self.net = ModelWithHooks(model, target_layers, register_forward=True, register_backward=True)
+        self.upsampler = upsampler
+        self.postprocessing = postprocessing
 
-    def norm_features(self, feature):
-        feature -= feature.min()
-        feature /= feature.max()
-        return 1.0 - feature
-
-    def __call__(self, x):
-        logits, acti, grad = self.net(x)
-        acti = acti[0]
-        grad = grad[0]
+    def compute_map(self, x, class_idx=None, retain_graph=False):
+        logits, acti, grad = self.net(x, class_idx=class_idx, retain_graph=retain_graph)
+        acti, grad = acti[0], grad[0]
         b, c, *spatial = grad.shape
         grad_ave = grad.view(b, c, -1).mean(2)
-        weights = grad_ave.view(b, c, 1, 1)
-        map = (weights * acti).sum(1)
-        return self.norm_features(map)
+        weights = grad_ave.view(b, c, *[1 for _ in spatial])
+        acti_map = (weights * acti).sum(1, keepdim=True)
+        return F.relu(acti_map)
+
+    def feature_map_size(self, input_size, device="cpu"):
+        return self.compute_map(torch.zeros(*input_size, device=device)).shape
+
+    def __call__(self, x, class_idx=None, retain_graph=False):
+        acti_map = self.compute_map(x, class_idx=class_idx, retain_graph=retain_graph)
+        if self.upsampler:
+            img_spatial = x.shape[2:]
+            acti_map = self.upsampler(img_spatial)(acti_map)
+        if self.postprocessing:
+            acti_map = self.postprocessing(acti_map)
+        return acti_map
 
 
 # if __name__ == "__main__":
-#     from torchvision import transforms
+#     import argparse
 #     import glob
+#
+#     import cv2
 #     import numpy as np
 #     import PIL
-#     import cv2
 #     from matplotlib import pyplot as plt
-#     import argparse
+#     from torchvision import transforms
 #
 #     parser = argparse.ArgumentParser()
 #     parser.add_argument("--device", default="0", type=str)
@@ -147,7 +231,7 @@ class GradCAM:
 #         image = PIL.Image.open(file)
 #         h, w, b = np.shape(np.array(image))
 #         img_tensor = preprocess(image).unsqueeze(0).to(device)
-#         cam_img = cam_computer(img_tensor)[0].detach().cpu().numpy()
+#         cam_img = cam_computer(img_tensor)[0, 0]
 #         img = np.array(image)
 #         cam_img = cv2.resize(cam_img, (h, w), interpolation=cv2.INTER_CUBIC)
 #         cam_img = np.uint8(cam_img * 255)
