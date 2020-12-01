2ab243c46f29c08ce3691cbb63d7e6e1bd43dfda
Wenqi Li <wenqil@nvidia.com>
2020-12-01 17:10:50 +0000

    adds cpu tests
    Signed-off-by: Wenqi Li <wenqil@nvidia.com>

--- monai/visualize/class_activation_maps.py
+++ monai/visualize/class_activation_maps.py
@@ -9,7 +9,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from typing import Callable, Dict
+import warnings
+from typing import Callable, Dict, Sequence, Union
 
 import numpy as np
 import torch
@@ -18,36 +19,50 @@ import torch.nn.functional as F
 from monai.transforms import ScaleIntensity
 from monai.utils import InterpolateMode, ensure_tuple
 
+__all__ = ["ModelWithHooks", "default_upsampler", "default_normalizer", "CAM", "GradCAM"]
+
 
 class ModelWithHooks:
     """
     A model wrapper to run model forward/backward steps and storing some intermediate feature/gradient information.
     """
 
-    def __init__(self, model, target_layer_names, register_forward: bool = False, register_backward: bool = False):
+    def __init__(
+        self,
+        nn_module,
+        target_layer_names: Union[str, Sequence[str]],
+        register_forward: bool = False,
+        register_backward: bool = False,
+    ):
         """
 
         Args:
-            model: the model to be wrapped.
+            nn_module: the model to be wrapped.
             target_layer_names: the names of the layer to cache.
             register_forward: whether to cache the forward pass output corresponding to `target_layer_names`.
             register_backward: whether to cache the backward pass output corresponding to `target_layer_names`.
         """
-        self.model = model
+        self.model = nn_module
         self.target_layers = ensure_tuple(target_layer_names)
 
         self.gradients: Dict[str, torch.Tensor] = {}
         self.activations: Dict[str, torch.Tensor] = {}
+        self.score = None
+        self.class_idx = None
         self.register_backward = register_backward
         self.register_forward = register_forward
 
-        for name, mod in model.named_modules():
+        _registered = []
+        for name, mod in nn_module.named_modules():
             if name not in self.target_layers:
                 continue
+            _registered.append(name)
             if self.register_backward:
                 mod.register_backward_hook(self.backward_hook(name))
             if self.register_forward:
                 mod.register_forward_hook(self.forward_hook(name))
+        if len(_registered) != len(self.target_layers):
+            warnings.warn(f"Not all target_layers exist in the network module: targets: {self.target_layers}.")
 
     def backward_hook(self, name):
         def _hook(_module, _grad_input, grad_output):
@@ -61,10 +76,29 @@ class ModelWithHooks:
 
         return _hook
 
+    def get_layer(self, layer_id: Union[str, Callable]):
+        """
+
+        Args:
+            layer_id: a layer name string or a callable. If it is a callable such as `lambda m: m.fc`,
+                this method will return the module `self.model.fc`.
+
+        Returns:
+            a submodule from self.model.
+        """
+        if callable(layer_id):
+            return layer_id(self.model)
+        if isinstance(layer_id, str):
+            for name, mod in self.model.named_modules():
+                if name == layer_id:
+                    return mod
+        raise NotImplementedError(f"Could not find {layer_id}.")
+
     def class_score(self, logits, class_idx=None):
         if class_idx is not None:
-            return logits[:, class_idx].squeeze()
-        return logits[:, logits.argmax(1)].squeeze()
+            return logits[:, class_idx].squeeze(), class_idx
+        class_idx = logits.argmax(1)
+        return logits[:, class_idx].squeeze(), class_idx
 
     def __call__(self, x, class_idx=None, retain_graph=False):
         logits = self.model(x)
@@ -72,9 +106,10 @@ class ModelWithHooks:
         if self.register_forward:
             acti = tuple(self.activations[layer] for layer in self.target_layers)
         if self.register_backward:
-            score = self.class_score(logits, class_idx)
+            score, class_idx = self.class_score(logits, class_idx)
             self.model.zero_grad()
-            score.backward(retain_graph=retain_graph)
+            self.score, self.class_idx = score, class_idx
+            score.sum().backward(retain_graph=retain_graph)
             grad = tuple(self.gradients[layer] for layer in self.target_layers)
         return logits, acti, grad
 
@@ -108,48 +143,100 @@ def default_normalizer(acti_map) -> np.ndarray:
 class CAM:
     """
     Compute class activation map from the last fully-connected layers before the spatial pooling.
+
+    Examples
+
+    .. code-block:: python
+
+        # densenet 2d
+        from monai.networks.nets import densenet121
+        from monai.visualize import CAM
+
+        model_2d = densenet121(spatial_dims=2, in_channels=1, out_channels=3)
+        cam = CAM(nn_module=model_2d, target_layers="class_layers.relu", fc_layers="class_layers.out")
+        result = cam(x=torch.rand((1, 1, 48, 64)))
+
+        # resnet 2d
+        from monai.networks.nets import se_resnet50
+        from monai.visualize import CAM
+
+        model_2d = se_resnet50(spatial_dims=2, in_channels=3, num_classes=4)
+        cam = CAM(nn_module=model_2d, target_layers="layer4", fc_layers="last_linear")
+        result = cam(x=torch.rand((2, 3, 48, 64)))
+
+    See Also:
+
+        - :py:class:`monai.visualize.class_activation_maps.GradCAM`
+
     """
 
     def __init__(
         self,
-        model,
-        target_layers,
-        fc_layers=lambda m: m.fc,
+        nn_module,
+        target_layers: str,
+        fc_layers: Union[str, Callable] = "fc",
         upsampler=default_upsampler,
         postprocessing: Callable = default_normalizer,
     ):
         """
 
         Args:
-            model: the model to be visualised
+            nn_module: the model to be visualised
             target_layers: name of the model layer to generate the feature map.
-            fc_layers: a callable used to get fully-connected weights to compute activation map
-                from the target_layers (without pooling). The default is `lambda m: m.fc`, that is
-                to get the fully-connected layer by `model.fc` and evaluate it at every spatial location.
+            fc_layers: a string or a callable used to get fully-connected weights to compute activation map
+                from the target_layers (without pooling).  and evaluate it at every spatial location.
             upsampler: an upsampling method to upsample the feature map.
             postprocessing: a callable that applies on the upsampled feature map.
         """
-        self.net = ModelWithHooks(model, target_layers, register_forward=True)
+        if not isinstance(nn_module, ModelWithHooks):
+            self.net = ModelWithHooks(nn_module, target_layers, register_forward=True)
+        else:
+            self.net = nn_module
         self.upsampler = upsampler
         self.postprocessing = postprocessing
         self.fc_layers = fc_layers
 
     def compute_map(self, x, class_idx=None, layer_idx=-1):
+        """
+        Compute the actual feature map with input tensor `x`.
+        """
         logits, acti, _ = self.net(x)
         acti = acti[layer_idx]
         if class_idx is None:
             class_idx = torch.argmax(logits, dim=1)
         b, c, *spatial = acti.shape
         acti = torch.split(acti.reshape(b, c, -1), 1, dim=2)  # make the spatial dims 1D
-        fc_layers = self.fc_layers(self.net.model)
+        fc_layers = self.net.get_layer(self.fc_layers)
         output = torch.stack([fc_layers(a[..., 0]) for a in acti], dim=2)
-        output = output[:, class_idx : class_idx + 1]  # only retain the spatial map of the selected class
-        return output.reshape(b, -1, *spatial)  # resume the spatial dims on the selected class
+        output = torch.stack([output[i, b : b + 1] for i, b in enumerate(class_idx)], dim=0)
+        return output.reshape(b, 1, *spatial)  # resume the spatial dims on the selected class
 
     def feature_map_size(self, input_size, device="cpu", layer_idx=-1):
+        """
+        Computes the actual feature map size given `nn_module` and the target_layer name.
+
+        Args:
+            input_size: shape of the input tensor
+            device: the device used to initialise the input tensor
+            layer_idx: index of the target layer if there are multiple target layers. Defaults to -1.
+
+        Returns:
+            shape of the actual feature map.
+        """
         return self.compute_map(torch.zeros(*input_size, device=device), layer_idx=layer_idx).shape
 
     def __call__(self, x, class_idx=None, layer_idx=-1):
+        """
+        Compute the activation map with upsampling and postprocessing.
+
+        Args:
+            x: input tensor, shape must be compatible with `nn_module`.
+            class_idx: index of the class to be visualised. Default to argmax(logits)
+            layer_idx: index of the target layer if there are multiple target layers. Defaults to -1.
+
+        Returns:
+            activation maps
+        """
         acti_map = self.compute_map(x, class_idx, layer_idx)
 
         # upsampling and postprocessing
@@ -169,34 +256,86 @@ class GradCAM:
         Selvaraju et al., Grad-CAM: Visual Explanations from Deep Networks via Gradient-based Localization,
         https://arxiv.org/abs/1610.02391
 
+    Examples
+
+    .. code-block:: python
+
+        # densenet 2d
+        from monai.networks.nets import densenet121
+        from monai.visualize import GradCAM
+
+        model_2d = densenet121(spatial_dims=2, in_channels=1, out_channels=3)
+        cam = GradCAM(nn_module=model_2d, target_layers="class_layers.relu")
+        result = cam(x=torch.rand((1, 1, 48, 64)))
+
+        # resnet 2d
+        from monai.networks.nets import se_resnet50
+        from monai.visualize import GradCAM
+
+        model_2d = se_resnet50(spatial_dims=2, in_channels=3, num_classes=4)
+        cam = GradCAM(nn_module=model_2d, target_layers="layer4")
+        result = cam(x=torch.rand((2, 3, 48, 64)))
+
+    See Also:
+
+        - :py:class:`monai.visualize.class_activation_maps.CAM`
+
     """
 
-    def __init__(self, model, target_layers, upsampler=default_upsampler, postprocessing=default_normalizer):
+    def __init__(self, nn_module, target_layers: str, upsampler=default_upsampler, postprocessing=default_normalizer):
         """
 
         Args:
-            model: the model to be used to generate the visualisations.
+            nn_module: the model to be used to generate the visualisations.
             target_layers: name of the model layer to generate the feature map.
             upsampler: an upsampling method to upsample the feature map.
             postprocessing: a callable that applies on the upsampled feature map.
         """
-        self.net = ModelWithHooks(model, target_layers, register_forward=True, register_backward=True)
+        if not isinstance(nn_module, ModelWithHooks):
+            self.net = ModelWithHooks(nn_module, target_layers, register_forward=True, register_backward=True)
+        else:
+            self.net = nn_module
         self.upsampler = upsampler
         self.postprocessing = postprocessing
 
     def compute_map(self, x, class_idx=None, retain_graph=False, layer_idx=-1):
+        """
+        Compute the actual feature map with input tensor `x`.
+        """
         logits, acti, grad = self.net(x, class_idx=class_idx, retain_graph=retain_graph)
         acti, grad = acti[layer_idx], grad[layer_idx]
         b, c, *spatial = grad.shape
-        grad_ave = grad.view(b, c, -1).mean(2)
-        weights = grad_ave.view(b, c, [1] * len(spatial))
+        weights = grad.view(b, c, -1).mean(2).view(b, c, *[1] * len(spatial))
         acti_map = (weights * acti).sum(1, keepdim=True)
         return F.relu(acti_map)
 
     def feature_map_size(self, input_size, device="cpu", layer_idx=-1):
+        """
+        Computes the actual feature map size given `nn_module` and the target_layer name.
+
+        Args:
+            input_size: shape of the input tensor
+            device: the device used to initialise the input tensor
+            layer_idx: index of the target layer if there are multiple target layers. Defaults to -1.
+
+        Returns:
+            shape of the actual feature map.
+        """
         return self.compute_map(torch.zeros(*input_size, device=device), layer_idx=layer_idx).shape
 
     def __call__(self, x, class_idx=None, layer_idx=-1, retain_graph=False):
+        """
+        Compute the activation map with upsampling and postprocessing.
+
+        Args:
+            x: input tensor, shape must be compatible with `nn_module`.
+            class_idx: index of the class to be visualised. Default to argmax(logits)
+            layer_idx: index of the target layer if there are multiple target layers. Defaults to -1.
+            retain_graph: whether to retain_graph for torch module backward call.
+
+        Returns:
+            activation maps
+        """
         acti_map = self.compute_map(x, class_idx=class_idx, retain_graph=retain_graph, layer_idx=layer_idx)
 
         # upsampling and postprocessing
@@ -206,58 +345,3 @@ class GradCAM:
         if self.postprocessing:
             acti_map = self.postprocessing(acti_map)
         return acti_map
-
-
-# if __name__ == "__main__":
-#     import argparse
-#     import glob
-#
-#     import cv2
-#     import numpy as np
-#     import PIL
-#     from matplotlib import pyplot as plt
-#     from torchvision import transforms
-#
-#     parser = argparse.ArgumentParser()
-#     parser.add_argument("--device", default="0", type=str)
-#     parser.add_argument(
-#         "--target_layer",
-#         default="layer4",
-#         type=str,
-#         help="Specify the name of the target layer (before the pooling layer)",
-#     )
-#     parser.add_argument(
-#         "--final_layer", default="fc", type=str, help="Specify the name of the last classification layer"
-#     )
-#     args = parser.parse_args()
-#     device = torch.device("cuda:" + args.device) if torch.cuda.is_available() else torch.device("cpu")
-#     model = torch.load("temp/resnet-cam.pt", map_location=device)
-#     # print(model)
-#     if torch.cuda.is_available():
-#         model.cuda()
-#     model.eval()
-#     cam_computer = CAM(model, target_layers=args.target_layer)
-#     # cam_computer = GradCAM(model, target_layers=args.target_layer)
-#     resize_param = (224, 224)
-#     norm_mean = [0.5528, 0.5528, 0.5528]
-#     norm_std = [0.1583, 0.1583, 0.1583]
-#     disp_size = 10
-#     preprocess = transforms.Compose(
-#         [transforms.Resize(resize_param), transforms.ToTensor(), transforms.Normalize(norm_mean, norm_std)]
-#     )
-#     plt.figure(figsize=(disp_size, disp_size))
-#     for i, file in enumerate(glob.glob("./temp/test_images/*")):
-#         image = PIL.Image.open(file)
-#         h, w, b = np.shape(np.array(image))
-#         img_tensor = preprocess(image).unsqueeze(0).to(device)
-#         cam_img = cam_computer(img_tensor)[0, 0]
-#         img = np.array(image)
-#         cam_img = cv2.resize(cam_img, (h, w), interpolation=cv2.INTER_CUBIC)
-#         cam_img = np.uint8(cam_img * 255)
-#         height, width, _ = img.shape
-#         heatmap = cv2.applyColorMap(cam_img, cv2.COLORMAP_JET)
-#         result = heatmap * 0.3 + img * 0.6
-#         plt.subplot(2, 1, i + 1)
-#         plt.imshow(result.astype(np.int))
-#
-#     plt.show()
