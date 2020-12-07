c766fe6c2e05206b7f75348a2f215e4114170dba
Nic Ma <nma@nvidia.com>
2020-12-08 01:13:42 +0800

    [DLMED] update ITK version
    Signed-off-by: Nic Ma <nma@nvidia.com>

--- monai/data/image_reader.py
+++ monai/data/image_reader.py
@@ -121,11 +121,7 @@ class ITKReader(ImageReader):
     def __init__(self, **kwargs):
         super().__init__()
         self.kwargs = kwargs
-        if (
-            int(itk.Version.GetITKMajorVersion()) == 5
-            and int(itk.Version.GetITKMinorVersion()) <= 2
-            and int(itk.Version.GetITKBuildVersion()) <= 0
-        ):
+        if (int(itk.Version.GetITKMajorVersion()) == 5 and int(itk.Version.GetITKMinorVersion()) < 2):
             # warning the ITK LazyLoading mechanism was not threadsafe until version 5.2.0,
             # requesting access to the itk.imread function triggers the lazy loading of the relevant itk modules
             # before the parallel use of the function.
