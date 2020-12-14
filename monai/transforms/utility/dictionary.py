bff4d3a51a477028448ccd3ebfa13196d133770f
Isaac Yang <isaacy@nvidia.com>
2020-12-14 01:17:43 -0800

    Add ConvertToMultiChannelBasedOnBratsClassesd to handle BraTS18 dataset label (#1347)
    Signed-off-by: Isaac Yang <isaacy@nvidia.com>
    Signed-off-by: Sachidanand Alle <salle@nvidia.com>

--- monai/transforms/utility/dictionary.py
+++ monai/transforms/utility/dictionary.py
@@ -636,6 +636,30 @@ class FgBgToIndicesd(MapTransform):
         return d
 
 
+class ConvertToMultiChannelBasedOnBratsClassesd(MapTransform):
+    """
+    Convert labels to multi channels based on brats18 classes:
+    label 1 is the necrotic and non-enhancing tumor core
+    label 2 is the the peritumoral edema
+    label 4 is the GD-enhancing tumor
+    The possible classes are TC (Tumor core), WT (Whole tumor)
+    and ET (Enhancing tumor).
+    """
+
+    def __call__(self, data: Mapping[Hashable, np.ndarray]) -> Dict[Hashable, np.ndarray]:
+        d = dict(data)
+        for key in self.keys:
+            result = list()
+            # merge labels 1 (tumor non-enh) and 4 (tumor enh) to TC
+            result.append(np.logical_or(d[key] == 1, d[key] == 4))
+            # merge labels 1 (tumor non-enh) and 4 (tumor enh) and 2 (large edema) to WT
+            result.append(np.logical_or(np.logical_or(d[key] == 1, d[key] == 4), d[key] == 2))
+            # label 4 is ET
+            result.append(d[key] == 4)
+            d[key] = np.stack(result, axis=0).astype(np.float32)
+        return d
+
+
 IdentityD = IdentityDict = Identityd
 AsChannelFirstD = AsChannelFirstDict = AsChannelFirstd
 AsChannelLastD = AsChannelLastDict = AsChannelLastd
@@ -653,3 +677,6 @@ ConcatItemsD = ConcatItemsDict = ConcatItemsd
 LambdaD = LambdaDict = Lambdad
 LabelToMaskD = LabelToMaskDict = LabelToMaskd
 FgBgToIndicesD = FgBgToIndicesDict = FgBgToIndicesd
+ConvertToMultiChannelBasedOnBratsClassesD = (
+    ConvertToMultiChannelBasedOnBratsClassesDict
+) = ConvertToMultiChannelBasedOnBratsClassesd
