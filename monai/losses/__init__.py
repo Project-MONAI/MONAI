5aa5e163265d0a1ea41c0e96d7f18573458abbb7
Yiwen Li <44606435+kate-sann5100@users.noreply.github.com>
2021-01-06 19:07:10 +0000

    1405 add bending energy loss (#1408)
    * 1405 add bending energy loss
    
    Signed-off-by: kate-sann5100 <yiwen.li@st-annes.ox.ac.uk>

--- monai/losses/__init__.py
+++ monai/losses/__init__.py
@@ -9,6 +9,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from .deform import BendingEnergyLoss
 from .dice import (
     Dice,
     DiceCELoss,
