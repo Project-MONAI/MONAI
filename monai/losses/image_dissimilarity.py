ac36a9fc877882cf8da48378479782a9e58c7b8e
kate-sann5100 <yiwen.li@st-annes.ox.ac.uk>
2021-01-07 15:00:23 +0000

    1412 reformat code
    Signed-off-by: kate-sann5100 <yiwen.li@st-annes.ox.ac.uk>

--- monai/losses/image_dissimilarity.py
+++ monai/losses/image_dissimilarity.py
@@ -1,3 +1,14 @@
+# Copyright 2020 - 2021 MONAI Consortium
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#     http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import torch
 from torch.nn import functional as F
 from torch.nn.modules.loss import _Loss
@@ -5,8 +16,6 @@ from torch.nn.modules.loss import _Loss
 from monai.utils import LossReduction, Union
 
 conv_dict = {1: F.conv1d, 2: F.conv2d, 3: F.conv3d}
-
-EPS = 1e-7
 
 
 class LocalNormalizedCrossCorrelationLoss(_Loss):
@@ -29,6 +38,7 @@ class LocalNormalizedCrossCorrelationLoss(_Loss):
         kernel_size: int = 9,
         kernel_type: str = "rectangular",
         reduction: Union[LossReduction, str] = LossReduction.MEAN,
+        smooth_dr: float = 1e-7,
     ) -> None:
         """
         Args:
@@ -42,12 +52,14 @@ class LocalNormalizedCrossCorrelationLoss(_Loss):
                 - ``"none"``: no reduction will be applied.
                 - ``"mean"``: the sum of the output will be divided by the number of elements in the output.
                 - ``"sum"``: the output will be summed.
+            smooth_dr: a small constant added to the denominator to avoid nan.
         """
         super(LocalNormalizedCrossCorrelationLoss, self).__init__(reduction=LossReduction(reduction).value)
         self.in_channels = in_channels
         self.ndim = ndim
         if self.ndim not in [1, 2, 3]:
             raise ValueError(f"Unsupported ndim: {self.ndim}-d, only 1-d, 2-d, and 3-d inputs are supported")
+        self.fn = conv_dict[self.ndim]
         self.kernel_size = kernel_size
         if kernel_type == "rectangular":
             self.kernel, self.kernel_vol, self.padding = self.make_rectangular_kernel()
@@ -59,26 +71,29 @@ class LocalNormalizedCrossCorrelationLoss(_Loss):
             raise ValueError(
                 f'Unsupported kernel_type: {kernel_type}, available options are ["rectangular", "triangular", "gaussian"].'
             )
+        self.smooth_dr = float(smooth_dr)
 
     def make_rectangular_kernel(self):
         shape = [1, self.in_channels] + [self.kernel_size] * self.ndim
         return torch.ones(shape, dtype=torch.float), self.kernel_size ** self.ndim, int((self.kernel_size - 1) / 2)
 
     def make_triangular_kernel(self):
-        fsize = torch.tensor((self.kernel_size + 1) / 2, dtype=torch.int)
-        f1 = torch.ones([1, 1] + [fsize] * self.ndim, dtype=torch.float) / fsize  # (1, 1, D, H, W)
-        f2 = (
-            torch.ones([self.in_channels, 1] + [fsize] * self.ndim, dtype=torch.float) / fsize
-        )  # (1, in_channels, D, H, W)
+        fsize = int((self.kernel_size + 1) // 2)
+        f1 = torch.ones([1, 1] + [fsize] * self.ndim, dtype=torch.float).div(fsize)  # (1, 1, D, H, W)
+        f1 = F.pad(f1, [(fsize - 1) // 2, (fsize - 1) // 2] * self.ndim)
+        f2 = torch.ones([self.in_channels, 1] + [fsize] * self.ndim, dtype=torch.float).div(fsize)
+        # (in_channels, 1, D, H, W)
         # (1, 1, D, H, W) -> (1, in_channels, D, H, W)
-        fn = conv_dict[self.ndim]
-        kernel = fn(f1, f2, padding=int((fsize - 1) / 2))
+        padding_needed = max(fsize - 1, 0)
+        padding = [padding_needed // 2, padding_needed - padding_needed // 2] * self.ndim
+        f1 = F.pad(f1, padding)
+        kernel = self.fn(f1, f2)
 
-        return kernel, torch.sum(kernel ** 2), int((fsize - 1) / 2)
+        return kernel, torch.sum(kernel ** 2), int((fsize - 1) / 2.0)
 
     def make_gaussian_kernel(self):
         mean = (self.kernel_size - 1) / 2.0
-        sigma = self.kernel_size / 3
+        sigma = self.kernel_size / 3.0
 
         grid_ndim = torch.arange(0, self.kernel_size)
         grid_ndim_ch = torch.arange(0, self.in_channels)
@@ -96,7 +111,7 @@ class LocalNormalizedCrossCorrelationLoss(_Loss):
         kernel = torch.exp(-torch.sum(torch.square(grid - mean), dim=-1) / (2 * sigma ** 2)).unsqueeze(
             0
         )  # (1, in_channel, kernel_size, kernel_size, kernel_size)
-        return kernel, torch.sum(kernel ** 2), int((self.kernel_size - 1) / 2)
+        return kernel, torch.sum(kernel ** 2), int((self.kernel_size - 1) / 2.0)
 
     def forward(self, input: torch.Tensor, target: torch.Tensor) -> torch.Tensor:
         """
@@ -119,12 +134,11 @@ class LocalNormalizedCrossCorrelationLoss(_Loss):
         t2, p2, tp = target ** 2, input ** 2, target * input
 
         # sum over kernel
-        fn = conv_dict[self.ndim]
-        t_sum = fn(target, weight=self.kernel, padding=self.padding)
-        p_sum = fn(input, weight=self.kernel, padding=self.padding)
-        t2_sum = fn(t2, weight=self.kernel, padding=self.padding)
-        p2_sum = fn(p2, weight=self.kernel, padding=self.padding)
-        tp_sum = fn(tp, weight=self.kernel, padding=self.padding)
+        t_sum = self.fn(target, weight=self.kernel, padding=self.padding)
+        p_sum = self.fn(input, weight=self.kernel, padding=self.padding)
+        t2_sum = self.fn(t2, weight=self.kernel, padding=self.padding)
+        p2_sum = self.fn(p2, weight=self.kernel, padding=self.padding)
+        tp_sum = self.fn(tp, weight=self.kernel, padding=self.padding)
 
         # average over kernel
         t_avg = t_sum / self.kernel_vol
@@ -142,12 +156,12 @@ class LocalNormalizedCrossCorrelationLoss(_Loss):
         cross = tp_sum - p_avg * t_sum
         t_var = t2_sum - t_avg * t_sum  # std[t] ** 2
         p_var = p2_sum - p_avg * p_sum  # std[p] ** 2
-        ncc = (cross * cross + EPS) / (t_var * p_var + EPS)  # shape = (batch, 1, D, H, W)
+        ncc = (cross * cross + self.smooth_dr) / (t_var * p_var + self.smooth_dr)  # shape = (batch, 1, D, H, W)
 
         if self.reduction == LossReduction.SUM.value:
-            return -torch.sum(ncc)  # sum over the batch and channel ndims
+            return -torch.sum(ncc).neg()  # sum over the batch and channel ndims
         if self.reduction == LossReduction.NONE.value:
-            return -ncc
+            return ncc.neg()
         if self.reduction == LossReduction.MEAN.value:
-            return -torch.mean(ncc)  # average over the batch and channel ndims
+            return torch.mean(ncc).neg()  # average over the batch and channel ndims
         raise ValueError(f'Unsupported reduction: {self.reduction}, available options are ["mean", "sum", "none"].')
--- tests/test_local_normalized_cross_correlation_loss.py
+++ tests/test_local_normalized_cross_correlation_loss.py
@@ -1,3 +1,14 @@
+# Copyright 2020 MONAI Consortium
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#     http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import unittest
 
 import numpy as np
@@ -72,7 +83,7 @@ TEST_CASES = [
         -0.06062524,
     ],
     [
-        {"in_channels": 3, "ndim": 3, "kernel_size": 3, "kernel_type": "triangular"},
+        {"in_channels": 3, "ndim": 3, "kernel_size": 6, "kernel_type": "triangular"},
         {
             "input": torch.arange(0, 3, dtype=torch.float)[None, :, None, None, None].expand(1, 3, 3, 3, 3),
             "target": torch.arange(0, 3, dtype=torch.float)[None, :, None, None, None].expand(1, 3, 3, 3, 3) ** 2,
@@ -90,7 +101,7 @@ TEST_CASES = [
 ]
 
 
-class TestBendingEnergy(unittest.TestCase):
+class TestLocalNormalizedCrossCorrelationLoss(unittest.TestCase):
     @parameterized.expand(TEST_CASES)
     def test_shape(self, input_param, input_data, expected_val):
         result = LocalNormalizedCrossCorrelationLoss(**input_param).forward(**input_data)
