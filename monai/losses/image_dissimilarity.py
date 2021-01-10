af4cab559fc56ed47139f6546b443273fbafc605
kate-sann5100 <yiwen.li@st-annes.ox.ac.uk>
2021-01-10 17:35:07 +0000

    1412 remove redundant import
    Signed-off-by: kate-sann5100 <yiwen.li@st-annes.ox.ac.uk>

--- monai/losses/image_dissimilarity.py
+++ monai/losses/image_dissimilarity.py
@@ -8,7 +8,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from typing import Tuple, Union
+from typing import Union
 
 import torch
 from torch.nn import functional as F
