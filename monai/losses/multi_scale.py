7e4f30656042c9e877e623c61ac48c13b6481569
kate-sann5100 <yiwen.li@st-annes.ox.ac.uk>
2021-02-03 23:47:57 +0000

    1525 add MultiScaleLoss
    Signed-off-by: kate-sann5100 <yiwen.li@st-annes.ox.ac.uk>

---
+++ monai/losses/multi_scale.py
@@ -0,0 +1,80 @@
+from typing import Optional, List, Union
+
+import torch
+from torch.nn.modules.loss import _Loss
+
+from monai.networks.layers import separable_filtering, gaussian_1d
+
+
+def make_gaussian_kernel(sigma: int) -> torch.Tensor:
+    if sigma <= 0:
+        raise ValueError(f"expecting postive sigma, got sign={sigma}")
+    sigma = torch.tensor(sigma)
+    kernel = gaussian_1d(sigma=sigma, truncated=3, approx="sampled", normalize=False)
+    return kernel
+
+
+def make_cauchy_kernel(sigma: int) -> torch.Tensor:
+    """
+    Approximating cauchy kernel in 1d.
+
+    :param sigma: int, defining standard deviation of kernel.
+    :return: shape = (dim, )
+    """
+    assert sigma > 0
+    tail = int(sigma * 5)
+    k = torch.tensor([((x / sigma) ** 2 + 1) for x in range(-tail, tail + 1)])
+    k = torch.reciprocal(k)
+    k = k / torch.sum(k)
+    return k
+
+
+kernel_fn_dict = {
+    "gaussian": make_gaussian_kernel,
+    "cauchy": make_cauchy_kernel,
+}
+
+
+class MultiScaleLoss(_Loss):
+    def __init__(
+        self,
+        loss: _Loss,
+        scales: Optional[List] = None,
+        kernel: str = "gaussian",
+    ):
+        """
+        Args:
+            scales: list of scalars or None, if None, do not apply any scaling.
+            kernel: gaussian or cauchy.
+            reduction: using SUM reduction over batch axis,
+                calling the loss like `loss(y_true, y_pred)` will return a scalar tensor.
+            name: str, name of the loss.
+        """
+        super(MultiScaleLoss, self).__init__()
+        if kernel not in kernel_fn_dict.keys():
+            raise ValueError(f"got unsupported kernel type: {kernel}",
+                             "only support gaussian and cauchy")
+        self.kernel_fn = kernel_fn_dict[kernel]
+        self.loss = loss
+        self.scales = scales
+
+    def forward(self, y_true: torch.Tensor, y_pred: torch.Tensor) -> torch.Tensor:
+        if self.scales is None:
+            return self.loss(y_pred, y_true)
+        losses = []
+        for s in self.scales:
+            if s == 0:
+                # no smoothing
+                losses.append(
+                    self.loss(y_pred, y_true)
+                )
+            else:
+                losses.append(
+                    self.loss(
+                        separable_filtering(y_pred, [self.kernel_fn(s)] * (y_true.ndim - 2)),
+                        separable_filtering(y_true, [self.kernel_fn(s)] * (y_true.ndim - 2)),
+                    )
+                )
+        loss = torch.mean(torch.stack(losses, dim=0), dim=0)
+        return loss
+
---
+++ tests/test_multi_scale.py
@@ -0,0 +1,84 @@
+# Copyright 2020 - 2021 MONAI Consortium
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#     http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import unittest
+
+import torch
+import numpy as np
+from parameterized import parameterized
+
+from monai.losses import DiceLoss
+from monai.losses.multi_scale import MultiScaleLoss
+
+dice_loss = DiceLoss(
+    include_background=True,
+    sigmoid=True,
+    smooth_nr=1e-5,
+    smooth_dr=1e-5
+)
+
+TEST_CASES = [
+    [
+        {
+            "loss": dice_loss,
+            "scales": None,
+            "kernel": "gaussian"
+        },
+        {
+            "y_pred": torch.tensor([[[[1.0, -1.0], [-1.0, 1.0]]]]),
+            "y_true": torch.tensor([[[[1.0, 0.0], [1.0, 1.0]]]])
+        },
+        0.307576,
+    ],
+    [
+        {
+            "loss": dice_loss,
+            "scales": [0, 1],
+            "kernel": "gaussian"
+        },
+        {
+            "y_pred": torch.tensor([[[[1.0, -1.0], [-1.0, 1.0]]]]),
+            "y_true": torch.tensor([[[[1.0, 0.0], [1.0, 1.0]]]])
+        },
+        0.463116,
+    ],
+    [
+        {
+            "loss": dice_loss,
+            "scales": [0, 1, 2],
+            "kernel": "cauchy"
+        },
+        {
+            "y_pred": torch.tensor([[[[[1.0, -1.0], [-1.0, 1.0]]]]]),
+            "y_true": torch.tensor([[[[[1.0, 0.0], [1.0, 1.0]]]]])
+        },
+        0.715228,
+    ]
+
+]
+
+
+class TestMultiScale(unittest.TestCase):
+    @parameterized.expand(TEST_CASES)
+    def test_shape(self, input_param, input_data, expected_val):
+        result = MultiScaleLoss(**input_param).forward(**input_data)
+        np.testing.assert_allclose(result.detach().cpu().numpy(), expected_val, rtol=1e-5)
+
+    def test_ill_opts(self):
+        with self.assertRaisesRegex(ValueError, ""):
+            MultiScaleLoss(loss=dice_loss, kernel="none")
+        with self.assertRaisesRegex(ValueError, ""):
+            MultiScaleLoss(loss=dice_loss, scales=[-1])(
+                torch.ones((1, 1, 3)), torch.ones((1, 1, 3))
+            )
+
+
+if __name__ == "__main__":
+    unittest.main()
