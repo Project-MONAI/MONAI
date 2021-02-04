7e7744935cf9e9e51322b7a7ccb46cccbb20fe92
Behrooz <3968947+behxyz@users.noreply.github.com>
2021-02-04 14:21:36 -0500

    Add unittests for OpenSlideReader
    Signed-off-by: Behrooz <3968947+behxyz@users.noreply.github.com>

---
+++ tests/test_openslide_reader.py
@@ -0,0 +1,60 @@
+import ftplib
+import os
+import unittest
+
+import numpy as np
+from numpy.testing import assert_array_equal
+from parameterized import parameterized
+from monai.data.image_reader import OpenSlideReader
+
+filename = "test_001.tif"
+
+
+TEST_CASE_1 = [
+    filename,
+    {"location": (86016 // 2, 89600 // 2), "size": (1, 2), "level": 4},
+    np.array([[[234], [223]], [[174], [163]], [[228], [217]]]),
+]
+
+TEST_CASE_2 = [
+    filename,
+    {"location": (86016 // 2, 89600 // 2), "size": (1, 2), "level": 2},
+    np.array([[[220], [197]], [[165], [143]], [[220], [195]]]),
+]
+
+TEST_CASE_3 = [
+    filename,
+    {"location": (86016 // 2, 89600 // 2), "size": (8, 8), "level": 2, "grid_shape": (2, 1), "patch_size": 2},
+    np.array(
+        [
+            [[[218, 242], [189, 198]], [[154, 173], [125, 132]], [[214, 236], [185, 194]]],
+            [[[190, 209], [221, 228]], [[120, 137], [149, 154]], [[180, 200], [212, 217]]],
+        ]
+    ),
+]
+
+
+class TestOpenSlideReader(unittest.TestCase):
+    @parameterized.expand([TEST_CASE_1, TEST_CASE_2])
+    def test_read_region(self, filename, patch_info, expected_img):
+        self.camelyon_data_download(filename)
+        reader = OpenSlideReader()
+        img_obj = reader.read(filename)
+        img = reader.get_data(img_obj, **patch_info)
+
+        self.assertTupleEqual(img.shape, expected_img.shape)
+        self.assertIsNone(assert_array_equal(img, expected_img))
+
+    def camelyon_data_download(self, filename):
+        if not os.path.exists(filename):
+            print(f"Test image [{filename}] does not exists downloading...")
+            path = "gigadb/pub/10.5524/100001_101000/100439/CAMELYON16/testing/images/"
+            ftp = ftplib.FTP("parrot.genomics.cn")
+            ftp.login("anonymous", "")
+            ftp.cwd(path)
+            ftp.retrbinary("RETR " + filename, open(filename, "wb").write)
+            ftp.quit()
+
+
+if __name__ == "__main__":
+    unittest.main()
