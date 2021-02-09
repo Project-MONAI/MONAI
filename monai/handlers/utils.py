f66b2e4de1df0ec516c7e4b4fe69aa6ed59e2587
Nic Ma <nma@nvidia.com>
2021-02-09 11:42:41 +0800

    [DLMED] fix classification saver issue
    Signed-off-by: Nic Ma <nma@nvidia.com>

--- monai/handlers/utils.py
+++ monai/handlers/utils.py
@@ -103,7 +103,7 @@ def string_list_all_gather(strings: List[str], delimiter: str = "\t") -> List[st
         # all gather across all ranks
         _joined = delimiter.join(idist.all_gather(_joined))
     else:
-        raise RuntimeError("MetricsSaver can not save metric details in distributed mode with PyTorch < 1.7.0.")
+        raise RuntimeError("string all_gather can not be supported in PyTorch < 1.7.0.")
 
     return _joined.split(delimiter)
 
