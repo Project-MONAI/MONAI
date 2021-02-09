3933045b53c1ea7d83cb18d4651cc134074dd1da
Leo Tam <leot@nvidia.com>
2021-02-09 00:17:47 +0000

    DeleteChannel array, dictionary, tests
    Signed-off-by: Leo Tam <leot@nvidia.com>

---
+++ tests/test_delete_channeld.py
@@ -0,0 +1,32 @@
+# Copyright 2020 - 2021 MONAI Consortium
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#     http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import unittest
+
+import numpy as np
+from parameterized import parameterized
+
+from monai.transforms import DeleteChanneld
+
+TEST_CASE_1 = [
+    {"keys": ["img"], "repeats": 2},
+    {"img": np.array([[1, 2], [1, 2], [3, 4], [3, 4]]), "seg": np.array([[1, 2], [1, 2], [3, 4], [3, 4]])},
+    (2, 2),
+]
+
+class TestAddChanneld(unittest.TestCase):
+    @parameterized.expand([TEST_CASE_1])
+    def test_shape(self, input_param, input_data, expected_shape):
+        result = DeleteChanneld(**input_param)(input_data)
+        self.assertEqual(result["img"].shape, expected_shape)
+
+if __name__ == "__main__":
+    unittest.main()
