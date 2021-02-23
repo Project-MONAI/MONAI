4080b6914c3f01e6f364ed1ace048f6a8f777cce
Rich <33289025+rijobro@users.noreply.github.com>
2021-02-23 15:27:24 +0000

    move transforms out of compose file
    Signed-off-by: Richard Brown <33289025+rijobro@users.noreply.github.com>

--- monai/transforms/croppad/array.py
+++ monai/transforms/croppad/array.py
@@ -20,7 +20,7 @@ import torch
 
 from monai.config import IndexSelection
 from monai.data.utils import get_random_patch, get_valid_patch_size
-from monai.transforms.compose import Randomizable, Transform
+from monai.transforms.transform import Randomizable, Transform
 from monai.transforms.utils import (
     generate_pos_neg_label_crop_centers,
     generate_spatial_bounding_box,
