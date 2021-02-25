175ae9112b6f2c176c4c214097924f3fb1a6a88e
Wenqi Li <wenqil@nvidia.com>
2021-02-25 23:26:14 +0000

    fixes metrics dist test
    Signed-off-by: Wenqi Li <wenqil@nvidia.com>

--- tests/test_handler_metrics_saver_dist.py
+++ tests/test_handler_metrics_saver_dist.py
@@ -12,7 +12,6 @@
 
 import csv
 import os
-import random
 import tempfile
 import unittest
 
@@ -28,85 +27,82 @@ from tests.utils import DistCall, DistTestCase, SkipIfBeforePyTorchVersion
 class DistributedMetricsSaver(DistTestCase):
     @DistCall(nnodes=1, nproc_per_node=2)
     def test_content(self):
-        self._run()
-
-    def _run(self):
         with tempfile.TemporaryDirectory() as tempdir:
-            metrics_saver = MetricsSaver(
-                save_dir=tempdir,
-                metrics=["metric1", "metric2"],
-                metric_details=["metric3", "metric4"],
-                batch_transform=lambda x: x["image_meta_dict"],
-                summary_ops="*",
-            )
+            self._run(tempdir)
 
-            def _val_func(engine, batch):
-                pass
+    def _run(self, tempdir):
+        fnames = ["aaa" * 300, "bbb" * 301, "ccc" * 302]
 
-            engine = Engine(_val_func)
+        metrics_saver = MetricsSaver(
+            save_dir=tempdir,
+            metrics=["metric1", "metric2"],
+            metric_details=["metric3", "metric4"],
+            batch_transform=lambda x: x["image_meta_dict"],
+            summary_ops="*",
+        )
 
-            # test the case that all_gather with string length > 1024 chars
-            filename_postfix = "abcdefghigklmnopqrstuvwxyz"
-            for _ in range(1100):
-                filename_postfix += filename_postfix[random.randint(0, 26)]
+        def _val_func(engine, batch):
+            pass
 
-            if dist.get_rank() == 0:
-                data = [{"image_meta_dict": {"filename_or_obj": [f"1{filename_postfix}"]}}]
+        engine = Engine(_val_func)
 
-                @engine.on(Events.EPOCH_COMPLETED)
-                def _save_metrics0(engine):
-                    engine.state.metrics = {"metric1": 1, "metric2": 2}
-                    engine.state.metric_details = {
-                        "metric3": torch.tensor([[1, 2]]),
-                        "metric4": torch.tensor([[5, 6]]),
-                    }
+        if dist.get_rank() == 0:
+            data = [{"image_meta_dict": {"filename_or_obj": [fnames[0]]}}]
 
-            if dist.get_rank() == 1:
-                # different ranks have different data length
-                data = [
-                    {"image_meta_dict": {"filename_or_obj": [f"2{filename_postfix}"]}},
-                    {"image_meta_dict": {"filename_or_obj": [f"3{filename_postfix}"]}},
-                ]
+            @engine.on(Events.EPOCH_COMPLETED)
+            def _save_metrics0(engine):
+                engine.state.metrics = {"metric1": 1, "metric2": 2}
+                engine.state.metric_details = {
+                    "metric3": torch.tensor([[1, 2]]),
+                    "metric4": torch.tensor([[5, 6]]),
+                }
 
-                @engine.on(Events.EPOCH_COMPLETED)
-                def _save_metrics1(engine):
-                    engine.state.metrics = {"metric1": 1, "metric2": 2}
-                    engine.state.metric_details = {
-                        "metric3": torch.tensor([[2, 3], [3, 4]]),
-                        "metric4": torch.tensor([[6, 7], [7, 8]]),
-                    }
+        if dist.get_rank() == 1:
+            # different ranks have different data length
+            data = [
+                {"image_meta_dict": {"filename_or_obj": [fnames[1]]}},
+                {"image_meta_dict": {"filename_or_obj": [fnames[2]]}},
+            ]
 
-            metrics_saver.attach(engine)
-            engine.run(data, max_epochs=1)
+            @engine.on(Events.EPOCH_COMPLETED)
+            def _save_metrics1(engine):
+                engine.state.metrics = {"metric1": 1, "metric2": 2}
+                engine.state.metric_details = {
+                    "metric3": torch.tensor([[2, 3], [3, 4]]),
+                    "metric4": torch.tensor([[6, 7], [7, 8]]),
+                }
 
-            if dist.get_rank() == 0:
-                # check the metrics.csv and content
-                self.assertTrue(os.path.exists(os.path.join(tempdir, "metrics.csv")))
-                with open(os.path.join(tempdir, "metrics.csv")) as f:
-                    f_csv = csv.reader(f)
-                    for i, row in enumerate(f_csv):
-                        self.assertEqual(row, [f"metric{i + 1}\t{i + 1}"])
-                self.assertTrue(os.path.exists(os.path.join(tempdir, "metric3_raw.csv")))
-                # check the metric_raw.csv and content
-                with open(os.path.join(tempdir, "metric3_raw.csv")) as f:
-                    f_csv = csv.reader(f)
-                    for i, row in enumerate(f_csv):
-                        if i > 0:
-                            expected = [f"{i}{filename_postfix[0: 1023]}\t{float(i)}\t{float(i + 1)}\t{i + 0.5}"]
-                            self.assertEqual(row, expected)
-                self.assertTrue(os.path.exists(os.path.join(tempdir, "metric3_summary.csv")))
-                # check the metric_summary.csv and content
-                with open(os.path.join(tempdir, "metric3_summary.csv")) as f:
-                    f_csv = csv.reader(f)
-                    for i, row in enumerate(f_csv):
-                        if i == 1:
-                            self.assertEqual(row, ["class0\t1.0000\t1.0000\t1.0000\t1.0000\t1.0000\t0.0000"])
-                        elif i == 2:
-                            self.assertEqual(row, ["class1\t2.0000\t2.0000\t2.0000\t2.0000\t2.0000\t0.0000"])
-                        elif i == 3:
-                            self.assertEqual(row, ["mean\t1.5000\t1.5000\t1.5000\t1.5000\t1.5000\t0.0000"])
-                self.assertTrue(os.path.exists(os.path.join(tempdir, "metric4_raw.csv")))
-                self.assertTrue(os.path.exists(os.path.join(tempdir, "metric4_summary.csv")))
+        metrics_saver.attach(engine)
+        engine.run(data, max_epochs=1)
+
+        if dist.get_rank() == 0:
+            # check the metrics.csv and content
+            self.assertTrue(os.path.exists(os.path.join(tempdir, "metrics.csv")))
+            with open(os.path.join(tempdir, "metrics.csv")) as f:
+                f_csv = csv.reader(f)
+                for i, row in enumerate(f_csv):
+                    self.assertEqual(row, [f"metric{i + 1}\t{i + 1}"])
+            self.assertTrue(os.path.exists(os.path.join(tempdir, "metric3_raw.csv")))
+            # check the metric_raw.csv and content
+            with open(os.path.join(tempdir, "metric3_raw.csv")) as f:
+                f_csv = csv.reader(f)
+                for i, row in enumerate(f_csv):
+                    if i > 0:
+                        expected = [f"{fnames[i-1]}\t{float(i)}\t{float(i + 1)}\t{i + 0.5}"]
+                        self.assertEqual(row, expected)
+            self.assertTrue(os.path.exists(os.path.join(tempdir, "metric3_summary.csv")))
+            # check the metric_summary.csv and content
+            with open(os.path.join(tempdir, "metric3_summary.csv")) as f:
+                f_csv = csv.reader(f)
+                for i, row in enumerate(f_csv):
+                    if i == 1:
+                        self.assertEqual(row, ["class0\t1.0000\t1.0000\t1.0000\t1.0000\t1.0000\t0.0000"])
+                    elif i == 2:
+                        self.assertEqual(row, ["class1\t2.0000\t2.0000\t2.0000\t2.0000\t2.0000\t0.0000"])
+                    elif i == 3:
+                        self.assertEqual(row, ["mean\t1.5000\t1.5000\t1.5000\t1.5000\t1.5000\t0.0000"])
+            self.assertTrue(os.path.exists(os.path.join(tempdir, "metric4_raw.csv")))
+            self.assertTrue(os.path.exists(os.path.join(tempdir, "metric4_summary.csv")))
 
 
 if __name__ == "__main__":
