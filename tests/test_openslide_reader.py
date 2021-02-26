b851859fe201ea8ce0b3c6f90e71e300ede2a8a8
Behrooz <3968947+behxyz@users.noreply.github.com>
2021-02-26 10:56:58 -0500

    Replace the test TIFF and some upgrades
    Signed-off-by: Behrooz <3968947+behxyz@users.noreply.github.com>

--- tests/test_openslide_reader.py
+++ tests/test_openslide_reader.py
@@ -1,63 +1,91 @@
-import ftplib
 import os
 import unittest
+from typing import TypedDict
+from unittest import skipUnless
+from urllib import request
 
 import numpy as np
 from numpy.testing import assert_array_equal
 from parameterized import parameterized
 
 from monai.data.image_reader import WSIReader
-from tests.utils import skip_if_quick
+from monai.utils import optional_import
 
-filename = "test_065.tif"
+_, has_osl = optional_import("openslide")
 
-TEST_CASE_0 = [filename, (3, 53760, 77824)]
+
+class SampleImage(TypedDict):
+    name: str
+    url: str
+    height: int
+    width: int
+
+
+FILE_INFO: SampleImage = {
+    "name": "CMU-1.tiff",
+    "url": "http://openslide.cs.cmu.edu/download/openslide-testdata/Generic-TIFF/CMU-1.tiff",
+    "height": 32914,
+    "width": 46000,
+}
+
+TEST_CASE_0 = [FILE_INFO, (3, FILE_INFO["height"], FILE_INFO["width"])]
 
 TEST_CASE_1 = [
-    filename,
-    {"location": (53760 // 2, 77824 // 2), "size": (2, 1), "level": 4},
-    np.array([[[218], [237]], [[211], [230]], [[219], [237]]]),
+    FILE_INFO,
+    {"location": (FILE_INFO["height"] // 2, FILE_INFO["width"] // 2), "size": (2, 1), "level": 4},
+    np.array([[[246], [246]], [[246], [246]], [[244], [244]]]),
 ]
 
 TEST_CASE_2 = [
-    filename,
-    {"location": (53760 // 2, 77824 // 2), "size": (2, 1), "level": 2},
-    np.array([[[229], [226]], [[218], [221]], [[232], [228]]]),
+    FILE_INFO,
+    {"location": (FILE_INFO["height"] // 2, FILE_INFO["width"] // 2), "size": (2, 1), "level": 2},
+    np.array([[[246], [246]], [[246], [246]], [[246], [246]]]),
 ]
 
 TEST_CASE_3 = [
-    filename,
-    {"location": (53760 // 2, 77824 // 2), "size": (8, 8), "level": 2, "grid_shape": (2, 1), "patch_size": 2},
+    FILE_INFO,
+    {
+        "location": (FILE_INFO["height"] // 2, FILE_INFO["width"] // 2),
+        "size": (8, 8),
+        "level": 2,
+        "grid_shape": (2, 1),
+        "patch_size": 2,
+    },
     np.array(
         [
-            [[[227, 228], [227, 228]], [[226, 228], [226, 228]], [[231, 228], [231, 230]]],
-            [[[224, 224], [224, 226]], [[227, 228], [227, 227]], [[232, 231], [232, 231]]],
+            [[[246, 246], [246, 246]], [[246, 246], [246, 246]], [[246, 246], [246, 246]]],
+            [[[246, 246], [246, 246]], [[246, 246], [246, 246]], [[246, 246], [246, 246]]],
         ]
     ),
 ]
 
 TEST_CASE_4 = [
-    filename,
-    {"location": (53760 // 2, 77824 // 2), "size": (8, 8), "level": 2, "grid_shape": (2, 1), "patch_size": 1},
-    np.array([[[[228]], [[228]], [[230]]], [[[226]], [[227]], [[231]]]]),
+    FILE_INFO,
+    {
+        "location": (FILE_INFO["height"] // 2, FILE_INFO["width"] // 2),
+        "size": (8, 8),
+        "level": 2,
+        "grid_shape": (2, 1),
+        "patch_size": 1,
+    },
+    np.array([[[[246]], [[246]], [[246]]], [[[246]], [[246]], [[246]]]]),
 ]
 
 
 class TestOpenSlideReader(unittest.TestCase):
     @parameterized.expand([TEST_CASE_0])
-    @skip_if_quick
-    def test_read_whole_image(self, filename, expected_shape):
-        self.camelyon_data_download(filename)
+    @skipUnless(has_osl, "Requires OpenSlide")
+    def test_read_whole_image(self, file_info, expected_shape):
+        filename = self.camelyon_data_download(file_info)
         reader = WSIReader("OpenSlide")
         img_obj = reader.read(filename)
         img = reader.get_data(img_obj)
-
         self.assertTupleEqual(img.shape, expected_shape)
 
     @parameterized.expand([TEST_CASE_1, TEST_CASE_2])
-    @skip_if_quick
-    def test_read_region(self, filename, patch_info, expected_img):
-        self.camelyon_data_download(filename)
+    @skipUnless(has_osl, "Requires OpenSlide")
+    def test_read_region(self, file_info, patch_info, expected_img):
+        filename = self.camelyon_data_download(file_info)
         reader = WSIReader("OpenSlide")
         img_obj = reader.read(filename)
         img = reader.get_data(img_obj, **patch_info)
@@ -65,24 +93,26 @@ class TestOpenSlideReader(unittest.TestCase):
         self.assertIsNone(assert_array_equal(img, expected_img))
 
     @parameterized.expand([TEST_CASE_3, TEST_CASE_4])
-    @skip_if_quick
-    def test_read_patches(self, filename, patch_info, expected_img):
-        self.camelyon_data_download(filename)
+    @skipUnless(has_osl, "Requires OpenSlide")
+    def test_read_patches(self, file_info, patch_info, expected_img):
+        filename = self.camelyon_data_download(file_info)
         reader = WSIReader("OpenSlide")
         img_obj = reader.read(filename)
         img = reader.get_data(img_obj, **patch_info)
         self.assertTupleEqual(img.shape, expected_img.shape)
         self.assertIsNone(assert_array_equal(img, expected_img))
 
-    def camelyon_data_download(self, filename):
+    def camelyon_data_download(self, file_info):
+        from time import perf_counter
+
+        filename = file_info["name"]
         if not os.path.exists(filename):
             print(f"Test image [{filename}] does not exist. Downloading...")
-            path = "gigadb/pub/10.5524/100001_101000/100439/CAMELYON16/testing/images/"
-            ftp = ftplib.FTP("parrot.genomics.cn")
-            ftp.login("anonymous", "")
-            ftp.cwd(path)
-            ftp.retrbinary("RETR " + filename, open(filename, "wb").write)
-            ftp.quit()
+            t0 = perf_counter()
+            request.urlretrieve(file_info["url"], filename)
+            t1 = perf_counter()
+            print(f"Elapsed time: {t1 - t0}s")
+        return filename
 
 
 if __name__ == "__main__":
