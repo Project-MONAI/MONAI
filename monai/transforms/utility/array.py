eefee474bd3b1ddc188958c4c06bbae358305a6a
Nic Ma <nma@nvidia.com>
2021-03-02 18:36:14 +0800

    [DLMED] fix doc-build issue
    Signed-off-by: Nic Ma <nma@nvidia.com>

--- monai/transforms/utility/array.py
+++ monai/transforms/utility/array.py
@@ -154,7 +154,7 @@ class AutoAdjustChannel(Transform):
     """
     Automatically adjust the channel dimension of input data.
     It extract the `original_channel_dim` info from provided meta_data dictionary.
-     Typical values of `original_channel_dim` can be: "no_channel", 0, -1.
+    Typical values of `original_channel_dim` can be: "no_channel", 0, -1.
     Convert the data to channel_first based on the `original_channel_dim` information.
 
     """
