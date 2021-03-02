20e17b2fcec0a878a51c5b8e947413a699c4da61
Richard Brown <33289025+rijobro@users.noreply.github.com>
2021-03-02 12:05:41 +0000

    allow dictionary image only and test endianness (#1669)
    fix big endianness problem for dictionary reading. Also allow dictionary image_only

---
+++ tests/test_nifti_endianness.py
@@ -0,0 +1,48 @@
+import tempfile
+import unittest
+from typing import TYPE_CHECKING, List, Tuple
+from unittest.case import skipUnless
+
+import numpy as np
+from parameterized import parameterized
+
+from monai.data import DataLoader, Dataset, create_test_image_2d
+from monai.transforms import LoadImage, LoadImaged
+from monai.utils.module import optional_import
+
+if TYPE_CHECKING:
+    import nibabel as nib
+
+    has_nib = True
+else:
+    nib, has_nib = optional_import("nibabel")
+
+TESTS: List[Tuple] = []
+for endianness in ["<", ">"]:
+    for use_array in [True, False]:
+        for image_only in [True, False]:
+            TESTS.append((endianness, use_array, image_only))
+
+
+class TestNiftiEndianness(unittest.TestCase):
+    def setUp(self):
+        self.im, _ = create_test_image_2d(100, 100)
+        self.fname = tempfile.NamedTemporaryFile(suffix=".nii.gz").name
+
+    @parameterized.expand(TESTS)
+    @skipUnless(has_nib, "Requires NiBabel")
+    def test_endianness(self, endianness, use_array, image_only):
+
+        hdr = nib.Nifti1Header(endianness=endianness)
+        nii = nib.Nifti1Image(self.im, np.eye(4), header=hdr)
+        nib.save(nii, self.fname)
+
+        data = [self.fname] if use_array else [{"image": self.fname}]
+        tr = LoadImage(image_only=image_only) if use_array else LoadImaged("image", image_only=image_only)
+        check_ds = Dataset(data, tr)
+        check_loader = DataLoader(check_ds, batch_size=1)
+        _ = next(iter(check_loader))
+
+
+if __name__ == "__main__":
+    unittest.main()
