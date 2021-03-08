0d6ce12859581f32501043a1d5e826347a61bf6f
Wenqi Li <wenqil@nvidia.com>
2021-03-08 10:12:44 +0000

    1693 update for pytorch 1.8 (#1700)
    * update for torch 1.8.0
    
    Signed-off-by: Wenqi Li <wenqil@nvidia.com>
    
    * mute codecov upload error
    
    Signed-off-by: Wenqi Li <wenqil@nvidia.com>
    Signed-off-by: Eric Kerfoot <eric.kerfoot@kcl.ac.uk>

--- tests/utils.py
+++ tests/utils.py
@@ -569,13 +569,13 @@ def query_memory(n=2):
     """
     Find best n idle devices and return a string of device ids.
     """
-    bash_string = "nvidia-smi --query-gpu=utilization.gpu,temperature.gpu,memory.used --format=csv,noheader,nounits"
+    bash_string = "nvidia-smi --query-gpu=utilization.gpu,power.draw,memory.used --format=csv,noheader,nounits"
 
     try:
         p1 = Popen(bash_string.split(), stdout=PIPE)
         output, error = p1.communicate()
         free_memory = [x.split(",") for x in output.decode("utf-8").split("\n")[:-1]]
-        free_memory = np.asarray(free_memory, dtype=np.float).T
+        free_memory = np.asarray(free_memory, dtype=float).T
         ids = np.lexsort(free_memory)[:n]
     except (FileNotFoundError, TypeError, IndexError):
         ids = range(n) if isinstance(n, int) else []
