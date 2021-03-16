7808b0df08f86ffdd702bf3107f80b70f6a2b5b8
Nic Ma <nma@nvidia.com>
2021-03-16 23:13:29 +0800

    [DLMED] fix flake8 issue
    Signed-off-by: Nic Ma <nma@nvidia.com>

--- monai/handlers/checkpoint_loader.py
+++ monai/handlers/checkpoint_loader.py
@@ -84,10 +84,12 @@ class CheckpointLoader:
         prior_max_epochs = engine.state.max_epochs
         Checkpoint.load_objects(to_load=self.load_dict, checkpoint=checkpoint)
         if engine.state.epoch > prior_max_epochs:
-            raise ValueError(f"Epoch count ({engine.state.epoch}) in checkpoint is larger than \
+            raise ValueError(
+                f"Epoch count ({engine.state.epoch}) in checkpoint is larger than \
                 the `engine.state.max_epochs` ({prior_max_epochs}) of engine. To further train from checkpoint, \
                 construct trainer with `max_epochs` larger than checkpoint's epoch count. \
-                To use checkpoint for inference, no need to load state_dict for the engine.")
+                To use checkpoint for inference, no need to load state_dict for the engine."
+            )
         engine.state.max_epochs = prior_max_epochs
 
         self.logger.info(f"Restored all variables from {self.load_path}")
