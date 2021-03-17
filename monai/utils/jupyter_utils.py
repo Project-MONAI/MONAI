32fb015c46fe8ed78dbaa32d35b32e2503f1f5a4
Eric Kerfoot <eric.kerfoot@kcl.ac.uk>
2021-03-17 20:25:17 +0000

    Jupyter utilities update
    Signed-off-by: Eric Kerfoot <eric.kerfoot@kcl.ac.uk>

--- monai/utils/jupyter_utils.py
+++ monai/utils/jupyter_utils.py
@@ -39,11 +39,16 @@ import torch
 
 try:
     import matplotlib.pyplot as plt
-    from ignite.engine import Engine, Events
 
     has_matplotlib = True
 except ImportError:
     has_matplotlib = False
+    
+try:
+    from ignite.engine import Engine, Events
+    has_ignite = True
+except ImportError:
+    has_ignite = False
 
 LOSS_NAME = "loss"
 
