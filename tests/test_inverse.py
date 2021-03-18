4e2b03b28c212b0e358692df029d5a4f77670ce0
Richard Brown <33289025+rijobro@users.noreply.github.com>
2021-03-18 15:31:38 +0000

    wyli changes
    Signed-off-by: Richard Brown <33289025+rijobro@users.noreply.github.com>

--- tests/test_inverse.py
+++ tests/test_inverse.py
@@ -514,8 +514,8 @@ class TestInverse(unittest.TestCase):
 
         # Check that error is thrown when inverse are used out of order.
         t = SpatialPadd("image", [10, 5])
-        # on windows we only check the name, so in this case we need to use a different transform
-        if sys.platform == "win32" and isinstance(transforms[-1], SpatialPadd):
+        # if multiprocessing uses 'spawn', only name is checked, so we need to use a different transform
+        if torch.multiprocessing.get_start_method(allow_none=True) == "spawn" and isinstance(transforms[-1], SpatialPadd):
             t = ResizeWithPadOrCropd("image", [10, 5])
         with self.assertRaises(RuntimeError):
             t.inverse(forwards[-1])
