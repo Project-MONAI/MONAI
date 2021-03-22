db2f47de9df9418f57ee3573fab32b69c1f42d31
Richard Brown <33289025+rijobro@users.noreply.github.com>
2021-03-22 21:42:42 +0000

    [1823] allow None for switch endianness (#1824)
    * allow None for switch endianness
    
    Signed-off-by: Richard Brown <33289025+rijobro@users.noreply.github.com>
    
    * add PIL testing
    
    Signed-off-by: Richard Brown <33289025+rijobro@users.noreply.github.com>
    
    * [MONAI] python code formatting
    
    Signed-off-by: monai-bot <monai.miccai2019@gmail.com>
    
    Co-authored-by: monai-bot <monai.miccai2019@gmail.com>

--- tests/test_nifti_endianness.py
+++ tests/test_nifti_endianness.py
@@ -1,3 +1,4 @@
+import os
 import tempfile
 import unittest
 from typing import TYPE_CHECKING, List, Tuple
@@ -7,16 +8,20 @@ import numpy as np
 from parameterized import parameterized
 
 from monai.data import DataLoader, Dataset, create_test_image_2d
+from monai.data.image_reader import PILReader
 from monai.transforms import LoadImage, LoadImaged
 from monai.transforms.io.array import switch_endianness
 from monai.utils.module import optional_import
 
 if TYPE_CHECKING:
     import nibabel as nib
+    from PIL import Image as PILImage
 
     has_nib = True
+    has_pil = True
 else:
     nib, has_nib = optional_import("nibabel")
+    PILImage, has_pil = optional_import("PIL.Image")
 
 TESTS: List[Tuple] = []
 for endianness in ["<", ">"]:
@@ -48,6 +53,16 @@ class TestNiftiEndianness(unittest.TestCase):
         for data in (np.zeros((2, 1)), ("test",), [24, 42], {"foo": "bar"}, True, 42):
             output = switch_endianness(data, ">", "<")
             self.assertEqual(type(data), type(output))
+
+    @skipUnless(has_pil, "Requires PIL")
+    def test_pil(self):
+        tempdir = tempfile.mkdtemp()
+        test_image = np.random.randint(0, 256, size=[128, 256])
+        filename = os.path.join(tempdir, "test_image.png")
+        PILImage.fromarray(test_image.astype("uint8")).save(filename)
+
+        loader = LoadImage(PILReader(converter=lambda image: image.convert("LA")))
+        _ = loader(filename)
 
 
 if __name__ == "__main__":
