d81ef163e2a19c7e7e16af44e3fa689c48c7e40b
Behrooz <3968947+behxyz@users.noreply.github.com>
2021-03-24 01:21:59 +0000

    Update docstrings and make minor changes
    Signed-off-by: Behrooz <3968947+behxyz@users.noreply.github.com>

--- monai/apps/pathology/datasets.py
+++ monai/apps/pathology/datasets.py
@@ -12,6 +12,8 @@
 import sys
 from typing import Callable, List, Optional, Sequence, Tuple, Union
 
+import numpy as np
+
 from monai.data import Dataset, SmartCacheDataset
 from monai.data.image_reader import WSIReader
 
@@ -21,16 +23,33 @@ __all__ = ["PatchWSIDataset", "SmartCachePatchWSIDataset"]
 class PatchWSIDataset(Dataset):
     """
     This dataset read whole slide images, extract regions, and crate patches.
-    It reads labels for each patch and privide each patch with its associated class labels.
+    It also reads labels for each patch and privide each patch with its associated class labels.
 
     Args:
-        data: The input image directory and the label file
-        [{"image": "path/to/image1", "label": [0,0,0,1,0,1,0,0,1]}, "location": [200, 500]]
+        data: the list of input samples including image, location, and label (see below for more details)
         region_size: the region to be extracted from the whole slide image
         grid_shape: the grid shape on which the patches should be extracted
         patch_size: the patches extracted from the region on the grid
-        image_reader_name: (cuCIM is default)
-        transform:
+        image_reader_name: the name of library to be used for loading whole slide imaging,
+            either CuCIM or OpenSlide (the default is CuCIM)
+        transform: transforms to be executed on input data.
+
+    Note:
+        The input data has the following form as an example:
+        [{"image": "path/to/image1.tiff", "location": [200, 500], "label": [0,0,0,1]}].
+
+        This means from "image1.tiff" extract a region at "location" with the side of "region_size", and
+        then extract patches with the size of "patch_size" from a square grid with the shape of "grid_shape".
+        Be aware the the "grid_shape" should construct a grid with the same number of element as "labels", so
+        for this example the "grid_size" should be (2, 2).
+
+        The output will look like the following:
+        [
+            {"image": np.array([...], dtype=np.uint8), "label": 0},
+            {"image": np.array([...], dtype=np.uint8), "label": 0},
+            {"image": np.array([...], dtype=np.uint8), "label": 0},
+            {"image": np.array([...], dtype=np.uint8), "label": 1},
+        ]
 
     """
 
@@ -39,7 +58,7 @@ class PatchWSIDataset(Dataset):
         data: List,
         region_size: Union[int, Tuple[int, int]],
         grid_shape: Union[int, Tuple[int, int]],
-        patch_size: Union[int, Tuple[int, int]],
+        patch_size: int,
         image_reader_name: str = "cuCIM",
         transform: Union[Sequence[Callable], Callable] = None,
     ):
@@ -58,13 +77,14 @@ class PatchWSIDataset(Dataset):
 
         self.transform = transform
         self.samples = data
-        self.num_samples = len(self.samples)
         self.image_path_list = list({x["image"] for x in self.samples})
 
         self.image_reader_name = image_reader_name
         self.image_reader = WSIReader(image_reader_name)
         self.wsi_object_dict = None
-        self._fetch_wsi_objects()
+        if self.image_reader_name != "openslide":
+            # OpenSlide causes memeory issue if we prefetch image objects
+            self._fetch_wsi_objects()
 
     def _fetch_wsi_objects(self):
         self.wsi_object_dict = {}
@@ -72,26 +92,26 @@ class PatchWSIDataset(Dataset):
             self.wsi_object_dict[image_path] = self.image_reader.read(image_path)
 
     def __len__(self):
-        return self.num_samples
+        return len(self.samples)
 
     def __getitem__(self, index):
-        data = self.samples[index]
-        # OpenSlide causes issue if using the stored image objects
+        sample = self.samples[index]
         if self.image_reader_name == "openslide":
-            img_obj = self.image_reader.read(data["image"])
+            img_obj = self.image_reader.read(sample["image"])
         else:
-            img_obj = self.wsi_object_dict[data["image"]]
+            img_obj = self.wsi_object_dict[sample["image"]]
         images, _ = self.image_reader.get_data(
             img=img_obj,
-            location=data["location"],
+            location=sample["location"],
             size=self.region_size,
             grid_shape=self.grid_shape,
             patch_size=self.patch_size,
         )
-        samples = [{"image": images[i], "label": data["label"][i]} for i in range(len(data["label"]))]
+        labels = np.array(sample["label"], dtype=np.float32)[:, np.newaxis, np.newaxis]
+        patches = [{"image": images[i], "label": labels[i]} for i in range(len(sample["label"]))]
         if self.transform:
-            samples = self.transform(samples)
-        return samples
+            patches = self.transform(patches)
+        return patches
 
 
 class SmartCachePatchWSIDataset(SmartCacheDataset):
@@ -104,7 +124,7 @@ class SmartCachePatchWSIDataset(SmartCacheDataset):
         data: List,
         region_size: Union[int, Tuple[int, int]],
         grid_shape: Union[int, Tuple[int, int]],
-        patch_size: Union[int, Tuple[int, int]],
+        patch_size: int,
         image_reader_name: str = "cuCIM",
         transform: Union[Sequence[Callable], Callable] = None,
         replace_rate: float = 0.5,
