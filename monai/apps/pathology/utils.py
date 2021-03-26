4306cde5bfca69be0186a6a4952ac4cd1f0ec9f4
Yiheng Wang <vennw@nvidia.com>
2021-03-26 15:44:49 +0800

    Update pathology nms
    Signed-off-by: Yiheng Wang <vennw@nvidia.com>

--- monai/apps/pathology/utils.py
+++ monai/apps/pathology/utils.py
@@ -30,35 +30,14 @@ class PathologyProbNMS(ProbNMS):
     ):
         """
         probs_map: the input probabilities map, it must have shape (H[, W, ...]).
-                resolution_level: the level at which the probabilities map is made.
+        resolution_level: the level at which the probabilities map is made.
         """
-        if self.sigma != 0:
-            if not isinstance(probs_map, torch.Tensor):
-                probs_map = torch.as_tensor(probs_map, dtype=torch.float)
-            self.filter.to(probs_map)
-            probs_map = self.filter(probs_map)
-        else:
-            if not isinstance(probs_map, torch.Tensor):
-                probs_map = probs_map.copy()
-
-        if isinstance(probs_map, torch.Tensor):
-            probs_map = probs_map.detach().cpu().numpy()
-
-        probs_map_shape = probs_map.shape
         resolution = pow(2, resolution_level)
-
+        org_outputs = ProbNMS.__call__(self, probs_map)
         outputs = []
-        while np.max(probs_map) > self.prob_threshold:
-            max_idx = np.unravel_index(probs_map.argmax(), probs_map_shape)
-            prob_max = probs_map[max_idx]
-            max_idx_arr = np.asarray(max_idx)
-            coord_wsi = ((max_idx_arr + 0.5) * resolution).astype(int)
-            outputs.append([prob_max] + list(coord_wsi))
-
-            idx_min_range = (max_idx_arr - self.box_lower_bd).clip(0, None)
-            idx_max_range = (max_idx_arr + self.box_upper_bd).clip(None, probs_map_shape)
-            # for each dimension, set values during index ranges to 0
-            slices = tuple([slice(idx_min_range[i], idx_max_range[i]) for i in range(self.spatial_dims)])
-            probs_map[slices] = 0
-
+        for org_output in org_outputs:
+            prob = org_output[0]
+            coord = np.asarray(org_output[1:])
+            coord_wsi = ((coord + 0.5) * resolution).astype(int)
+            outputs.append([prob] + list(coord_wsi))
         return outputs
