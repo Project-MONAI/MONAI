57d86efd242295c7c32ebfd8123dde68395b241d
Behrooz <3968947+behxyz@users.noreply.github.com>
2021-03-30 16:11:14 -0400

    Remove non-relevant files
    Signed-off-by: Behrooz <3968947+behxyz@users.noreply.github.com>

--- monai/apps/pathology/__init__.py
+++ monai/apps/pathology/__init__.py
@@ -9,6 +9,5 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from .datasets import MaskedInferenceWSIDataset, PatchWSIDataset, SmartCacheDataset
-from .evaluators import EvaluateTumorFROC
+from .datasets import PatchWSIDataset, SmartCacheDataset
 from .utils import ProbNMS
