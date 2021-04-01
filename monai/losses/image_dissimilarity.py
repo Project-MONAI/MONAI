cbc69212cc0acb3d0b0cab460bcca544cbdf6a75
kate-sann5100 <yiwen.li@st-annes.ox.ac.uk>
2021-04-01 13:22:47 +0100

    clip variance to be >= 0
    Signed-off-by: kate-sann5100 <yiwen.li@st-annes.ox.ac.uk>

--- monai/losses/image_dissimilarity.py
+++ monai/losses/image_dissimilarity.py
@@ -65,8 +65,8 @@ class LocalNormalizedCrossCorrelationLoss(_Loss):
         kernel_size: int = 3,
         kernel_type: str = "rectangular",
         reduction: Union[LossReduction, str] = LossReduction.MEAN,
-        smooth_nr: float = 1e-7,
-        smooth_dr: float = 1e-7,
+        smooth_nr: float = 1e-5,
+        smooth_dr: float = 1e-5,
     ) -> None:
         """
         Args:
@@ -146,6 +146,8 @@ class LocalNormalizedCrossCorrelationLoss(_Loss):
         cross = tp_sum - p_avg * t_sum
         t_var = t2_sum - t_avg * t_sum  # std[t] ** 2
         p_var = p2_sum - p_avg * p_sum  # std[p] ** 2
+        t_var = torch.maximum(t_var, torch.zeros_like(t_var))
+        p_var = torch.maximum(p_var, torch.zeros_like(p_var))
         ncc: torch.Tensor = (cross * cross + self.smooth_nr) / (t_var * p_var + self.smooth_dr)
         # shape = (batch, 1, D, H, W)
 
