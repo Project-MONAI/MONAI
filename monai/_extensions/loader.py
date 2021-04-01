4b027be7cb7fb5a9dacdc2d903ed5ade85eee9eb
chaliebudd <charles.budd@kcl.ac.uk>
2021-04-01 15:57:21 +0100

    adding docstring
    Signed-off-by: chaliebudd <charles.budd@kcl.ac.uk>

--- monai/_extensions/loader.py
+++ monai/_extensions/loader.py
@@ -27,7 +27,9 @@ def load_module(module_name, defines=None, verbose_build=False):
     Handles the loading of c++ extention modules.
 
     Args:
-    module_name: name of the mdule to load. Must match the name of the relevant source directory in the _extensions directory
+    module_name: Name of the module to load. Must match the name of the relevant source directory in the _extensions directory.
+    defines: Dictionary containing names and values of compilation defines
+    verbose_build: Set to true to enable build logging.
     """
 
     # Ensuring named module exists in _extensions directory.
