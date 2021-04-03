d5afef37054d97484eb18d5afeb9d0cc159176f9
Wenqi Li <wenqil@nvidia.com>
2021-04-03 14:22:48 +0100

    update docstrings (#1931)
    Signed-off-by: Wenqi Li <wenqil@nvidia.com>
    Signed-off-by: Neha Srivathsa <nsrivathsa@nvidia.com>

--- monai/networks/blocks/regunet_block.py
+++ monai/networks/blocks/regunet_block.py
@@ -8,6 +8,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
 from typing import List, Optional, Sequence, Tuple, Type, Union
 
 import torch
