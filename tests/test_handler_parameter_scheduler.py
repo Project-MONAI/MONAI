4b9ab9b2eddfd929721eb69c0ed52bfe90e217ab
Petru-Daniel Tudosiu <petru.daniel@tudosiu.com>
2021-04-06 17:31:48 +0100

    Fixes
    Signed-off-by: Petru-Daniel Tudosiu <petru.daniel@tudosiu.com>

--- tests/test_handler_parameter_scheduler.py
+++ tests/test_handler_parameter_scheduler.py
@@ -48,7 +48,7 @@ class TestHandlerParameterScheduler(unittest.TestCase):
             event=Events.EPOCH_COMPLETED,
         ).attach(engine)
         engine.run([0] * 8, max_epochs=2)
-        torch.testing.assert_allclose(net.get_value(), 3.33, atol=0.001)
+        torch.testing.assert_allclose(net.get_value(), 3.33, atol=0.001, rtol=0.0)
 
         # Testing max_value
         net = ToyNet(value=-1)
