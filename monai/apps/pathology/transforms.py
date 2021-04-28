05ec7868c4a58c9394824efe1c47c1eda6549034
Neha Srivathsa <nsrivathsa@nvidia.com>
2021-04-28 14:34:32 -0700

    refactored classes
    Signed-off-by: Neha Srivathsa <nsrivathsa@nvidia.com>

--- monai/apps/pathology/transforms.py
+++ monai/apps/pathology/transforms.py
@@ -95,7 +95,8 @@ class ExtractStainsMacenko(Transform):
 class NormalizeStainsMacenko(Transform):
     """Class to normalize patches/images to a reference or target image stain, using the Macenko method.
 
-    Performs stain deconvolution of the source image to obtain the stain concentration matrix
+    Performs stain deconvolution of the source image using the ExtractStainsMacenko
+    class, to obtain the stain matrix and calculate the stain concentration matrix
     for the image. Then, performs the inverse Beer-Lambert transform to recreate the
     patch using the target H&E stain matrix provided. If no target stain provided, a default
     reference stain is used. Similarly, if no maximum stain concentrations are provided, a
@@ -131,57 +132,6 @@ class NormalizeStainsMacenko(Transform):
         if self.max_cref is None:
             self.max_cref = cp.array([1.9705, 1.0308])
 
-    def _deconvolution_extract_conc(self, img: cp.ndarray) -> cp.ndarray:
-        """Perform Stain Deconvolution using the Macenko Method, and return stain concentration.
-
-        Args:
-            img: uint8 RGB image to perform stain deconvolution of
-
-        Return:
-            conc_norm: stain concentration matrix for the input image
-        """
-        # reshape image
-        img = img.reshape((-1, 3))
-
-        # calculate absorbance
-        absorbance = -cp.log(cp.clip(img.astype(cp.float32) + 1, a_max=self.tli) / self.tli)
-
-        # remove transparent pixels
-        absorbance_hat = absorbance[cp.all(absorbance > self.beta, axis=1)]
-        if len(absorbance_hat) == 0:
-            raise ValueError("All pixels of the input image are below the absorbance threshold.")
-
-        # compute eigenvectors
-        _, eigvecs = cp.linalg.eigh(cp.cov(absorbance_hat.T).astype(cp.float32))
-
-        # project on the plane spanned by the eigenvectors corresponding to the two largest eigenvalues
-        t_hat = absorbance_hat.dot(eigvecs[:, 1:3])
-
-        # find the min and max vectors and project back to absorbance space
-        phi = cp.arctan2(t_hat[:, 1], t_hat[:, 0])
-        min_phi = cp.percentile(phi, self.alpha)
-        max_phi = cp.percentile(phi, 100 - self.alpha)
-        v_min = eigvecs[:, 1:3].dot(cp.array([(cp.cos(min_phi), cp.sin(min_phi))], dtype=cp.float32).T)
-        v_max = eigvecs[:, 1:3].dot(cp.array([(cp.cos(max_phi), cp.sin(max_phi))], dtype=cp.float32).T)
-
-        # a heuristic to make the vector corresponding to hematoxylin first and the one corresponding to eosin second
-        if v_min[0] > v_max[0]:
-            he = cp.array((v_min[:, 0], v_max[:, 0]), dtype=cp.float32).T
-        else:
-            he = cp.array((v_max[:, 0], v_min[:, 0]), dtype=cp.float32).T
-
-        # rows correspond to channels (RGB), columns to absorbance values
-        y = cp.reshape(absorbance, (-1, 3)).T
-
-        # determine concentrations of the individual stains
-        conc = cp.linalg.lstsq(he, y, rcond=None)[0]
-
-        # normalize stain concentrations
-        max_conc = cp.array([cp.percentile(conc[0, :], 99), cp.percentile(conc[1, :], 99)], dtype=cp.float32)
-        tmp = cp.divide(max_conc, self.max_cref, dtype=cp.float32)
-        conc_norm = cp.divide(conc, tmp[:, cp.newaxis], dtype=cp.float32)
-        return conc_norm
-
     def __call__(self, image: cp.ndarray) -> cp.ndarray:
         """Perform stain normalization.
 
@@ -194,8 +144,26 @@ class NormalizeStainsMacenko(Transform):
         if not isinstance(image, cp.ndarray):
             raise TypeError("Image must be of type cupy.ndarray.")
 
+        # extract stain of the image
+        stain_extractor = ExtractStainsMacenko(tli=self.tli, alpha=self.alpha, beta=self.beta, max_cref=self.max_cref)
+        he = stain_extractor(image)
+
         h, w, _ = image.shape
-        image_c = self._deconvolution_extract_conc(image)
+
+        # reshape image and calculate absorbance
+        image = image.reshape((-1, 3))
+        absorbance = -cp.log(cp.clip(image.astype(cp.float32) + 1, a_max=self.tli) / self.tli)
+
+        # rows correspond to channels (RGB), columns to absorbance values
+        y = cp.reshape(absorbance, (-1, 3)).T
+
+        # determine concentrations of the individual stains
+        conc = cp.linalg.lstsq(he, y, rcond=None)[0]
+
+        # normalize stain concentrations
+        max_conc = cp.array([cp.percentile(conc[0, :], 99), cp.percentile(conc[1, :], 99)], dtype=cp.float32)
+        tmp = cp.divide(max_conc, self.max_cref, dtype=cp.float32)
+        image_c = cp.divide(conc, tmp[:, cp.newaxis], dtype=cp.float32)
 
         image_norm = cp.multiply(self.tli, cp.exp(-self.target_he.dot(image_c)), dtype=cp.float32)
         image_norm[image_norm > 255] = 254
