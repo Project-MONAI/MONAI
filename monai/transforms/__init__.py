0edfc196aa310bae3e72639c221ea9602f03af76
Richard Brown <33289025+rijobro@users.noreply.github.com>
2021-05-05 11:04:11 +0100

    Transposed (#2144)
    * Transposed
    
    Signed-off-by: Richard Brown <33289025+rijobro@users.noreply.github.com>
    
    * convert numpy array to list
    
    Signed-off-by: Richard Brown <33289025+rijobro@users.noreply.github.com>
    Signed-off-by: Yaniel Cabrera <yaniel.cabrera@gmail.com>

--- monai/transforms/__init__.py
+++ monai/transforms/__init__.py
@@ -381,6 +381,9 @@ from .utility.dictionary import (
     ToTensord,
     ToTensorD,
     ToTensorDict,
+    Transposed,
+    TransposeD,
+    TransposeDict,
 )
 from .utils import (
     allow_missing_keys_mode,
