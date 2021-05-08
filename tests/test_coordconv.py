8f46ad56027e8bbc8d9163b5a428028aeeb77e69
Mark Graham <markgraham539@gmail.com>
2021-05-08 14:00:03 +0100

    Add tests for CoordConv
    Signed-off-by: Mark Graham <markgraham539@gmail.com>

---
+++ tests/test_coordconv.py
@@ -0,0 +1,47 @@
+# Copyright 2020 - 2021 MONAI Consortium
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#     http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import unittest
+
+import numpy as np
+from parameterized import parameterized
+
+from monai.transforms import CoordConv
+
+TEST_CASE_1 = [{"spatial_channels": (1, 2, 3)}, np.random.randint(0, 2, size=(1, 3, 3, 3)), (4, 3, 3, 3)]
+
+TEST_CASE_2 = [{"spatial_channels": (1,)}, np.random.randint(0, 2, size=(1, 3, 3, 3)), (2, 3, 3, 3)]
+
+TEST_CASE_ERROR_3 = [{"spatial_channels": (3,)}, np.random.randint(0, 2, size=(1, 3, 3))]
+
+TEST_CASE_ERROR_4 = [{"spatial_channels": (0, 1, 2)}, np.random.randint(0, 2, size=(1, 3, 3))]
+
+
+class TestCoordConv(unittest.TestCase):
+    @parameterized.expand([TEST_CASE_1, TEST_CASE_2])
+    def test_shape(self, input_param, input, expected_shape):
+        result = CoordConv(**input_param)(input)
+        print(result.shape, expected_shape)
+        self.assertEqual(list(result.shape), list(expected_shape))
+
+    @parameterized.expand([TEST_CASE_ERROR_3])
+    def test_max_channel(self, input_param, input):
+        with self.assertRaises(ValueError):
+            CoordConv(**input_param)(input)
+
+    @parameterized.expand([TEST_CASE_ERROR_4])
+    def test_channel_dim(self, input_param, input):
+        with self.assertRaises(ValueError):
+            CoordConv(**input_param)(input)
+
+
+if __name__ == "__main__":
+    unittest.main()
