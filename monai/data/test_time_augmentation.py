1726a64128b1197c200ab65a649ed956ab79a775
Richard Brown <33289025+rijobro@users.noreply.github.com>
2021-05-12 15:09:39 +0100

    remove unused import Optional
    Signed-off-by: Richard Brown <33289025+rijobro@users.noreply.github.com>

--- monai/data/test_time_augmentation.py
+++ monai/data/test_time_augmentation.py
@@ -9,7 +9,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from typing import TYPE_CHECKING, Any, Callable, Dict, List, Optional, Tuple, Union
+from typing import TYPE_CHECKING, Any, Callable, Dict, List, Tuple, Union
 
 import numpy as np
 import torch
