82fc4c1b543c2469ef4464a9a60b9a5cd89c67fc
Nic Ma <nma@nvidia.com>
2021-06-17 17:19:27 +0800

    Fix and enhance wrong doc-strings of all `batch_transform` and `output_transform` (#2384)
    * [DLMED] enhance all doc-string of handlers
    
    Signed-off-by: Nic Ma <nma@nvidia.com>
    
    * [DLMED] update according to comments
    
    Signed-off-by: Nic Ma <nma@nvidia.com>

--- monai/handlers/confusion_matrix.py
+++ monai/handlers/confusion_matrix.py
@@ -40,7 +40,11 @@ class ConfusionMatrix(IgniteMetric):
                 ``"informedness"``, ``"markedness"``]
                 Some of the metrics have multiple aliases (as shown in the wikipedia page aforementioned),
                 and you can also input those names instead.
-            output_transform: transform the ignite.engine.state.output into [y_pred, y] pair.
+            output_transform: callable to extract `y_pred` and `y` from `ignite.engine.state.output` then
+                construct `(y_pred, y)` pair, where `y_pred` and `y` can be `batch-first` Tensors or
+                lists of `channel-first` Tensors. the form of `(y_pred, y)` is required by the `update()`.
+                for example: if `ignite.engine.state.output` is `{"pred": xxx, "label": xxx, "other": xxx}`,
+                output_transform can be `lambda x: (x["pred"], x["label"])`.
             save_details: whether to save metric computation details per image, for example: TP/TN/FP/FN of every image.
                 default to True, will save to `engine.state.metric_details` dict with the metric name as key.
 
