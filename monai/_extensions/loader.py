1c790880b6e01ad153b655e9c73baa2f8ef9234e
chaliebudd <charles.budd@kcl.ac.uk>
2021-06-18 11:32:53 +0100

    changing to pytorch default build directory
    Signed-off-by: chaliebudd <charles.budd@kcl.ac.uk>

--- monai/_extensions/loader.py
+++ monai/_extensions/loader.py
@@ -12,11 +12,10 @@
 from _thread import interrupt_main
 from contextlib import contextmanager
 from glob import glob
-from os import makedirs, path
+from os import path
 from threading import Timer
 
-from torch import cuda
-from torch.utils.cpp_extension import load
+import torch
 
 dir_path = path.dirname(path.realpath(__file__))
 
@@ -49,37 +48,34 @@ def load_module(module_name, defines=None, verbose_build=False, build_timeout=30
     if not path.exists(module_dir):
         raise ValueError(f"No extention module named {module_name}")
 
-    # Naming build.
-    build_tag = "" if defines is None else "_".join(str(v) for v in defines.values())
-    build_name = "build" if build_tag == "" else f"build_{build_tag}"
-    module_name = module_name if build_tag == "" else f"{module_name}_{build_tag}"
-    build_dir = path.join(module_dir, "build", build_name)
-
-    # Ensuring build directory exists.
-    if not path.exists(build_dir):
-        makedirs(build_dir)
+    # Adding configuration to module name.
+    if defines is not None:
+        module_name = "_".join(
+            [
+                module_name,
+            ]
+            + [str(v) for v in defines.values()]
+        )
 
     # Gathering source files.
     source = glob(path.join(module_dir, "**/*.cpp"), recursive=True)
-    if cuda.is_available:
+    if torch.cuda.is_available:
         source += glob(path.join(module_dir, "**/*.cu"), recursive=True)
 
     # Constructing compilation argument list.
     define_args = [] if defines is None else [f"-D {key}={defines[key]}" for key in defines]
 
     # Ninja may be blocked by something out of our control.
-    # This will error if the build takes longer than usual.
+    # This will error if the build takes longer than expected.
     with timeout(
         build_timeout, "Build appears to be blocked. Is there a stopped proccess building the same extention?"
     ):
-
         # This will either run the build or return the existing .so object.
-        module = load(
+        module = torch.utils.cpp_extension.load(
             name=module_name,
             sources=source,
             extra_cflags=define_args,
             extra_cuda_cflags=define_args,
-            build_directory=build_dir,
             verbose=verbose_build,
         )
 
