a2b9bd89e3fd2ba2906779e859c2c750d39d6cf2
Richard Brown <33289025+rijobro@users.noreply.github.com>
2021-07-06 14:35:05 +0100

    fix test_scale_intensity
    Signed-off-by: Richard Brown <33289025+rijobro@users.noreply.github.com>

--- tests/test_scale_intensity.py
+++ tests/test_scale_intensity.py
@@ -12,6 +12,7 @@
 import unittest
 
 import numpy as np
+import torch
 
 from monai.transforms import ScaleIntensity
 from tests.utils import TEST_NDARRAYS, NumpyImageTestCase2D
@@ -25,15 +26,15 @@ class TestScaleIntensity(NumpyImageTestCase2D):
             mina = self.imt.min()
             maxa = self.imt.max()
             norm = (self.imt - mina) / (maxa - mina)
-            expected = (norm * (2.0 - 1.0)) + 1.0
-            np.testing.assert_allclose(result, expected)
+            expected = p((norm * (2.0 - 1.0)) + 1.0)
+            torch.testing.assert_allclose(result, expected, rtol=1e-7, atol=0)
 
     def test_factor_scale(self):
         for p in TEST_NDARRAYS:
             scaler = ScaleIntensity(minv=None, maxv=None, factor=0.1)
             result = scaler(p(self.imt))
-            expected = (self.imt * (1 + 0.1)).astype(np.float32)
-            np.testing.assert_allclose(result, expected)
+            expected = p((self.imt * (1 + 0.1)).astype(np.float32))
+            torch.testing.assert_allclose(result, expected, rtol=1e-7, atol=0)
 
 
 if __name__ == "__main__":
