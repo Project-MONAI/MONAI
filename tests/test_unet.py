990870517a0d5ba4517cf855ebc3098611dd53cc
Nic Ma <nma@nvidia.com>
2021-07-22 14:53:00 +0800

    [DLMED] add ill test case
    Signed-off-by: Nic Ma <nma@nvidia.com>

--- tests/test_unet.py
+++ tests/test_unet.py
@@ -144,6 +144,18 @@ class TestUNET(unittest.TestCase):
         test_data = torch.randn(16, 1, 16, 4)
         test_script_save(net, test_data)
 
+    def test_ill_input_shape(self):
+        net = UNet(
+            dimensions=2,
+            in_channels=1,
+            out_channels=3,
+            channels=(16, 32, 64),
+            strides=(2, 2),
+        )
+        with eval_mode(net):
+            with self.assertRaisesRegex(RuntimeError, r"torch.cat\(\): Sizes of tensors must match"):
+                net.forward(torch.randn(2, 1, 16, 5))
+
 
 if __name__ == "__main__":
     unittest.main()
