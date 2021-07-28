d35ea5dbb01296f8ce73a50413ed89d74af3fa97
Nic Ma <nma@nvidia.com>
2021-07-28 20:10:59 +0800

    [DLMED] complete array transform
    Signed-off-by: Nic Ma <nma@nvidia.com>

--- monai/transforms/spatial/array.py
+++ monai/transforms/spatial/array.py
@@ -14,6 +14,7 @@ https://github.com/Project-MONAI/MONAI/wiki/MONAI_Design
 """
 
 import warnings
+from math import ceil
 from typing import Any, List, Optional, Sequence, Tuple, Union
 
 import numpy as np
@@ -1847,22 +1848,13 @@ class LongestRescale(Transform):
                 See also: https://pytorch.org/docs/stable/nn.functional.html#interpolate
 
         """
-        input_ndim = img.ndim - 1  # spatial ndim
-        output_ndim = len(self.spatial_size)
-        if output_ndim > input_ndim:
-            input_shape = ensure_tuple_size(img.shape, output_ndim + 1, 1)
-            img = img.reshape(input_shape)
-        elif output_ndim < input_ndim:
-            raise ValueError(
-                "len(spatial_size) must be greater or equal to img spatial dimensions, "
-                f"got spatial_size={output_ndim} img={input_ndim}."
-            )
-        spatial_size = fall_back_tuple(self.spatial_size, img.shape[1:])
+        img_size = img.shape[1:]
+        scale = self.spatial_size / max(img_size)
+        new_size = [ceil(s * scale) for s in img_size]
         resized = torch.nn.functional.interpolate(  # type: ignore
             input=torch.as_tensor(np.ascontiguousarray(img), dtype=torch.float).unsqueeze(0),
-            size=spatial_size,
+            size=new_size,
             mode=look_up_option(self.mode if mode is None else mode, InterpolateMode).value,
             align_corners=self.align_corners if align_corners is None else align_corners,
         )
-        resized = resized.squeeze(0).detach().cpu().numpy()
-        return np.asarray(resized)
+        return resized.squeeze(0).detach().cpu().numpy()
