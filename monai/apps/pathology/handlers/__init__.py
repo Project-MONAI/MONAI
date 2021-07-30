86b99cf6cd6064c38c94f516eb52ac552b0efd83
Behrooz <3968947+behxyz@users.noreply.github.com>
2021-07-30 15:06:32 +0000

    Fix a typo
    Signed-off-by: Behrooz <3968947+behxyz@users.noreply.github.com>

--- monai/apps/pathology/handlers/__init__.py
+++ monai/apps/pathology/handlers/__init__.py
@@ -9,5 +9,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from .prob_map_produce import ProbMapProducer
-
+from .prob_map_producer import ProbMapProducer
