06cf6a0b8b962c40c54490d237d12ecb25f02f0f
Richard Brown <33289025+rijobro@users.noreply.github.com>
2021-08-18 13:24:46 +0100

    get number of data conversions
    Signed-off-by: Richard Brown <33289025+rijobro@users.noreply.github.com>

--- monai/transforms/compose.py
+++ monai/transforms/compose.py
@@ -13,9 +13,10 @@ A collection of generic interfaces for MONAI transforms.
 """
 
 import warnings
-from typing import Any, Callable, Mapping, Optional, Sequence, Union
+from typing import Any, Callable, Hashable, Mapping, Optional, Sequence, Union
 
 import numpy as np
+import torch
 
 from monai.transforms.inverse import InvertibleTransform
 
@@ -169,6 +170,39 @@ class Compose(Randomizable, InvertibleTransform):
         for t in reversed(invertible_transforms):
             data = apply_transform(t.inverse, data, self.map_items, self.unpack_items)
         return data
+
+    def get_number_image_type_conversions(self, test_data, key: Optional[Hashable] = None):
+        """
+        Get the number of times that the data need to be converted (e.g., from numpy to torch or vice versa).
+
+        Args:
+            test_data: data to be used to count the number of conversions
+            key: if using dictionary transforms, this key will be used to check the number of conversions.
+        """
+
+        def _get_data(obj, key):
+            return obj if key is None else obj[key]
+
+        # if the starting point is a string (e.g., input to LoadImage), start
+        # at -1 since we don't want to count the string -> image conversion.
+        num_conversions = 0 if not isinstance(_get_data(test_data, key), str) else -1
+
+        tr = self.flatten().transforms
+
+        if isinstance(self, OneOf) or any(isinstance(i, OneOf) for i in tr):
+            raise RuntimeError("Not compatible with `OneOf`, as the applied transform is deterministically chosen.")
+
+        for _transform in tr:
+            prev_data = _get_data(test_data, key)
+            prev_type = type(prev_data)
+            prev_device = prev_data.device if isinstance(prev_data, torch.Tensor) else None
+            test_data = apply_transform(_transform, test_data, self.map_items, self.unpack_items)
+            # every time the type or device changes, increment the counter
+            curr_data = _get_data(test_data, key)
+            curr_device = curr_data.device if isinstance(curr_data, torch.Tensor) else None
+            if not isinstance(curr_data, prev_type) or curr_device != prev_device:
+                num_conversions += 1
+        return num_conversions
 
 
 class OneOf(Compose):
