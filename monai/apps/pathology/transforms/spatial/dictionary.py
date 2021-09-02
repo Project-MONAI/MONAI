d51a6a495ecfc8d0dcc6ddc63dbe8397ddf3a853
Behrooz <3968947+drbeh@users.noreply.github.com>
2021-09-02 15:41:27 +0000

    Remove optional
    Signed-off-by: Behrooz <3968947+drbeh@users.noreply.github.com>

--- monai/apps/pathology/transforms/spatial/dictionary.py
+++ monai/apps/pathology/transforms/spatial/dictionary.py
@@ -9,7 +9,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from typing import Dict, Hashable, Mapping, Optional, Tuple, Union
+from typing import Dict, Hashable, Mapping, Tuple, Union
 
 import torch
 
