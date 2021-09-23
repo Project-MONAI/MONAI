f343fd646b41d37dab96091f1635b9952eb93cd0
Richard Brown <33289025+rijobro@users.noreply.github.com>
2021-09-23 14:00:23 +0100

    fix
    Signed-off-by: Richard Brown <33289025+rijobro@users.noreply.github.com>

--- monai/utils/misc.py
+++ monai/utils/misc.py
@@ -125,7 +125,7 @@ def ensure_tuple_rep(tup: Any, dim: int) -> Tuple[Any, ...]:
 
     """
     if isinstance(tup, torch.Tensor):
-        tup = tup.detach().cpu().numpy()
+        tup = tup.detach().tolist()
     if isinstance(tup, np.ndarray):
         tup = tup.tolist()
     if not issequenceiterable(tup):
