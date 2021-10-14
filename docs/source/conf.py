6e61a5dbbc6e83c2b9e778aa4b4136df1e2fc143
Wenqi Li <wenqil@nvidia.com>
2021-10-14 12:11:25 +0100

    ignore
    Signed-off-by: Wenqi Li <wenqil@nvidia.com>

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,7 +18,7 @@ sys.path.insert(0, os.path.abspath(".."))
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..")))
 print(sys.path)
 
-import monai
+import monai  # noqa: E402
 
 # -- Project information -----------------------------------------------------
 project = "MONAI"
