6c610b73712c1adbb1c5c18bd0c50c5540cfa008
Behrooz <3968947+drbeh@users.noreply.github.com>
2021-10-17 17:18:07 +0000

    Add unittests for CuPy dict stain normalization
    Signed-off-by: Behrooz <3968947+drbeh@users.noreply.github.com>

--- tests/test_pathology_he_stain_dict.py
+++ tests/test_pathology_he_stain_dict.py
@@ -10,11 +10,15 @@
 # limitations under the License.
 
 import unittest
+from unittest.case import skipUnless
 
 import numpy as np
 from parameterized import parameterized
 
 from monai.apps.pathology.transforms import ExtractHEStainsD, NormalizeHEStainsD
+from monai.utils import optional_import
+
+cp, has_cp = optional_import("cupy")
 
 # None inputs
 EXTRACT_STAINS_TEST_CASE_0 = (None,)
@@ -82,6 +86,26 @@ class TestExtractHEStainsD(unittest.TestCase):
             with self.assertRaises(ValueError):
                 ExtractHEStainsD([key])({key: image})
 
+    @parameterized.expand([EXTRACT_STAINS_TEST_CASE_0, EXTRACT_STAINS_TEST_CASE_1])
+    @skipUnless(has_cp, "CuPy is required.")
+    def test_transparent_image_cupy(self, image):
+        """
+        [For CuPy arrays]
+        Test HE stain extraction on an image that comprises
+        only transparent pixels - pixels with absorbance below the
+        beta absorbance threshold. A ValueError should be raised,
+        since once the transparent pixels are removed, there are no
+        remaining pixels to compute eigenvectors.
+        """
+        key = "image"
+        if image is None:
+            with self.assertRaises(TypeError):
+                ExtractHEStainsD([key], cuda=True)({key: image})
+        else:
+            image = cp.array(image)
+            with self.assertRaises(ValueError):
+                ExtractHEStainsD([key], cuda=True)({key: image})
+
     @parameterized.expand([EXTRACT_STAINS_TEST_CASE_0, EXTRACT_STAINS_TEST_CASE_2, EXTRACT_STAINS_TEST_CASE_3])
     def test_identical_result_vectors(self, image):
         """
@@ -99,6 +123,27 @@ class TestExtractHEStainsD(unittest.TestCase):
         else:
             result = ExtractHEStainsD([key])({key: image})
             np.testing.assert_array_equal(result[key][:, 0], result[key][:, 1])
+
+    @parameterized.expand([EXTRACT_STAINS_TEST_CASE_0, EXTRACT_STAINS_TEST_CASE_2, EXTRACT_STAINS_TEST_CASE_3])
+    @skipUnless(has_cp, "CuPy is required.")
+    def test_identical_result_vectors_cupy(self, image):
+        """
+        [For CuPy arrays]
+        Test HE stain extraction on input images that are
+        uniformly filled with pixels that have absorbance above the
+        beta absorbance threshold. Since input image is uniformly filled,
+        the two extracted stains should have the same RGB values. So,
+        we assert that the first column is equal to the second column
+        of the returned stain matrix.
+        """
+        key = "image"
+        if image is None:
+            with self.assertRaises(TypeError):
+                ExtractHEStainsD([key], cuda=True)({key: image})
+        else:
+            image = cp.array(image)
+            result = ExtractHEStainsD([key], cuda=True)({key: image})
+            cp.testing.assert_array_equal(result[key][:, 0], result[key][:, 1])
 
     @parameterized.expand(
         [
@@ -142,6 +187,52 @@ class TestExtractHEStainsD(unittest.TestCase):
             result = ExtractHEStainsD([key])({key: image})
             np.testing.assert_allclose(result[key], expected_data)
 
+    @parameterized.expand(
+        [
+            EXTRACT_STAINS_TEST_CASE_00,
+            EXTRACT_STAINS_TEST_CASE_4,
+            EXTRACT_STAINS_TEST_CASE_5,
+        ]
+    )
+    @skipUnless(has_cp, "CuPy is required.")
+    def test_result_value_cupy(self, image, expected_data):
+        """
+        [For CuPy arrays]
+        Test that an input image returns an expected stain matrix.
+
+        For test case 4:
+        - a uniformly filled input image should result in
+          eigenvectors [[1,0,0],[0,1,0],[0,0,1]]
+        - phi should be an array containing only values of
+          arctan(1) since the ratio between the eigenvectors
+          corresponding to the two largest eigenvalues is 1
+        - maximum phi and minimum phi should thus be arctan(1)
+        - thus, maximum vector and minimum vector should be
+          [[0],[0.70710677],[0.70710677]]
+        - the resulting extracted stain should be
+          [[0,0],[0.70710678,0.70710678],[0.70710678,0.70710678]]
+
+        For test case 5:
+        - the non-uniformly filled input image should result in
+          eigenvectors [[0,0,1],[1,0,0],[0,1,0]]
+        - maximum phi and minimum phi should thus be 0.785 and
+          0.188 respectively
+        - thus, maximum vector and minimum vector should be
+          [[0.18696113],[0],[0.98236734]] and
+          [[0.70710677],[0],[0.70710677]] respectively
+        - the resulting extracted stain should be
+          [[0.70710677,0.18696113],[0,0],[0.70710677,0.98236734]]
+        """
+        key = "image"
+        if image is None:
+            with self.assertRaises(TypeError):
+                ExtractHEStainsD([key], cuda=True)({key: image})
+        else:
+            image = cp.array(image)
+            expected_data = cp.array(expected_data)
+            result = ExtractHEStainsD([key], cuda=True)({key: image})
+            cp.testing.assert_allclose(result[key], expected_data)
+
 
 class TestNormalizeHEStainsD(unittest.TestCase):
     @parameterized.expand([NORMALIZE_STAINS_TEST_CASE_0, NORMALIZE_STAINS_TEST_CASE_1])
@@ -161,6 +252,26 @@ class TestNormalizeHEStainsD(unittest.TestCase):
             with self.assertRaises(ValueError):
                 NormalizeHEStainsD([key])({key: image})
 
+    @parameterized.expand([NORMALIZE_STAINS_TEST_CASE_0, NORMALIZE_STAINS_TEST_CASE_1])
+    @skipUnless(has_cp, "CuPy is required.")
+    def test_transparent_image_cupy(self, image):
+        """
+        [For CuPy arrays]
+        Test HE stain normalization on an image that comprises
+        only transparent pixels - pixels with absorbance below the
+        beta absorbance threshold. A ValueError should be raised,
+        since once the transparent pixels are removed, there are no
+        remaining pixels to compute eigenvectors.
+        """
+        key = "image"
+        if image is None:
+            with self.assertRaises(TypeError):
+                NormalizeHEStainsD([key], cuda=True)({key: image})
+        else:
+            image = cp.array(image)
+            with self.assertRaises(ValueError):
+                NormalizeHEStainsD([key], cuda=True)({key: image})
+
     @parameterized.expand(
         [
             NORMALIZE_STAINS_TEST_CASE_00,
@@ -169,7 +280,7 @@ class TestNormalizeHEStainsD(unittest.TestCase):
             NORMALIZE_STAINS_TEST_CASE_4,
         ]
     )
-    def test_result_value(self, argments, image, expected_data):
+    def test_result_value(self, arguments, image, expected_data):
         """
         Test that an input image returns an expected normalized image.
 
@@ -219,8 +330,73 @@ class TestNormalizeHEStainsD(unittest.TestCase):
             with self.assertRaises(TypeError):
                 NormalizeHEStainsD([key])({key: image})
         else:
-            result = NormalizeHEStainsD([key], **argments)({key: image})
+            result = NormalizeHEStainsD([key], **arguments)({key: image})
             np.testing.assert_allclose(result[key], expected_data)
+
+    @parameterized.expand(
+        [
+            NORMALIZE_STAINS_TEST_CASE_00,
+            NORMALIZE_STAINS_TEST_CASE_2,
+            NORMALIZE_STAINS_TEST_CASE_3,
+            NORMALIZE_STAINS_TEST_CASE_4,
+        ]
+    )
+    @skipUnless(has_cp, "CuPy is required.")
+    def test_result_value_cupy(self, arguments, image, expected_data):
+        """
+        [For CuPy arrays]
+        Test that an input image returns an expected normalized image.
+
+        For test case 2:
+        - This case tests calling the stain normalizer, after the
+          _deconvolution_extract_conc function. This is because the normalized
+          concentration returned for each pixel is the same as the reference
+          maximum stain concentrations in the case that the image is uniformly
+          filled, as in this test case. This is because the maximum concentration
+          for each stain is the same as each pixel's concentration.
+        - Thus, the normalized concentration matrix should be a (2, 6) matrix
+          with the first row having all values of 1.9705, second row all 1.0308.
+        - Taking the matrix product of the target stain matrix and the concentration
+          matrix, then using the inverse Beer-Lambert transform to obtain the RGB
+          image from the absorbance image, and finally converting to uint8,
+          we get that the stain normalized image should be a matrix of
+          dims (3, 2, 3), with all values 11.
+
+        For test case 3:
+        - This case also tests calling the stain normalizer, after the
+          _deconvolution_extract_conc function returns the image concentration
+          matrix.
+        - As in test case 2, the normalized concentration matrix should be a (2, 6) matrix
+          with the first row having all values of 1.9705, second row all 1.0308.
+        - Taking the matrix product of the target default stain matrix and the concentration
+          matrix, then using the inverse Beer-Lambert transform to obtain the RGB
+          image from the absorbance image, and finally converting to uint8,
+          we get that the stain normalized image should be [[[63, 25, 60], [63, 25, 60]],
+          [[63, 25, 60], [63, 25, 60]], [[63, 25, 60], [63, 25, 60]]]
+
+        For test case 4:
+        - For this non-uniformly filled image, the stain extracted should be
+          [[0.70710677,0.18696113],[0,0],[0.70710677,0.98236734]], as validated for the
+          ExtractHEStains class. Solving the linear least squares problem (since
+          absorbance matrix = stain matrix * concentration matrix), we obtain the concentration
+          matrix that should be [[-0.3101, 7.7508, 7.7508, 7.7508, 7.7508, 7.7508],
+          [5.8022, 0, 0, 0, 0, 0]]
+        - Normalizing the concentration matrix, taking the matrix product of the
+          target stain matrix and the concentration matrix, using the inverse
+          Beer-Lambert transform to obtain the RGB image from the absorbance
+          image, and finally converting to uint8, we get that the stain normalized
+          image should be [[[87, 87, 87], [33, 33, 33]], [[33, 33, 33], [33, 33, 33]],
+          [[33, 33, 33], [33, 33, 33]]]
+        """
+        key = "image"
+        if image is None:
+            with self.assertRaises(TypeError):
+                NormalizeHEStainsD([key], cuda=True)({key: image})
+        else:
+            image = cp.array(image)
+            expected_data = cp.array(expected_data)
+            result = NormalizeHEStainsD([key], cuda=True, **arguments)({key: image})
+            cp.testing.assert_allclose(result[key], expected_data)
 
 
 if __name__ == "__main__":
