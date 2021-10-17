6de6e032340995bcb8ee47cba7bcd518474a6470
Behrooz <3968947+drbeh@users.noreply.github.com>
2021-10-17 16:40:41 +0000

    Implement CuPy support for array stain normalization
    Signed-off-by: Behrooz <3968947+drbeh@users.noreply.github.com>

--- monai/apps/pathology/transforms/stain/array.py
+++ monai/apps/pathology/transforms/stain/array.py
@@ -9,11 +9,14 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from typing import Union
+from typing import TYPE_CHECKING, Union
 
 import numpy as np
 
 from monai.transforms.transform import Transform
+from monai.utils import optional_import
+
+cp, has_cp = optional_import("cupy")
 
 
 class ExtractHEStains(Transform):
@@ -41,24 +44,31 @@ class ExtractHEStains(Transform):
         tli: float = 240,
         alpha: float = 1,
         beta: float = 0.15,
-        max_cref: Union[tuple, np.ndarray] = (1.9705, 1.0308),
+        max_cref: tuple = (1.9705, 1.0308),
+        backend: str = "numpy",
     ) -> None:
+        # set backend to numpy or cupy
+        self.backend, has_it = optional_import(backend)
+        if not has_it:
+            raise ValueError(f"The requested backend [{backend}] is not installed!")
         self.tli = tli
         self.alpha = alpha
         self.beta = beta
-        self.max_cref = np.array(max_cref)
+        self.max_cref = self.backend.array(max_cref)
 
-    def _deconvolution_extract_stain(self, image: np.ndarray) -> np.ndarray:
+    def _deconvolution_extract_stain(self, image):
         """Perform Stain Deconvolution and return stain matrix for the image.
 
         Args:
-            img: uint8 RGB image to perform stain deconvolution on
+            img: numpy.ndarray or cupy.ndarray
+                uint8 RGB image to perform stain deconvolution on
 
         Return:
-            he: H&E absorbance matrix for the image (first column is H, second column is E, rows are RGB values)
+            he: numpy.ndarray or cupy.ndarray
+                H&E absorbance matrix for the image (first column is H, second column is E, rows are RGB values)
         """
-        # check image type and vlues
-        if not isinstance(image, np.ndarray):
+        # check image type and values
+        if not isinstance(image, self.backend.ndarray):
             raise TypeError("Image must be of type numpy.ndarray.")
         if image.min() < 0:
             raise ValueError("Image should not have negative values.")
@@ -67,45 +77,52 @@ class ExtractHEStains(Transform):
 
         # reshape image and calculate absorbance
         image = image.reshape((-1, 3))
-        image = image.astype(np.float32) + 1.0
-        absorbance = -np.log(image.clip(max=self.tli) / self.tli)
+        image = image.astype(self.backend.float32) + 1.0
+        absorbance = -self.backend.log(self.backend.clip(image, a_min=None, a_max=self.tli) / self.tli)
 
         # remove transparent pixels
-        absorbance_hat = absorbance[np.all(absorbance > self.beta, axis=1)]
+        absorbance_hat = absorbance[self.backend.all(absorbance > self.beta, axis=1)]
         if len(absorbance_hat) == 0:
             raise ValueError("All pixels of the input image are below the absorbance threshold.")
 
         # compute eigenvectors
-        _, eigvecs = np.linalg.eigh(np.cov(absorbance_hat.T).astype(np.float32))
+        _, eigvecs = self.backend.linalg.eigh(self.backend.cov(absorbance_hat.T).astype(self.backend.float32))
 
         # project on the plane spanned by the eigenvectors corresponding to the two largest eigenvalues
         t_hat = absorbance_hat.dot(eigvecs[:, 1:3])
 
         # find the min and max vectors and project back to absorbance space
-        phi = np.arctan2(t_hat[:, 1], t_hat[:, 0])
-        min_phi = np.percentile(phi, self.alpha)
-        max_phi = np.percentile(phi, 100 - self.alpha)
-        v_min = eigvecs[:, 1:3].dot(np.array([(np.cos(min_phi), np.sin(min_phi))], dtype=np.float32).T)
-        v_max = eigvecs[:, 1:3].dot(np.array([(np.cos(max_phi), np.sin(max_phi))], dtype=np.float32).T)
+        phi = self.backend.arctan2(t_hat[:, 1], t_hat[:, 0])
+        min_phi = self.backend.percentile(phi, self.alpha)
+        max_phi = self.backend.percentile(phi, 100 - self.alpha)
+        v_min = eigvecs[:, 1:3].dot(
+            self.backend.array([(self.backend.cos(min_phi), self.backend.sin(min_phi))], dtype=self.backend.float32).T
+        )
+        v_max = eigvecs[:, 1:3].dot(
+            self.backend.array([(self.backend.cos(max_phi), self.backend.sin(max_phi))], dtype=self.backend.float32).T
+        )
 
         # a heuristic to make the vector corresponding to hematoxylin first and the one corresponding to eosin second
         if v_min[0] > v_max[0]:
-            he = np.array((v_min[:, 0], v_max[:, 0]), dtype=np.float32).T
+            he = self.backend.array((v_min[:, 0], v_max[:, 0]), dtype=self.backend.float32).T
         else:
-            he = np.array((v_max[:, 0], v_min[:, 0]), dtype=np.float32).T
+            he = self.backend.array((v_max[:, 0], v_min[:, 0]), dtype=self.backend.float32).T
 
         return he
 
-    def __call__(self, image: np.ndarray) -> np.ndarray:
+    def __call__(self, image):
         """Perform stain extraction.
 
         Args:
-            image: uint8 RGB image to extract stain from
+            image: numpy.ndarray or cupy.ndarray
+                uint8 RGB image to extract stain from numpy.ndarray or cupy.ndarray
 
         return:
-            target_he: H&E absorbance matrix for the image (first column is H, second column is E, rows are RGB values)
+            target_he: numpy.ndarray or cupy.ndarray
+                H&E absorbance matrix for the image (first column is H, second column is E, rows are RGB values)
+
         """
-        if not isinstance(image, np.ndarray):
+        if not isinstance(image, self.backend.ndarray):
             raise TypeError("Image must be of type numpy.ndarray.")
 
         target_he = self._deconvolution_extract_stain(image)
@@ -146,24 +163,37 @@ class NormalizeHEStains(Transform):
         alpha: float = 1,
         beta: float = 0.15,
         target_he: Union[tuple, np.ndarray] = ((0.5626, 0.2159), (0.7201, 0.8012), (0.4062, 0.5581)),
-        max_cref: Union[tuple, np.ndarray] = (1.9705, 1.0308),
+        max_cref: tuple = (1.9705, 1.0308),
+        backend: str = "numpy",
     ) -> None:
+        # set backend to numpy or cupy
+        self.backend, has_it = optional_import(backend)
+        if not has_it:
+            raise ValueError(f"The requested backend [{backend}] is not installed!")
         self.tli = tli
-        self.target_he = np.array(target_he)
-        self.max_cref = np.array(max_cref)
-        self.stain_extractor = ExtractHEStains(tli=self.tli, alpha=alpha, beta=beta, max_cref=self.max_cref)
+        self.target_he = self.backend.array(target_he)
+        self.max_cref = self.backend.array(max_cref)
+        self.stain_extractor = ExtractHEStains(
+            tli=self.tli,
+            alpha=alpha,
+            beta=beta,
+            max_cref=self.max_cref,
+            backend=backend,
+        )
 
-    def __call__(self, image: np.ndarray) -> np.ndarray:
+    def __call__(self, image):
         """Perform stain normalization.
 
         Args:
-            image: uint8 RGB image/patch to be stain normalized, pixel values between 0 and 255
+            image: numpy.ndarray or cupy.ndarray
+                uint8 RGB image/patch to be stain normalized, pixel values between 0 and 255
 
         Return:
-            image_norm: stain normalized image/patch
+            image_norm: numpy.ndarray or cupy.ndarray
+                stain normalized image/patch
         """
-        # check image type and vlues
-        if not isinstance(image, np.ndarray):
+        # check image type and values
+        if not isinstance(image, self.backend.ndarray):
             raise TypeError("Image must be of type numpy.ndarray.")
         if image.min() < 0:
             raise ValueError("Image should not have negative values.")
@@ -176,21 +206,26 @@ class NormalizeHEStains(Transform):
         # reshape image and calculate absorbance
         h, w, _ = image.shape
         image = image.reshape((-1, 3))
-        image = image.astype(np.float32) + 1.0
-        absorbance = -np.log(image.clip(max=self.tli) / self.tli)
+        image = image.astype(self.backend.float32) + 1.0
+        absorbance = -self.backend.log(self.backend.clip(image, a_min=None, a_max=self.tli) / self.tli)
 
         # rows correspond to channels (RGB), columns to absorbance values
-        y = np.reshape(absorbance, (-1, 3)).T
+        y = self.backend.reshape(absorbance, (-1, 3)).T
 
         # determine concentrations of the individual stains
-        conc = np.linalg.lstsq(he, y, rcond=None)[0]
+        conc = self.backend.linalg.lstsq(he, y, rcond=None)[0]
 
         # normalize stain concentrations
-        max_conc = np.array([np.percentile(conc[0, :], 99), np.percentile(conc[1, :], 99)], dtype=np.float32)
-        tmp = np.divide(max_conc, self.max_cref, dtype=np.float32)
-        image_c = np.divide(conc, tmp[:, np.newaxis], dtype=np.float32)
+        max_conc = self.backend.array(
+            [self.backend.percentile(conc[0, :], 99), self.backend.percentile(conc[1, :], 99)],
+            dtype=self.backend.float32,
+        )
+        tmp = self.backend.divide(max_conc, self.max_cref, dtype=self.backend.float32)
+        image_c = self.backend.divide(conc, tmp[:, self.backend.newaxis], dtype=self.backend.float32)
 
-        image_norm: np.ndarray = np.multiply(self.tli, np.exp(-self.target_he.dot(image_c)), dtype=np.float32)
+        image_norm: self.backend.ndarray = self.backend.multiply(
+            self.tli, self.backend.exp(-self.target_he.dot(image_c)), dtype=self.backend.float32
+        )
         image_norm[image_norm > 255] = 254
-        image_norm = np.reshape(image_norm.T, (h, w, 3)).astype(np.uint8)
+        image_norm = self.backend.reshape(image_norm.T, (h, w, 3)).astype(self.backend.uint8)
         return image_norm
