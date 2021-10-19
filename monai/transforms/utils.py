d1eb0defb85fd0940b2e3aaa6d88e840e245d388
Richard Brown <33289025+rijobro@users.noreply.github.com>
2021-10-19 14:21:16 +0100

    fix
    Signed-off-by: Richard Brown <33289025+rijobro@users.noreply.github.com>

--- monai/transforms/utils.py
+++ monai/transforms/utils.py
@@ -913,8 +913,8 @@ def generate_spatial_bounding_box(
         min_d = max(arg_max[0] - margin[di], 0)
         max_d = arg_max[-1] + margin[di] + 1
 
-        box_start[di] = min_d.detach().cpu().item() if isinstance(max_d, torch.Tensor) else min_d
-        box_end[di] = max_d.detach().cpu().item() if isinstance(max_d, torch.Tensor) else max_d
+        box_start[di] = min_d.detach().cpu().item() if isinstance(min_d, torch.Tensor) else min_d  # type: ignore
+        box_end[di] = max_d.detach().cpu().item() if isinstance(max_d, torch.Tensor) else max_d  # type: ignore
 
     return box_start, box_end
 
