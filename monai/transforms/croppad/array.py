f74c2888fe950f7d7423dadac8d0d4e355990788
Nic Ma <nma@nvidia.com>
2021-10-22 16:40:00 +0800

    [DLMED] fix flake8
    Signed-off-by: Nic Ma <nma@nvidia.com>

--- monai/transforms/croppad/array.py
+++ monai/transforms/croppad/array.py
@@ -398,8 +398,8 @@ class SpatialCrop(Transform):
                 )
                 roi_size, *_ = convert_to_dst_type(src=roi_size, dst=roi_center, wrap_sequence=True)
                 roi_start_torch = maximum(
-                    roi_center - floor_divide(roi_size, 2), torch.zeros_like(roi_center)
-                )  # type: ignore
+                    roi_center - floor_divide(roi_size, 2), torch.zeros_like(roi_center)  # type: ignore
+                )
                 roi_end_torch = maximum(roi_start_torch + roi_size, roi_start_torch)
             else:
                 if roi_start is None or roi_end is None:
