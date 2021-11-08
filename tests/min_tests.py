6bbdc1ee3057771fb6802cee8911341bb52cd9c9
Nic Ma <nma@nvidia.com>
2021-11-09 07:15:49 +0800

    [DLMED] fix min tests
    Signed-off-by: Nic Ma <nma@nvidia.com>

--- tests/min_tests.py
+++ tests/min_tests.py
@@ -148,6 +148,7 @@ def run_testsuit():
         "test_prepare_batch_extra_input",
         "test_prepare_batch_default",
         "test_check_missing_files",
+        "test_cumulative_average_dist",
     ]
     assert sorted(exclude_cases) == sorted(set(exclude_cases)), f"Duplicated items in {exclude_cases}"
 
