b3fdfbf7891f51e5a90559d9adafe949353609cb
Vishwesh <vishwesh.nath@gmail.com>
2021-11-11 17:54:42 -0600

    Contrastive Loss added, First Draft Done (#3286)
    * Contrastive Loss added, first draft
    
    Signed-off-by: vnath <vnath@nvidia.com>
    
    * Almost all requirements covered
    
    Signed-off-by: vnath <vnath@nvidia.com>
    
    * Review Ready
    
    Signed-off-by: vnath <vnath@nvidia.com>
    
    * CL Loss needs negative mask to be on the device, fix commited
    
    Signed-off-by: vnath <vnath@nvidia.com>
    
    * Cuda test added to CL loss test file
    
    Signed-off-by: vnath <vnath@nvidia.com>
    
    * Fix for tests failing on PT16 due to device mismatch
    
    Signed-off-by: vnath <vnath@nvidia.com>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    Co-authored-by: vnath <vnath@nvidia.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- monai/losses/__init__.py
+++ monai/losses/__init__.py
@@ -9,6 +9,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from .contrastive import ContrastiveLoss
 from .deform import BendingEnergyLoss
 from .dice import (
     Dice,
