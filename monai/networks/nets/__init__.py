0ae6d07e27020703769d8020d58cbfec2b7d61b1
dongy <dongy@nvidia.com>
2021-11-16 08:30:20 -0800

    update scripts
    Signed-off-by: dongy <dongy@nvidia.com>

--- monai/networks/nets/__init__.py
+++ monai/networks/nets/__init__.py
@@ -29,6 +29,7 @@ from .densenet import (
     densenet201,
     densenet264,
 )
+from .dints import DiNTS
 from .dynunet import DynUNet, DynUnet, Dynunet
 from .efficientnet import (
     BlockArgs,
