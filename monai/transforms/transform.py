12f74d342ca82bb8de4bdcc00edd620fbcfe344a
Nic Ma <nma@nvidia.com>
2021-11-29 21:06:37 +0800

    3402 Add support to set other pickle related args (#3412)
    * [DLMED] add support for other pickle
    
    Signed-off-by: Nic Ma <nma@nvidia.com>
    
    * [DLMED] fix flake8
    
    Signed-off-by: Nic Ma <nma@nvidia.com>
    
    * [MONAI] python code formatting
    
    Signed-off-by: monai-bot <monai.miccai2019@gmail.com>
    
    * [DLMED] fix flake8
    
    Signed-off-by: Nic Ma <nma@nvidia.com>
    
    * [DLMED] fix flake8
    
    Signed-off-by: Nic Ma <nma@nvidia.com>
    
    * [DLMED] test flake8
    
    Signed-off-by: Nic Ma <nma@nvidia.com>
    
    * [DLMED] test flake8
    
    Signed-off-by: Nic Ma <nma@nvidia.com>
    
    * [DLMED] fix flake8
    
    Signed-off-by: Nic Ma <nma@nvidia.com>
    
    * [DLMED] update according to comments
    
    Signed-off-by: Nic Ma <nma@nvidia.com>
    
    Co-authored-by: monai-bot <monai.miccai2019@gmail.com>

--- monai/transforms/transform.py
+++ monai/transforms/transform.py
@@ -203,10 +203,10 @@ class Transform(ABC):
         :py:class:`monai.transforms.Compose`
     """
 
+    # Transforms should add data types to this list if they are capable of performing a transform without
+    # modifying the input type. For example, ["torch.Tensor", "np.ndarray"] means that no copies of the data
+    # are required if the input is either `torch.Tensor` or `np.ndarray`.
     backend: List[TransformBackends] = []
-    """Transforms should add data types to this list if they are capable of performing a transform without
-    modifying the input type. For example, [\"torch.Tensor\", \"np.ndarray\"] means that no copies of the data
-    are required if the input is either \"torch.Tensor\" or \"np.ndarray\"."""
 
     @abstractmethod
     def __call__(self, data: Any):
