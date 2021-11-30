071264d733ab0d16e87a4a7bf088c59aa348c890
Wenqi Li <wenqil@nvidia.com>
2021-11-30 01:58:13 +0000

    fixes pickle mod (#3416)
    * fixes pickle mod
    
    Signed-off-by: Wenqi Li <wenqil@nvidia.com>
    
    * update pickle
    
    Signed-off-by: Wenqi Li <wenqil@nvidia.com>
    
    * docstring
    
    Signed-off-by: Wenqi Li <wenqil@nvidia.com>

--- tests/test_persistentdataset.py
+++ tests/test_persistentdataset.py
@@ -61,7 +61,7 @@ class TestDataset(unittest.TestCase):
                 data=items,
                 transform=_InplaceXform(),
                 cache_dir=tempdir,
-                pickle_module=pickle,
+                pickle_module="pickle",
                 pickle_protocol=pickle.HIGHEST_PROTOCOL,
             )
             self.assertEqual(items, [[[]], [[0]], [[0, 1]], [[0, 1, 2]], [[0, 1, 2, 3]]])
