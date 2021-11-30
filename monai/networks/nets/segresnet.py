a58cd2157997b7e898503996bb23e0a373b9d576
Patricio Astudillo <patricio.astudillo@robovision.ai>
2021-11-30 10:09:11 +0100

    signed
    Signed-off-by: Patricio Astudillo <patricio.astudillo@robovision.ai>

--- monai/networks/nets/segresnet.py
+++ monai/networks/nets/segresnet.py
@@ -130,7 +130,6 @@ class SegResNet(nn.Module):
                     *[ResBlock(spatial_dims, sample_in_channels // 2, norm=norm, act=self.act) for _ in range(blocks_up[i])]
                 )
             )
-
             up_samples.append(
                 nn.Sequential(
                     *[
