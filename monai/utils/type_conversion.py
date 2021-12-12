e3d5580e7d7da893f48ba0349852f797f4b69978
Nic Ma <nma@nvidia.com>
2021-12-12 20:36:47 +0800

    [DLMED] support string dtype
    Signed-off-by: Nic Ma <nma@nvidia.com>

--- monai/utils/type_conversion.py
+++ monai/utils/type_conversion.py
@@ -59,7 +59,7 @@ def dtype_torch_to_numpy(dtype):
 def dtype_numpy_to_torch(dtype):
     """Convert a numpy dtype to its torch equivalent."""
     # np dtypes can be given as np.float32 and np.dtype(np.float32) so unify them
-    dtype = np.dtype(dtype) if isinstance(dtype, type) else dtype
+    dtype = np.dtype(dtype) if isinstance(dtype, (type, str)) else dtype
     return look_up_option(dtype, _np_to_torch_dtype)
 
 
