7818227aac501c2b981b2f1eefbfc20552f54780
Eric Kerfoot <eric.kerfoot@kcl.ac.uk>
2021-12-19 23:51:16 +0000

    Updates
    Signed-off-by: Eric Kerfoot <eric.kerfoot@kcl.ac.uk>

--- tests/test_torchscript_utils.py
+++ tests/test_torchscript_utils.py
@@ -102,7 +102,7 @@ class TestTorchscript(unittest.TestCase):
 
             _, _, loaded_extra_files = load_net_with_metadata(f"{tempdir}/test.pt", more_extra_files=("test.txt",))
 
-            if pytorch_after(1, 6):
+            if pytorch_after(1, 7):
                 self.assertEqual(more_extra_files["test.txt"], loaded_extra_files["test.txt"])
             else:
                 self.assertEqual(more_extra_files["test.txt"].decode(), loaded_extra_files["test.txt"])
