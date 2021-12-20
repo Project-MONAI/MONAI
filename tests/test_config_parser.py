2d2c4b4593d378b1b69fb934fda2a62e55814f0e
Nic Ma <nma@nvidia.com>
2021-12-20 23:52:59 +0800

    [DLMED] fix test error
    Signed-off-by: Nic Ma <nma@nvidia.com>

--- tests/test_config_parser.py
+++ tests/test_config_parser.py
@@ -16,43 +16,51 @@ from parameterized import parameterized
 
 from monai.apps import ConfigParser
 from monai.transforms import LoadImaged
+from tests.utils import skip_if_windows
 
-TEST_CASES = [
-    # test MONAI components
-    [
-        dict(pkgs=["torch.nn", "monai"], modules=["transforms"]),
-        {"name": "LoadImaged", "args": {"keys": ["image"]}},
-        LoadImaged,
-    ],
-    # test non-monai modules
-    [
-        dict(pkgs=["torch.optim", "monai"], modules=["adam"]),
-        {"name": "Adam", "args": {"params": torch.nn.PReLU().parameters(), "lr": 1e-4}},
-        torch.optim.Adam,
-    ],
-    # test python `path`
-    [dict(pkgs=[], modules=[]), {"path": "monai.transforms.LoadImaged", "args": {"keys": ["image"]}}, LoadImaged],
-    # test `disabled`
-    [
-        dict(pkgs=["torch.utils", "monai"], modules=["transforms"]),
-        {"name": "LoadImaged", "disabled": True, "args": {"keys": ["image"]}},
-        None,
-    ],
+TEST_CASE_1 = [
+    dict(pkgs=["monai"], modules=["transforms"]),
+    {"name": "LoadImaged", "args": {"keys": ["image"]}},
+    LoadImaged,
+]
+# test python `path`
+TEST_CASE_2 = [
+    dict(pkgs=[], modules=[]),
+    {"path": "monai.transforms.LoadImaged", "args": {"keys": ["image"]}},
+    LoadImaged,
+]
+# test `disabled`
+TEST_CASE_3 = [
+    dict(pkgs=["monai"], modules=["transforms"]),
+    {"name": "LoadImaged", "disabled": True, "args": {"keys": ["image"]}},
+    None,
+]
+# test non-monai modules
+TEST_CASE_4 = [
+    dict(pkgs=["torch.optim", "monai"], modules=["adam"]),
+    {"name": "Adam", "args": {"params": torch.nn.PReLU().parameters(), "lr": 1e-4}},
+    torch.optim.Adam,
 ]
 
 
 class TestConfigParser(unittest.TestCase):
-    @parameterized.expand(TEST_CASES)
+    @parameterized.expand([TEST_CASE_1, TEST_CASE_2, TEST_CASE_3])
     def test_type(self, input_param, test_input, output_type):
         configer = ConfigParser(**input_param)
         result = configer.build_component(test_input)
         if result is not None:
             self.assertTrue(isinstance(result, output_type))
-            if isinstance(result, LoadImaged):
-                self.assertEqual(result.keys[0], "image")
+            self.assertEqual(result.keys[0], "image")
         else:
             # test `disabled` works fine
             self.assertEqual(result, output_type)
+
+    @skip_if_windows
+    @parameterized.expand([TEST_CASE_4])
+    def test_non_monai(self, input_param, test_input, output_type):
+        configer = ConfigParser(**input_param)
+        result = configer.build_component(test_input)
+        self.assertTrue(isinstance(result, output_type))
 
 
 if __name__ == "__main__":
