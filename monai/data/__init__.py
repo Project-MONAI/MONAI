9734478fc6c49e35b464a0448bd3260a7d0f8aaf
Eric Kerfoot <17726042+ericspod@users.noreply.github.com>
2021-12-21 00:05:06 +0000

    Adding Torchscript utility functions (#3138)
    * Adding Torchscript utility functions
    
    Signed-off-by: Eric Kerfoot <eric.kerfoot@kcl.ac.uk>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * [MONAI] python code formatting
    
    Signed-off-by: monai-bot <monai.miccai2019@gmail.com>
    
    * Adding Torchscript utility functions
    
    Signed-off-by: Eric Kerfoot <eric.kerfoot@kcl.ac.uk>
    
    * Added test for extra files
    
    Signed-off-by: Eric Kerfoot <eric.kerfoot@kcl.ac.uk>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Update
    
    Signed-off-by: Eric Kerfoot <eric.kerfoot@kcl.ac.uk>
    
    * Update
    
    Signed-off-by: Eric Kerfoot <eric.kerfoot@kcl.ac.uk>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Update
    
    Signed-off-by: Eric Kerfoot <eric.kerfoot@kcl.ac.uk>
    
    * Update
    
    Signed-off-by: Eric Kerfoot <eric.kerfoot@kcl.ac.uk>
    
    * Updates
    
    Signed-off-by: Eric Kerfoot <eric.kerfoot@kcl.ac.uk>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Updates
    
    Signed-off-by: Eric Kerfoot <eric.kerfoot@kcl.ac.uk>
    
    * Updates
    
    Signed-off-by: Eric Kerfoot <eric.kerfoot@kcl.ac.uk>
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: monai-bot <monai.miccai2019@gmail.com>
    Co-authored-by: Nic Ma <nma@nvidia.com>

--- monai/data/__init__.py
+++ monai/data/__init__.py
@@ -43,6 +43,7 @@ from .samplers import DistributedSampler, DistributedWeightedRandomSampler
 from .synthetic import create_test_image_2d, create_test_image_3d
 from .test_time_augmentation import TestTimeAugmentation
 from .thread_buffer import ThreadBuffer, ThreadDataLoader
+from .torchscript_utils import load_net_with_metadata, save_net_with_metadata
 from .utils import (
     compute_importance_map,
     compute_shape_offset,
