967c643ad9465c4ccabd469529f5fe640681ef51
Nic Ma <nma@nvidia.com>
2022-01-19 10:24:33 +0800

    3672 Add more messages for AUC warning (#3676)
    * [DLMED] change to warning
    
    Signed-off-by: Nic Ma <nma@nvidia.com>
    
    * [DLMED] add more error message
    
    Signed-off-by: Nic Ma <nma@nvidia.com>
    
    * [DLMED] simplify the messages
    
    Signed-off-by: Nic Ma <nma@nvidia.com>

--- monai/metrics/rocauc.py
+++ monai/metrics/rocauc.py
@@ -66,9 +66,14 @@ class ROCAUCMetric(CumulativeIterationMetric):
 def _calculate(y_pred: torch.Tensor, y: torch.Tensor) -> float:
     if not (y.ndimension() == y_pred.ndimension() == 1 and len(y) == len(y_pred)):
         raise AssertionError("y and y_pred must be 1 dimension data with same length.")
-    if not y.unique().equal(torch.tensor([0, 1], dtype=y.dtype, device=y.device)):
-        warnings.warn("y values must be 0 or 1, can not be all 0 or all 1, skip AUC computation and return `Nan`.")
+    y_unique = y.unique()
+    if len(y_unique) == 1:
+        warnings.warn(f"y values can not be all {y_unique.item()}, skip AUC computation and return `Nan`.")
         return float("nan")
+    if not y_unique.equal(torch.tensor([0, 1], dtype=y.dtype, device=y.device)):
+        warnings.warn(f"y values must be 0 or 1, but in {y_unique.tolist()}, skip AUC computation and return `Nan`.")
+        return float("nan")
+
     n = len(y)
     indices = y_pred.argsort()
     y = y[indices].cpu().numpy()
