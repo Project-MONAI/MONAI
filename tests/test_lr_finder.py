db61a08a904aaa9751b1ce4e5501fa0f102fce47
Wenqi Li <wenqil@nvidia.com>
2022-01-28 10:12:39 +0000

    3725 fixes download test (#3728)
    * fixes test
    
    Signed-off-by: Wenqi Li <wenqil@nvidia.com>
    
    * skip when downloading fails
    
    Signed-off-by: Wenqi Li <wenqil@nvidia.com>

--- tests/test_lr_finder.py
+++ tests/test_lr_finder.py
@@ -24,6 +24,7 @@ from monai.networks.nets import DenseNet
 from monai.optimizers import LearningRateFinder
 from monai.transforms import AddChanneld, Compose, LoadImaged, ScaleIntensityd, ToTensord
 from monai.utils import optional_import, set_determinism
+from tests.utils import skip_if_downloading_fails
 
 if TYPE_CHECKING:
     import matplotlib.pyplot as plt
@@ -61,14 +62,15 @@ class TestLRFinder(unittest.TestCase):
 
     def test_lr_finder(self):
         # 0.001 gives 54 examples
-        train_ds = MedNISTDataset(
-            root_dir=self.root_dir,
-            transform=self.transforms,
-            section="validation",
-            val_frac=0.001,
-            download=True,
-            num_workers=10,
-        )
+        with skip_if_downloading_fails():
+            train_ds = MedNISTDataset(
+                root_dir=self.root_dir,
+                transform=self.transforms,
+                section="validation",
+                val_frac=0.001,
+                download=True,
+                num_workers=10,
+            )
         train_loader = DataLoader(train_ds, batch_size=300, shuffle=True, num_workers=10)
         num_classes = train_ds.get_num_classes()
 
