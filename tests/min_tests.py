edbe6ac77436fa5844ff2f3e5695e59fec386a20
Nic Ma <nma@nvidia.com>
2022-01-29 16:55:57 +0800

    [DLMED] fix flake8
    Signed-off-by: Nic Ma <nma@nvidia.com>

--- tests/min_tests.py
+++ tests/min_tests.py
@@ -155,7 +155,7 @@ def run_testsuit():
         "test_zoom_affine",
         "test_zoomd",
         "test_prepare_batch_default_dist",
-        "test_hashcachedataset"
+        "test_hashcachedataset",
     ]
     assert sorted(exclude_cases) == sorted(set(exclude_cases)), f"Duplicated items in {exclude_cases}"
 
