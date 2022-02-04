227bad078d325a97c5942b60bb310ed6146e4242
Nic Ma <nma@nvidia.com>
2022-02-05 07:49:46 +0800

    3765 Enhance `create_multigpu_supervised_XXX` for distributed (#3768)
    * [DLMED] add check for devices
    
    Signed-off-by: Nic Ma <nma@nvidia.com>
    
    * [DLMED] update according to comments
    
    Signed-off-by: Nic Ma <nma@nvidia.com>

--- tests/min_tests.py
+++ tests/min_tests.py
@@ -154,6 +154,7 @@ def run_testsuit():
         "test_zoom_affine",
         "test_zoomd",
         "test_prepare_batch_default_dist",
+        "test_parallel_execution_dist",
     ]
     assert sorted(exclude_cases) == sorted(set(exclude_cases)), f"Duplicated items in {exclude_cases}"
 
