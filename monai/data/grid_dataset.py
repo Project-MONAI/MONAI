769a4c3362961ed40d659d77f8f043187372adf1
Nic Ma <nma@nvidia.com>
2022-04-05 20:13:23 +0800

    [DLMED] restore the doc-string
    Signed-off-by: Nic Ma <nma@nvidia.com>

--- monai/data/grid_dataset.py
+++ monai/data/grid_dataset.py
@@ -29,23 +29,6 @@ class PatchIter:
     Return a patch generator with predefined properties such as `patch_size`.
     Typically used with :py:class:`monai.data.GridPatchDataset`.
 
-    Note:
-        The `patch_size` is the size of the patch to sample from the input arrays.
-        It is assumed the arrays first dimension is the channel dimension which will be yielded in its entirety
-        so this should not be specified in `patch_size`. For example, for an input 3D array with 1 channel of size
-        (1, 20, 20, 20) a regular grid sampling of eight patches (1, 10, 10, 10) would be specified by
-        a `patch_size` of (10, 10, 10).
-
-    Args:
-        patch_size: size of patches to generate slices for, 0/None selects whole dimension
-        start_pos: starting position in the array, default is 0 for each dimension
-        mode: {``"constant"``, ``"edge"``, ``"linear_ramp"``, ``"maximum"``, ``"mean"``,
-            ``"median"``, ``"minimum"``, ``"reflect"``, ``"symmetric"``, ``"wrap"``, ``"empty"``}
-            One of the listed string values or a user supplied function. Defaults to ``"wrap"``.
-            See also: https://numpy.org/doc/1.18/reference/generated/numpy.pad.html
-        pad_opts: other arguments for the `np.pad` function.
-            note that `np.pad` treats channel dimension as the first dimension.
-
     """
 
     def __init__(
@@ -55,6 +38,25 @@ class PatchIter:
         mode: Union[NumpyPadMode, str] = NumpyPadMode.WRAP,
         **pad_opts: Dict,
     ):
+        """
+
+        Args:
+            patch_size: size of patches to generate slices for, 0/None selects whole dimension
+            start_pos: starting position in the array, default is 0 for each dimension
+            mode: {``"constant"``, ``"edge"``, ``"linear_ramp"``, ``"maximum"``, ``"mean"``,
+                ``"median"``, ``"minimum"``, ``"reflect"``, ``"symmetric"``, ``"wrap"``, ``"empty"``}
+                One of the listed string values or a user supplied function. Defaults to ``"wrap"``.
+                See also: https://numpy.org/doc/1.18/reference/generated/numpy.pad.html
+            pad_opts: other arguments for the `np.pad` function.
+                note that `np.pad` treats channel dimension as the first dimension.
+
+        Note:
+            The `patch_size` is the size of the
+            patch to sample from the input arrays. It is assumed the arrays first dimension is the channel dimension which
+            will be yielded in its entirety so this should not be specified in `patch_size`. For example, for an input 3D
+            array with 1 channel of size (1, 20, 20, 20) a regular grid sampling of eight patches (1, 10, 10, 10) would be
+            specified by a `patch_size` of (10, 10, 10).
+        """
         self.patch_size = (None,) + tuple(patch_size)  # expand to have the channel dim
         self.start_pos = ensure_tuple(start_pos)
         self.mode: NumpyPadMode = look_up_option(mode, NumpyPadMode)
