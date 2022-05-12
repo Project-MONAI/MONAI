90c4bd0c52fe64a1cd1e8576b8dba2474bb74187
Wenqi Li <wenqil@nvidia.com>
2022-05-12 06:36:29 -0400

    fixes
    Signed-off-by: Wenqi Li <wenqil@nvidia.com>

--- tests/test_rotated.py
+++ tests/test_rotated.py
@@ -66,8 +66,8 @@ class TestRotated2D(NumpyImageTestCase2D):
             self.segn[0, 0], -np.rad2deg(angle), (0, 1), not keep_size, order=0, mode=_mode, prefilter=False
         )
         expected = np.stack(expected).astype(int)
-        if isinstance(rotated["seg"][0], MetaTensor):
-            rotated["seg"][0] = rotated["seg"][0].as_tensor()  # pytorch 1.7 compatible
+        if isinstance(rotated["seg"], MetaTensor):
+            rotated["seg"] = rotated["seg"].as_tensor()  # pytorch 1.7 compatible
         self.assertLessEqual(np.count_nonzero(expected != rotated["seg"][0]), 30)
 
 
@@ -99,8 +99,8 @@ class TestRotated3D(NumpyImageTestCase3D):
             self.segn[0, 0], np.rad2deg(angle), (0, 2), not keep_size, order=0, mode=_mode, prefilter=False
         )
         expected = np.stack(expected).astype(int)
-        if isinstance(rotated["seg"][0], MetaTensor):
-            rotated["seg"][0] = rotated["seg"][0].as_tensor()  # pytorch 1.7 compatible
+        if isinstance(rotated["seg"], MetaTensor):
+            rotated["seg"] = rotated["seg"].as_tensor()  # pytorch 1.7 compatible
         self.assertLessEqual(np.count_nonzero(expected != rotated["seg"][0]), 160)
 
 
@@ -132,8 +132,8 @@ class TestRotated3DXY(NumpyImageTestCase3D):
             self.segn[0, 0], -np.rad2deg(angle), (0, 1), not keep_size, order=0, mode=_mode, prefilter=False
         )
         expected = np.stack(expected).astype(int)
-        if isinstance(rotated["seg"][0], MetaTensor):
-            rotated["seg"][0] = rotated["seg"][0].as_tensor()  # pytorch 1.7 compatible
+        if isinstance(rotated["seg"], MetaTensor):
+            rotated["seg"] = rotated["seg"].as_tensor()  # pytorch 1.7 compatible
         self.assertLessEqual(np.count_nonzero(expected != rotated["seg"][0]), 160)
 
 
