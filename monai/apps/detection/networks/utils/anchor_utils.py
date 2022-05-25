880dee2cb362c375af673b165a5a4df76db97f2d
Can Zhao <canz@nvidia.com>
2022-05-25 15:48:40 -0400

    license
    Signed-off-by: Can Zhao <canz@nvidia.com>

--- monai/apps/detection/networks/utils/anchor_utils.py
+++ monai/apps/detection/networks/utils/anchor_utils.py
@@ -9,6 +9,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# =========================================================================
+
+# https://github.com/pytorch/vision/blob/main/LICENSE
+
 # BSD 3-Clause License
 
 # Copyright (c) Soumith Chintala 2016,
