5299e959adef175fc9c9a134ee9f3c4735d8d6cc
Wenqi Li <wenqil@nvidia.com>
2022-05-27 13:53:00 +0100

    merge apps/detection/inferers into monai.inferers
    Signed-off-by: Wenqi Li <wenqil@nvidia.com>

--- tests/test_sliding_window_inference.py
+++ tests/test_sliding_window_inference.py
@@ -227,6 +227,7 @@ class TestSlidingWindowInference(unittest.TestCase):
             device,
             device,
             has_tqdm,
+            None,
             t1,
             test2=t2,
         )
@@ -237,6 +238,67 @@ class TestSlidingWindowInference(unittest.TestCase):
             roi_shape, sw_batch_size, overlap=0.5, mode="constant", cval=-1, progress=has_tqdm
         )(inputs, compute, t1, test2=t2)
         np.testing.assert_allclose(result.cpu().numpy(), expected, rtol=1e-4)
+
+    def test_multioutput(self):
+        device = "cuda" if torch.cuda.is_available() else "cpu:0"
+        inputs = torch.ones((1, 1, 20, 20)).to(device=device)
+        roi_shape = (8, 8)
+        sw_batch_size = 10
+
+        def compute(data):
+            return data + 1, data[:, :, ::2, ::2] + 2, data[:, :, ::4, ::4] + 3
+
+        def compute_dict(data):
+            return {1: data + 1, 2: data[:, :, ::2, ::2] + 2, 3: data[:, :, ::4, ::4] + 3}
+
+        result = sliding_window_inference(
+            inputs,
+            roi_shape,
+            sw_batch_size,
+            compute,
+            0.5,
+            "constant",
+            1.0,
+            "constant",
+            0.0,
+            device,
+            device,
+            has_tqdm,
+            None,
+        )
+        result_dict = sliding_window_inference(
+            inputs,
+            roi_shape,
+            sw_batch_size,
+            compute_dict,
+            0.5,
+            "constant",
+            1.0,
+            "constant",
+            0.0,
+            device,
+            device,
+            has_tqdm,
+            None,
+        )
+        expected = (np.ones((1, 1, 20, 20)) + 1, np.ones((1, 1, 10, 10)) + 2, np.ones((1, 1, 5, 5)) + 3)
+        expected_dict = {1: np.ones((1, 1, 20, 20)) + 1, 2: np.ones((1, 1, 10, 10)) + 2, 3: np.ones((1, 1, 5, 5)) + 3}
+        for rr, ee in zip(result, expected):
+            np.testing.assert_allclose(rr.cpu().numpy(), ee, rtol=1e-4)
+        for rr, _ in zip(result_dict, expected_dict):
+            np.testing.assert_allclose(result_dict[rr].cpu().numpy(), expected_dict[rr], rtol=1e-4)
+
+        result = SlidingWindowInferer(
+            roi_shape, sw_batch_size, overlap=0.5, mode="constant", cval=-1, progress=has_tqdm
+        )(inputs, compute)
+        for rr, ee in zip(result, expected):
+            np.testing.assert_allclose(rr.cpu().numpy(), ee, rtol=1e-4)
+
+        result_dict = SlidingWindowInferer(
+            roi_shape, sw_batch_size, overlap=0.5, mode="constant", cval=-1, progress=has_tqdm
+        )(inputs, compute_dict)
+        for rr, _ in zip(result_dict, expected_dict):
+            np.testing.assert_allclose(result_dict[rr].cpu().numpy(), expected_dict[rr], rtol=1e-4)
 
 
 if __name__ == "__main__":
