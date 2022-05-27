1e0c1e10256e140b776addd304433f623593853b
Can Zhao <canz@nvidia.com>
2022-05-27 01:07:07 -0400

    update tuple
    Signed-off-by: Can Zhao <canz@nvidia.com>

--- monai/apps/detection/inferers/inferer.py
+++ monai/apps/detection/inferers/inferer.py
@@ -9,7 +9,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from typing import Any, Callable, Dict, Sequence, Union
+from typing import Any, Callable, Dict, Sequence, Union, Tuple
 
 import torch
 
