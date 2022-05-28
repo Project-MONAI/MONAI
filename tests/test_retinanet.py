5be201d11fbb8639ec12c2e68495652efa48fb9b
Can Zhao <canz@nvidia.com>
2022-05-28 16:06:02 -0400

    clean unused par
    Signed-off-by: Can Zhao <canz@nvidia.com>

--- tests/test_retinanet.py
+++ tests/test_retinanet.py
@@ -95,11 +95,6 @@ for case in [TEST_CASE_1, TEST_CASE_2, TEST_CASE_3, TEST_CASE_2_A, TEST_CASE_3_A
     for model in [resnet10, resnet18, resnet34, resnet50, resnet101, resnet152, resnet200]:
         TEST_CASES.append([model, *case])
 
-TEST_SCRIPT_CASES = [
-    [model, *TEST_CASE_1] for model in [resnet10, resnet18, resnet34, resnet50, resnet101, resnet152, resnet200]
-]
-
-
 class TestRetinaNet(unittest.TestCase):
     @parameterized.expand(TEST_CASES)
     def test_retina_shape(self, model, input_param, input_shape):
