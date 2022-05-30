54d830091489c47acdcc87c343de4a31984049b8
Wenqi Li <wenqil@nvidia.com>
2022-05-30 09:09:29 +0100

    fixes unit tests
    - torchvision compatibility
    - torchscript compatibility
    
    Signed-off-by: Wenqi Li <wenqil@nvidia.com>

--- tests/test_anchor_box.py
+++ tests/test_anchor_box.py
@@ -16,7 +16,7 @@ from parameterized import parameterized
 
 from monai.apps.detection.utils.anchor_utils import AnchorGenerator, AnchorGeneratorWithAnchorShape
 from monai.utils import optional_import
-from tests.utils import assert_allclose, test_script_save
+from tests.utils import SkipIfBeforePyTorchVersion, assert_allclose, test_script_save
 
 _, has_torchvision = optional_import("torchvision")
 
@@ -39,6 +39,7 @@ TEST_CASES_SHAPE_3D.append(
 )
 
 
+@SkipIfBeforePyTorchVersion((1, 8))
 @unittest.skipUnless(has_torchvision, "Requires torchvision")
 class TestAnchorGenerator(unittest.TestCase):
     @parameterized.expand(TEST_CASES_2D)
