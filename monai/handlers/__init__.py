8aafa5de7fc56e28bbab792ce90dbba1e9f3c3de
Behrooz <3968947+drbeh@users.noreply.github.com>
2022-05-30 13:22:16 +0000

    Fix sort
    Signed-off-by: Behrooz <3968947+drbeh@users.noreply.github.com>

--- monai/handlers/__init__.py
+++ monai/handlers/__init__.py
@@ -25,8 +25,8 @@ from .metrics_saver import MetricsSaver
 from .mlflow_handler import MLFlowHandler
 from .nvtx_handlers import MarkHandler, RangeHandler, RangePopHandler, RangePushHandler
 from .parameter_scheduler import ParamSchedulerHandler
-from .probability_maps import ProbMapProducer
 from .postprocessing import PostProcessing
+from .probability_maps import ProbMapProducer
 from .regression_metrics import MeanAbsoluteError, MeanSquaredError, PeakSignalToNoiseRatio, RootMeanSquaredError
 from .roc_auc import ROCAUC
 from .smartcache_handler import SmartCacheHandler
