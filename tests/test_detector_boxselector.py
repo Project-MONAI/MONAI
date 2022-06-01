77ed563d371e00303b6e632ea6fe22728aa981df
Can Zhao <canz@nvidia.com>
2022-06-01 17:48:48 -0400

    typo
    Signed-off-by: Can Zhao <canz@nvidia.com>

--- tests/test_detector_boxselector.py
+++ tests/test_detector_boxselector.py
@@ -31,7 +31,7 @@ TEST_CASE.append(
             "detections_per_img": 5,
         },
         [torch.tensor([[1, 2, 3, 2, 3, 4], [5, 6, 7, 8, 9, 10]]).to(torch.float32)],
-        [torch.Tensor([[0.1, 0.6], [0.2, 0.2]])],
+        [torch.tensor([[0.1, 0.6], [0.2, 0.2]])],
         (20, 20, 20),
         torch.tensor([[1, 2, 3, 2, 3, 4], [5, 6, 7, 8, 9, 10]]),
     ]
@@ -46,7 +46,7 @@ TEST_CASE.append(
             "detections_per_img": 5,
         },
         [torch.tensor([[1, 2, 3, 2, 3, 4]]).to(torch.float32), torch.tensor([[5, 6, 7, 8, 9, 10]]).to(torch.float32)],
-        [torch.Tensor([[0.3, 0.6]]), torch.Tensor([[0.2, 0.2]])],
+        [torch.tensor([[0.3, 0.6]]), torch.tensor([[0.2, 0.2]])],
         (20, 20, 8),
         torch.tensor([[1, 2, 3, 2, 3, 4], [5, 6, 7, 8, 9, 8]]),
     ]
