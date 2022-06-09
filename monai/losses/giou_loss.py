29fb491d4c9dfd54f46db49dc922e4971147340d
Behrooz <3968947+drbeh@users.noreply.github.com>
2022-06-09 00:07:16 +0000

    Fix autoformat issues
    Signed-off-by: Behrooz <3968947+drbeh@users.noreply.github.com>

--- monai/losses/giou_loss.py
+++ monai/losses/giou_loss.py
@@ -50,9 +50,9 @@ class BoxGIoULoss(_Loss):
             raise ValueError(f"ground truth has different shape ({target.shape}) from input ({input.shape})")
 
         box_dtype = input.dtype
-        giou: torch.Tensor = box_pair_giou(
+        giou: torch.Tensor = box_pair_giou(  # type: ignore
             target.to(dtype=COMPUTE_DTYPE), input.to(dtype=COMPUTE_DTYPE)
-        )  # type: ignore
+        )
         loss: torch.Tensor = 1.0 - giou
         if self.reduction == LossReduction.MEAN.value:
             loss = loss.mean()
