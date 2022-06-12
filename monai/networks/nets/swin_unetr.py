a23c7f54f899c183052db4b8ecd6a21c190b2c18
Ali Hatamizadeh <ahatamizadeh@nvidia.com>
2022-06-12 02:32:11 -0700

    enhance swin_unetr relative position (#4486)
    * enhance swin_unetr relative position
    
    Signed-off-by: ahatamizadeh <ahatamizadeh@nvidia.com>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- monai/networks/nets/swin_unetr.py
+++ monai/networks/nets/swin_unetr.py
@@ -474,7 +474,7 @@ class WindowAttention(nn.Module):
         q = q * self.scale
         attn = q @ k.transpose(-2, -1)
         relative_position_bias = self.relative_position_bias_table[
-            self.relative_position_index[:n, :n].reshape(-1)
+            self.relative_position_index.clone()[:n, :n].reshape(-1)
         ].reshape(n, n, -1)
         relative_position_bias = relative_position_bias.permute(2, 0, 1).contiguous()
         attn = attn + relative_position_bias.unsqueeze(0)
