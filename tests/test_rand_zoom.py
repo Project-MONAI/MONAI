6c095a80b280dbb64c8c77c54a73dc1490c40bab
Wenqi Li <831580+wyli@users.noreply.github.com>
2022-06-22 06:17:13 +0100

    4351 - adds StrEnum (#4556)
    * adds strenum
    
    Signed-off-by: Wenqi Li <wenqil@nvidia.com>
    
    * fixes tests
    
    Signed-off-by: Wenqi Li <wenqil@nvidia.com>
    
    * fixes tests
    
    Signed-off-by: Wenqi Li <wenqil@nvidia.com>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- tests/test_rand_zoom.py
+++ tests/test_rand_zoom.py
@@ -16,7 +16,7 @@ from parameterized import parameterized
 from scipy.ndimage import zoom as zoom_scipy
 
 from monai.transforms import RandZoom
-from monai.utils import GridSampleMode, InterpolateMode
+from monai.utils import InterpolateMode
 from tests.utils import TEST_NDARRAYS, NumpyImageTestCase2D, assert_allclose
 
 VALID_CASES = [(0.8, 1.2, "nearest", False), (0.8, 1.2, InterpolateMode.NEAREST, False)]
@@ -49,11 +49,7 @@ class TestRandZoom(NumpyImageTestCase2D):
             self.assertTrue(np.array_equal(zoomed.shape, self.imt.shape[1:]))
 
     @parameterized.expand(
-        [
-            ("no_min_zoom", None, 1.1, "bilinear", TypeError),
-            ("invalid_mode", 0.9, 1.1, "s", ValueError),
-            ("invalid_mode", 0.9, 1.1, GridSampleMode.NEAREST, ValueError),
-        ]
+        [("no_min_zoom", None, 1.1, "bilinear", TypeError), ("invalid_mode", 0.9, 1.1, "s", ValueError)]
     )
     def test_invalid_inputs(self, _, min_zoom, max_zoom, mode, raises):
         for p in TEST_NDARRAYS:
