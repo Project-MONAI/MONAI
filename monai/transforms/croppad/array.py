8ddbea91600646f47ec098675c57bc16d99200db
Wenqi Li <wenqil@nvidia.com>
2022-07-05 09:01:59 +0100

    fixes padding bug
    Signed-off-by: Wenqi Li <wenqil@nvidia.com>

--- monai/transforms/croppad/array.py
+++ monai/transforms/croppad/array.py
@@ -190,8 +190,8 @@ class Pad(InvertibleTransform):
         transform = self.pop_transform(data)
         padded = transform[TraceKeys.EXTRA_INFO]["padded"]
         if padded[0][0] > 0 or padded[0][1] > 0:  # slicing the channel dimension
-            s = [padded][0][0]
-            e = min(max([padded][0][1], s + 1), len(data))
+            s = padded[0][0]
+            e = min(max(padded[0][1], s + 1), len(data))
             data = data[s : len(data) - e]  # type: ignore
         roi_start = [i[0] for i in padded[1:]]
         roi_end = [i - j[1] for i, j in zip(data.shape[1:], padded[1:])]
