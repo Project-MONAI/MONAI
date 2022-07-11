c0268266f688c3bacd9c92ef7458f54b6afdf609
Wenqi Li <wenqil@nvidia.com>
2022-07-11 12:05:11 +0100

    adds tests
    Signed-off-by: Wenqi Li <wenqil@nvidia.com>

--- tests/test_pad_collation.py
+++ tests/test_pad_collation.py
@@ -11,6 +11,7 @@
 
 import random
 import unittest
+from functools import wraps
 from typing import List, Tuple
 
 import numpy as np
@@ -20,6 +21,7 @@ from parameterized import parameterized
 from monai.data import CacheDataset, DataLoader
 from monai.data.utils import decollate_batch, pad_list_data_collate
 from monai.transforms import (
+    BatchInverseTransform,
     Compose,
     PadListDataCollate,
     RandRotate,
@@ -34,11 +36,17 @@ from monai.transforms import (
 )
 from monai.utils import set_determinism
 
+
+@wraps(pad_list_data_collate)
+def _testing_collate(x):
+    return pad_list_data_collate(batch=x, method="end", mode="constant")
+
+
 TESTS: List[Tuple] = []
 
 for pad_collate in [
-    lambda x: pad_list_data_collate(batch=x, method="end", mode="constant"),
-    PadListDataCollate(method="end", mode="constant"),
+    _testing_collate,
+    PadListDataCollate(method="end", mode="constant")
 ]:
     TESTS.append((dict, pad_collate, RandSpatialCropd("image", roi_size=[8, 7], random_size=True)))
     TESTS.append((dict, pad_collate, RandRotated("image", prob=1, range_x=np.pi, keep_size=False, dtype=np.float64)))
@@ -57,13 +65,13 @@ class _Dataset(torch.utils.data.Dataset):
     def __init__(self, images, labels, transforms):
         self.images = images
         self.labels = labels
-        self.transforms = transforms
+        self.transform = transforms
 
     def __len__(self):
         return len(self.images)
 
     def __getitem__(self, index):
-        return self.transforms(self.images[index]), self.labels[index]
+        return self.transform(self.images[index]), self.labels[index]
 
 
 class TestPadCollation(unittest.TestCase):
@@ -105,6 +113,12 @@ class TestPadCollation(unittest.TestCase):
                     shapes.append(output["image"].shape)
                     self.assertTrue(len(output["image"].applied_operations), len(dataset.transform.transforms))
                 self.assertTrue(len(set(shapes)) > 1)  # inverted shapes must be different because of random xforms
+
+        if t_type == dict:
+            batch_inverse = BatchInverseTransform(dataset.transform, loader)
+            for data in loader:
+                output = batch_inverse(data)
+                self.assertTrue(output[0]["image"].shape, (1, 10, 9))
 
 
 if __name__ == "__main__":
