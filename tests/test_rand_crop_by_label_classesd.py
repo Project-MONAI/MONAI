ef8d3f502bd3cfc06fe8a4470bf4f677191f9b09
KumoLiu <yunl@nvidia.com>
2022-07-15 19:12:42 +0800

    preserve key order
    Signed-off-by: KumoLiu <yunl@nvidia.com>

--- tests/test_rand_crop_by_label_classesd.py
+++ tests/test_rand_crop_by_label_classesd.py
@@ -126,6 +126,8 @@ class TestRandCropByLabelClassesd(unittest.TestCase):
         result = RandCropByLabelClassesd(**input_param)(input_data)
         self.assertIsInstance(result, expected_type)
         self.assertTupleEqual(result[0]["img"].shape, expected_shape)
+        _len = len(tuple(input_data.keys()))
+        self.assertTupleEqual(tuple(result[0].keys())[:_len], tuple(input_data.keys()))
 
 
 if __name__ == "__main__":
