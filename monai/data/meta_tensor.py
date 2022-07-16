0f4d5a33b741e544c78737abb98a3f579fb3b27d
Wenqi Li <wenqil@nvidia.com>
2022-07-16 18:09:12 +0100

    update indexing methods
    Signed-off-by: Wenqi Li <wenqil@nvidia.com>

--- monai/data/meta_tensor.py
+++ monai/data/meta_tensor.py
@@ -215,20 +215,13 @@ class MetaTensor(MetaObj, torch.Tensor):
                         # if using e.g., `batch[:, -1]` or `batch[..., -1]`, then the
                         # first element will be `slice(None, None, None)` and `Ellipsis`,
                         # respectively. Don't need to do anything with the metadata.
-                        if batch_idx not in (slice(None, None, None), Ellipsis, None):
-                            # only decollate metadata once
-                            if metas is None:
-                                metas = decollate_batch(ret.meta)
-                            meta = metas[batch_idx]
-                            # if using e.g., `batch[0:2]`, then `is_batch` should still be
-                            # `True`. Also re-collate the remaining elements.
-                            if isinstance(meta, list):
-                                ret.meta = list_data_collate(meta)
-                            # if using e.g., `batch[0]` or `batch[0, 1]`, then return single
-                            # element from batch, and set `is_batch` to `False`.
-                            else:
-                                ret.meta = meta
-                                ret.is_batch = False
+                        if batch_idx not in (slice(None, None, None), Ellipsis, None) and idx == 0:
+                            ret_meta = decollate_batch(args[0], detach=False)[batch_idx]
+                            if isinstance(ret_meta, list):  # e.g. batch[0:2], re-collate
+                                ret_meta = list_data_collate(ret_meta)
+                            else:  # e.g. `batch[0]` or `batch[0, 1]`, batch index is an integer
+                                ret_meta.is_batch = False
+                            ret.__dict__ = ret_meta.__dict__.copy()
                     # `unbind` is used for `next(iter(batch))`. Also for `decollate_batch`.
                     # But we only want to split the batch if the `unbind` is along the 0th
                     # dimension.
@@ -241,8 +234,8 @@ class MetaTensor(MetaObj, torch.Tensor):
                             dim = 0
                         if dim == 0:
                             if metas is None:
-                                metas = decollate_batch(ret.meta)
-                            ret.meta = metas[idx]
+                                metas = decollate_batch(args[0], detach=False)
+                            ret.__dict__ = metas[idx].__dict__.copy()
                             ret.is_batch = False
 
             out.append(ret)
