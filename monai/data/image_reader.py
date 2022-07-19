f94cee6b18655da1dd80e6cd2bb09a5d53ebbf1c
Wenqi Li <wenqil@nvidia.com>
2022-07-19 17:02:03 +0100

    update license
    Signed-off-by: Wenqi Li <wenqil@nvidia.com>

--- monai/data/image_reader.py
+++ monai/data/image_reader.py
@@ -688,10 +688,36 @@ class PydicomReader(ImageReader):
     def _get_frame_data(self, img) -> Iterator:
         """
         yield frames and description from the segmentation image.
-        This function refers to Highdicom:
-        https://github.com/herrmannlab/highdicom/blob/master/src/highdicom/seg/utils.py
+        This function is adapted from Highdicom:
+        https://github.com/herrmannlab/highdicom/blob/v0.18.2/src/highdicom/seg/utils.py
 
-        Aegs:
+        which has the following license...
+
+        # =========================================================================
+        # https://github.com/herrmannlab/highdicom/blob/v0.18.2/LICENSE
+        #
+        # Copyright 2020 MGH Computational Pathology
+        # Permission is hereby granted, free of charge, to any person obtaining a
+        # copy of this software and associated documentation files (the
+        # "Software"), to deal in the Software without restriction, including
+        # without limitation the rights to use, copy, modify, merge, publish,
+        # distribute, sublicense, and/or sell copies of the Software, and to
+        # permit persons to whom the Software is furnished to do so, subject to
+        # the following conditions:
+        # The above copyright notice and this permission notice shall be included
+        # in all copies or substantial portions of the Software.
+        # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+        # OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+        # MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+        # IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+        # CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+        # TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+        # SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+        # =========================================================================
+
+        (https://github.com/herrmannlab/highdicom/issues/188)
+
+        Args:
             img: a Pydicom dataset object that has attribute "SegmentSequence".
 
         """
