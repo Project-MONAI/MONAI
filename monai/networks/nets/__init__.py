9a17621a4ad17666ffbf742bb607122a60db2c4f
Wenqi Li <831580+wyli@users.noreply.github.com>
2022-07-25 17:06:58 +0100

    4757 update patch merging (#4758)
    * update patch merging
    
    Signed-off-by: Wenqi Li <wenqil@nvidia.com>
    
    * fixes unit tests
    
    Signed-off-by: Wenqi Li <wenqil@nvidia.com>
    Signed-off-by: Mason Ma <masoner6429@gmail.com>

--- monai/networks/nets/__init__.py
+++ monai/networks/nets/__init__.py
@@ -81,7 +81,7 @@ from .senet import (
     seresnext50,
     seresnext101,
 )
-from .swin_unetr import SwinUNETR
+from .swin_unetr import PatchMerging, PatchMergingV2, SwinUNETR
 from .torchvision_fc import TorchVisionFCModel
 from .transchex import BertAttention, BertMixedLayer, BertOutput, BertPreTrainedModel, MultiModal, Pooler, Transchex
 from .unet import UNet, Unet
