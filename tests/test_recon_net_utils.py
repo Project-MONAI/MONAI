92bcbb56059ffec9a5d877017c0ac573ac1a273c
mersad95zd <m.zalbagi@gmail.com>
2022-07-29 16:00:37 -0700

    minor fix to unittest
    Signed-off-by: mersad95zd <m.zalbagi@gmail.com>

--- tests/test_recon_net_utils.py
+++ tests/test_recon_net_utils.py
@@ -13,28 +13,29 @@ import unittest
 
 import torch
 from parameterized import parameterized
-from tests.utils import assert_allclose
-from monai.apps.reconstruction.networks.nets.utils import (
-    reshape_channel_complex_to_last_dim,
-    complex_normalize,
-    reshape_complex_to_channel_dim,
-    pad,
-    inverse_complex_normalize,
-)
 
+from monai.apps.reconstruction.networks.nets.utils import (
+    complex_normalize,
+    inverse_complex_normalize,
+    pad,
+    reshape_channel_complex_to_last_dim,
+    reshape_complex_to_channel_dim,
+)
+from tests.utils import assert_allclose
 
 # no need for checking devices, these functions don't change device format
 # reshape test case
-im_2d, im_3d = torch.ones([3,4,50,70,2]), torch.ones([3,4,50,70,80,2])
+im_2d, im_3d = torch.ones([3, 4, 50, 70, 2]), torch.ones([3, 4, 50, 70, 80, 2])
 TEST_RESHAPE = [(im_2d,), (im_3d,)]
 
 # normalize test case
-im_2d, im_3d = torch.randint(0,3,[3,4,50,70]).float(), torch.randint(0,3,[3,4,50,70,80]).float()
+im_2d, im_3d = torch.randint(0, 3, [3, 4, 50, 70]).float(), torch.randint(0, 3, [3, 4, 50, 70, 80]).float()
 TEST_NORMALIZE = [(im_2d,), (im_3d,)]
 
 # pad test case
-im_2d, im_3d = torch.ones([3,4,50,70]), torch.ones([3,4,50,70,80])
+im_2d, im_3d = torch.ones([3, 4, 50, 70]), torch.ones([3, 4, 50, 70, 80])
 TEST_PAD = [(im_2d,), (im_3d,)]
+
 
 class TestReconNetUtils(unittest.TestCase):
     @parameterized.expand(TEST_RESHAPE)
@@ -47,7 +48,7 @@ class TestReconNetUtils(unittest.TestCase):
     def test_complex_normalize(self, test_data):
         result, mean, std = complex_normalize(test_data)
         result = inverse_complex_normalize(result, mean, std)
-        self.assertTrue( (((result-test_data)**2).mean()**0.5).item() < 1e-5 )
+        self.assertTrue((((result - test_data) ** 2).mean() ** 0.5).item() < 1e-5)
 
     @parameterized.expand(TEST_PAD)
     def test_pad(self, test_data):
