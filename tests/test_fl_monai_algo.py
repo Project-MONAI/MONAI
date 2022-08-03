32ff0bac22858318d863ed5fa5c33bd3c09be200
Holger Roth <hroth@nvidia.com>
2022-08-03 16:15:39 -0400

    add train test

--- tests/test_fl_monai_algo.py
+++ tests/test_fl_monai_algo.py
@@ -9,73 +9,26 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-
-import json
 import os
 import unittest
+from typing import Any, Dict
 
+import torch
+from ignite.engine import Engine
 from parameterized import parameterized
 
 from monai.bundle import ConfigParser
 from monai.fl.client.monai_algo import MonaiAlgo
-from monai.fl.utils.constants import ExtraItems
 from monai.fl.utils.exchange_object import ExchangeObject
-from tests.utils import SkipIfNoModule
+from monai.fl.utils.constants import ExtraItems
 
-TEST_TRAIN_1 = [
-    {
-        "config_train_file": os.path.join(os.path.dirname(__file__), "testing_data", "config_fl_train.json"),
-        "config_filters_file": os.path.join(os.path.dirname(__file__), "testing_data", "config_fl_filters.json"),
-    }
-]
-TEST_TRAIN_2 = [
-    {
-        "config_train_file": os.path.join(os.path.dirname(__file__), "testing_data", "config_fl_train.json"),
-        "config_filters_file": None,
-    }
-]
+TEST_CASE_1 = [{"config_train_file": os.path.join(os.path.dirname(__file__), "testing_data", "config_fl_train.json"),
+                "config_filters_file": os.path.join(os.path.dirname(__file__), "testing_data", "config_fl_filters.json")}]
 
-TEST_EVALUATE_1 = [
-    {
-        "config_evaluate_file": os.path.join(os.path.dirname(__file__), "testing_data", "config_fl_evaluate.json"),
-        "config_filters_file": os.path.join(os.path.dirname(__file__), "testing_data", "config_fl_filters.json"),
-    }
-]
-TEST_EVALUATE_2 = [
-    {
-        "config_evaluate_file": os.path.join(os.path.dirname(__file__), "testing_data", "config_fl_evaluate.json"),
-        "config_filters_file": None,
-    }
-]
-
-TEST_GET_WEIGHTS_1 = [
-    {
-        "config_train_file": os.path.join(os.path.dirname(__file__), "testing_data", "config_fl_train.json"),
-        "config_filters_file": os.path.join(os.path.dirname(__file__), "testing_data", "config_fl_filters.json"),
-    }
-]
-TEST_GET_WEIGHTS_2 = [
-    {
-        "config_train_file": None,
-        "config_filters_file": os.path.join(os.path.dirname(__file__), "testing_data", "config_fl_filters.json"),
-    }
-]
-
-
-@SkipIfNoModule("ignite")
 class TestFLMonaiAlgo(unittest.TestCase):
-    @parameterized.expand([TEST_TRAIN_1, TEST_TRAIN_2])
-    def test_train(self, input_params):
-        # get testing data dir and update train config
-        with open(input_params["config_train_file"]) as f:
-            config_train = json.load(f)
 
-        config_train["dataset_dir"] = os.path.join(os.path.dirname(__file__), "testing_data")
-
-        with open(input_params["config_train_file"], "w") as f:
-            json.dump(config_train, f, indent=4)
-
-        # initialize algo
+    @parameterized.expand([TEST_CASE_1])
+    def test_compute(self, input_params):
         algo = MonaiAlgo(**input_params)
         algo.initialize(extra={ExtraItems.CLIENT_NAME: "test_fl"})
 
@@ -87,57 +40,8 @@ class TestFLMonaiAlgo(unittest.TestCase):
 
         data = ExchangeObject(weights=network.state_dict())
 
-        # test train
-        algo.train(data=data, extra={})
-
-    @parameterized.expand([TEST_EVALUATE_1, TEST_EVALUATE_2])
-    def test_evaluate(self, input_params):
-        # get testing data dir and update train config
-        with open(input_params["config_evaluate_file"]) as f:
-            config_evaluate = json.load(f)
-
-        config_evaluate["dataset_dir"] = os.path.join(os.path.dirname(__file__), "testing_data")
-
-        with open(input_params["config_evaluate_file"], "w") as f:
-            json.dump(config_evaluate, f, indent=4)
-
-        # initialize algo
-        algo = MonaiAlgo(**input_params)
-        algo.initialize(extra={ExtraItems.CLIENT_NAME: "test_fl"})
-
-        # initialize model
-        parser = ConfigParser()
-        parser.read_config(input_params["config_evaluate_file"])
-        parser.parse()
-        network = parser.get_parsed_content("network")
-
-        data = ExchangeObject(weights=network.state_dict())
-
-        # test evaluate
-        algo.evaluate(data=data, extra={})
-
-    @parameterized.expand([TEST_GET_WEIGHTS_1, TEST_GET_WEIGHTS_2])
-    def test_get_weights(self, input_params):
-        # get testing data dir and update train config
-        if input_params["config_train_file"]:
-            with open(input_params["config_train_file"]) as f:
-                config_train = json.load(f)
-
-            config_train["dataset_dir"] = os.path.join(os.path.dirname(__file__), "testing_data")
-
-            with open(input_params["config_train_file"], "w") as f:
-                json.dump(config_train, f, indent=4)
-
-        # initialize algo
-        algo = MonaiAlgo(**input_params)
-        algo.initialize(extra={ExtraItems.CLIENT_NAME: "test_fl"})
-
-        # test train
-        weights = algo.get_weights(extra={})
-        self.assertIsInstance(weights, ExchangeObject)
-
-    # TODO: test abort and finalize
-
+        # test training
+        algo.train(data=data, extra = {})
 
 if __name__ == "__main__":
     unittest.main()
