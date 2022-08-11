a4f94b381cc0542a13b90edd7dc59f9158e51a6d
Holger Roth <hroth@nvidia.com>
2022-08-11 18:31:23 -0400

    add dummy filter
    Signed-off-by: Holger Roth <hroth@nvidia.com>

---
+++ monai/fl/utils/dummy_filter.py
@@ -0,0 +1,24 @@
+# coding: utf8
+from monai.fl.utils.exchange_object import ExchangeObject
+from monai.fl.utils.filter import Filter
+
+
+class DummyFilter(Filter):
+    """
+    Dummy filter to content of ExchangeObject.
+    """
+
+    def __call__(self, data: ExchangeObject, extra=None) -> ExchangeObject:
+        """
+        Dummy filter doesn't filter anything but only prints data summary.
+
+        Arguments:
+            data: ExchangeObject containing some data.
+
+        Returns:
+            ExchangeObject: filtered data.
+        """
+
+        print(f"Dummy filtering ExchangeObject: {data.summary()}")
+
+        return data
---
+++ monai/fl/utils/filter.py
@@ -0,0 +1,23 @@
+# coding: utf8
+import abc
+
+from monai.fl.utils.exchange_object import ExchangeObject
+
+
+class Filter(abc.ABC):
+    """
+    Used to apply filter to content of ExchangeObject.
+    """
+
+    def __call__(self, data: ExchangeObject, extra=None) -> ExchangeObject:
+        """
+        Run the filtering.
+
+        Arguments:
+            data: ExchangeObject containing some data.
+
+        Returns:
+            ExchangeObject: filtered data.
+        """
+
+        raise NotImplementedError
