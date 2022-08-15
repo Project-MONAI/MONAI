db2a9716f645140a2fea17bdf8c0dd8fa141b790
Mingxin Zheng <18563433+mingxin-zheng@users.noreply.github.com>
2022-08-15 23:24:18 +0800

    fix type hint
    Signed-off-by: Mingxin Zheng <18563433+mingxin-zheng@users.noreply.github.com>

--- monai/apps/auto3d/data_analyzer.py
+++ monai/apps/auto3d/data_analyzer.py
@@ -463,7 +463,7 @@ class DataAnalyzer:
         Returns
             a dictonary with following property of data in keys like "max", "mean" and others defined in operations_summary
         """
-        axis = None
+        axis: Tuple[int, int] = None
         if isinstance(datastat_list[0], list) or isinstance(datastat_list[0], np.ndarray):
             if not is_label:
                 # size = [num of cases, number of modalities, stats]
