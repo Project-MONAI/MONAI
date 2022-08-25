e7a72b9ebbb9709fe937ba7cbca45038ab4d3d2c
heyufan1995 <heyufan1995@gmail.com>
2022-08-25 13:48:26 -0400

    Add optuna support

---
+++ monai/apps/auto3d/hpowrapper/optuna/optuna_wrapper.py
@@ -0,0 +1,35 @@
+import optuna
+import pdb
+import sys
+import yaml
+sys.path.append('..')
+from dummy_trainner import trainner
+from hpo_wrapper import HPO_wrapper
+
+class OPTUNA_wrapper(HPO_wrapper):
+    def __init__(self, trainner=None, **kwargs):
+        super().__init__(trainner, **kwargs)
+    
+    def _get_hyperparameters(self, trial):
+        return {'lr': trial.suggest_float("lr", -10, 10)}
+        
+    def _update_model(self, params):
+        self.trainner.update(params)
+
+    def __call__(self, trial):
+        # step1 sample hyperparams
+        params = self._get_hyperparameters(trial)
+        # step 2 update model
+        self._update_model(params)
+        # step 3 train
+        acc = self.trainner.train()
+        # step 4 report validation acc to controller
+        # optuna minizes
+        return  - acc
+
+if __name__ == "__main__":
+    model = trainner(config={'lr':0.1, 'patch_size':[96,96,96]})
+    optuna_wrapper = OPTUNA_wrapper(trainner=model)
+    study = optuna.create_study()
+    study.optimize(optuna_wrapper, n_trials=100)
+    print("Best value: {} (params: {})\n".format(study.best_value, study.best_params))  
