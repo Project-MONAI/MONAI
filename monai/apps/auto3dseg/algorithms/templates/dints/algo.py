b3f727a49162ddd09e7ff136731ffaefdda5ab69
dongy <dongy@nvidia.com>
2022-08-25 11:25:22 -0700

    update repo
    Signed-off-by: dongy <dongy@nvidia.com>

--- monai/apps/auto3dseg/algorithms/templates/dints/algo.py
+++ monai/apps/auto3dseg/algorithms/templates/dints/algo.py
@@ -9,7 +9,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import fire
+import importlib
+import os
+import sys
 
 from monai.apps.auto3dseg.algorithms.algorithm import Algorithm
 
@@ -18,8 +20,34 @@ class Algo(Algorithm):
     def __init__(self, algorithm_dir: str = None):
         Algorithm.__init__(self, algorithm_dir)
 
-    def train(self):
-        pass
+        self.best_metric = -1.0
+        self.params = {}
 
-    def update(self):
-        pass
+        config_file = [
+            "configs/hyper_parameters.yaml",
+            "configs/network.yaml",
+            "configs/transforms_train.yaml",
+            "configs/transforms_validate.yaml",
+        ]
+        self.config_file = [
+            os.path.join(self.algorithm_dir, _item) for _item in config_file
+        ]
+
+    def train(self, module_path: str = ""):
+        if module_path == "":
+            module_path = "{0:s}.scripts.train".format(
+                self.algorithm_dir.split(os.sep)[-1]
+            )
+
+        module = importlib.import_module(module_path)
+        func_ = getattr(module, "run")
+
+        if len(self.params) == 0:
+            self.best_metric = func_(config_file=self.config_file)
+        else:
+            self.best_metric = func_(config_file=self.config_file, **self.params)
+
+        return self.best_metric
+
+    def update(self, params: dict = {}):
+        self.params = params
