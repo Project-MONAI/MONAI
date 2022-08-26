dea2c79bfa4c78dbe31b3bcfe61481c5dac9911a
Holger Roth <hroth@nvidia.com>
2022-08-25 22:28:38 -0400

    mention model type when logging returned weights
    Signed-off-by: Holger Roth <hroth@nvidia.com>

--- monai/fl/client/monai_algo.py
+++ monai/fl/client/monai_algo.py
@@ -307,7 +307,7 @@ class MonaiAlgo(ClientAlgo):
                 weights = torch.load(model_path, map_location="cpu")
                 weigh_type = WeightType.WEIGHTS
                 stats = dict()
-                self.logger.info(f"Returning best checkpoint weights from {model_path}.")
+                self.logger.info(f"Returning {model_type} checkpoint weights from {model_path}.")
             else:
                 raise ValueError(
                     f"Requested model type {model_type} not specified in `model_filepahts`: {self.model_filepaths}"
