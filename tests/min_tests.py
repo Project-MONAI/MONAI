302c5bce68f7904981858f0b3504a872e5d7b929
Mingxin Zheng <mingxinz@nvidia.com>
2022-08-26 17:35:35 +0800

    misc fix on mintests
    Signed-off-by: Mingxin Zheng <mingxinz@nvidia.com>

--- tests/min_tests.py
+++ tests/min_tests.py
@@ -29,8 +29,8 @@ def run_testsuit():
     exclude_cases = [  # these cases use external dependencies
         "test_ahnet",
         "test_arraydataset",
-        "test_auto3dseg",
         "test_auto3dseg_apps",
+        "test_auto3dseg",
         "test_cachedataset",
         "test_cachedataset_parallel",
         "test_cachedataset_persistent_workers",
