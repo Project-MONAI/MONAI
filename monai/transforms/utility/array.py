16dd9b9d203fea20b9aa4474028538969669a2f1
Wenqi Li <wenqil@nvidia.com>
2022-09-01 10:12:17 +0100

    update
    Signed-off-by: Wenqi Li <wenqil@nvidia.com>

--- monai/transforms/utility/array.py
+++ monai/transforms/utility/array.py
@@ -641,9 +641,7 @@ class SqueezeDim(Transform):
                 affine = affine[torch.arange(0, h, device=device) != dim - 1]
             if w > dim:
                 affine = affine[:, torch.arange(0, w, device=device) != dim - 1]
-            if (affine.shape[0] == affine.shape[1]) and not np.linalg.det(
-                convert_to_numpy(img.affine, wrap_sequence=True)
-            ):
+            if (affine.shape[0] == affine.shape[1]) and not np.linalg.det(convert_to_numpy(affine, wrap_sequence=True)):
                 warnings.warn(f"After SqueezeDim, img.affine is ill-posed: \n{img.affine}.")
             img.affine = affine
         return img
