45f34f8a96b553d4e41559dbc6b50b767026be44
Eric Kerfoot <eric.kerfoot@kcl.ac.uk>
2022-09-02 15:46:00 +0100

    Update
    Signed-off-by: Eric Kerfoot <eric.kerfoot@kcl.ac.uk>

--- monai/engines/utils.py
+++ monai/engines/utils.py
@@ -96,24 +96,24 @@ def default_prepare_batch(
     **kwargs,
 ) -> Union[Tuple[torch.Tensor, Optional[torch.Tensor]], torch.Tensor]:
     """
-    Default function to prepare the data for current iteration. 
-    
+    Default function to prepare the data for current iteration.
+
     The input `batchdata` is either a single tensor, a pair of tensors, or a dictionary of data. In the first case the
     return value is the tensor and None, in the second case the return value is the two tensors, and in the dictionary
     case the return value depends on what keys are present. if `CommonKeys.IMAGE` and `CommonKeys.LABEL` are present
     then the tensors they key to are returned, if only `CommonKeys.IMAGE` is present that tensor and None is returned.
     If `CommonKeys.REALS` is present this is returned with None. All returned tensors are moved to the given device
-    using the given non-blocking argument before being returned. 
-    
-    This function implemenets the expected API for a `prepare_batch` callable in Ignite: 
+    using the given non-blocking argument before being returned.
+
+    This function implemenets the expected API for a `prepare_batch` callable in Ignite:
     https://pytorch.org/ignite/v0.4.8/generated/ignite.engine.create_supervised_trainer.html
-    
+
     Args:
         batchdata: input batch data which is either a single tensor, a pair, or a dictionary
         device: device to move every returned tensor to
         non_blocking: equivalent argument for `Tensor.to`
         kwargs: further arguments for `Tensor.to`
-        
+
     Returns:
         image, label(optional).
     """
@@ -121,23 +121,23 @@ def default_prepare_batch(
         if isinstance(batchdata, torch.Tensor):
             return batchdata.to(device=device, non_blocking=non_blocking, **kwargs), None
         elif len(batchdata) == 2:
-            image,label=batchdata
+            image, label = batchdata
             return (
                 image.to(device=device, non_blocking=non_blocking, **kwargs),
                 label.to(device=device, non_blocking=non_blocking, **kwargs),
             )
-            
+
         raise AssertionError("Default prepare_batch expects a single tensor, a tensor pair, or dictionary input data.")
-        
+
     if isinstance(batchdata.get(CommonKeys.LABEL), torch.Tensor):
         return (
             batchdata[CommonKeys.IMAGE].to(device=device, non_blocking=non_blocking, **kwargs),
             batchdata[CommonKeys.LABEL].to(device=device, non_blocking=non_blocking, **kwargs),
         )
-        
+
     if GanKeys.REALS in batchdata:
         return batchdata[GanKeys.REALS].to(device=device, non_blocking=non_blocking, **kwargs)
-        
+
     return batchdata[CommonKeys.IMAGE].to(device=device, non_blocking=non_blocking, **kwargs), None
 
 
