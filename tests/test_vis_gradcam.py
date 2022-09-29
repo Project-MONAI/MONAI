52df2baf347809008dbf26dfa6a2f716eb12ae68
Richard Brown <33289025+rijobro@users.noreply.github.com>
2022-09-29 13:16:57 +0100

    Visualisation classes to allow kwargs. combine GradCAM and ++ test files (#5224)
    ### Description
    
    Some users may have models that take adjoint information. This PR allows
    the adjoint information to be passed through the visualisation classes
    to the model via `**kwargs`.
    
    Also combined the test files of GradCAM and GradCAM++ as they were the
    same.
    
    ### Types of changes
    <!--- Put an `x` in all the boxes that apply, and remove the not
    applicable items -->
    - [x] Non-breaking change (fix or new feature that would not break
    existing functionality).
    - [x] New tests added to cover the changes.
    - [x] Quick tests passed locally by running `./runtests.sh --quick
    --unittests --disttests`.
    - [x] In-line docstrings updated.
    
    Signed-off-by: Richard Brown <33289025+rijobro@users.noreply.github.com>

--- tests/test_vis_gradcam.py
+++ tests/test_vis_gradcam.py
@@ -10,81 +10,136 @@
 # limitations under the License.
 
 import unittest
+from typing import Any, List
 
 import numpy as np
 import torch
 from parameterized import parameterized
 
 from monai.networks.nets import DenseNet, DenseNet121, SEResNet50
-from monai.visualize import GradCAM
+from monai.visualize import GradCAM, GradCAMpp
 from tests.utils import assert_allclose
 
-# 2D
-TEST_CASE_0 = [
-    {
-        "model": "densenet2d",
-        "shape": (2, 1, 48, 64),
-        "feature_shape": (2, 1, 1, 2),
-        "target_layers": "class_layers.relu",
-    },
-    (2, 1, 48, 64),
-]
-# 3D
-TEST_CASE_1 = [
-    {
-        "model": "densenet3d",
-        "shape": (2, 1, 6, 6, 6),
-        "feature_shape": (2, 1, 2, 2, 2),
-        "target_layers": "class_layers.relu",
-    },
-    (2, 1, 6, 6, 6),
-]
-# 2D
-TEST_CASE_2 = [
-    {"model": "senet2d", "shape": (2, 3, 64, 64), "feature_shape": (2, 1, 2, 2), "target_layers": "layer4"},
-    (2, 1, 64, 64),
-]
 
-# 3D
-TEST_CASE_3 = [
-    {"model": "senet3d", "shape": (2, 3, 8, 8, 48), "feature_shape": (2, 1, 1, 1, 2), "target_layers": "layer4"},
-    (2, 1, 8, 8, 48),
-]
+class DenseNetAdjoint(DenseNet121):
+    def __call__(self, x, adjoint_info):
+        if adjoint_info != 42:
+            raise ValueError
+        return super().__call__(x)
+
+
+TESTS: List[Any] = []
+TESTS_ILL: List[Any] = []
+
+for cam in (GradCAM, GradCAMpp):
+    # 2D
+    TESTS.append(
+        [
+            cam,
+            {
+                "model": "densenet2d",
+                "shape": (2, 1, 48, 64),
+                "feature_shape": (2, 1, 1, 2),
+                "target_layers": "class_layers.relu",
+            },
+            (2, 1, 48, 64),
+        ]
+    )
+    # 3D
+    TESTS.append(
+        [
+            cam,
+            {
+                "model": "densenet3d",
+                "shape": (2, 1, 6, 6, 6),
+                "feature_shape": (2, 1, 2, 2, 2),
+                "target_layers": "class_layers.relu",
+            },
+            (2, 1, 6, 6, 6),
+        ]
+    )
+    # 2D
+    TESTS.append(
+        [
+            cam,
+            {"model": "senet2d", "shape": (2, 3, 64, 64), "feature_shape": (2, 1, 2, 2), "target_layers": "layer4"},
+            (2, 1, 64, 64),
+        ]
+    )
+
+    # 3D
+    TESTS.append(
+        [
+            cam,
+            {
+                "model": "senet3d",
+                "shape": (2, 3, 8, 8, 48),
+                "feature_shape": (2, 1, 1, 1, 2),
+                "target_layers": "layer4",
+            },
+            (2, 1, 8, 8, 48),
+        ]
+    )
+
+    # adjoint info
+    TESTS.append(
+        [
+            cam,
+            {
+                "model": "adjoint",
+                "shape": (2, 1, 48, 64),
+                "feature_shape": (2, 1, 1, 2),
+                "target_layers": "class_layers.relu",
+            },
+            (2, 1, 48, 64),
+        ]
+    )
+
+    TESTS_ILL.append([cam])
 
 
 class TestGradientClassActivationMap(unittest.TestCase):
-    @parameterized.expand([TEST_CASE_0, TEST_CASE_1, TEST_CASE_2, TEST_CASE_3])
-    def test_shape(self, input_data, expected_shape):
+    @parameterized.expand(TESTS)
+    def test_shape(self, cam_class, input_data, expected_shape):
         if input_data["model"] == "densenet2d":
             model = DenseNet121(spatial_dims=2, in_channels=1, out_channels=3)
-        if input_data["model"] == "densenet3d":
+        elif input_data["model"] == "densenet3d":
             model = DenseNet(
                 spatial_dims=3, in_channels=1, out_channels=3, init_features=2, growth_rate=2, block_config=(6,)
             )
-        if input_data["model"] == "senet2d":
+        elif input_data["model"] == "senet2d":
             model = SEResNet50(spatial_dims=2, in_channels=3, num_classes=4)
-        if input_data["model"] == "senet3d":
+        elif input_data["model"] == "senet3d":
             model = SEResNet50(spatial_dims=3, in_channels=3, num_classes=4)
+        elif input_data["model"] == "adjoint":
+            model = DenseNetAdjoint(spatial_dims=2, in_channels=1, out_channels=3)
+
+        # optionally test for adjoint info
+        kwargs = {"adjoint_info": 42} if input_data["model"] == "adjoint" else {}
+
         device = "cuda:0" if torch.cuda.is_available() else "cpu"
         model.to(device)
         model.eval()
-        cam = GradCAM(nn_module=model, target_layers=input_data["target_layers"])
+        cam = cam_class(nn_module=model, target_layers=input_data["target_layers"])
         image = torch.rand(input_data["shape"], device=device)
-        result = cam(x=image, layer_idx=-1)
-        np.testing.assert_array_equal(cam.nn_module.class_idx.cpu(), model(image).max(1)[-1].cpu())
-        fea_shape = cam.feature_map_size(input_data["shape"], device=device)
+        inferred = model(image, **kwargs).max(1)[-1].cpu()
+        result = cam(x=image, layer_idx=-1, **kwargs)
+        np.testing.assert_array_equal(cam.nn_module.class_idx.cpu(), inferred)
+
+        fea_shape = cam.feature_map_size(input_data["shape"], device=device, **kwargs)
         self.assertTupleEqual(fea_shape, input_data["feature_shape"])
         self.assertTupleEqual(result.shape, expected_shape)
         # check result is same whether class_idx=None is used or not
-        result2 = cam(x=image, layer_idx=-1, class_idx=model(image).max(1)[-1].cpu())
+        result2 = cam(x=image, layer_idx=-1, class_idx=inferred, **kwargs)
         assert_allclose(result, result2)
 
-    def test_ill(self):
+    @parameterized.expand(TESTS_ILL)
+    def test_ill(self, cam_class):
         model = DenseNet121(spatial_dims=2, in_channels=1, out_channels=3)
         for name, x in model.named_parameters():
             if "features" in name:
                 x.requires_grad = False
-        cam = GradCAM(nn_module=model, target_layers="class_layers.relu")
+        cam = cam_class(nn_module=model, target_layers="class_layers.relu")
         image = torch.rand((2, 1, 48, 64))
         with self.assertRaises(IndexError):
             cam(x=image)
