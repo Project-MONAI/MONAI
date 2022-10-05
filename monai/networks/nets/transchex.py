20dd20ecc8a7f5a7671f2e86c074c795feb53421
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-05 23:21:38 +0000

    Adding tarfile member sanitization to extractall()

--- monai/networks/nets/transchex.py
+++ monai/networks/nets/transchex.py
@@ -72,7 +72,26 @@ class BertPreTrainedModel(nn.Module):
         else:
             tempdir = tempfile.mkdtemp()
             with tarfile.open(resolved_archive_file, "r:gz") as archive:
-                archive.extractall(tempdir)
+                def is_within_directory(directory, target):
+                    
+                    abs_directory = os.path.abspath(directory)
+                    abs_target = os.path.abspath(target)
+                
+                    prefix = os.path.commonprefix([abs_directory, abs_target])
+                    
+                    return prefix == abs_directory
+                
+                def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                
+                    for member in tar.getmembers():
+                        member_path = os.path.join(path, member.name)
+                        if not is_within_directory(path, member_path):
+                            raise Exception("Attempted Path Traversal in Tar File")
+                
+                    tar.extractall(path, members, numeric_owner=numeric_owner) 
+                    
+                
+                safe_extract(archive, tempdir)
             serialization_dir = tempdir
         model = cls(num_language_layers, num_vision_layers, num_mixed_layers, bert_config, *inputs, **kwargs)
         if state_dict is None and not from_tf:
