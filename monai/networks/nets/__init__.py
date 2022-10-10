d0edb6c06d48a9235310d43252febee4b74a6f4a
binliu <binliu@nvidia.com>
2022-10-10 14:13:37 +0000

    add backbone to nets init
    Signed-off-by: binliu <binliu@nvidia.com>

--- monai/networks/nets/__init__.py
+++ monai/networks/nets/__init__.py
@@ -42,7 +42,7 @@ from .efficientnet import (
     drop_connect,
     get_efficientnet_image_size,
 )
-from .flexible_unet import FlexibleUNet
+from .flexible_unet import BACKBONE, FlexibleUNet
 from .fullyconnectednet import FullyConnectedNet, VarFullyConnectedNet
 from .generator import Generator
 from .highresnet import HighResBlock, HighResNet
