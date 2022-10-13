656f5f32317cfd8cb269960d57b22d434c909bf4
Wenqi Li <wenqil@nvidia.com>
2022-10-13 22:13:37 +0100

    update tests
    Signed-off-by: Wenqi Li <wenqil@nvidia.com>

--- tests/test_fl_monai_algo_dist.py
+++ tests/test_fl_monai_algo_dist.py
@@ -12,15 +12,15 @@
 import json
 import os
 import unittest
-import torch.distributed as dist
 
+import torch.distributed as dist
 from parameterized import parameterized
 
 from monai.bundle import ConfigParser
 from monai.fl.client.monai_algo import MonaiAlgo
 from monai.fl.utils.constants import ExtraItems
 from monai.fl.utils.exchange_object import ExchangeObject
-from tests.utils import SkipIfNoModule, DistCall, DistTestCase
+from tests.utils import DistCall, DistTestCase, SkipIfNoModule
 
 TEST_TRAIN_1 = [
     {
@@ -36,40 +36,36 @@ TEST_TRAIN_1 = [
 @SkipIfNoModule("ignite")
 class TestFLMonaiAlgo(DistTestCase):
     def setUp(self):
-        for input_params in TEST_TRAIN_1:
-            # get testing data dir and update train config; using the first to define data dir
-            if isinstance(input_params["config_train_filename"], list):
-                config_train_filename = input_params["config_train_filename"][0]
-            else:
-                config_train_filename = input_params["config_train_filename"]
-            with open(os.path.join(input_params["bundle_root"], config_train_filename)) as f:
-                config_train = json.load(f)
+        input_params = TEST_TRAIN_1[0]
+        # get testing data dir and update train config; using the first to define data dir
+        if isinstance(input_params["config_train_filename"], list):
+            config_train_filename = input_params["config_train_filename"][0]
+        else:
+            config_train_filename = input_params["config_train_filename"]
+        with open(os.path.join(input_params["bundle_root"], config_train_filename)) as f:
+            config_train = json.load(f)
 
-            config_train["dataset_dir"] = os.path.join(os.path.dirname(__file__), "testing_data")
+        config_train["dataset_dir"] = os.path.join(os.path.dirname(__file__), "testing_data")
 
-            with open(os.path.join(input_params["bundle_root"], config_train_filename), "w") as f:
-                json.dump(config_train, f, indent=4)
-
+        with open(os.path.join(input_params["bundle_root"], config_train_filename), "w") as f:
+            json.dump(config_train, f, indent=4)
 
     @parameterized.expand([TEST_TRAIN_1])
     @DistCall(nnodes=1, nproc_per_node=2)
     def test_train(self, input_params):
         # initialize algo
-        print(f"rank {dist.get_rank()}")
         algo = MonaiAlgo(**input_params)
         algo.initialize(extra={ExtraItems.CLIENT_NAME: "test_fl"})
+        self.assertTrue(dist.get_rank() in (0, 1))
 
         # initialize model
         parser = ConfigParser()
-        parser.read_config(os.path.join(input_params["bundle_root"], config_train_filename))
+        parser.read_config(os.path.join(input_params["bundle_root"], input_params["config_train_filename"]))
         parser.parse()
         network = parser.get_parsed_content("network")
-
         data = ExchangeObject(weights=network.state_dict())
-
         # test train
         algo.train(data=data, extra={})
-
 
 
 if __name__ == "__main__":
