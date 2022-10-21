95d9fd0f8d56371871979972344e390612d79fc5
JHancox <48477639+JHancox@users.noreply.github.com>
2022-10-21 17:04:27 +0100

    Update hovernet_loss.py
    Signed-off-by: Behrooz <3968947+drbeh@users.noreply.github.com>

--- monai/apps/pathology/losses/hovernet_loss.py
+++ monai/apps/pathology/losses/hovernet_loss.py
@@ -23,35 +23,41 @@ from monai.utils.enums import HoVerNetBranch
 
 class HoVerNetLoss(_Loss):
     """
-    Loss function for HoVerNet pipeline, which is combination of losses across three branches
-
-    Args:
-        lambda_hv_mse: Weight factor to apply to the HV regression MSE part of the overall loss
-        lambda_hv_mse_grad: Weight factor to apply to the MSE of the HV gradient part of the overall loss
-        lambda_nuclei_ce: Weight factor to apply to the nuclei prediction CrossEntropyLoss part
-            of the overall loss
-        lambda_nuclei_dice: Weight factor to apply to the nuclei prediction DiceLoss part of overall loss
-        lambda_type_ce: Weight factor to apply to the nuclei class prediction CrossEntropyLoss part
-            of the overall loss
-        lambda_type_dice: Weight factor to apply to the nuclei class prediction DiceLoss part of the
-            overall loss
+    Loss function for HoVerNet pipeline, which is combination of losses across the three branches.
+    The NP (nucleus prediction) branch uses Dice + CrossEntropy.
+    The HV (Horizontal and Vertical) distance from centroid branch uses MSE + MSE of the gradient.
+    The NC (Nuclear Class prediction) branch uses Dice + CrossEntropy
+    The result is a weighted sum of these losses.
+    
     """
 
     def __init__(
         self,
         lambda_hv_mse: float = 2.0,
         lambda_hv_mse_grad: float = 1.0,
-        lambda_nuclei_ce: float = 1.0,
-        lambda_nuclei_dice: float = 1.0,
-        lambda_type_ce: float = 1.0,
-        lambda_type_dice: float = 1.0,
+        lambda_np_ce: float = 1.0,
+        lambda_np_dice: float = 1.0,
+        lambda_nc_ce: float = 1.0,
+        lambda_nc_dice: float = 1.0,
     ) -> None:
+        """
+        Args:
+            lambda_hv_mse: Weight factor to apply to the HV regression MSE part of the overall loss
+            lambda_hv_mse_grad: Weight factor to apply to the MSE of the HV gradient part of the overall loss
+            lambda_np_ce: Weight factor to apply to the nuclei prediction CrossEntropyLoss part
+                of the overall loss
+            lambda_np_dice: Weight factor to apply to the nuclei prediction DiceLoss part of overall loss
+            lambda_nc_ce: Weight factor to apply to the nuclei class prediction CrossEntropyLoss part
+                of the overall loss
+            lambda_nc_dice: Weight factor to apply to the nuclei class prediction DiceLoss part of the
+                overall loss
+        """
         self.lambda_hv_mse = lambda_hv_mse
         self.lambda_hv_mse_grad = lambda_hv_mse_grad
-        self.lambda_nuclei_ce = lambda_nuclei_ce
-        self.lambda_nuclei_dice = lambda_nuclei_dice
-        self.lambda_type_ce = lambda_type_ce
-        self.lambda_type_dice = lambda_type_dice
+        self.lambda_np_ce = lambda_np_ce
+        self.lambda_np_dice = lambda_np_dice
+        self.lambda_nc_ce = lambda_nc_ce
+        self.lambda_nc_dice = lambda_nc_dice
         super().__init__()
 
         self.dice = DiceLoss(softmax=True, smooth_dr=1e-03, smooth_nr=1e-03, reduction="sum", batch=True)
@@ -67,12 +73,15 @@ class HoVerNetLoss(_Loss):
         return torch.cat([result_h[:, None, ...], result_v[:, None, ...]], dim=1)
 
     def _mse_gradient_loss(self, prediction: torch.Tensor, target: torch.Tensor, focus: torch.Tensor) -> torch.Tensor:
-
+        # Compute the MSE loss of the gradients of the horizontal and vertical centroid distance maps
+        
         pred_grad = self._compute_sobel(prediction)
         true_grad = self._compute_sobel(target)
 
         loss = pred_grad - true_grad
-
+        
+        # The focus constrains the loss computation to the detected nuclear regions
+        # (i.e. background is excluded)
         focus = focus[:, None, ...]
         focus = torch.cat((focus, focus), 1)
 
@@ -109,14 +118,16 @@ class HoVerNetLoss(_Loss):
                 "type_prediction (NC) must be present in both or neither of the prediction and target parameters"
             )
 
-        dice_loss_nuclei = (
-            self.dice(prediction[HoVerNetBranch.NP.value], target[HoVerNetBranch.NP.value]) * self.lambda_nuclei_dice
+        # Compute the NP branch loss
+        dice_loss_np = (
+            self.dice(prediction[HoVerNetBranch.NP.value], target[HoVerNetBranch.NP.value]) * self.lambda_np_dice
         )
-        ce_loss_nuclei = (
-            self.ce(prediction[HoVerNetBranch.NP.value], target[HoVerNetBranch.NP.value]) * self.lambda_nuclei_ce
+        ce_loss_np = (
+            self.ce(prediction[HoVerNetBranch.NP.value], target[HoVerNetBranch.NP.value]) * self.lambda_np_ce
         )
-        loss_nuclei = dice_loss_nuclei + ce_loss_nuclei
+        loss_np = dice_loss_np + ce_loss_np
 
+        # Compute the HV branch loss
         loss_hv_mse = (
             F.mse_loss(prediction[HoVerNetBranch.HV.value], target[HoVerNetBranch.HV.value]) * self.lambda_hv_mse
         )
@@ -130,17 +141,20 @@ class HoVerNetLoss(_Loss):
             )
             * self.lambda_hv_mse_grad
         )
+        loss_hv = loss_hv_mse_grad + loss_hv_mse
 
-        loss_type = 0
+        # Compute the NC branch loss
+        loss_nc = 0
         if HoVerNetBranch.NC.value in prediction:
-            dice_loss_type = (
-                self.dice(prediction[HoVerNetBranch.NC.value], target[HoVerNetBranch.NC.value]) * self.lambda_type_dice
+            dice_loss_nc = (
+                self.dice(prediction[HoVerNetBranch.NC.value], target[HoVerNetBranch.NC.value]) * self.lambda_nc_dice
             )
-            ce_loss_type = (
-                self.ce(prediction[HoVerNetBranch.NC.value], target[HoVerNetBranch.NC.value]) * self.lambda_type_ce
+            ce_loss_nc = (
+                self.ce(prediction[HoVerNetBranch.NC.value], target[HoVerNetBranch.NC.value]) * self.lambda_nc_ce
             )
-            loss_type = dice_loss_type + ce_loss_type
+            loss_nc = dice_loss_nc + ce_loss_nc
 
-        loss: torch.Tensor = loss_hv_mse + loss_hv_mse_grad + loss_nuclei + loss_type
+        # Sum the losses from each branch
+        loss: torch.Tensor = loss_hv + loss_np + loss_nc
 
         return loss
