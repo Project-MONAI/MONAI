a55fde4cbe1c05ce2a3b01715b43c7005ca0a3d7
Ben Murray <ben.murray@gmail.com>
2022-10-27 13:43:42 +0100

    Removing obsolete test now that RandomizableTransformType doesn't require set_random_state
    Signed-off-by: Ben Murray <ben.murray@gmail.com>

--- tests/test_randomizable_transform_type.py
+++ tests/test_randomizable_transform_type.py
@@ -28,11 +28,6 @@ class TestRandomizableTransformType(unittest.TestCase):
         inst = InheritsInterface()
         self.assertIsInstance(inst, RandomizableTransformType)
 
-    def test_set_random_state_default_impl(self):
-        inst = InheritsInterface()
-        with self.assertRaises(TypeError):
-            inst.set_random_state(seed=0)
-
     def test_set_random_state_randomizable_transform(self):
         inst = InheritsImplementation()
         inst.set_random_state(0)
