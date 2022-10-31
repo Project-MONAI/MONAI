95b8825cdfb002abf1b9d84efbac52d1e8b74513
myron <amyronenko@nvidia.com>
2022-10-30 20:09:42 -0700

    remove self.device
    Signed-off-by: myron <amyronenko@nvidia.com>

--- monai/metrics/cumulative_average.py
+++ monai/metrics/cumulative_average.py
@@ -15,7 +15,6 @@ from typing import Any, Optional, Union
 import torch
 import torch.distributed as dist
 from numpy.typing import NDArray
-from torch.distributed import ProcessGroup
 
 
 class CumulativeAverage:
@@ -31,71 +30,27 @@ class CumulativeAverage:
         from monai.metrics import CumulativeAverage
 
         run_avg = CumulativeAverage()
+        batch_size = 8
         for i in range(len(train_set)):
             ...
             val = calc_metric(x,y) #some metric value
-            run_avg.append(val)
+            run_avg.append(val, count=batch_size)
 
         val_avg = run_avg.aggregate() #average value
 
     """
 
-    def __init__(self, group: Optional[ProcessGroup] = None) -> None:
-        """
-        Args:
-            group: Optional process group to use in multi-gpu distributed setup. Defaults to None
-            to use the default process group created during DistributedDataParallel initialization.
-        """
-
-        self.is_distributed = dist.is_available() and dist.is_initialized()
-        self.device = None
-
-        if self.is_distributed:
-            self.group = group
-            if dist.get_backend(group=group) == dist.Backend.NCCL:
-                self.device = torch.device("cuda", dist.get_rank(group=group))
-        else:
-            self.group = None
-
-        if self.device is None:
-            self.device = torch.device("cpu")
-
+    def __init__(self) -> None:
         self.reset()
 
     def reset(self) -> None:
         """
-        Reset all the running status
-
+        Reset all  stats
         """
-        self.val: torch.Tensor = 0  # type: ignore
-        self.sum: torch.Tensor = 0  # type: ignore
-        self.count: torch.Tensor = 0  # type: ignore
-
-    def proper_tensor(self, a: Any) -> torch.Tensor:
-        """
-        Ensure torch.Tensor float format and optionally copy to the proper gpu device
-        """
-        x: torch.Tensor = a.detach() if isinstance(a, torch.Tensor) else torch.tensor(a)
-
-        return x.to(device=self.device, dtype=torch.float)
-
-    def reduce(self, x: torch.Tensor, avg: bool = False) -> torch.Tensor:
-        """
-        Reduce the tensor across processes if in DDP (using summation reduction)
-
-        Args:
-            x: tensor to reduce.
-            avg: use average reduction instead of summation. Defaults to False
-
-        """
-        if self.is_distributed:
-            if avg:
-                x = x / dist.get_world_size(group=self.group)
-            else:
-                x = x.clone()
-            dist.all_reduce(x, group=self.group)
-
-        return x
+        self.val: torch.Tensor = None  # type: ignore
+        self.sum = torch.tensor(0, dtype=torch.float)
+        self.count = torch.tensor(0, dtype=torch.float)
+        self.is_distributed = dist.is_available() and dist.is_initialized()
 
     def get_current(self, to_numpy: bool = True) -> Union[NDArray, torch.Tensor]:
         """
@@ -104,10 +59,20 @@ class CumulativeAverage:
         Args:
             to_numpy: whether to convert to numpy array. Defaults to True
         """
-        x = self.reduce(self.val, avg=True)
+        if self.val is None:
+            return 0
+
+        val = self.val.clone()
+        val[~torch.is_finite(val)] = 0
+
+        if self.is_distributed:
+            val = val / dist.get_world_size()
+            dist.all_reduce(val)
+
         if to_numpy:
-            x = x.cpu().numpy()
-        return x
+            val = val.cpu().numpy()
+
+        return val
 
     def aggregate(self, to_numpy: bool = True) -> Union[NDArray, torch.Tensor]:
         """
@@ -116,21 +81,32 @@ class CumulativeAverage:
         Args:
             to_numpy: whether to convert to numpy array. Defaults to True
         """
-        sum = self.reduce(self.sum)
-        count = self.reduce(self.count)
+        if self.val is None:
+            return 0
 
-        x = torch.where(count > 0, sum / count, sum)
+        sum = self.sum
+        count = self.count
+
+        if self.is_distributed:
+            sum = sum.to(self.val, copy=True)
+            count = count.to(self.val, copy=True)
+            dist.all_reduce(sum)
+            dist.all_reduce(count)
+
+        val = torch.where(count > 0, sum / count, sum)
+
         if to_numpy:
-            x = x.cpu().numpy()
-        return x
+            val = val.cpu().numpy()
+        return val
 
-    def append(self, val: Any, count: Any = 1) -> None:
+    def append(self, val: Any, count: Optional[Any] = 1) -> None:
         """
-        Append with a new value, and an optional count
+        Append with a new value, and an optional count. Any data type is supported that is convertable
+            with torch.as_tensor() e.g. number, list, numpy array, or Tensor.
 
         Args:
-            val: new value (e.g. constant, list, numpy array or Tensor) to keep track of.
-            count: new count (e.g. constant, list, numpy array or Tensor), to update the contribution count
+            val: value (e.g. number, list, numpy array or Tensor) to keep track of
+            count: count (e.g. number, list, numpy array or Tensor), to update the contribution count
 
         For example:
             # a simple constant tracking
@@ -158,19 +134,24 @@ class CumulativeAverage:
             print(avg.aggregate()) #prints [0.5, 0.5, 0,5] == ([0.5, 0.5, 0] + [0.5, 0.5, 0.5]) / ([1, 1, 0] + [1, 1, 1])
 
         """
+        self.val = torch.as_tensor(val, dtype=torch.float)
+        if self.val.requires_grad:
+            self.val = self.val.detach().clone()
 
-        val = self.proper_tensor(val)
-        count = self.proper_tensor(count)
+        count = torch.as_tensor(count, dtype=torch.float, device="cpu")
+        if count.ndim > 0 and count.shape != self.val.shape:
+            raise ValueError(
+                f"Count shape must match val shape, unless count is a single number: {count} val {self.val.cpu()}"
+            )
 
-        val_count = val * count
-        nfin = ~torch.isfinite(val_count)
-        if torch.any(nfin):
+        val = count * self.val.cpu()
+
+        # account for possible non-finite numbers in val and replace them with 0s
+        nfin = torch.isfinite(val)
+        if not torch.all(nfin):
             warnings.warn(f"non-finite inputs received: val: {val}, count: {count}")
-            zero = torch.tensor(0).to(val)
-            val = torch.where(nfin, zero, val)
-            count = torch.where(nfin, zero, count)
-            val_count = torch.where(nfin, zero, val_count)
+            count = torch.where(nfin, count, torch.zeros_like(count))
+            val = torch.where(nfin, val, torch.zeros_like(val))
 
-        self.val = val
-        self.count += count
-        self.sum += val_count
+        self.count = self.count + count
+        self.sum = self.sum + val
