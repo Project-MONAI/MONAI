cab7390332fd7e92212b54d5fbfb00158f7d0260
KumoLiu <yunl@nvidia.com>
2022-11-03 11:26:10 +0800

    minor fix
    Signed-off-by: KumoLiu <yunl@nvidia.com>

--- monai/apps/pathology/engines/__init__.py
+++ monai/apps/pathology/engines/__init__.py
@@ -9,4 +9,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from .utils import PrepareBatchWithDictLabel
+from .utils import PrepareBatchHoVerNet
