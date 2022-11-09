DCO fix sign-off
DCO Remediation Commit for Joeycho <joeyadamcho@gmail.com>

I, Joeycho <joeyadamcho@gmail.com>, hereby add my Signed-off-by to this commit: a444ea822f7b6431b8054c0c845d7909ab3f2ab2

Signed-off-by: Joeycho <joeyadamcho@gmail.com>

tests/test_ssim_metric.py
@@ -39,7 +39,7 @@ class TestSSIMMetric(unittest.TestCase):
         self.ssim = SSIMMetric(data_range=drange, spatial_dims=2)
         self.ssim.(x,y)
         result2 = self.ssim.aggregate()
-        self.assertTrue(isinstance(result2, torch.Tensor))
+        self.assertTrue(isinstance(result2, torch.Tensor)) 
         self.assertTrue(torch.abs(result2 - result).item() < 0.001)
 
 