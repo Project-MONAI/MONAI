e39aa0ed329a33270de22d1fc50c1abc5b89ba48
Roland Kica <roland.kica@student.tuke.sk>
2022-11-09 19:24:59 +0100

    flake8-py3 black warning fixed

--- monai/metrics/f_beta_score.py
+++ monai/metrics/f_beta_score.py
@@ -19,9 +19,7 @@ from monai.utils import MetricReduction
 from .metric import CumulativeIterationMetric
 
 
-class FBetaScore(
-    CumulativeIterationMetric
-):
+class FBetaScore(CumulativeIterationMetric):
     def __init__(
         self,
         beta: float = 1.0,
@@ -99,7 +97,7 @@ def compute_f_beta_score(confusion_matrix: torch.Tensor, beta: float):
 
     tp = confusion_matrix[..., 0]
     fp = confusion_matrix[..., 1]
-    #tn = confusion_matrix[..., 2]
+    # tn = confusion_matrix[..., 2]
     fn = confusion_matrix[..., 3]
 
     nan_tensor = torch.tensor(float("nan"), device=confusion_matrix.device)
