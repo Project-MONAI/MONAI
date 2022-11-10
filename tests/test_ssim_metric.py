3b28d57a7af49c8828d7d154bfac018650838604
Joeycho <joeyadamcho@gmail.com>
2022-11-10 10:22:55 +0100

    'self.ssim' attr replaced by local variable.
    Signed-off-by: Joeycho <joeyadamcho@gmail.com>

--- tests/test_ssim_metric.py
+++ tests/test_ssim_metric.py
@@ -36,9 +36,9 @@ class TestSSIMMetric(unittest.TestCase):
         self.assertTrue(isinstance(result, torch.Tensor))
         self.assertTrue(torch.abs(res - result).item() < 0.001)
 
-        self.ssim = SSIMMetric(data_range=drange, spatial_dims=2)
-        self.ssim.(x,y)
-        result2 = self.ssim.aggregate()
+        ssim = SSIMMetric(data_range=drange, spatial_dims=2)
+        ssim.(x,y)
+        result2 = ssim.aggregate()
         self.assertTrue(isinstance(result2, torch.Tensor))
         self.assertTrue(torch.abs(result2 - result).item() < 0.001)
 
@@ -49,9 +49,9 @@ class TestSSIMMetric(unittest.TestCase):
         self.assertTrue(isinstance(result, torch.Tensor))
         self.assertTrue(torch.abs(res - result).item() < 0.001)
 
-        self.ssim = SSIMMetric(data_range=drange, spatial_dims=3)
-        self.ssim.(x,y)
-        result2 = self.ssim.aggregate()
+        ssim = SSIMMetric(data_range=drange, spatial_dims=3)
+        ssim.(x,y)
+        result2 = ssim.aggregate()
         self.assertTrue(isinstance(result2, torch.Tensor))
         self.assertTrue(torch.abs(result2 - result).item() < 0.001)
 
