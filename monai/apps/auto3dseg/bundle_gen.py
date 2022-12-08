75940df8414c2c0adf7b88552634e0df2edbfc9f
Mingxin Zheng <18563433+mingxin-zheng@users.noreply.github.com>
2022-12-08 23:06:08 +0800

    use algo hash in the main commit history
    Signed-off-by: Mingxin Zheng <18563433+mingxin-zheng@users.noreply.github.com>

--- monai/apps/auto3dseg/bundle_gen.py
+++ monai/apps/auto3dseg/bundle_gen.py
@@ -32,7 +32,7 @@ from monai.bundle.config_parser import ConfigParser
 from monai.utils import ensure_tuple
 
 logger = get_logger(module_name=__name__)
-ALGO_HASH = os.environ.get("MONAI_ALGO_HASH", "dc68e4d")
+ALGO_HASH = os.environ.get("MONAI_ALGO_HASH", "5895e1b")
 
 __all__ = ["BundleAlgo", "BundleGen"]
 
