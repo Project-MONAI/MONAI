4a0d9248fd2755c2a6f5bd8fc89b329256be5d83
dongy <dongy@nvidia.com>
2022-12-13 23:11:08 -0800

    update changes
    Signed-off-by: dongy <dongy@nvidia.com>

--- tests/test_auto3dseg_ensemble_with_custom_param.py
+++ tests/test_auto3dseg_ensemble_with_custom_param.py
@@ -125,7 +125,7 @@ class TestEnsembleBuilder(unittest.TestCase):
 
         gpu_customization_specs = {
             "universal": {
-                "num_trials": 2,
+                "num_trials": 1,
                 "range_num_images_per_batch": [1, 2],
                 "range_num_sw_batch_size": [1, 2],
             }
