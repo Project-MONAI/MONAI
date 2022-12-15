4a5e58ef310b46745d5d7cb1e568cdc08cdfb59b
dongy <dongy@nvidia.com>
2022-12-14 18:46:22 -0800

    update changes
    Signed-off-by: dongy <dongy@nvidia.com>

--- monai/apps/auto3dseg/bundle_gen.py
+++ monai/apps/auto3dseg/bundle_gen.py
@@ -32,7 +32,7 @@ from monai.bundle.config_parser import ConfigParser
 from monai.utils import ensure_tuple
 
 logger = get_logger(module_name=__name__)
-ALGO_HASH = os.environ.get("MONAI_ALGO_HASH", "a5c5960")
+ALGO_HASH = os.environ.get("MONAI_ALGO_HASH", "c812e5f")
 
 __all__ = ["BundleAlgo", "BundleGen"]
 
