434855a95f8f2f493190044015e834b394dee1d5
Behrooz <3968947+drbeh@users.noreply.github.com>
2023-01-25 11:25:32 -0500

    Implement Merger based class and AvgMerger
    Signed-off-by: Behrooz <3968947+drbeh@users.noreply.github.com>

--- monai/inferers/__init__.py
+++ monai/inferers/__init__.py
@@ -11,5 +11,7 @@
 
 from __future__ import annotations
 
-from .inferer import Inferer, SaliencyInferer, SimpleInferer, SliceInferer, SlidingWindowInferer
+from .inferer import Inferer, PatchInferer, SaliencyInferer, SimpleInferer, SliceInferer, SlidingWindowInferer
+from .merger import Merger, AvgMerger
+from .splitter import Splitter, SlidingWindowSplitter
 from .utils import sliding_window_inference
---
+++ monai/inferers/merger.py
@@ -0,0 +1,152 @@
+# Copyright (c) MONAI Consortium
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#     http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from __future__ import annotations
+
+from abc import ABC, abstractmethod
+
+import torch
+from monai.utils.enums import PatchKeys
+from monai.utils import ensure_tuple_size
+
+__all__ = ["Merger"]
+
+
+class Merger(ABC):
+    """
+    A base class for merging patches.
+    Extend this class to support operations for `PatchInference`, e.g. `AvgMerger`.
+
+    Args:
+        device: the device where Merger tensors should reside.
+    """
+
+    def __init__(self, device: torch.device | str | None = None) -> None:
+        self.device = device
+
+    @abstractmethod
+    def initialize(self, inputs: torch.Tensor, in_patch: torch.Tensor, out_patch: torch.Tensor):
+        """
+        Initialize the merger.
+
+        Args:
+            inputs: a tensor of shape BxCxHxW[xD], representing a batch of input images
+            in_patch: a tensor of shape BxCxH'xW'[xD'], representing a batch of input patches
+            out_patch: a tensor of shape BxC"xH"xW"[xD"], representing a batch of input patches
+
+        Raises:
+            NotImplementedError: When the subclass does not override this method.
+
+        """
+        raise NotImplementedError(f"Subclass {self.__class__.__name__} must implement this method.")
+
+    @abstractmethod
+    def aggregate(self, values: torch.Tensor):
+        """
+        Aggregate values for merging.
+
+        Args:
+            values: a tensor of shape BxCxHxW[xD], representing the values of inference output
+
+        Raises:
+            NotImplementedError: When the subclass does not override this method.
+
+        """
+        raise NotImplementedError(f"Subclass {self.__class__.__name__} must implement this method.")
+
+    @abstractmethod
+    def finalize(self):
+        """
+        Perform final operations for merging patches.
+
+        Raises:
+            NotImplementedError: When the subclass does not override this method.
+
+        """
+        raise NotImplementedError(f"Subclass {self.__class__.__name__} must implement this method.")
+
+
+class AvgMerger(Merger):
+    """Merge patches by taking average of the overlapping area
+
+    Args:
+        device: the device for `values` and `count` tensor aggregator.
+        dtype: the dtype of `values` tensor aggregator.
+    """
+
+    def __init__(self, device: torch.device | str | None = None, dtype=torch.float32) -> None:
+        super().__init__(device)
+        self.dtype = dtype
+
+    def initialize(self, inputs: torch.Tensor, in_patch: torch.Tensor, out_patch: torch.Tensor):
+        """
+        Initialize the merger by creating tensors for aggregation (`values` and `counts`).
+
+        Args:
+            inputs: a tensor of shape BxCxHxW[xD], representing a batch of input images
+            in_patch: a tensor of shape BxCxH'xW'[xD'], representing a batch of input patches
+            out_patch: a tensor of shape BxC"xH"xW"[xD"], representing a batch of input patches
+        """
+        super().__init__()
+        in_spatial_shape = torch.tensor(inputs.shape[2:])
+        in_patch_shape = torch.tensor(in_patch.shape[2:])
+        out_patch_shape = torch.tensor(out_patch.shape[2:])
+        batch_channel_shape = out_patch.shape[:2]
+        spatial_shape = torch.round(in_spatial_shape * out_patch_shape / in_patch_shape).to(int).tolist()
+        output_shape = batch_channel_shape + tuple(spatial_shape.tolist())
+        # decide on the device for aggregators
+        if self.device is None:
+            device = inputs.device
+        elif isinstance(self.device, str):
+            if self.device.lower() == "input":
+                device = inputs.device
+            elif self.device.lower() == "output":
+                device = out_patch.device
+            else:
+                device = self.device
+        else:
+            device = self.device
+        # initialize values and counts tensors for aggregation
+        self.values = torch.zeros(output_shape, dtype=self.dtype, device=device)
+        self.counts = torch.zeros(output_shape, dtype=torch.uint8, device=device)
+
+    def aggregate(self, values: torch.Tensor):
+        """
+        Aggregate values for merging.
+
+        Args:
+            values: a tensor of shape BxCxHxW[xD], representing the values of inference output.
+
+        Raises:
+            NotImplementedError: When the subclass does not override this method.
+
+        """
+        map_slice = [slice(loc, loc + size) for loc, size in zip(values[PatchKeys.LOCATION], values[PatchKeys.SIZE])]
+        map_slice = ensure_tuple_size(map_slice, values.ndim, pad_val=slice(None), pad_from_start=True)
+        self.values[map_slice] += values
+        self.counts[map_slice] += 1
+
+    def finalize(self):
+        """
+        Finalize the merging by dividing values by counts.
+        Note that after finalize, `values` tensor represents the average and not the aggregated values.
+        This avoid creating a new tensor for the final results and save memory.
+
+        Returns:
+            torch.tensor: a tensor of merged patches
+        """
+        return self.values.div_(self.counts)
+
+    def get_values(self):
+        return self.values
+
+    def get_counts(self):
+        return self.counts
