1a018a7b3034a86360d999a6bcc796bad330bba4
Yiheng Wang <68361391+yiheng-wang-nv@users.noreply.github.com>
2023-01-31 21:07:16 +0800

    5849 Add metrics reloaded handler (#5920)
    Fixes #5849 .
    
    ### Description
    
    This PR adds the missing handlers for `MetricsReloadedBinary` and
    `MetricsReloadedCategorical`, it also modifies wrong places of their doc
    strings.
    
    ### Types of changes
    <!--- Put an `x` in all the boxes that apply, and remove the not
    applicable items -->
    - [x] Non-breaking change (fix or new feature that would not break
    existing functionality).
    - [x] New tests added to cover the changes.
    - [ ] Integration tests passed locally by running `./runtests.sh -f -u
    --net --coverage`.
    - [ ] Quick tests passed locally by running `./runtests.sh --quick
    --unittests --disttests`.
    - [x] In-line docstrings updated.
    - [x] Documentation updated, tested `make html` command in the `docs/`
    folder.
    
    ---------
    
    Signed-off-by: Yiheng Wang <vennw@nvidia.com>

--- tests/min_tests.py
+++ tests/min_tests.py
@@ -67,6 +67,7 @@ def run_testsuit():
         "test_global_mutual_information_loss",
         "test_grid_patch",
         "test_gmm",
+        "test_handler_metrics_reloaded",
         "test_handler_checkpoint_loader",
         "test_handler_checkpoint_saver",
         "test_handler_classification_saver",
