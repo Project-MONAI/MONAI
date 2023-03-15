c96b66ab13119cd0af5a88b2939f3614aad5113f
Peter Hessey <a-phessey@microsoft.com>
2023-03-15 11:10:44 +0000

    Add hi-ml-azure to dev reqs
    Signed-off-by: Peter Hessey <a-phessey@microsoft.com>

--- monai/apps/auto3dseg/utils.py
+++ monai/apps/auto3dseg/utils.py
@@ -74,8 +74,7 @@ def export_bundle_algo_history(history: list[dict[str, BundleAlgo]]) -> None:
             algo_to_pickle(algo, template_path=algo.template_path)
 
 
-# can't give actual return type from health_azure because it's an optional import
-def submit_auto3dseg_module_to_azureml_if_needed(azure_cfg: dict[str, Any]) -> Any:
+def submit_auto3dseg_module_to_azureml_if_needed(azure_cfg: dict[str, Any]) -> health_azure.AzureRunInfo:
     """
     Submit Auto3dSeg modules to run as AzureML jobs if the user has requested it.
 
