5886a7575c2bd2acdc8b4d2d8339e975f2d4adb6
dongy <dongy@nvidia.com>
2023-03-20 03:42:42 -0700

    update
    Signed-off-by: dongy <dongy@nvidia.com>

--- monai/apps/nnunet/nnunetv2_runner.py
+++ monai/apps/nnunet/nnunetv2_runner.py
@@ -334,8 +334,28 @@ class nnUNetV2Runner:
         )
 
     def preprocess(
-        self, c=["2d", "3d_fullres", "3d_lowres"], np=[8, 8, 8], overwrite_plans_name="nnUNetPlans", verbose=False
+        self, c = list: ["2d", "3d_fullres", "3d_lowres"], np = list: [8, 8, 8], overwrite_plans_name = str: "nnUNetPlans", verbose = bool: False
     ):
+        """
+        Args:
+            overwrite_plans_name: [OPTIONAL] You can use this to specify a custom plans file that you may have generated
+            c: [OPTIONAL] Configurations for which the preprocessing should be run. Default: 2d 3f_fullres
+                3d_lowres. 3d_cascade_fullres does not need to be specified because it uses the data
+                from 3f_fullres. Configurations that do not exist for some dataset will be skipped)
+            np: [OPTIONAL] Use this to define how many processes are to be used. If this is just one number then
+                this number of processes is used for all configurations specified with -c. If it's a
+                list of numbers this list must have as many elements as there are configurations. We
+                then iterate over zip(configs, num_processes) to determine then umber of processes
+                used for each configuration. More processes is always faster (up to the number of
+                threads your PC can support, so 8 for a 4 core CPU with hyperthreading. If you don't
+                know what that is then dont touch it, or at least don't increase it!). DANGER: More
+                often than not the number of processes that can be used is limited by the amount of
+                RAM available. Image resampling takes up a lot of RAM. MONITOR RAM USAGE AND
+                DECREASE -np IF YOUR RAM FILLS UP TOO MUCH!. Default: 8 4 8 (=8 processes for 2d, 4
+                for 3d_fullres and 8 for 3d_lowres if -c is at its default)
+            verbose:Set this to print a lot of stuff. Useful for debugging. Will disable progrewss bar!
+                Recommended for cluster environments
+            """
         from nnunetv2.experiment_planning.plan_and_preprocess_api import preprocess
 
         logger.warning("Preprocessing...")
@@ -359,6 +379,58 @@ class nnUNetV2Runner:
         np=[8, 8, 8],
         verbose=False,
     ):
+    """
+    Args:
+        fpe: [OPTIONAL] Name of the Dataset Fingerprint Extractor class that should be used. Default is
+            'DatasetFingerprintExtractor'
+        npfp: [OPTIONAL] Number of processes used for fingerprint extraction. Default: 8
+        verify_dataset_integrity: [RECOMMENDED] set this flag to check the dataset integrity. This is useful and should be done once for
+            each dataset!
+        no_pp: [OPTIONAL] Set this to only run fingerprint extraction and experiment planning (no
+            preprocesing). Useful for debugging.
+        clean:[OPTIONAL] Set this flag to overwrite existing fingerprints. If this flag is not set and a
+            fingerprint already exists, the fingerprint extractor will not run. REQUIRED IF YOU
+            CHANGE THE DATASET FINGERPRINT EXTRACTOR OR MAKE CHANGES TO THE DATASET!
+        pl: [OPTIONAL] Name of the Experiment Planner class that should be used. Default is
+            ExperimentPlanner'. Note: There is no longer a distinction between 2d and 3d planner.
+            It's an all in one solution now. Wuch. Such amazing.
+        gpu_memory_target: [OPTIONAL] DANGER ZONE! Sets a custom GPU memory target. Default: 8 [GB]. Changing this will
+            affect patch and batch size and will
+            definitely affect your models performance! Only use this if you really know what you
+            are doing and NEVER use this without running the default nnU-Net first (as a baseline).
+        preprocessor_name: [OPTIONAL] DANGER ZONE! Sets a custom preprocessor class. This class must be located in
+            nnunetv2.preprocessing. Default: 'DefaultPreprocessor'. Changing this may affect your
+            models performance! Only use this if you really know what you
+            are doing and NEVER use this without running the default nnU-Net first (as a baseline).
+        overwrite_target_spacing: [OPTIONAL] DANGER ZONE! Sets a custom target spacing for the 3d_fullres and 3d_cascade_fullres
+            configurations. Default: None [no changes]. Changing this will affect image size and
+            potentially patch and batch
+            size. This will definitely affect your models performance! Only use this if you really
+            know what you are doing and NEVER use this without running the default nnU-Net first
+            (as a baseline). Changing the target spacing for the other configurations is currently
+            not implemented. New target spacing must be a list of three numbers!
+        overwrite_plans_name: [OPTIONAL] uSE A CUSTOM PLANS IDENTIFIER. If you used -gpu_memory_target,
+            -preprocessor_name or -overwrite_target_spacing it is best practice to use -overwrite_plans_name to generate a
+            differently named plans file such that the nnunet default plans are not
+            overwritten. You will then need to specify your custom plans file with -p whenever
+            running other nnunet commands (training, inference etc)
+        c: [OPTIONAL] Configurations for which the preprocessing should be run. Default: 2d 3f_fullres
+            3d_lowres. 3d_cascade_fullres does not need to be specified because it uses the data
+            from 3f_fullres. Configurations that do not exist for some dataset will be skipped.
+        np: [OPTIONAL] Use this to define how many processes are to be used. If this is just one number then
+            this number of processes is used for all configurations specified with -c. If it's a
+            list of numbers this list must have as many elements as there are configurations. We
+            then iterate over zip(configs, num_processes) to determine then umber of processes
+            used for each configuration. More processes is always faster (up to the number of
+            threads your PC can support, so 8 for a 4 core CPU with hyperthreading. If you don't
+            know what that is then dont touch it, or at least don't increase it!). DANGER: More
+            often than not the number of processes that can be used is limited by the amount of
+            RAM available. Image resampling takes up a lot of RAM. MONITOR RAM USAGE AND
+            DECREASE -np IF YOUR RAM FILLS UP TOO MUCH!. Default: 8 4 8 (=8 processes for 2d, 4
+            for 3d_fullres and 8 for 3d_lowres if -c is at its default)
+        verbose: Set this to print a lot of stuff. Useful for debugging. Will disable progrewss bar!
+            Recommended for cluster environments')
+    """
         self.extract_fingerprints(fpe, npfp, verify_dataset_integrity, clean, verbose)
         self.plan_experiments(pl, gpu_memory_target, preprocessor_name, overwrite_target_spacing, overwrite_plans_name)
         self.preprocess(c, np, overwrite_plans_name, verbose)
