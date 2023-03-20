d7f0fd0300590e1c87029ce317aed899086693b5
Wenqi Li <wenqil@nvidia.com>
2023-03-20 20:22:34 +0000

    mute import-error lint
    Signed-off-by: Wenqi Li <wenqil@nvidia.com>

--- monai/apps/nnunet/nnunetv2_runner.py
+++ monai/apps/nnunet/nnunetv2_runner.py
@@ -9,6 +9,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# pylint: disable=import-error
 from __future__ import annotations
 
 import glob
