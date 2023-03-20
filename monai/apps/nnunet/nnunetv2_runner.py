dd2c7de0f9579f0beb00e33b682631ed378791dc
dongy <dongy@nvidia.com>
2023-03-20 03:31:16 -0700

    update
    Signed-off-by: dongy <dongy@nvidia.com>

--- monai/apps/nnunet/nnunetv2_runner.py
+++ monai/apps/nnunet/nnunetv2_runner.py
@@ -295,13 +295,32 @@ class nnUNetV2Runner:
 
     def plan_experiments(
         self,
-        pl="ExperimentPlanner",
-        gpu_memory_target=8,
-        preprocessor_name="DefaultPreprocessor",
-        overwrite_target_spacing=None,
-        overwrite_plans_name="nnUNetPlans",
-        verbose=False,
+        pl: str = "ExperimentPlanner",
+        gpu_memory_target: float = 8,
+        preprocessor_name: str = "DefaultPreprocessor",
+        overwrite_target_spacing = None,
+        overwrite_plans_name: str = "nnUNetPlans",
+        verbose: bool = False,
     ):
+        """
+        Args:
+            pl: [OPTIONAL] Name of the Experiment Planner class that should be used. Default is 'ExperimentPlanner'.
+                Note: There is no longer a distinction between 2d and 3d planner. It's an all in one solution now.
+            gpu_memory_target:[OPTIONAL] DANGER ZONE! Sets a custom GPU memory target. Default: 8 [GB]. Changing this will
+                affect patch and batch size and will definitely affect your models performance! Only use this if you really
+                know what you are doing and NEVER use this without running the default nnU-Net first (as a baseline).
+            preprocessor_name:[OPTIONAL] DANGER ZONE! Sets a custom preprocessor class. This class must be located in
+                nnunetv2.preprocessing. Default: 'DefaultPreprocessor'. Changing this may affect your models performance!
+                Only use this if you really know what you are doing and NEVER use this without running the default nnU-Net first
+                (as a baseline).
+            overwrite_target_spacing':[OPTIONAL] DANGER ZONE! Sets a custom target spacing for the 3d_fullres and 3d_cascade_fullres
+                configurations. Default: None [no changes]. Changing this will affect image size and potentially patch and batch
+                size. This will definitely affect your models performance! Only use this if you really know what you are doing and
+                NEVER use this without running the default nnU-Net first (as a baseline). Changing the target spacing for the other configurations is currently not implemented. New target spacing must be a list of three numbers!
+            overwrite_plans_name':[OPTIONAL] DANGER ZONE! If you used -gpu_memory_target, -preprocessor_name or
+                -overwrite_target_spacing it is best practice to use -overwrite_plans_name to generate a differently named plans
+                file such that the nnunet default plans are not overwritten. You will then need to specify your custom plan
+        """
         from nnunetv2.experiment_planning.plan_and_preprocess_api import plan_experiments
 
         logger.warning("Experiment planning...")
