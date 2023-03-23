79424bd64a0841753bb31bcb4deea745dfb0ea6f
YanxuanLiu <yanxuanl@nvidia.com>
2023-03-23 09:56:27 +0800

    upgrade pytorch version
    Signed-off-by: YanxuanLiu <yanxuanl@nvidia.com>

--- tests/test_dynunet.py
+++ tests/test_dynunet.py
@@ -122,9 +122,9 @@ class TestDynUNet(unittest.TestCase):
         test_script_save(net, test_data)
 
 
-# @skip_if_no_cuda
-# @skip_if_windows
-@unittest.skip("temporary skip for 22.12")
+@skip_if_no_cuda
+@skip_if_windows
+# @unittest.skip("temporary skip for 22.12")
 class TestDynUNetWithInstanceNorm3dNVFuser(unittest.TestCase):
     @parameterized.expand([TEST_CASE_DYNUNET_3D[0]])
     def test_consistency(self, input_param, input_shape, _):
