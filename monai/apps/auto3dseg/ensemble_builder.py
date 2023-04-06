e4b313d59b2702cd2a441d7a557650ced78dc6c8
myron <myron@users.noreply.github.com>
2023-04-06 06:26:14 -0700

    Auto3DSeg continue training (skip trained algos) (#6310)
    Second PR for issue https://github.com/Project-MONAI/MONAI/issues/6291
    
    Since the previous PR https://github.com/Project-MONAI/MONAI/pull/6290
    was reverted https://github.com/Project-MONAI/MONAI/pull/6295
    
    Allows to skip the already trained algos, and continue training only for
    the non-trained ones.
    
    after this PR, the default option AutoRunner(train=None) will have this
    behavior, whereas manually setting AutoRunner(train=True/False) will
    always train all or skip all training. Previously we can only train all
    or skip all (without any option to resume)
    
    I changed  import_bundle_algo_history() to return a better algo_dict 
    
    previously it returned "list[dict(name: algo)]" - a list of dict, but
    each dict must have a single key name "name => algo". Not it returns a
    list of dicts, each with several keys dict(AlgoEnsembleKeys.ID: name,
    AlgoEnsembleKeys.ALGO, algo, "is_trained": bool, etc).
    this allows to put additional metadata inside of each algo_dict, and
    it's easier to read it back.
    
    previously, to get a name we had to use "name = history[0].keys()[0]",
    now it's more elegant "name = history[0][AlgoEnsembleKeys.ID]".
    
    this however required to change many files, everywhere where
    import_bundle_algo_history and export_bundle_algo_history was used.
    
    All the tests have passed, except for "integration GPU utilization
    tests" , but those errors seems unrelated
    
    
    
    After this PR, tutorials need to be updated too
    https://github.com/Project-MONAI/tutorials/pull/1288
    
    ---------
    
    Signed-off-by: myron <amyronenko@nvidia.com>

--- monai/apps/auto3dseg/ensemble_builder.py
+++ monai/apps/auto3dseg/ensemble_builder.py
@@ -267,7 +267,7 @@ class AlgoEnsembleBuilder:
 
     """
 
-    def __init__(self, history: Sequence[dict], data_src_cfg_filename: str | None = None):
+    def __init__(self, history: Sequence[dict[str, Any]], data_src_cfg_filename: str | None = None):
         self.infer_algos: list[dict[AlgoEnsembleKeys, Any]] = []
         self.ensemble: AlgoEnsemble
         self.data_src_cfg = ConfigParser(globals=False)
@@ -275,14 +275,12 @@ class AlgoEnsembleBuilder:
         if data_src_cfg_filename is not None and os.path.exists(str(data_src_cfg_filename)):
             self.data_src_cfg.read_config(data_src_cfg_filename)
 
-        for h in history:
+        for algo_dict in history:
             # load inference_config_paths
-            # raise warning/error if not found
-            if len(h) > 1:
-                raise ValueError(f"{h} should only contain one set of genAlgo key-value")
 
-            name = list(h.keys())[0]
-            gen_algo = h[name]
+            name = algo_dict[AlgoEnsembleKeys.ID]
+            gen_algo = algo_dict[AlgoEnsembleKeys.ALGO]
+
             best_metric = gen_algo.get_score()
             algo_path = gen_algo.output_path
             infer_path = os.path.join(algo_path, "scripts", "infer.py")
