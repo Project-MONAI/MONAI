af30d79e4e660c179330202ff865e9cf90f127ec
tangy5 <yucheng.tang@vanderbilt.edu>
2023-04-12 12:28:52 -0700

    add skip if downloading fails
    Signed-off-by: tangy5 <yucheng.tang@vanderbilt.edu>

--- tests/test_text_encoding.py
+++ tests/test_text_encoding.py
@@ -15,29 +15,33 @@ import unittest
 
 import torch
 from monai.networks.blocks.text_embedding import TextEncoder
+from tests.utils import skip_if_downloading_fails
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
 
 class TestTextEncoder(unittest.TestCase):
     def test_test_encoding_shape(self):
-        # test 2D encoder
-        text_encoder = TextEncoder(spatial_dims=2, out_channels=32, encoding="clip_encoding_univeral_model_32", pretrained=True).to(device)
-        text_encoding = text_encoder()
-        print(text_encoding.shape)
-        self.assertEqual(text_encoding.shape, (32,256,1,1))
+        with skip_if_downloading_fails():
+            # test 2D encoder
+            text_encoder = TextEncoder(spatial_dims=2, out_channels=32, encoding="clip_encoding_univeral_model_32", pretrained=True).to(device)
+            text_encoding = text_encoder()
+            self.assertEqual(text_encoding.shape, (32,256,1,1))
 
-        # test 3D encoder
-        text_encoder = TextEncoder(spatial_dims=3, out_channels=32, encoding="clip_encoding_univeral_model_32", pretrained=True).to(device)
-        text_encoding = text_encoder()
-        print(text_encoding.shape)
-        self.assertEqual(text_encoding.shape, (32,256,1,1,1))
+            # test 3D encoder
+            text_encoder = TextEncoder(spatial_dims=3, out_channels=32, encoding="clip_encoding_univeral_model_32", pretrained=True).to(device)
+            text_encoding = text_encoder()
+            self.assertEqual(text_encoding.shape, (32,256,1,1,1))
 
-        # test random enbedding 
+        # test random enbedding 3D
         text_encoder = TextEncoder(spatial_dims=3, out_channels=32, encoding="rand_embedding", pretrained=True).to(device)
         text_encoding = text_encoder()
-        print(text_encoding.shape)
         self.assertEqual(text_encoding.shape, (32,256,1,1,1))
+
+        # test random enbedding 2D
+        text_encoder = TextEncoder(spatial_dims=2, out_channels=32, encoding="rand_embedding", pretrained=True).to(device)
+        text_encoding = text_encoder()
+        self.assertEqual(text_encoding.shape, (32,256,1,1))
 
 if __name__ == "__main__":
     unittest.main()
