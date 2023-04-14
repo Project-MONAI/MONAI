c7e670aa7f43f51ac5ceba6fdb8acbee6bca5e04
Wenqi Li <wenqil@nvidia.com>
2023-04-14 19:31:38 +0100

    closes #6359
    Signed-off-by: Wenqi Li <wenqil@nvidia.com>

--- tests/test_retinanet.py
+++ tests/test_retinanet.py
@@ -20,7 +20,7 @@ from monai.apps.detection.networks.retinanet_network import RetinaNet, resnet_fp
 from monai.networks import eval_mode
 from monai.networks.nets import resnet10, resnet18, resnet34, resnet50, resnet101, resnet152, resnet200
 from monai.utils import ensure_tuple, optional_import
-from tests.utils import SkipIfBeforePyTorchVersion, test_onnx_save, test_script_save
+from tests.utils import SkipIfBeforePyTorchVersion, skip_if_quick, test_onnx_save, test_script_save
 
 _, has_torchvision = optional_import("torchvision")
 
@@ -99,6 +99,7 @@ for case in [TEST_CASE_1]:
 
 @SkipIfBeforePyTorchVersion((1, 12))
 @unittest.skipUnless(has_torchvision, "Requires torchvision")
+@skip_if_quick
 class TestRetinaNet(unittest.TestCase):
     @parameterized.expand(TEST_CASES)
     def test_retina_shape(self, model, input_param, input_shape):
