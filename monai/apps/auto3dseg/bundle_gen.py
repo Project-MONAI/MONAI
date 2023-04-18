0ac6169b55cd7b7fb2a37438bceebe1a0d933162
Mingxin Zheng <18563433+mingxin-zheng@users.noreply.github.com>
2023-04-18 13:45:37 +0000

    bug fix in templates
    Signed-off-by: Mingxin Zheng <18563433+mingxin-zheng@users.noreply.github.com>

--- monai/apps/auto3dseg/bundle_gen.py
+++ monai/apps/auto3dseg/bundle_gen.py
@@ -36,7 +36,7 @@ from monai.utils import ensure_tuple
 from monai.utils.enums import AlgoKeys
 
 logger = get_logger(module_name=__name__)
-ALGO_HASH = os.environ.get("MONAI_ALGO_HASH", "ed3f302")
+ALGO_HASH = os.environ.get("MONAI_ALGO_HASH", "629ec7e")
 
 __all__ = ["BundleAlgo", "BundleGen"]
 
