99ea3edc49557169b65c0592187e275d742d7b5a
Mingxin Zheng <18563433+mingxin-zheng@users.noreply.github.com>
2023-04-18 06:15:05 +0000

    Update ALGO_HAHS
    Signed-off-by: Mingxin Zheng <18563433+mingxin-zheng@users.noreply.github.com>

--- monai/apps/auto3dseg/bundle_gen.py
+++ monai/apps/auto3dseg/bundle_gen.py
@@ -36,7 +36,7 @@ from monai.utils import ensure_tuple
 from monai.utils.enums import AlgoKeys
 
 logger = get_logger(module_name=__name__)
-ALGO_HASH = os.environ.get("MONAI_ALGO_HASH", "80c832e")
+ALGO_HASH = os.environ.get("MONAI_ALGO_HASH", "ed3f302")
 
 __all__ = ["BundleAlgo", "BundleGen"]
 
