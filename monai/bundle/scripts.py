99d0cb1faa084367e38005c1b239cd7f966123c8
Nic Ma <nma@nvidia.com>
2023-04-19 18:47:52 +0800

    [DLMED] update according to comments
    Signed-off-by: Nic Ma <nma@nvidia.com>

--- monai/bundle/scripts.py
+++ monai/bundle/scripts.py
@@ -627,9 +627,9 @@ def run(
         run_id: ID name of the expected config expression to run, default to "run".
             to run the config, the target config must contain this ID.
         init_id: ID name of the expected config expression to initialize before running, default to "initialize".
-            allow a config to have no `initialize` logic and the ID.
+            it's optional for both configs and this `run` function.
         final_id: ID name of the expected config expression to finalize after running, default to "finalize".
-            allow a config to have no `finalize` logic and the ID.
+            it's optional for both configs and this `run` function.
         meta_file: filepath of the metadata file, if it is a list of file paths, the content of them will be merged.
             Default to "configs/metadata.json", which is commonly used for bundles in MONAI model zoo.
         config_file: filepath of the config file, if `None`, must be provided in `args_file`.
