3ab35630e2184f789fdabd1df51690289adfa568
myron <amyronenko@nvidia.com>
2023-04-21 00:00:21 -0700

    bcprun
    Signed-off-by: myron <amyronenko@nvidia.com>

--- monai/apps/auto3dseg/ensemble_builder.py
+++ monai/apps/auto3dseg/ensemble_builder.py
@@ -563,14 +563,22 @@ class EnsembleRunner:
             logger.info(f"Ensembling on {self.device_setting['NUM_NODES']} nodes!")
             cmd = "python " if cmd is None else cmd
             cmd = f"{cmd} -m {base_cmd}"
-            cmd_list=["bcprun", "-n", str(self.device_setting['NUM_NODES']), "-p", str(self.device_setting['n_devices']), "-c", cmd]
+            cmd_list = [
+                "bcprun",
+                "-n",
+                str(self.device_setting["NUM_NODES"]),
+                "-p",
+                str(self.device_setting["n_devices"]),
+                "-c",
+                cmd,
+            ]
 
         else:
             logger.info(f"Ensembling using {self.device_setting['n_devices']} GPU!")
             if cmd is None:
                 cmd = f"torchrun --nnodes={1:d} --nproc_per_node={self.device_setting['n_devices']:d} "
             cmd = f"{cmd} -m {base_cmd}"
-            cmd_list=cmd.split()
+            cmd_list = cmd.split()
 
         _ = subprocess.run(cmd_list, env=ps_environ, check=True)
         return
