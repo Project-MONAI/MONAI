cc12a672cfd6bc67ef4b9eff9068b2ab46a49e29
ge96lip <73938628+ge96lip@users.noreply.github.com>
2023-06-20 14:49:57 +0200

    Updated the constructor of ConvConcatDense
    the default value is now nn.Identity. this makes it possible to take out the None check in the forward method
    
    Signed-off-by: ge96lip <73938628+ge96lip@users.noreply.github.com>

--- monai/networks/blocks/denseblock.py
+++ monai/networks/blocks/denseblock.py
@@ -153,8 +153,8 @@ class ConvConcatDenseBlock(ConvDenseBlock):
     def __init__(
         self,
         in_channels: int,
-        se_layer: nn.Module | None = None,
-        dropout_layer: type[nn.Dropout2d] | None = None,
+        se_layer: nn.Module | None = nn.Identity,
+        dropout_layer: type[nn.Dropout2d] | None = nn.Identity,
         kernel_size: Sequence[int] | int = 5,
         num_filters: int = 64,
     ):
@@ -214,11 +214,9 @@ class ConvConcatDenseBlock(ConvDenseBlock):
                 result = torch.cat((result1, result, input), dim=1)
             i = i + 1
 
-        # if SELayer or Dropout layer defined put output through layer before returning
-        if self.se_layer is not None:
-            result = self.se_layer(result)
-        if self.dropout_layer is not None:
-            result = self.dropout_layer(result)
+        # if SELayer or Dropout layer defined put output through layer before returning, else it just goes through nn.Identity and the output does not change
+        result = self.se_layer(result)
+        result = self.dropout_layer(result)
 
         return result, None
 
