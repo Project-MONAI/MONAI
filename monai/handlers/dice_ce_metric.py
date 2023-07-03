324e53f2a1a829d755637ef96579636c5caebf36
Matthias Hadlich <matthiashadlich@posteo.de>
2023-07-03 19:52:46 +0200

    Add DiceCEMetric
    Signed-off-by: Matthias Hadlich <matthiashadlich@posteo.de>

---
+++ monai/handlers/dice_ce_metric.py
@@ -0,0 +1,64 @@
+# Copyright (c) MONAI Consortium
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#     http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from __future__ import annotations
+
+from collections.abc import Callable
+
+from monai.handlers.ignite_metric import IgniteMetric
+from monai.metrics import DiceMetric
+from monai.utils import MetricReduction
+from monai.losses import DiceCELoss
+from monai.metrics import LossMetric
+
+class DiceCEMetric(IgniteMetric):
+    """
+    Computes DiceCE score metric from full size Tensor and collects average over batch, class-channels, iterations.
+    """
+
+    def __init__(
+        self,
+        # include_background: bool = True,
+        reduction: MetricReduction | str = MetricReduction.MEAN,
+        # num_classes: int | None = None,
+        output_transform: Callable = lambda x: x,
+        save_details: bool = True,
+        *args,
+        **kwargs
+    ) -> None:
+        """
+
+        Args:
+            include_background: whether to include dice computation on the first channel of the predicted output.
+                Defaults to True.
+            reduction: define the mode to reduce metrics, will only execute reduction on `not-nan` values,
+                available reduction modes: {``"none"``, ``"mean"``, ``"sum"``, ``"mean_batch"``, ``"sum_batch"``,
+                ``"mean_channel"``, ``"sum_channel"``}, default to ``"mean"``. if "none", will not do reduction.
+            num_classes: number of input channels (always including the background). When this is None,
+                ``y_pred.shape[1]`` will be used. This option is useful when both ``y_pred`` and ``y`` are
+                single-channel class indices and the number of classes is not automatically inferred from data.
+            output_transform: callable to extract `y_pred` and `y` from `ignite.engine.state.output` then
+                construct `(y_pred, y)` pair, where `y_pred` and `y` can be `batch-first` Tensors or
+                lists of `channel-first` Tensors. the form of `(y_pred, y)` is required by the `update()`.
+                `engine.state` and `output_transform` inherit from the ignite concept:
+                https://pytorch.org/ignite/concepts.html#state, explanation and usage example are in the tutorial:
+                https://github.com/Project-MONAI/tutorials/blob/master/modules/batch_output_transform.ipynb.
+            save_details: whether to save metric computation details per image, for example: mean dice of every image.
+                default to True, will save to `engine.state.metric_details` dict with the metric name as key.
+            args: Arguments for the DiceCELoss
+
+        See also:
+            :py:meth:`monai.metrics.meandice.compute_dice`
+        """
+        # metric_fn = DiceMetric(include_background=include_background, reduction=reduction, num_classes=num_classes)
+        loss_function = DiceCELoss(*args, **kwargs)
+        metric_fn = LossMetric(loss_fn=loss_function, reduction=reduction, get_not_nans=False)
+        super().__init__(metric_fn=metric_fn, output_transform=output_transform, save_details=save_details)
