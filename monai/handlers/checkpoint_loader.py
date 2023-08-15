3dc111eb7228eefb782a89b0dc1d00e5bda233f3
Matthias Hadlich <matthiashadlich@posteo.de>
2023-08-15 13:47:50 +0200

    Move the example up
    Signed-off-by: Matthias Hadlich <matthiashadlich@posteo.de>

--- monai/handlers/checkpoint_loader.py
+++ monai/handlers/checkpoint_loader.py
@@ -36,6 +36,22 @@ class CheckpointLoader:
     If saving checkpoint after `torch.nn.DataParallel`, need to save `model.module` instead
     as PyTorch recommended and then use this loader to load the model.
 
+    Usage example::
+        trainer = SupervisedTrainer(...)
+        save_dict = {
+            "trainer": trainer,
+            "net": network,
+            "opt": optimizer,
+            "lr": lr_scheduler,
+        }
+
+        map_location = "cuda:0"
+        # checkpoint needs to have same save_dict for this to work
+        handler = CheckpointLoader(load_path="/test/checkpoint.pt", load_dict=save_dict, map_location=map_location, strict=True)
+        handler(trainer)
+        # Trainer now has the same state as stored, including the number of epochs and iterations completed
+        # so you can resume an interrupted training at the place where it left
+
     Args:
         load_path: the file path of checkpoint, it should be a PyTorch `pth` file.
         load_dict: target objects that load checkpoint to. examples::
@@ -66,20 +82,6 @@ class CheckpointLoader:
         #ignite.handlers.checkpoint.Checkpoint.load_objects.
         https://pytorch.org/docs/stable/generated/torch.nn.Module.html#torch.nn.Module.load_state_dict.
 
-    Example:: 
-        save_dict = {
-            "trainer": trainer,
-            "net": network,
-            "opt": optimizer,
-            "lr": lr_scheduler,
-        }
-
-        map_location = "cuda:0"
-        # checkpoint needs to have same save_dict for this to work
-        handler = CheckpointLoader(load_path="/test/checkpoint.pt", load_dict=save_dict, map_location=map_location, strict=True)
-        handler(trainer)
-        # Trainer now has the same state as stored, including the number of epochs and iterations completed
-        # so you can resume an interrupted training at the place where it left
     """
 
     def __init__(
