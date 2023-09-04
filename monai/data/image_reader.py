a28a423c526b5852fc69493f34b0cd61338ef7a7
KumoLiu <yunl@nvidia.com>
2023-09-04 13:48:09 +0800

    try `SegmentLabel` first
    Signed-off-by: KumoLiu <yunl@nvidia.com>

--- monai/data/image_reader.py
+++ monai/data/image_reader.py
@@ -766,7 +766,7 @@ class PydicomReader(ImageReader):
             all_segs = np.zeros([*spatial_shape, n_classes])
 
         for i, (frames, description) in enumerate(self._get_frame_data(img)):
-            class_name = description.SegmentDescription if hasattr(description, "SegmentDescription") else f"label_{i}"
+            class_name = description.SegmentLabel if hasattr(description, "SegmentLabel") else description.SegmentDescription
             if class_name not in metadata["labels"].keys():
                 metadata["labels"][class_name] = i
             class_num = metadata["labels"][class_name]
