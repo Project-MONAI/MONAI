8e77e03105c745936d4ffd98ed536f2bdc132b48
YunLiu <55491388+KumoLiu@users.noreply.github.com>
2023-09-05 19:52:20 +0800

    Fix reading dicom series error in `ITKReader` (#6943)
    Fixes #6942
    fixes #6944.
    
    ### Description
    When using itk backend reading dicom series and there is only one file
    in the folder, return the first name since itk assuming that there must
    be more than one file in the tuple.
    
    https://github.com/InsightSoftwareConsortium/ITK/blob/452cb3f9716e95532200de3196d51cd705a0ec5e/Wrapping/Generators/Python/itk/support/extras.py#L1275-L1284
    
    ### Types of changes
    <!--- Put an `x` in all the boxes that apply, and remove the not
    applicable items -->
    - [x] Non-breaking change (fix or new feature that would not break
    existing functionality).
    - [ ] Breaking change (fix or new feature that would cause existing
    functionality to change).
    - [ ] New tests added to cover the changes.
    - [ ] Integration tests passed locally by running `./runtests.sh -f -u
    --net --coverage`.
    - [ ] Quick tests passed locally by running `./runtests.sh --quick
    --unittests --disttests`.
    - [ ] In-line docstrings updated.
    - [ ] Documentation updated, tested `make html` command in the `docs/`
    folder.
    
    ---------
    
    Signed-off-by: KumoLiu <yunl@nvidia.com>

--- monai/data/image_reader.py
+++ monai/data/image_reader.py
@@ -244,6 +244,7 @@ class ITKReader(ImageReader):
                 series_identifier = series_uid[0] if not self.series_name else self.series_name
                 name = names_generator.GetFileNames(series_identifier)
 
+                name = name[0] if len(name) == 1 else name  # type: ignore
                 _obj = itk.imread(name, **kwargs_)
                 if self.series_meta:
                     _reader = itk.ImageSeriesReader.New(FileNames=name)
@@ -631,7 +632,7 @@ class PydicomReader(ImageReader):
 
         if self.prune_metadata:
             prune_metadata = {}
-            for key in ["00200037", "00200032", "52009229", "52009230"]:
+            for key in ["00200037", "00200032", "00280030", "52009229", "52009230"]:
                 if key in metadata.keys():
                     prune_metadata[key] = metadata[key]
             return prune_metadata
@@ -661,7 +662,9 @@ class PydicomReader(ImageReader):
         rx, ry, rz, cx, cy, cz = metadata["00200037"]["Value"]
         # "00200032" is the tag of `ImagePositionPatient`
         sx, sy, sz = metadata["00200032"]["Value"]
-        dr, dc = metadata.get("spacing", (1.0, 1.0))[:2]
+        # "00280030" is the tag of `PixelSpacing`
+        spacing = metadata["00280030"]["Value"]
+        dr, dc = metadata.get("spacing", spacing)[:2]
         affine[0, 0] = cx * dr
         affine[0, 1] = rx * dc
         affine[0, 3] = sx
@@ -670,7 +673,7 @@ class PydicomReader(ImageReader):
         affine[1, 3] = sy
         affine[2, 0] = cz * dr
         affine[2, 1] = rz * dc
-        affine[2, 2] = 0
+        affine[2, 2] = 1.0
         affine[2, 3] = sz
 
         # 3d
