d14ab24c083b221dec2e9468f3242b009c827ee1
KumoLiu <yunl@nvidia.com>
2023-09-07 00:47:38 +0800

    fix flake8
    Signed-off-by: KumoLiu <yunl@nvidia.com>

--- tests/ngc_bundle_download.py
+++ tests/ngc_bundle_download.py
@@ -83,7 +83,12 @@ class TestNgcBundleDownload(unittest.TestCase):
                 self.assertTrue(check_hash(filepath=full_file_path, val=hash_val))
 
                 model = load(
-                    name=bundle_name, source="ngc", version=version, bundle_dir=tempdir, remove_prefix=remove_prefix, return_state_dict=False
+                    name=bundle_name,
+                    source="ngc",
+                    version=version,
+                    bundle_dir=tempdir,
+                    remove_prefix=remove_prefix,
+                    return_state_dict=False,
                 )
                 assert_allclose(
                     model.state_dict()[TESTCASE_WEIGHTS["key"]],
