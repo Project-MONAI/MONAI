5eefa2c5b66beb29f6cf9d44689af42f13c0e22f
KumoLiu <yunl@nvidia.com>
2023-09-11 18:29:42 +0800

    update unittests
    Signed-off-by: KumoLiu <yunl@nvidia.com>

--- tests/test_bundle_ckpt_export.py
+++ tests/test_bundle_ckpt_export.py
@@ -41,37 +41,37 @@ class TestCKPTExport(unittest.TestCase):
         else:
             del os.environ["CUDA_VISIBLE_DEVICES"]  # previously unset
 
-    # @parameterized.expand([TEST_CASE_1, TEST_CASE_2, TEST_CASE_3])
-    # def test_export(self, key_in_ckpt, use_trace):
-    #     meta_file = os.path.join(os.path.dirname(__file__), "testing_data", "metadata.json")
-    #     config_file = os.path.join(os.path.dirname(__file__), "testing_data", "inference.json")
-    #     with tempfile.TemporaryDirectory() as tempdir:
-    #         def_args = {"meta_file": "will be replaced by `meta_file` arg"}
-    #         def_args_file = os.path.join(tempdir, "def_args.yaml")
+    @parameterized.expand([TEST_CASE_1, TEST_CASE_2, TEST_CASE_3])
+    def test_export(self, key_in_ckpt, use_trace):
+        meta_file = os.path.join(os.path.dirname(__file__), "testing_data", "metadata.json")
+        config_file = os.path.join(os.path.dirname(__file__), "testing_data", "inference.json")
+        with tempfile.TemporaryDirectory() as tempdir:
+            def_args = {"meta_file": "will be replaced by `meta_file` arg"}
+            def_args_file = os.path.join(tempdir, "def_args.yaml")
 
-    #         ckpt_file = os.path.join(tempdir, "model.pt")
-    #         ts_file = os.path.join(tempdir, "model.ts")
+            ckpt_file = os.path.join(tempdir, "model.pt")
+            ts_file = os.path.join(tempdir, "model.ts")
 
-    #         parser = ConfigParser()
-    #         parser.export_config_file(config=def_args, filepath=def_args_file)
-    #         parser.read_config(config_file)
-    #         net = parser.get_parsed_content("network_def")
-    #         save_state(src=net if key_in_ckpt == "" else {key_in_ckpt: net}, path=ckpt_file)
+            parser = ConfigParser()
+            parser.export_config_file(config=def_args, filepath=def_args_file)
+            parser.read_config(config_file)
+            net = parser.get_parsed_content("network_def")
+            save_state(src=net if key_in_ckpt == "" else {key_in_ckpt: net}, path=ckpt_file)
 
-    #         cmd = ["coverage", "run", "-m", "monai.bundle", "ckpt_export", "network_def", "--filepath", ts_file]
-    #         cmd += ["--meta_file", meta_file, "--config_file", f"['{config_file}','{def_args_file}']", "--ckpt_file"]
-    #         cmd += [ckpt_file, "--key_in_ckpt", key_in_ckpt, "--args_file", def_args_file]
-    #         if use_trace == "True":
-    #             cmd += ["--use_trace", use_trace, "--input_shape", "[1, 1, 96, 96, 96]"]
-    #         command_line_tests(cmd)
-    #         self.assertTrue(os.path.exists(ts_file))
+            cmd = ["coverage", "run", "-m", "monai.bundle", "ckpt_export", "network_def", "--filepath", ts_file]
+            cmd += ["--meta_file", meta_file, "--config_file", f"['{config_file}','{def_args_file}']", "--ckpt_file"]
+            cmd += [ckpt_file, "--key_in_ckpt", key_in_ckpt, "--args_file", def_args_file]
+            if use_trace == "True":
+                cmd += ["--use_trace", use_trace, "--input_shape", "[1, 1, 96, 96, 96]"]
+            command_line_tests(cmd)
+            self.assertTrue(os.path.exists(ts_file))
 
-    #         _, metadata, extra_files = load_net_with_metadata(
-    #             ts_file, more_extra_files=["inference.json", "def_args.json"]
-    #         )
-    #         self.assertTrue("schema" in metadata)
-    #         self.assertTrue("meta_file" in json.loads(extra_files["def_args.json"]))
-    #         self.assertTrue("network_def" in json.loads(extra_files["inference.json"]))
+            _, metadata, extra_files = load_net_with_metadata(
+                ts_file, more_extra_files=["inference.json", "def_args.json"]
+            )
+            self.assertTrue("schema" in metadata)
+            self.assertTrue("meta_file" in json.loads(extra_files["def_args.json"]))
+            self.assertTrue("network_def" in json.loads(extra_files["inference.json"]))
 
     @parameterized.expand([TEST_CASE_1, TEST_CASE_2, TEST_CASE_3])
     def test_default_value(self, key_in_ckpt, use_trace):
