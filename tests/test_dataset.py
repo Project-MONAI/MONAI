d4dc055ef2ac321c0e468e82f8a73d637f4463e5
YunLiu <55491388+KumoLiu@users.noreply.github.com>
2023-09-13 16:45:15 +0800

    Fix `Lazy=True` ignored when using `Dataset` call  (#6975)
    Fixes #6974.
    
    ### Description
    Change default value of `lazy` in `apply_transform` to None.
    
    ### Types of changes
    <!--- Put an `x` in all the boxes that apply, and remove the not
    applicable items -->
    - [x] Non-breaking change (fix or new feature that would not break
    existing functionality).
    - [ ] Breaking change (fix or new feature that would cause existing
    functionality to change).
    - [ ] New tests added to cover the changes.
    - [ ] Integration tests passed locally by running `./runtests.sh -f -u
    --net --coverage`.
    - [ ] Quick tests passed locally by running `./runtests.sh --quick
    --unittests --disttests`.
    - [ ] In-line docstrings updated.
    - [ ] Documentation updated, tested `make html` command in the `docs/`
    folder.
    
    ---------
    
    Signed-off-by: KumoLiu <yunl@nvidia.com>

--- tests/test_dataset.py
+++ tests/test_dataset.py
@@ -11,9 +11,12 @@
 
 from __future__ import annotations
 
+import logging
 import os
 import tempfile
 import unittest
+from copy import deepcopy
+from io import StringIO
 
 import nibabel as nib
 import numpy as np
@@ -21,6 +24,7 @@ from parameterized import parameterized
 
 from monai.data import Dataset
 from monai.transforms import Compose, LoadImaged, SimulateDelayd
+from tests.test_compose import TEST_COMPOSE_LAZY_ON_CALL_LOGGING_TEST_CASES, data_from_keys
 
 TEST_CASE_1 = [(128, 128, 128)]
 
@@ -88,6 +92,39 @@ class TestDataset(unittest.TestCase):
             self.assertEqual(len(data4_list), 1)
             for d in data4_list:
                 self.assertTupleEqual(d["image"].shape, expected_shape)
+
+    def test_dataset_lazy_on_call(self):
+        data = np.zeros((1, 5, 5))
+        data[0, 0:2, 0:2] = 1
+
+
+class TestDatsesetWithLazy(unittest.TestCase):
+    LOGGER_NAME = "a_logger_name"
+
+    def init_logger(self, name=LOGGER_NAME):
+        stream = StringIO()
+        handler = logging.StreamHandler(stream)
+        formatter = logging.Formatter("%(levelname)s - %(message)s")
+        handler.setFormatter(formatter)
+        logger = logging.getLogger(name)
+        logger.setLevel(logging.INFO)
+        while len(logger.handlers) > 0:
+            logger.removeHandler(logger.handlers[-1])
+        logger.addHandler(handler)
+        return handler, stream
+
+    @parameterized.expand(TEST_COMPOSE_LAZY_ON_CALL_LOGGING_TEST_CASES)
+    def test_dataset_lazy_with_logging(self, compose_type, pipeline, lazy, expected):
+        handler, stream = self.init_logger(name=self.LOGGER_NAME)
+
+        data = data_from_keys(None, 12, 16)
+        c = compose_type(deepcopy(pipeline), log_stats=self.LOGGER_NAME, lazy=lazy)
+        ds = Dataset([data], transform=c)
+        ds[0]
+
+        handler.flush()
+        actual = stream.getvalue()
+        self.assertEqual(actual, expected)
 
 
 if __name__ == "__main__":
