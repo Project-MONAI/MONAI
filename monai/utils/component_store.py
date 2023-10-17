1d95d92d695053fffc2b2bb6222745cea1610fc0
Mark Graham <markgraham539@gmail.com>
2023-10-17 11:34:08 +0100

    Updates component store to make compliant with mypy

--- monai/utils/component_store.py
+++ monai/utils/component_store.py
@@ -49,11 +49,10 @@ class ComponentStore:
         result = func(7, 6)
 
     """
-
-    _Component = namedtuple("Component", ("description", "value"))  # internal value pair
+    _Component = namedtuple("_Component", ("description", "value"))  # internal value pair
 
     def __init__(self, name: str, description: str) -> None:
-        self.components: dict[str, self._Component] = {}
+        self.components: dict[str, ComponentStore._Component] = {}
         self.name: str = name
         self.description: str = description
 
