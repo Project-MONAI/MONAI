bbc1f7848eb989f85c1431a315b7ae43150a2dd7
KumoLiu <yunl@nvidia.com>
2023-10-26 18:01:04 +0800

    backwards compatibility
    Signed-off-by: KumoLiu <yunl@nvidia.com>

--- monai/data/utils.py
+++ monai/data/utils.py
@@ -28,7 +28,7 @@ from typing import Any
 
 import numpy as np
 import torch
-from torch.utils.data._utils.collate import collate_tensor_fn, default_collate, default_collate_fn_map
+from torch.utils.data._utils.collate import default_collate
 
 from monai import config
 from monai.config.type_definitions import NdarrayOrTensor, NdarrayTensor, PathLike
@@ -50,8 +50,11 @@ from monai.utils import (
     issequenceiterable,
     look_up_option,
     optional_import,
+    pytorch_after,
 )
 
+if pytorch_after(1, 13):
+    from torch.utils.data._utils.collate import collate_tensor_fn, default_collate_fn_map
 pd, _ = optional_import("pandas")
 DataFrame, _ = optional_import("pandas", name="DataFrame")
 nib, _ = optional_import("nibabel")
@@ -444,10 +447,11 @@ def pickle_operations(data, key=PICKLE_KEY_SUFFIX, is_encode: bool = True):
     return data
 
 
-def collate_meta_tensor(batch, *, collate_fn_map=None):
+def collate_meta_tensor_fn(batch, *, collate_fn_map=None):
     """collate a sequence of meta tensor sequences/dictionaries into
     a single batched metatensor or a dictionary of batched metatensor"""
-    collated = collate_tensor_fn(batch)
+    collate_fn = collate_tensor_fn if pytorch_after(1, 13) else default_collate
+    collated = collate_fn(batch)
     meta_dicts = [i.meta or TraceKeys.NONE for i in batch]
     common_ = set.intersection(*[set(d.keys()) for d in meta_dicts if isinstance(d, dict)])
     if common_:
@@ -456,6 +460,23 @@ def collate_meta_tensor(batch, *, collate_fn_map=None):
     collated.applied_operations = [i.applied_operations or TraceKeys.NONE for i in batch]
     collated.is_batch = True
     return collated
+
+
+def collate_meta_tensor(batch):
+    """collate a sequence of meta tensor sequences/dictionaries into
+    a single batched metatensor or a dictionary of batched metatensor"""
+    if not isinstance(batch, Sequence):
+        raise NotImplementedError()
+    elem_0 = first(batch)
+    if isinstance(elem_0, MetaObj):
+        return collate_meta_tensor_fn(batch)
+    if isinstance(elem_0, Mapping):
+        return {k: collate_meta_tensor([d[k] for d in batch]) for k in elem_0}
+    if isinstance(elem_0, (tuple, list)):
+        return [collate_meta_tensor([d[i] for d in batch]) for i in range(len(elem_0))]
+
+    # no more recursive search for MetaTensor
+    return default_collate(batch)
 
 
 def list_data_collate(batch: Sequence):
@@ -468,12 +489,15 @@ def list_data_collate(batch: Sequence):
         Need to use this collate if apply some transforms that can generate batch data.
 
     """
-    from monai.data.meta_tensor import MetaTensor
 
-    default_collate_fn_map.update({MetaTensor: collate_meta_tensor})
+    if pytorch_after(1, 13):
+        from monai.data.meta_tensor import MetaTensor
+
+        default_collate_fn_map.update({MetaTensor: collate_meta_tensor_fn})
     elem = batch[0]
     data = [i for k in batch for i in k] if isinstance(elem, list) else batch
     key = None
+    collate_fn = default_collate if pytorch_after(1, 13) else collate_meta_tensor
     try:
         if config.USE_META_DICT:
             data = pickle_operations(data)  # bc 0.9.0
@@ -482,9 +506,9 @@ def list_data_collate(batch: Sequence):
             for k in elem:
                 key = k
                 data_for_batch = [d[key] for d in data]
-                ret[key] = default_collate(data_for_batch)
+                ret[key] = collate_fn(data_for_batch)
         else:
-            ret = default_collate(data)
+            ret = collate_fn(data)
         return ret
     except RuntimeError as re:
         re_str = str(re)
