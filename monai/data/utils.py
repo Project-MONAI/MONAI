46ef51821baf8166d9cdc9942378be940581b291
KumoLiu <yunl@nvidia.com>
2023-10-26 22:21:38 +0800

    minor fix
    Signed-off-by: KumoLiu <yunl@nvidia.com>

--- monai/data/utils.py
+++ monai/data/utils.py
@@ -54,7 +54,7 @@ from monai.utils import (
 )
 
 if pytorch_after(1, 13):
-    # import private code for reuse purposes
+    # import private code for reuse purposes, comment in case things break in the future
     from torch.utils.data._utils.collate import collate_tensor_fn, default_collate_fn_map
 pd, _ = optional_import("pandas")
 DataFrame, _ = optional_import("pandas", name="DataFrame")
