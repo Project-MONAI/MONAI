107e00ac1f25e1974aa4d9079beb5be61713d5b3
Mark Graham <markgraham539@gmail.com>
2023-12-08 15:45:33 +0000

    Formatting
    Signed-off-by: Mark Graham <markgraham539@gmail.com>

--- tests/test_transformer.py
+++ tests/test_transformer.py
@@ -25,34 +25,31 @@ for dropout_rate in np.linspace(0, 1, 2):
     for attention_layer_dim in [360, 480, 600, 768]:
         for num_heads in [4, 6, 8, 12]:
             TEST_CASES.append(
-                [{   "num_tokens": 10,
-                    "max_seq_len": 16,
-                    "attn_layers_dim": attention_layer_dim,
-                    "attn_layers_depth": 2,
-                    "attn_layers_heads": num_heads,
-                    "embedding_dropout_rate": dropout_rate,
-
-                    }]
+                [
+                    {
+                        "num_tokens": 10,
+                        "max_seq_len": 16,
+                        "attn_layers_dim": attention_layer_dim,
+                        "attn_layers_depth": 2,
+                        "attn_layers_heads": num_heads,
+                        "embedding_dropout_rate": dropout_rate,
+                    }
+                ]
             )
+
 
 class TestDecoderOnlyTransformer(unittest.TestCase):
     @parameterized.expand(TEST_CASES)
-
     def test_unconditioned_models(self, input_param):
-        net = DecoderOnlyTransformer(
-            **input_param
-        )
+        net = DecoderOnlyTransformer(**input_param)
         with eval_mode(net):
             net.forward(torch.randint(0, 10, (1, 16)))
-    @parameterized.expand(TEST_CASES)
 
+    @parameterized.expand(TEST_CASES)
     def test_conditioned_models(self, input_param):
-        net = DecoderOnlyTransformer(
-            **input_param, with_cross_attention=True
-        )
+        net = DecoderOnlyTransformer(**input_param, with_cross_attention=True)
         with eval_mode(net):
             net.forward(torch.randint(0, 10, (1, 16)), context=torch.randn(1, 3, input_param["attn_layers_dim"]))
-
 
     def test_attention_dim_not_multiple_of_heads(self):
         with self.assertRaises(ValueError):
@@ -63,7 +60,12 @@ class TestDecoderOnlyTransformer(unittest.TestCase):
     def test_dropout_rate_negative(self):
         with self.assertRaises(ValueError):
             DecoderOnlyTransformer(
-                num_tokens=10, max_seq_len=16, attn_layers_dim=8, attn_layers_depth=2, attn_layers_heads=2, embedding_dropout_rate=-1
+                num_tokens=10,
+                max_seq_len=16,
+                attn_layers_dim=8,
+                attn_layers_depth=2,
+                attn_layers_heads=2,
+                embedding_dropout_rate=-1,
             )
 
 
