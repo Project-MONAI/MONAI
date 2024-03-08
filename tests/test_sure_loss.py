e5b9d13fab98150abea7e248aaa7dedb4b1f7448
cxlcl <chaoliucxl@gmail.com>
2024-03-07 18:59:43 -0800

    Update tests/test_sure_loss.py
    Co-authored-by: YunLiu <55491388+KumoLiu@users.noreply.github.com>
    Signed-off-by: cxlcl <chaoliucxl@gmail.com>

--- tests/test_sure_loss.py
+++ tests/test_sure_loss.py
@@ -66,7 +66,6 @@ class TestSURELoss(unittest.TestCase):
         loss_real = sure_loss_real(operator, x_real, y_pseudo_gt_real, complex_input=False)
         loss_complex = sure_loss_complex(operator, x_complex, y_pseudo_gt_complex, complex_input=True)
         self.assertAlmostEqual(loss_real.item(), loss_complex.abs().item(), places=6)
-        print("complex general input test passed")
 
 
 if __name__ == "__main__":
