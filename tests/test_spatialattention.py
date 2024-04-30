a3ca76dab56147bf78ea1af38f820b3911c7252d
Mark Graham <markgraham539@gmail.com>
2024-04-30 16:07:49 +0100

    DCO Remediation Commit for Mark Graham <markgraham539@gmail.com>
    I, Mark Graham <markgraham539@gmail.com>, hereby add my Signed-off-by to this commit: a4088ece674aff81a4e1a22de3e8f47d4851bccf
    I, Mark Graham <markgraham539@gmail.com>, hereby add my Signed-off-by to this commit: 6d3b4a753ca633b221f022ca449528538654e5b3
    
    Signed-off-by: Mark Graham <markgraham539@gmail.com>

--- tests/test_spatialattention.py
+++ tests/test_spatialattention.py
@@ -31,8 +31,8 @@ TEST_CASES = [
     ],
     [
         {"spatial_dims": 3, "num_channels": 16, "num_head_channels": 8, "norm_num_groups": 8, "norm_eps": 1e-6},
-        (1, 16, 32, 32, 32),
-        (1, 16, 32, 32, 32),
+        (1, 16, 8, 8, 8),
+        (1, 16, 8, 8, 8),
     ],
 ]
 
