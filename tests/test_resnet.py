ae3def89b7f1590dfeab73523361ab329d2701b2
Peter Kaplinsky <peterkaplinsky@gmail.com>
2024-05-09 11:07:24 -0400

    add test case
    Signed-off-by: Peter Kaplinsky <peterkaplinsky@gmail.com>

--- tests/test_resnet.py
+++ tests/test_resnet.py
@@ -225,7 +225,7 @@ for case in [TEST_CASE_1, TEST_CASE_2, TEST_CASE_3, TEST_CASE_2_A, TEST_CASE_3_A
     for model in [resnet10, resnet18, resnet34, resnet50, resnet101, resnet152, resnet200]:
         TEST_CASES.append([model, *case])
         PRETRAINED_TEST_CASES.append([model, *case])
-for case in [TEST_CASE_5, TEST_CASE_5_A, TEST_CASE_6, TEST_CASE_7, TEST_CASE_8]:
+for case in [TEST_CASE_5, TEST_CASE_5_A, TEST_CASE_6, TEST_CASE_7, TEST_CASE_8, TEST_CASE_9]:
     TEST_CASES.append([ResNet, *case])
 
 TEST_SCRIPT_CASES = [
