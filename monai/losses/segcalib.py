93ee114a739bcc929c5a1c75afb3545ea2dc5410
Balamurali <balamuralim.1993@gmail.com>
2024-06-01 20:50:32 -0700

    Update segcalib.py
    Fixed the formatting and minor issues.

--- monai/losses/segcalib.py
+++ monai/losses/segcalib.py
@@ -9,116 +9,157 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torch.nn.modules.loss import _Loss
 import math
+import warnings
+from monai.utils import pytorch_after
+
 
 def get_gaussian_kernel_2d(ksize=3, sigma=1):
     x_grid = torch.arange(ksize).repeat(ksize).view(ksize, ksize)
     y_grid = x_grid.t()
     xy_grid = torch.stack([x_grid, y_grid], dim=-1).float()
-    mean = (ksize - 1)/2.
-    variance = sigma**2.
-    gaussian_kernel = (1./(2.*math.pi*variance + 1e-16)) * torch.exp(
-        -torch.sum((xy_grid - mean)**2., dim=-1) / (2*variance + 1e-16)
-        )
+    mean = (ksize - 1) / 2.0
+    variance = sigma**2.0
+    gaussian_kernel = (1.0 / (2.0 * math.pi * variance + 1e-16)) * torch.exp(
+        -torch.sum((xy_grid - mean) ** 2.0, dim=-1) / (2 * variance + 1e-16)
+    )
     return gaussian_kernel / torch.sum(gaussian_kernel)
+
 
 class get_svls_filter_2d(torch.nn.Module):
     def __init__(self, ksize=3, sigma=1, channels=0):
         super(get_svls_filter_2d, self).__init__()
         gkernel = get_gaussian_kernel_2d(ksize=ksize, sigma=sigma)
-        neighbors_sum = (1 - gkernel[1,1]) + 1e-16
-        gkernel[int(ksize/2), int(ksize/2)] = neighbors_sum
+        neighbors_sum = (1 - gkernel[1, 1]) + 1e-16
+        gkernel[int(ksize / 2), int(ksize / 2)] = neighbors_sum
         self.svls_kernel = gkernel / neighbors_sum
         svls_kernel_2d = self.svls_kernel.view(1, 1, ksize, ksize)
         svls_kernel_2d = svls_kernel_2d.repeat(channels, 1, 1, 1)
-        padding = int(ksize/2)
-        self.svls_layer = torch.nn.Conv2d(in_channels=channels, out_channels=channels,
-                                    kernel_size=ksize, groups=channels,
-                                    bias=False, padding=padding, padding_mode='replicate')
+        padding = int(ksize / 2)
+        self.svls_layer = torch.nn.Conv2d(
+            in_channels=channels,
+            out_channels=channels,
+            kernel_size=ksize,
+            groups=channels,
+            bias=False,
+            padding=padding,
+            padding_mode="replicate",
+        )
         self.svls_layer.weight.data = svls_kernel_2d
         self.svls_layer.weight.requires_grad = False
+
     def forward(self, x):
         return self.svls_layer(x) / self.svls_kernel.sum()
 
+
 class NACLLoss(_Loss):
-    """Add marginal penalty to logits:
-        CE + alpha * max(0, max(l^n) - l^n - margin)
     """
-    def __init__(self,
-                 classes=None,
-                 kernel_size=3,
-                 kernel_ops='mean',
-                 distance_type='l1',
-                 is_softmax=False,
-                 alpha=0.1,
-                 ignore_index=-100,
-                 sigma=1,
-                 schedule=""):
+    Murugesan, Balamurali, et al.
+    "Trust your neighbours: Penalty-based constraints for model calibration."
+    International Conference on Medical Image Computing and Computer-Assisted Intervention, 2023.
+    https://arxiv.org/abs/2303.06268
+    """
+
+    def __init__(self, classes, kernel_size=3, kernel_ops="mean", distance_type="l1", alpha=0.1, sigma=1):
+        """
+        Args:
+            classes: number of classes
+            kernel_size: size of the spatial kernel
+            kenel_ops: type of kernel operation (mean/gaussian)
+            distance_type: l1/l2 distance between spatial kernel and predicted logits
+            alpha: weightage between cross entropy and logit constraint
+            sigma: sigma if the kernel type is gaussian
+        """
 
         super().__init__()
-        assert schedule in ("", "add", "multiply", "step")
 
+        if kernel_ops not in ["mean", "gaussian"]:
+            raise ValueError("Kernel ops must be either mean or gaussian")
+
+        if distance_type not in ["l1", "l2"]:
+            raise ValueError("Distance type must be either L1 or L2")
+
+        self.kernel_ops = kernel_ops
         self.distance_type = distance_type
-
         self.alpha = alpha
-        self.ignore_index = ignore_index
-
-        self.is_softmax = is_softmax
 
         self.nc = classes
         self.ks = kernel_size
-        self.kernel_ops = kernel_ops
         self.cross_entropy = nn.CrossEntropyLoss()
-        if kernel_ops == 'gaussian':
+
+        if kernel_ops == "gaussian":
             self.svls_layer = get_svls_filter_2d(ksize=kernel_size, sigma=sigma, channels=classes)
+
+        self.old_pt_ver = not pytorch_after(1, 10)
+
+    def ce(self, input: torch.Tensor, target: torch.Tensor) -> torch.Tensor:
+        """
+        Compute CrossEntropy loss for the input logits and target.
+        Will remove the channel dim according to PyTorch CrossEntropyLoss:
+        https://pytorch.org/docs/stable/generated/torch.nn.CrossEntropyLoss.html?#torch.nn.CrossEntropyLoss.
+
+        """
+        n_pred_ch, n_target_ch = input.shape[1], target.shape[1]
+        if n_pred_ch != n_target_ch and n_target_ch == 1:
+            target = torch.squeeze(target, dim=1)
+            target = target.long()
+        elif self.old_pt_ver:
+            warnings.warn(
+                f"Multichannel targets are not supported in this older Pytorch version {torch.__version__}. "
+                "Using argmax (as a workaround) to convert target to a single channel."
+            )
+            target = torch.argmax(target, dim=1)
+        elif not torch.is_floating_point(target):
+            target = target.to(dtype=input.dtype)
+
+        return self.cross_entropy(input, target)  # type: ignore[no-any-return]
 
     def get_constr_target(self, mask):
 
-        mask = mask.unsqueeze(1) ## unfold works for 4d.
+        mask = mask.unsqueeze(1)  ## unfold works for 4d.
 
         bs, _, h, w = mask.shape
-        unfold = torch.nn.Unfold(kernel_size=(self.ks, self.ks),padding=self.ks // 2)
+        unfold = torch.nn.Unfold(kernel_size=(self.ks, self.ks), padding=self.ks // 2)
 
         rmask = []
 
-        if self.kernel_ops == 'mean':
+        if self.kernel_ops == "mean":
             umask = unfold(mask.float())
 
             for ii in range(self.nc):
-                rmask.append(torch.sum(umask == ii,1)/self.ks**2)
+                rmask.append(torch.sum(umask == ii, 1) / self.ks**2)
 
-        if self.kernel_ops == 'gaussian':
+        if self.kernel_ops == "gaussian":
 
-            oh_labels = F.one_hot(mask[:,0].to(torch.int64), num_classes = self.nc).contiguous().permute(0,3,1,2).float()
+            oh_labels = (
+                F.one_hot(mask[:, 0].to(torch.int64), num_classes=self.nc).contiguous().permute(0, 3, 1, 2).float()
+            )
             rmask = self.svls_layer(oh_labels)
 
             return rmask
 
-        rmask = torch.stack(rmask,dim=1)
+        rmask = torch.stack(rmask, dim=1)
         rmask = rmask.reshape(bs, self.nc, h, w)
 
         return rmask
 
+    def forward(self, inputs, targets):
 
-    def forward(self, inputs, targets, imgs):
+        loss_ce = self.ce(inputs, targets)
 
-        loss_ce = self.cross_entropy(inputs, targets)
+        utargets = self.get_constr_target(targets)
 
-        utargets = self.get_constr_target(targets, imgs)
-
-        if self.is_softmax:
-            inputs = F.softmax(inputs, dim=1)
-
-        if self.distance_type == 'l1':
+        if self.distance_type == "l1":
             loss_conf = torch.abs(utargets - inputs).mean()
 
-        if self.distance_type == 'l2':
-            loss_conf = (torch.abs(utargets - inputs)**2).mean()
+        if self.distance_type == "l2":
+            loss_conf = (torch.abs(utargets - inputs) ** 2).mean()
 
         loss = loss_ce + self.alpha * loss_conf
 
-        return loss, loss_ce, loss_conf
+        return loss  # , loss_ce, loss_conf
