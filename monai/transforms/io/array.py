56d8df5756586782038d9d66e5b8f036310767e0
staydelight <kevin295643815697236@gmail.com>
2024-06-03 16:42:35 +0800

    Fixes #7557
    Fixes unseccessful ckecks. (if mapping_json_path is not None)
    
    Signed-off-by: staydelight <kevin295643815697236@gmail.com>

--- monai/transforms/io/array.py
+++ monai/transforms/io/array.py
@@ -440,7 +440,7 @@ class SaveImage(Transform):
         self.write_kwargs = {"verbose": print_log}
         self._data_index = 0
         self.savepath_in_metadict = savepath_in_metadict
-        if mapping_json_path:
+        if mapping_json_path is not None:
             self.mapping_json_path = Path(mapping_json_path)
             self.savepath_in_metadict = True
         else:
