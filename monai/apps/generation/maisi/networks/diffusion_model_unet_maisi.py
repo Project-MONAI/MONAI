473ef4be313a65fbf40c9e405b8b6c8c49fb9994
dongyang0122 <don.yang.mech@gmail.com>
2024-06-28 18:43:49 +0000

    refactor forward function
    Signed-off-by: dongyang0122 <don.yang.mech@gmail.com>

--- monai/apps/generation/maisi/networks/diffusion_model_unet_maisi.py
+++ monai/apps/generation/maisi/networks/diffusion_model_unet_maisi.py
@@ -311,6 +311,64 @@ class DiffusionModelUNetMaisi(nn.Module):
             ),
         )
 
+    def _get_time_and_class_embedding(self, x, timesteps, class_labels):
+        t_emb = get_timestep_embedding(timesteps, self.block_out_channels[0])
+
+        # timesteps does not contain any weights and will always return f32 tensors
+        # but time_embedding might actually be running in fp16. so we need to cast here.
+        # there might be better ways to encapsulate this.
+        t_emb = t_emb.to(dtype=x.dtype)
+        emb = self.time_embed(t_emb)
+
+        if self.num_class_embeds is not None:
+            if class_labels is None:
+                raise ValueError("class_labels should be provided when num_class_embeds > 0")
+            class_emb = self.class_embedding(class_labels)
+            class_emb = class_emb.to(dtype=x.dtype)
+            emb = emb + class_emb
+        return emb
+
+    def _get_input_embeddings(self, emb, top_index, bottom_index, spacing):
+        if self.input_top_region_index:
+            _emb = self.top_region_index_layer(top_index)
+            emb = torch.cat((emb, _emb), dim=1)
+        if self.input_bottom_region_index:
+            _emb = self.bottom_region_index_layer(bottom_index)
+            emb = torch.cat((emb, _emb), dim=1)
+        if self.input_spacing:
+            _emb = self.spacing_layer(spacing)
+            emb = torch.cat((emb, _emb), dim=1)
+        return emb
+
+    def _apply_down_blocks(self, h, emb, context, down_block_additional_residuals):
+        if context is not None and self.with_conditioning is False:
+            raise ValueError("model should have with_conditioning = True if context is provided")
+        down_block_res_samples: list[torch.Tensor] = [h]
+        for downsample_block in self.down_blocks:
+            h, res_samples = downsample_block(hidden_states=h, temb=emb, context=context)
+            for residual in res_samples:
+                down_block_res_samples.append(residual)
+
+        # Additional residual conections for Controlnets
+        if down_block_additional_residuals is not None:
+            new_down_block_res_samples: list[torch.Tensor] = []
+            for down_block_res_sample, down_block_additional_residual in zip(
+                down_block_res_samples, down_block_additional_residuals
+            ):
+                down_block_res_sample = down_block_res_sample + down_block_additional_residual
+                new_down_block_res_samples.append(down_block_res_sample)
+
+            down_block_res_samples = new_down_block_res_samples
+        return h, down_block_res_samples
+
+    def _apply_up_blocks(self, h, emb, context, down_block_res_samples):
+        for upsample_block in self.up_blocks:
+            res_samples = down_block_res_samples[-len(upsample_block.resnets) :]
+            down_block_res_samples = down_block_res_samples[: -len(upsample_block.resnets)]
+            h = upsample_block(hidden_states=h, res_hidden_states_list=res_samples, temb=emb, context=context)
+
+        return h
+
     def forward(
         self,
         x: torch.Tensor,
@@ -340,71 +398,18 @@ class DiffusionModelUNetMaisi(nn.Module):
         Returns:
             A tensor representing the output of the UNet model.
         """
-        # 1. time
-        t_emb = get_timestep_embedding(timesteps, self.block_out_channels[0])
 
-        # timesteps does not contain any weights and will always return f32 tensors
-        # but time_embedding might actually be running in fp16. so we need to cast here.
-        # there might be better ways to encapsulate this.
-        t_emb = t_emb.to(dtype=x.dtype)
-        emb = self.time_embed(t_emb)
-
-        # 2. class
-        if self.num_class_embeds is not None:
-            if class_labels is None:
-                raise ValueError("class_labels should be provided when num_class_embeds > 0")
-            class_emb = self.class_embedding(class_labels)
-            class_emb = class_emb.to(dtype=x.dtype)
-            emb = emb + class_emb
-
-        # 3. input
-        if self.input_top_region_index:
-            _emb = self.top_region_index_layer(top_region_index_tensor)
-            emb = torch.cat((emb, _emb), dim=1)
-        if self.input_bottom_region_index:
-            _emb = self.bottom_region_index_layer(bottom_region_index_tensor)
-            emb = torch.cat((emb, _emb), dim=1)
-        if self.input_spacing:
-            _emb = self.spacing_layer(spacing_tensor)
-            emb = torch.cat((emb, _emb), dim=1)
-
-        # 3. initial convolution
+        emb = self._get_time_and_class_embedding(x, timesteps, class_labels)
+        emb = self._get_input_embeddings(emb, top_region_index_tensor, bottom_region_index_tensor, spacing_tensor)
         h = self.conv_in(x)
-
-        # 4. down
-        if context is not None and self.with_conditioning is False:
-            raise ValueError("model should have with_conditioning = True if context is provided")
-        down_block_res_samples: list[torch.Tensor] = [h]
-        for downsample_block in self.down_blocks:
-            h, res_samples = downsample_block(hidden_states=h, temb=emb, context=context)
-            for residual in res_samples:
-                down_block_res_samples.append(residual)
-
-        # Additional residual conections for Controlnets
-        if down_block_additional_residuals is not None:
-            new_down_block_res_samples: list[torch.Tensor] = []
-            for down_block_res_sample, down_block_additional_residual in zip(
-                down_block_res_samples, down_block_additional_residuals
-            ):
-                down_block_res_sample = down_block_res_sample + down_block_additional_residual
-                new_down_block_res_samples.append(down_block_res_sample)
-
-            down_block_res_samples = new_down_block_res_samples
-
-        # 5. mid
-        h = self.middle_block(hidden_states=h, temb=emb, context=context)
+        h, _updated_down_block_res_samples = self._apply_down_blocks(h, emb, context, down_block_additional_residuals)
+        h = self.middle_block(h, emb, context)
 
         # Additional residual conections for Controlnets
         if mid_block_additional_residual is not None:
             h = h + mid_block_additional_residual
 
-        # 6. up
-        for upsample_block in self.up_blocks:
-            res_samples = down_block_res_samples[-len(upsample_block.resnets) :]
-            down_block_res_samples = down_block_res_samples[: -len(upsample_block.resnets)]
-            h = upsample_block(hidden_states=h, res_hidden_states_list=res_samples, temb=emb, context=context)
-
-        # 7. output block
+        h = self._apply_up_blocks(h, emb, context, _updated_down_block_res_samples)
         h = self.out(h)
         h_tensor: torch.Tensor = convert_to_tensor(h)
         return h_tensor
