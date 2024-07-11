e503d6acdc12e6db6f7fbbc2db1ec3e1bab656a8
Ben Murray <ben.murray@gmail.com>
2024-07-11 14:01:39 +0100

    Adding flip functionality for geometric_tensors
    Signed-off-by: Ben Murray <ben.murray@gmail.com>

--- monai/transforms/spatial/functional.py
+++ monai/transforms/spatial/functional.py
@@ -24,16 +24,18 @@ import torch
 import monai
 from monai.config import USE_COMPILED
 from monai.config.type_definitions import NdarrayOrTensor
-from monai.data.meta_obj import get_track_meta
+from monai.data.meta_obj import get_track_meta, MetaObj
 from monai.data.meta_tensor import MetaTensor
 from monai.data.utils import AFFINE_TOL, compute_shape_offset, to_affine_nd
 from monai.networks.layers import AffineTransform
 from monai.transforms.croppad.array import ResizeWithPadOrCrop
 from monai.transforms.intensity.array import GaussianSmooth
 from monai.transforms.inverse import TraceableTransform
+from monai.transforms.lazy.functional import apply_to_geometry
 from monai.transforms.utils import create_rotate, create_translate, resolves_modes, scale_affine
 from monai.transforms.utils_pytorch_numpy_unification import allclose
 from monai.utils import (
+    KindKeys,
     LazyAttr,
     TraceKeys,
     convert_to_dst_type,
@@ -229,7 +231,42 @@ def orientation(img, original_affine, spatial_ornt, lazy, transform_info) -> tor
     return out.copy_meta_from(meta_info) if isinstance(out, MetaTensor) else out  # type: ignore
 
 
-def flip(img, sp_axes, lazy, transform_info):
+# def flip(img, sp_axes, lazy, transform_info):
+#     """
+#     Functional implementation of flip.
+#     This function operates eagerly or lazily according to
+#     ``lazy`` (default ``False``).
+
+#     Args:
+#         img: data to be changed, assuming `img` is channel-first.
+#         sp_axes: spatial axes along which to flip over.
+#             If None, will flip over all of the axes of the input array.
+#             If axis is negative it counts from the last to the first axis.
+#             If axis is a tuple of ints, flipping is performed on all of the axes
+#             specified in the tuple.
+#         lazy: a flag that indicates whether the operation should be performed lazily or not
+#         transform_info: a dictionary with the relevant information pertaining to an applied transform.
+#     """
+#     sp_size = img.peek_pending_shape() if isinstance(img, MetaTensor) else img.shape[1:]
+#     sp_size = convert_to_numpy(sp_size, wrap_sequence=True).tolist()
+#     extra_info = {"axes": sp_axes}  # track the spatial axes
+#     axes = monai.transforms.utils.map_spatial_axes(img.ndim, sp_axes)  # use the axes with channel dim
+#     rank = img.peek_pending_rank() if isinstance(img, MetaTensor) else torch.tensor(3.0, dtype=torch.double)
+#     # axes include the channel dim
+#     xform = torch.eye(int(rank) + 1, dtype=torch.double)
+#     for axis in axes:
+#         sp = axis - 1
+#         xform[sp, sp], xform[sp, -1] = xform[sp, sp] * -1, sp_size[sp] - 1
+#     meta_info = TraceableTransform.track_transform_meta(
+#         img, sp_size=sp_size, affine=xform, extra_info=extra_info, transform_info=transform_info, lazy=lazy
+#     )
+#     out = _maybe_new_metatensor(img)
+#     if lazy:
+#         return out.copy_meta_from(meta_info) if isinstance(out, MetaTensor) else meta_info
+#     out = torch.flip(out, axes)
+#     return out.copy_meta_from(meta_info) if isinstance(out, MetaTensor) else out
+
+def flip_impl(img, sp_axes, lazy, transform_info):
     """
     Functional implementation of flip.
     This function operates eagerly or lazily according to
@@ -258,11 +295,37 @@ def flip(img, sp_axes, lazy, transform_info):
     meta_info = TraceableTransform.track_transform_meta(
         img, sp_size=sp_size, affine=xform, extra_info=extra_info, transform_info=transform_info, lazy=lazy
     )
+    return axes, meta_info
+
+
+def flip_raster(img, sp_axes, lazy, transform_info):
+    axes, meta_info = flip_impl(img, sp_axes, lazy, transform_info)
     out = _maybe_new_metatensor(img)
     if lazy:
         return out.copy_meta_from(meta_info) if isinstance(out, MetaTensor) else meta_info
     out = torch.flip(out, axes)
     return out.copy_meta_from(meta_info) if isinstance(out, MetaTensor) else out
+
+
+def flip_geom(img, sp_axes, lazy, transform_info):
+    _, meta_info = flip_impl(img, sp_axes, lazy, transform_info)
+    out = _maybe_new_metatensor(img)
+    if lazy:
+        return out.copy_meta_from(meta_info) if isinstance(out, MetaTensor) else meta_info
+    out = apply_to_geometry(out, meta_info)
+    return out.copy_meta_from(meta_info) if isinstance(out, MetaTensor) else out
+
+
+def flip(image, sp_axes, lazy, transform_info):
+    """
+    Flip the tensor / MetaTensor according to `sp_axes`.
+    """
+
+    if isinstance(image, MetaTensor):
+        if image.kind == KindKeys.RASTER:
+            return flip_raster(image, sp_axes, lazy, transform_info)
+        elif image.kind == KindKeys.GEOM:
+            return flip_geom(image, sp_axes, lazy, transform_info)
 
 
 def resize(
