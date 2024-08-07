de9eef0b580e9a7aad7059224dba5792af31f9b9
YunLiu <55491388+KumoLiu@users.noreply.github.com>
2024-08-07 11:45:56 +0800

    remove transpose in sablock
    Signed-off-by: YunLiu <55491388+KumoLiu@users.noreply.github.com>

--- monai/networks/blocks/selfattention.py
+++ monai/networks/blocks/selfattention.py
@@ -172,13 +172,13 @@ class SABlock(nn.Module):
 
         if self.use_flash_attention:
             x = F.scaled_dot_product_attention(
-                query=q.transpose(1, 2),
-                key=k.transpose(1, 2),
-                value=v.transpose(1, 2),
+                query=q,
+                key=k,
+                value=v,
                 scale=self.scale,
                 dropout_p=self.dropout_rate,
                 is_causal=self.causal,
-            ).transpose(1, 2)
+            )
         else:
             att_mat = torch.einsum("blxd,blyd->blxy", q, k) * self.scale
 
