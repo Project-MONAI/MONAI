e3c34909578a46488a96168a39bbec6f80779767
YunLiu <55491388+KumoLiu@users.noreply.github.com>
2024-08-09 15:05:20 +0800

    Revert "minor fix"
    This reverts commit f8689c88264d8d31f80455e9605a3f4218434abf.

--- monai/utils/module.py
+++ monai/utils/module.py
@@ -564,7 +564,7 @@ def version_leq(lhs: str, rhs: str) -> bool:
     """
 
     lhs, rhs = str(lhs), str(rhs)
-    pkging, has_ver = optional_import("packaging")
+    pkging, has_ver = optional_import("packaging.Version")
     if has_ver:
         try:
             return cast(bool, pkging.version.Version(lhs) <= pkging.version.Version(rhs))
@@ -591,7 +591,7 @@ def version_geq(lhs: str, rhs: str) -> bool:
 
     """
     lhs, rhs = str(lhs), str(rhs)
-    pkging, has_ver = optional_import("packaging")
+    pkging, has_ver = optional_import("packaging.Version")
     if has_ver:
         try:
             return cast(bool, pkging.version.Version(lhs) >= pkging.version.Version(rhs))
