58da97c32d6b9587e01b802cdfe14d62b2ad9c8a
YunLiu <55491388+KumoLiu@users.noreply.github.com>
2024-09-30 13:20:22 +0800

    fix #8120
    Signed-off-by: YunLiu <55491388+KumoLiu@users.noreply.github.com>

--- monai/config/deviceconfig.py
+++ monai/config/deviceconfig.py
@@ -23,6 +23,8 @@ import numpy as np
 import torch
 
 import monai
+from monai.utils.deprecate_utils import deprecated
+from monai.utils.enums import IgniteInfo as NewIgniteInfo
 from monai.utils.module import OptionalImportError, get_package_version, optional_import
 
 try:
@@ -258,6 +260,15 @@ def print_debug_info(file: TextIO = sys.stdout) -> None:
     print("Printing GPU config...")
     print("================================", file=file, flush=True)
     print_gpu_info(file)
+
+
+@deprecated(since=1.4, removed=1.6, msg_suffix="Please use `monai.utils.enums.IgniteInfo` instead.")
+class IgniteInfo:
+    """
+    IgniteInfo enum is moved to `monai.utils.enums.IgniteInfo`.
+    """
+
+    OPT_IMPORT_VERSION = NewIgniteInfo.OPT_IMPORT_VERSION
 
 
 if __name__ == "__main__":
