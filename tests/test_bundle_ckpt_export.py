55fcd220fa3e465d78e59c6de1c0334ed246f344
Rafael Dias <rafaelagd@gmail.com>
2024-11-23 13:37:26 +0000

    Revert "Style fix"
    This reverts commit bfc68a494ace8a367a61515d37e2d1c206032766.

--- tests/test_bundle_ckpt_export.py
+++ tests/test_bundle_ckpt_export.py
@@ -96,7 +96,7 @@ class TestCKPTExport(unittest.TestCase):
         self.assertTrue(os.path.exists(self.ts_file))
 
         _, metadata, extra_files = load_net_with_metadata(
-            self.ts_file, more_extra_files=["inference.json", "def_args.json"]
+                self.ts_file, more_extra_files=["inference.json", "def_args.json"]
         )
         self.assertIn("schema", metadata)
         self.assertIn("meta_file", json.loads(extra_files["def_args.json"]))
