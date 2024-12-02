185d2e142122ad0de2a131a3cb74831eb1db2238
Zifu Wang <zifuwang94@gmail.com>
2024-12-02 11:46:14 +0100

    DCO Remediation Commit for Zifu Wang <zifuwang94@gmail.com>
    I, Zifu Wang <zifuwang94@gmail.com>, hereby add my Signed-off-by to this commit: 3f74183c5776d4746229006acad5b52c8c9df6ec
    I, Zifu Wang <zifuwang94@gmail.com>, hereby add my Signed-off-by to this commit: a778e583d32082480811a4bec3bed9a3622b5871
    I, Zifu Wang <zifuwang94@gmail.com>, hereby add my Signed-off-by to this commit: aeef0afccd762f9b0f90b4b42ac12db8fb8b9031
    I, Zifu Wang <zifuwang94@gmail.com>, hereby add my Signed-off-by to this commit: 58c5396a341edfebaf109a24bfb6753bb2b7a171
    
    Signed-off-by: Zifu Wang <zifuwang94@gmail.com>

--- monai/losses/utils.py
+++ monai/losses/utils.py
@@ -24,6 +24,16 @@ def compute_tp_fp_fn(
     decoupled: bool = True,
 ) -> tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
     """
+    Args:
+        input: the shape should be BNH[WD], where N is the number of classes.
+        target: the shape should be BNH[WD] or B1H[WD], where N is the number of classes.
+        reduce_axis: the axis to be reduced.
+        ord: the order of the vector norm.
+        soft_label: whether the target contains non-binary values (soft labels) or not.
+            If True a soft label formulation of the loss will be used.
+        decoupled: whether the input and the target should be decoupled when computing fp and fn.
+            Only for the original implementation when soft_label is False.
+
     Adapted from:
         https://github.com/zifuwanggg/JDTLosses
     """
@@ -39,6 +49,8 @@ def compute_tp_fp_fn(
         else:
             fp = torch.sum(input * (1 - target), dim=reduce_axis)
             fn = torch.sum((1 - input) * target, dim=reduce_axis)
+    # the new implementation that is correct with soft labels
+    # and it is identical to the original implementation with hard labels
     else:
         pred_o = LA.vector_norm(input, ord=ord, dim=reduce_axis)
         ground_o = LA.vector_norm(target, ord=ord, dim=reduce_axis)
