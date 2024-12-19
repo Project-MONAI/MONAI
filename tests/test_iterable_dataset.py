e1e3d8ebc1c7247aad9f1bffc649c5a20084340f
Eric Kerfoot <17726042+ericspod@users.noreply.github.com>
2024-12-19 02:50:42 +0000

    Modify Workflow to Allow IterableDataset Inputs (#8263)
    ### Description
    
    This modifies the behaviour of `Workflow` to permit `IterableDataset` to
    be used correctly. A check against the `epoch_length` value is removed,
    to allow that value to be `None`, and a test is added to verify this.
    The length of a data loader is not defined when using iterable datasets,
    so try/raise is added to allow that to be queried safely. This is
    related to my work on the streaming support, in my [prototype
    gist](https://gist.github.com/ericspod/1904713716b45631260784ac3fcd6fb3)
    I had to provide a bogus epoch length value in the then change it to
    `None` later once the evaluator object was created. This PR will remove
    the need for this hack.
    
    ### Types of changes
    <!--- Put an `x` in all the boxes that apply, and remove the not
    applicable items -->
    - [x] Non-breaking change (fix or new feature that would not break
    existing functionality).
    - [ ] Breaking change (fix or new feature that would cause existing
    functionality to change).
    - [x] New tests added to cover the changes.
    - [ ] Integration tests passed locally by running `./runtests.sh -f -u
    --net --coverage`.
    - [ ] Quick tests passed locally by running `./runtests.sh --quick
    --unittests --disttests`.
    - [ ] In-line docstrings updated.
    - [ ] Documentation updated, tested `make html` command in the `docs/`
    folder.
    
    ---------
    
    Signed-off-by: Eric Kerfoot <eric.kerfoot@kcl.ac.uk>
    Signed-off-by: Eric Kerfoot <eric.kerfoot@gmail>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Eric Kerfoot <eric.kerfoot@gmail>

--- tests/test_iterable_dataset.py
+++ tests/test_iterable_dataset.py
@@ -18,8 +18,10 @@ import unittest
 
 import nibabel as nib
 import numpy as np
+import torch.nn as nn
 
 from monai.data import DataLoader, Dataset, IterableDataset
+from monai.engines import SupervisedEvaluator
 from monai.transforms import Compose, LoadImaged, SimulateDelayd
 
 
@@ -58,6 +60,17 @@ class TestIterableDataset(unittest.TestCase):
             dataloader = DataLoader(dataset=dataset, batch_size=3, num_workers=num_workers)
             for d in dataloader:
                 self.assertTupleEqual(d["image"].shape[1:], expected_shape)
+
+    def test_supervisedevaluator(self):
+        """
+        Test that a SupervisedEvaluator is compatible with IterableDataset in conjunction with DataLoader.
+        """
+        data = list(range(10))
+        dl = DataLoader(IterableDataset(data))
+        evaluator = SupervisedEvaluator(device="cpu", val_data_loader=dl, network=nn.Identity())
+        evaluator.run()  # fails if the epoch length or other internal setup is not done correctly
+
+        self.assertEqual(evaluator.state.iteration, len(data))
 
 
 if __name__ == "__main__":
