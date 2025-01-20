068688f50bb7e11a61f8411539cc368bdf1b6937
tisalon <tisalon@outlook.com>
2025-01-20 16:29:15 +0100

    remove relative imports

--- tests/test_CABlock.py
+++ tests/test_CABlock.py
@@ -11,14 +11,6 @@
 
 from __future__ import annotations
 
-import os
-import sys
-
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../')))
-
-
-import unittest
-
 
 import unittest
 from unittest import skipUnless
