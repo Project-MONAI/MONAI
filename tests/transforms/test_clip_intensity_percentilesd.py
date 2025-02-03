be546500f3c12265a6843ad1c5d7ba2f193796d6
R. Garcia-Dias <rafaelagd@gmail.com>
2025-02-03 10:22:21 +0000

    autofix

--- tests/transforms/test_clip_intensity_percentilesd.py
+++ tests/transforms/test_clip_intensity_percentilesd.py
@@ -18,8 +18,8 @@ from parameterized import parameterized
 from monai.transforms import ClipIntensityPercentilesd
 from monai.transforms.utils_pytorch_numpy_unification import clip, percentile
 from monai.utils.type_conversion import convert_to_tensor
-from tests.transforms.test_clip_intensity_percentiles import test_hard_clip_func, test_soft_clip_func
 from tests.test_utils import TEST_NDARRAYS, NumpyImageTestCase2D, NumpyImageTestCase3D, assert_allclose
+from tests.transforms.test_clip_intensity_percentiles import test_hard_clip_func, test_soft_clip_func
 
 
 class TestClipIntensityPercentilesd2D(NumpyImageTestCase2D):
