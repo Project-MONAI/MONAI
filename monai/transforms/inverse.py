b6dd4607f8bd2c8331a1d9059c08ccb5d7ef92bc
Wei_Chuan, Chiang <jamesis1356@gmail.com>
2025-02-18 01:21:06 +0800

    fix format
    Signed-off-by: Wei_Chuan, Chiang <jamesis1356@gmail.com>
    Co-authored-by: einsyang723 <tina910723@gmail.com>
    Co-authored-by: IamTingTing  <6121smile@gmail.com>

--- monai/transforms/inverse.py
+++ monai/transforms/inverse.py
@@ -198,7 +198,7 @@ class TraceableTransform(Transform):
                     raise
             out_obj.meta[MetaKeys.AFFINE] = convert_to_tensor(affine, device=torch.device("cpu"), dtype=torch.float64)
             spacing = affine_to_spacing(out_obj.meta[MetaKeys.AFFINE])
-            out_obj.meta[MetaKeys.PIXDIM][1:1+len(spacing)] = spacing
+            out_obj.meta[MetaKeys.PIXDIM][1 : 1 + len(spacing)] = spacing
 
         if not (get_track_meta() and transform_info and transform_info.get(TraceKeys.TRACING)):
             if isinstance(data, Mapping):
