83e3dd63aae534183b34d4ef2691eadcd521c8be
YunLiu <55491388+KumoLiu@users.noreply.github.com>
2025-02-18 18:45:14 +0800

    Update monai/inferers/inferer.py
    Signed-off-by: YunLiu <55491388+KumoLiu@users.noreply.github.com>

--- monai/inferers/inferer.py
+++ monai/inferers/inferer.py
@@ -1211,8 +1211,6 @@ class LatentDiffusionInferer(DiffusionInferer):
         decode = autoencoder_model.decode_stage_2_outputs
         if isinstance(autoencoder_model, SPADEAutoencoderKL):
             decode = partial(autoencoder_model.decode_stage_2_outputs, seg=seg)
-        print("Decoding latents...")
-        print(latent.shape)
         image = decode(latent / self.scale_factor)
         if save_intermediates:
             intermediates = []
