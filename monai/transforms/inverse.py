9e515763f360cebd85a9bec330a4c7ec09a53af9
Wei_Chuan, Chiang <jamesis1356@gmail.com>
2025-02-19 14:26:52 +0800

    fix constraint for pixdim in track_transform_meta() function
    Signed-off-by: Wei_Chuan, Chiang <jamesis1356@gmail.com>
    Co-authored-by: einsyang723 <tina910723@gmail.com>
    Co-authored-by: IamTingTing  <6121smile@gmail.com>

--- monai/transforms/inverse.py
+++ monai/transforms/inverse.py
@@ -197,8 +197,9 @@ class TraceableTransform(Transform):
                 else:
                     raise
             out_obj.meta[MetaKeys.AFFINE] = convert_to_tensor(affine, device=torch.device("cpu"), dtype=torch.float64)
-            spacing = affine_to_spacing(out_obj.meta[MetaKeys.AFFINE])
-            out_obj.meta[MetaKeys.PIXDIM][1 : 1 + len(spacing)] = spacing
+            if MetaKeys.PIXDIM in out_obj.meta:
+                spacing = affine_to_spacing(out_obj.meta[MetaKeys.AFFINE])
+                out_obj.meta[MetaKeys.PIXDIM][1 : 1 + len(spacing)] = spacing
 
         if not (get_track_meta() and transform_info and transform_info.get(TraceKeys.TRACING)):
             if isinstance(data, Mapping):
