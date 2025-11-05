Fix timesteps calculation in ddpm.py
Signed-off-by: ytl0623 <david89062388@gmail.com>

monai/networks/schedulers/ddpm.py
@@ -122,9 +122,7 @@ class DDPMScheduler(Scheduler):
             )
 
         self.num_inference_steps = num_inference_steps
-        # creates integer timesteps by multiplying by ratio
-        # casting to int to avoid issues when num_inference_step is power of 3
-        timesteps = np.linspace(self.num_train_timesteps - 1, 0, num_inference_steps).round().astype(np.int64)
+        timesteps = np.linspace(self.num_train_timesteps - 1, 0, self.num_inference_steps).round().astype(np.int64)
         self.timesteps = torch.from_numpy(timesteps).to(device)
 
     def _get_mean(self, timestep: int, x_0: torch.Tensor, x_t: torch.Tensor) -> torch.Tensor: