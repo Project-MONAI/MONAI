9b861a136ab9dd9c35ec4bc5bf4f6975ef8dafa6
Adrian Caderno <adriancaderno@gmail.com>
2025-12-31 07:58:38 +0100

    Fix docstring formatting for PerceptualLoss network_type parameter
    Signed-off-by: Adrian Caderno <adriancaderno@gmail.com>

--- monai/losses/perceptual.py
+++ monai/losses/perceptual.py
@@ -50,7 +50,7 @@ class PerceptualLoss(nn.Module):
     Args:
         spatial_dims: number of spatial dimensions.
         network_type: {``"alex"``, ``"vgg"``, ``"squeeze"``, ``"radimagenet_resnet50"``,
-        ``"medicalnet_resnet10_23datasets"``, ``"medicalnet_resnet50_23datasets"``, ``"resnet50"``}
+            ``"medicalnet_resnet10_23datasets"``, ``"medicalnet_resnet50_23datasets"``, ``"resnet50"``}
             Specifies the network architecture to use. Defaults to ``"alex"``.
         is_fake_3d: if True use 2.5D approach for a 3D perceptual loss.
         fake_3d_ratio: ratio of how many slices per axis are used in the 2.5D approach.
