69da7bf39f7ca79a52203d1673da9020299b5b00
ytl0623 <david89062388@gmail.com>
2026-01-16 22:56:20 +0800

    resolve template signature mismatch in pushpull header
    Signed-off-by: ytl0623 <david89062388@gmail.com>

--- monai/csrc/resample/pushpull.h
+++ monai/csrc/resample/pushpull.h
@@ -22,25 +22,25 @@ limitations under the License.
 
 #define MONAI_PUSHPULL_DECLARE(space)                                            \
   namespace space {                                                              \
-  template <typename BoundType, typename InterpolationType, typename SourceType> \
+  template <typename SourceType>                                                 \
   std::deque<at::Tensor> pushpull(                                               \
       const SourceType& source,                                                  \
       const at::Tensor& grid,                                                    \
-      BoundType bound,                                                           \
-      InterpolationType interpolation,                                           \
+      monai::BoundVectorRef bound,                                               \
+      monai::InterpolationVectorRef interpolation,                               \
       bool extrapolate,                                                          \
       bool do_pull,                                                              \
       bool do_push,                                                              \
       bool do_count,                                                             \
       bool do_grad,                                                              \
       bool do_sgrad);                                                            \
-  template <typename BoundType, typename InterpolationType, typename SourceType> \
+  template <typename SourceType>                                                 \
   std::deque<at::Tensor> pushpull(                                               \
       const SourceType& source,                                                  \
       const at::Tensor& grid,                                                    \
       const at::Tensor& target,                                                  \
-      BoundType bound,                                                           \
-      InterpolationType interpolation,                                           \
+      monai::BoundVectorRef bound,                                               \
+      monai::InterpolationVectorRef interpolation,                               \
       bool extrapolate,                                                          \
       bool do_pull,                                                              \
       bool do_push,                                                              \
