8b6c29425575b0deeeb9017d1bb437d37d533f19
ytl0623 <david89062388@gmail.com>
2026-01-19 10:02:51 +0800

    minor fixes
    Signed-off-by: ytl0623 <david89062388@gmail.com>

--- monai/losses/unified_focal_loss.py
+++ monai/losses/unified_focal_loss.py
@@ -44,7 +44,7 @@ class AsymmetricFocalTverskyLoss(_Loss):
         Args:
             to_onehot_y: whether to convert `y` into the one-hot format. Defaults to False.
             delta : weight of the background. Defaults to 0.7.
-            gamma : value of the exponent gamma in the definition of the Focal loss  . Defaults to 0.75.
+            gamma : value of the exponent gamma in the definition of the Focal loss. Defaults to 0.75.
             epsilon : it defines a very small number each time. simmily smooth value. Defaults to 1e-7.
         """
         super().__init__(reduction=LossReduction(reduction).value)
@@ -108,7 +108,7 @@ class AsymmetricFocalLoss(_Loss):
         Args:
             to_onehot_y : whether to convert `y` into the one-hot format. Defaults to False.
             delta : weight of the background. Defaults to 0.7.
-            gamma : value of the exponent gamma in the definition of the Focal loss  . Defaults to 2.
+            gamma : value of the exponent gamma in the definition of the Focal loss. Defaults to 2.
             epsilon : it defines a very small number each time. simmily smooth value. Defaults to 1e-7.
         """
         super().__init__(reduction=LossReduction(reduction).value)
@@ -158,9 +158,9 @@ class AsymmetricUnifiedFocalLoss(_Loss):
         self,
         to_onehot_y: bool = False,
         num_classes: int = 2,
-        weight: float = 0.5,
-        gamma: float = 0.5,
         delta: float = 0.7,
+        gamma: float = 0.5,
+        weight: float = 0.5,
         reduction: LossReduction | str = LossReduction.MEAN,
     ):
         """
