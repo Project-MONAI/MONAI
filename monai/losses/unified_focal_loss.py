64bc876e55d642217ec81873bd80468a1c874dd6
ytl0623 <david89062388@gmail.com>
2026-02-23 09:07:52 +0800

    reorder docstring args to match declaration
    Signed-off-by: ytl0623 <david89062388@gmail.com>

--- monai/losses/unified_focal_loss.py
+++ monai/losses/unified_focal_loss.py
@@ -45,7 +45,7 @@ class AsymmetricFocalTverskyLoss(_Loss):
             to_onehot_y: whether to convert `y` into the one-hot format. Defaults to False.
             delta : weight of the background. Defaults to 0.7.
             gamma : value of the exponent gamma in the definition of the Focal loss. Defaults to 0.75.
-            epsilon : it defines a very small number each time. simmily smooth value. Defaults to 1e-7.
+            epsilon : it defines a very small number each time. similarly smooth value. Defaults to 1e-7.
         """
         super().__init__(reduction=LossReduction(reduction).value)
         self.to_onehot_y = to_onehot_y
@@ -109,7 +109,7 @@ class AsymmetricFocalLoss(_Loss):
             to_onehot_y : whether to convert `y` into the one-hot format. Defaults to False.
             delta : weight of the background. Defaults to 0.7.
             gamma : value of the exponent gamma in the definition of the Focal loss. Defaults to 2.
-            epsilon : it defines a very small number each time. simmily smooth value. Defaults to 1e-7.
+            epsilon : it defines a very small number each time. similarly smooth value. Defaults to 1e-7.
         """
         super().__init__(reduction=LossReduction(reduction).value)
         self.to_onehot_y = to_onehot_y
@@ -158,18 +158,20 @@ class AsymmetricUnifiedFocalLoss(_Loss):
         self,
         to_onehot_y: bool = False,
         num_classes: int = 2,
-        delta: float = 0.7,
-        gamma: float = 0.5,
         weight: float = 0.5,
+        gamma: float = 0.5,
+        delta: float = 0.7,
         reduction: LossReduction | str = LossReduction.MEAN,
     ):
         """
         Args:
             to_onehot_y : whether to convert `y` into the one-hot format. Defaults to False.
             num_classes : number of classes, it only supports 2 now. Defaults to 2.
-            delta : weight of the background. Defaults to 0.7.
-            gamma : value of the exponent gamma in the definition of the Focal loss. Defaults to 0.5.
             weight : weight for each loss function. Defaults to 0.5.
+            gamma : value of the exponent gamma in the definition of the Focal loss. Defaults to 0.5.
+            delta : weight of the background. Defaults to 0.7.
+
+
 
         Example:
             >>> import torch
