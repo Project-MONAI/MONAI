226deb6fe42aeeb7b5153112c03aca6046c85a72
sewon.jeon <irocks0922@gmail.com>
2026-02-26 01:16:54 +0900

    Apply black 25.11.0 formatting fixes
    Signed-off-by: sewon.jeon <irocks0922@gmail.com>

--- monai/losses/dice.py
+++ monai/losses/dice.py
@@ -204,9 +204,11 @@ class DiceLoss(_Loss):
                 self.class_weight = torch.as_tensor([self.class_weight] * num_of_classes)
             else:
                 if self.class_weight.shape[0] != num_of_classes:
-                    raise ValueError("""the length of the `weight` sequence should be the same as the number of classes.
+                    raise ValueError(
+                        """the length of the `weight` sequence should be the same as the number of classes.
                         If `include_background=False`, the weight should not include
-                        the background category class 0.""")
+                        the background category class 0."""
+                    )
             if self.class_weight.min() < 0:
                 raise ValueError("the value/values of the `weight` should be no less than 0.")
             # apply class_weight to loss
