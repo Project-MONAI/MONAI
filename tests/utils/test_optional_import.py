b45e9112395d46830b15aff531a137d0b6023a15
Soumya Snigdha Kundu <soumyawork15@gmail.com>
2026-04-14 13:13:45 +0100

    Update tests/utils/test_optional_import.py with feedback
    Co-authored-by: Eric Kerfoot <17726042+ericspod@users.noreply.github.com>
    Signed-off-by: Soumya Snigdha Kundu <soumyawork15@gmail.com>

--- tests/utils/test_optional_import.py
+++ tests/utils/test_optional_import.py
@@ -101,7 +101,7 @@ class TestOptionalImport(unittest.TestCase):
         mod, flag = optional_import("nonexistent_module_for_tb_test")
         self.assertFalse(flag)
         with self.assertRaises(OptionalImportError) as ctx:
-            mod.something
+            _ = getattr(mod, "something")
         self.assertIn("Original traceback", str(ctx.exception))
         self.assertIn("ModuleNotFoundError", str(ctx.exception))
 
