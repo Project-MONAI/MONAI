37db0adf747f10d304da9d379fe035b5f08e5a5f
Oleksandr Yizchak Sanin <alexaaander.sanin@gmail.com>
2026-05-19 00:48:46 +0200

    Fix RandGridDistortiond converting entire dict when transform is skipped (#8604)
    When _do_transform is False, RandGridDistortiond called
    convert_to_tensor on the entire data dictionary, which converted
    non-image metadata values (integers, strings) into tensors. This
    caused AttributeError ('int' object has no attribute 'numel') during
    DataLoader collation.
    
    Only convert the transform's target keys instead of the whole dict,
    consistent with how the transform handles its keys in the normal
    (do_transform=True) path.
    
    Signed-off-by: Oleksandr Sanin <alexaaander.sanin@gmail.com>

--- monai/transforms/spatial/dictionary.py
+++ monai/transforms/spatial/dictionary.py
@@ -2305,8 +2305,9 @@ class RandGridDistortiond(RandomizableTransform, MapTransform):
         d = dict(data)
         self.randomize(None)
         if not self._do_transform:
-            out: dict[Hashable, torch.Tensor] = convert_to_tensor(d, track_meta=get_track_meta())
-            return out
+            for key in self.key_iterator(d):
+                d[key] = convert_to_tensor(d[key], track_meta=get_track_meta())
+            return d
 
         first_key: Hashable = self.first_key(d)
         if first_key == ():
