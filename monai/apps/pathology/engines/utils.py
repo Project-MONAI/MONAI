32ee6d70e9a99ec398e0388caa22fc91c81859cf
VedhSontha <vedh.mitmpl2024@learner.manipal.edu>
2026-07-01 17:46:15 +0530

    refactor(pathology): bind extra_keys to PrepareBatchHoVerNet directly to prevent reliance on internal nested class attributes

--- monai/apps/pathology/engines/utils.py
+++ monai/apps/pathology/engines/utils.py
@@ -35,8 +35,10 @@ class PrepareBatchHoVerNet(PrepareBatch):
     """
 
     def __init__(self, extra_keys: Sequence[str]) -> None:
-        if len(ensure_tuple(extra_keys)) != 2:
-            raise ValueError(f"length of `extra_keys` should be 2, get {len(ensure_tuple(extra_keys))}")
+        extra_keys_tuple = ensure_tuple(extra_keys)
+        if len(extra_keys_tuple) != 2:
+            raise ValueError(f"length of `extra_keys` should be 2, get {len(extra_keys_tuple)}")
+        self.extra_keys = extra_keys_tuple
         self.prepare_batch = PrepareBatchExtraInput(extra_keys)
 
     def __call__(
@@ -55,13 +57,12 @@ class PrepareBatchHoVerNet(PrepareBatch):
             raise TypeError(f"PrepareBatchHoVerNet: expected batchdata to be a dictionary, got {type(batchdata)}")
 
         # Validate that all extra_keys exist in batchdata to provide a helpful error message
-        if isinstance(self.prepare_batch.extra_keys, (list, tuple)):
-            for key in self.prepare_batch.extra_keys:
-                if key not in batchdata:
-                    raise KeyError(
-                        f"PrepareBatchHoVerNet: extra_key '{key}' not found in batchdata. "
-                        f"Available keys are: {list(batchdata.keys())}"
-                    )
+        for key in self.extra_keys:
+            if key not in batchdata:
+                raise KeyError(
+                    f"PrepareBatchHoVerNet: extra_key '{key}' not found in batchdata. "
+                    f"Available keys are: {list(batchdata.keys())}"
+                )
 
         image, _label, extra_label, _ = self.prepare_batch(batchdata, device, non_blocking, **kwargs)
         label = {HoVerNetBranch.NP: _label, HoVerNetBranch.NC: extra_label[0], HoVerNetBranch.HV: extra_label[1]}
