Akritai1/MONAI_imported

Migrate tests from TorchScript to torch.export
Add new test suites for export_utils, convert_to_export, and the
export_checkpoint bundle CLI.  Migrate ~60 existing test files from
test_script (torch.jit.script) to test_export (torch.export.export),
removing TorchScript-specific skip decorators where no longer needed.

Signed-off-by: Soumya Snigdha Kundu <soumya_snigdha.kundu@kcl.ac.uk>

tests/networks/nets/test_efficientnet.py
@@ -29,7 +29,7 @@ from monai.networks.nets import (
     get_efficientnet_image_size,
 )
 from monai.utils import optional_import
-from tests.test_utils import skip_if_downloading_fails, skip_if_quick, test_pretrained_networks, test_script_save
+from tests.test_utils import skip_if_downloading_fails, skip_if_quick, test_pretrained_networks, test_export_save
 
 TESTS_PATH = Path(__file__).parents[2]
 
@@ -370,12 +370,12 @@ class TestEFFICIENTNET(unittest.TestCase):
             expected_shape = get_expected_model_shape(model)
             self.assertEqual(result_shape, expected_shape)
 
-    def test_script(self):
+    def test_export(self):
         with skip_if_downloading_fails():
             net = EfficientNetBN(model_name="efficientnet-b0", spatial_dims=2, in_channels=3, num_classes=1000)
         net.set_swish(memory_efficient=False)  # at the moment custom memory efficient swish is not exportable with jit
         test_data = torch.randn(1, 3, 224, 224)
-        test_script_save(net, test_data)
+        test_export_save(net, test_data)
 
 
 class TestExtractFeatures(unittest.TestCase):

tests/networks/test_varnet.py
@@ -20,7 +20,7 @@ from monai.apps.reconstruction.networks.nets.coil_sensitivity_model import CoilS
 from monai.apps.reconstruction.networks.nets.complex_unet import ComplexUnet
 from monai.apps.reconstruction.networks.nets.varnet import VariationalNetworkModel
 from monai.networks import eval_mode
-from tests.test_utils import test_script_save
+from tests.test_utils import test_export_save
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 coil_sens_model = CoilSensitivityModel(spatial_dims=2, features=[8, 16, 32, 64, 128, 8])
@@ -45,7 +45,7 @@ class TestVarNet(unittest.TestCase):
         self.assertEqual(result.shape, expected_shape)
 
     @parameterized.expand(TESTS)
-    def test_script(self, coil_sens_model, refinement_model, num_cascades, input_shape, expected_shape):
+    def test_export(self, coil_sens_model, refinement_model, num_cascades, input_shape, expected_shape):
         net = VariationalNetworkModel(coil_sens_model, refinement_model, num_cascades)
 
         mask_shape = [1 for _ in input_shape]
@@ -55,7 +55,7 @@ class TestVarNet(unittest.TestCase):
 
         test_data = torch.randn(input_shape)
 
-        test_script_save(net, test_data, mask.bool())
+        test_export_save(net, test_data, mask.bool())
 
 
 if __name__ == "__main__":

Project-MONAI/MONAI

tests/networks/nets/test_efficientnet.py
@@ -29,7 +29,7 @@ from monai.networks.nets import (
     get_efficientnet_image_size,
 )
 from monai.utils import optional_import
-from tests.test_utils import skip_if_downloading_fails, skip_if_quick, test_pretrained_networks, test_script_save
+from tests.test_utils import skip_if_downloading_fails, skip_if_quick, test_export_save, test_pretrained_networks
 
 TESTS_PATH = Path(__file__).parents[2]
 
@@ -370,12 +370,12 @@ class TestEFFICIENTNET(unittest.TestCase):
             expected_shape = get_expected_model_shape(model)
             self.assertEqual(result_shape, expected_shape)
 
-    def test_script(self):
+    def test_export(self):
         with skip_if_downloading_fails():
             net = EfficientNetBN(model_name="efficientnet-b0", spatial_dims=2, in_channels=3, num_classes=1000)
         net.set_swish(memory_efficient=False)  # at the moment custom memory efficient swish is not exportable with jit
         test_data = torch.randn(1, 3, 224, 224)
-        test_script_save(net, test_data)
+        test_export_save(net, test_data)
 
 
 class TestExtractFeatures(unittest.TestCase):

tests/networks/test_varnet.py
@@ -20,7 +20,7 @@ from monai.apps.reconstruction.networks.nets.coil_sensitivity_model import CoilS
 from monai.apps.reconstruction.networks.nets.complex_unet import ComplexUnet
 from monai.apps.reconstruction.networks.nets.varnet import VariationalNetworkModel
 from monai.networks import eval_mode
-from tests.test_utils import test_script_save
+from tests.test_utils import test_export_save
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 coil_sens_model = CoilSensitivityModel(spatial_dims=2, features=[8, 16, 32, 64, 128, 8])
@@ -45,7 +45,7 @@ class TestVarNet(unittest.TestCase):
         self.assertEqual(result.shape, expected_shape)
 
     @parameterized.expand(TESTS)
-    def test_script(self, coil_sens_model, refinement_model, num_cascades, input_shape, expected_shape):
+    def test_export(self, coil_sens_model, refinement_model, num_cascades, input_shape, _expected_shape):
         net = VariationalNetworkModel(coil_sens_model, refinement_model, num_cascades)
 
         mask_shape = [1 for _ in input_shape]
@@ -55,7 +55,7 @@ class TestVarNet(unittest.TestCase):
 
         test_data = torch.randn(input_shape)
 
-        test_script_save(net, test_data, mask.bool())
+        test_export_save(net, test_data, mask.bool())
 
 
 if __name__ == "__main__":